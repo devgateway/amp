123285550e809bd6f7ea388175f688c802eec69f
priyajith <priyajith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2006-03-09 08:44:05 +0000

    fixed AMP-455. Commit of donor access change.
    git-svn-id: file:///svn/trunk@376 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- digijava/WEB-INF/src/org/digijava/module/aim/action/GetUnAssignedWorkspaces.java
+++ digijava/WEB-INF/src/org/digijava/module/aim/action/GetUnAssignedWorkspaces.java
@@ -1,5 +1,6 @@
 /*
  * GetUnAssignedWorkspaces.java @Author Priyajith C Created: 07-Apr-2005
+ * Modified by Akashs 22-Feb-2006
  */
 
 package org.digijava.module.aim.action;
@@ -20,9 +21,7 @@ import org.digijava.module.aim.util.TeamUtil;
 /**
  * The action class will retreive all workspaces which is not associated with
  * any parent team. It also filters the workspaces based on the 'Workspace Type' 
- * and 'Team Category'
- * 
- * @author Priyajith
+ * & 'Team Category'
  */
 public class GetUnAssignedWorkspaces extends Action {
 	
@@ -31,25 +30,32 @@ public class GetUnAssignedWorkspaces extends Action {
 	public ActionForward execute(ActionMapping mapping,ActionForm form,
 			HttpServletRequest request,HttpServletResponse response) throws Exception {
 		
-		String workspaceType = request.getParameter("wType");
-		String teamCategory = request.getParameter("tCategory");
-		String dest = request.getParameter("dest");
-		
-		UpdateWorkspaceForm uwForm = (UpdateWorkspaceForm) form;
-		
-		if ((workspaceType == null || workspaceType.trim().length() == 0) 
-				&& (teamCategory == null || teamCategory.trim().length() == 0)) {
-			uwForm.setChildWorkspaceType(null);
-			uwForm.setChildTeamCategory(null);
+		if (null != form) {
+			
+			UpdateWorkspaceForm uwForm = (UpdateWorkspaceForm) form;
+			
+			String dest = request.getParameter("dest");
+			String workspaceType = request.getParameter("wType");
+			String teamCategory  = request.getParameter("tCategory");
+			String team = uwForm.getCategory();
+			
+			if ((workspaceType == null || workspaceType.trim().length() == 0) 
+					&& (teamCategory == null || teamCategory.trim().length() == 0)) {
+				uwForm.setChildWorkspaceType(null);
+				uwForm.setChildTeamCategory(null);
+			}
+			
+			Collection col = TeamUtil.getUnassignedWorkspaces(workspaceType, teamCategory, team);
+			logger.debug("Unassigned workspaces retreived, size = " + col.size());
+			
+			uwForm.setAvailChildWorkspaces(col);
+			uwForm.setReset(false);
+			//uwForm.setActionEvent(dest);
+			uwForm.setDest(dest);
+
+			return mapping.findForward("forward");
 		}
 		
-		Collection col = TeamUtil.getUnassignedWorkspaces(workspaceType, teamCategory);
-		logger.debug("Unassigned workspaces retreived, size = " + col.size());
-		
-		uwForm.setAvailChildWorkspaces(col);
-		uwForm.setReset(false);
-		uwForm.setActionEvent(dest);
-
-		return mapping.findForward("forward");
+		return null;
 	}
 }

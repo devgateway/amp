07302e6392e50b29cbdf63cbe71adeb654d064be
akashs <akashs@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2006-05-12 07:02:45 +0000

    AMP-457 MNE-feature
    git-svn-id: file:///svn/trunk@481 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- digijava/WEB-INF/src/org/digijava/module/aim/form/EditActivityForm.java
+++ digijava/WEB-INF/src/org/digijava/module/aim/form/EditActivityForm.java
@@ -441,7 +441,7 @@ public class EditActivityForm extends ActionForm implements Serializable{
 			totalCommitments = 0;
 			totalDisbursements = 0;
 			totalExpenditures = 0;
-			indicators = null;
+			indicatorsME = null;
 		}
 
 		if (orgSelReset) {

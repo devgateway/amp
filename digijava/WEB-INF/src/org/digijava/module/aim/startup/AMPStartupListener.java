f435d46263dbb2ef8ceac135642db216cc167129
steve <steve@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2006-10-04 16:39:55 +0000

    Making code JDK 1.5 -comiliant to remove compilation warnings
    git-svn-id: file:///svn/trunk@739 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- digijava/WEB-INF/src/org/digijava/module/aim/startup/AMPStartupListener.java
+++ digijava/WEB-INF/src/org/digijava/module/aim/startup/AMPStartupListener.java
@@ -18,23 +18,23 @@ import org.digijava.module.aim.helper.Constants;
 import org.digijava.module.aim.util.FeaturesUtil;
 
 
-public class AMPStartupListener extends HttpServlet 
+public class AMPStartupListener extends HttpServlet
 	implements ServletContextListener {
-    
+
 	private static final String PATCH_METHOD_KEY = "patchAMP";
-	
+
     private static Logger logger = Logger.getLogger(
             AMPStartupListener.class);
-    
+
     public void contextDestroyed(ServletContextEvent sce) {
 
     }
     public void contextInitialized(ServletContextEvent sce) {
     	ServletContext ampContext = null;
-    	
+
     	try {
         	ampContext = sce.getServletContext();
-        	
+
         	Iterator itr = FeaturesUtil.
         		getActiveFeatures().iterator();
         	while (itr.hasNext()) {
@@ -44,7 +44,7 @@ public class AMPStartupListener extends HttpServlet
         			//logger.debug("ME Feature..");
         		} else if (f.getCode().equalsIgnoreCase(Constants.AA_FEATURE)) {
         			ampContext.setAttribute(Constants.AA_FEATURE,new Boolean(true));
-        			//logger.debug("AA Feature..");        			
+        			//logger.debug("AA Feature..");
         		} else if (f.getCode().equalsIgnoreCase(Constants.PI_FEATURE)) {
         			ampContext.setAttribute(Constants.PI_FEATURE,new Boolean(true));
         			//logger.debug("PI Feature..");
@@ -55,7 +55,7 @@ public class AMPStartupListener extends HttpServlet
         	}
         	boolean defFlagExist = FeaturesUtil.defaultFlagExist();
         	ampContext.setAttribute(Constants.DEF_FLAG_EXIST,new Boolean(defFlagExist));
-        	
+
         	Enumeration params = ampContext.getInitParameterNames();
         	if (params != null) {
         		logger.info("params is not null");
@@ -73,14 +73,14 @@ public class AMPStartupListener extends HttpServlet
         	        		try {
         	        			Class c = Class.forName(className);
         	        			Object obj = c.newInstance();
-        	        			Method m = c.getMethod(methodName,null);
-        	        			m.invoke(obj,null);
+                                Method m = c.getMethod(methodName, (Class[])null);
+                                m.invoke(obj, (Object[])null);
         	        		} catch (ClassNotFoundException cnfe) {
         	        			logger.error("Cannot find the patching class:" + className);
         	        		}
         	        	} else {
         	        		logger.error("Wrong paramater values specified for patchMethod");
-        	        	}        				
+        	        	}
         			}
         		}
         	} else {

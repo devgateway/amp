dba55c5359631acd3becff8a53ef962c188a8743
steve <steve@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2006-11-03 09:12:18 +0000

    Fixed AMP-805
    git-svn-id: file:///svn/trunk@870 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- digijava/WEB-INF/src/org/digijava/module/aim/action/SearchOrganisation.java
+++ digijava/WEB-INF/src/org/digijava/module/aim/action/SearchOrganisation.java
@@ -13,6 +13,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
+import org.digijava.module.aim.helper.OrgProjectId;
+import java.util.HashSet;
+import java.util.Arrays;
 
 public class SearchOrganisation extends Action {
 
@@ -24,15 +27,15 @@ public class SearchOrganisation extends Action {
 		EditActivityForm eaForm = (EditActivityForm) form;
 		eaForm.setReset(false);
 		eaForm.setOrgPopupReset(false);
-		
-		String alpha = request.getParameter("alpha");
+
+        String alpha = request.getParameter("alpha");
 		Collection col = null;
 		Collection colAlpha = null;
-		
+
 		if (alpha == null || alpha.trim().length() == 0) {
 			col = new ArrayList();
 			eaForm.setNumResults(eaForm.getTempNumResults());
-			
+
 			if (!eaForm.getAmpOrgTypeId().equals(new Long(-1))) {
 				if (eaForm.getKeyword().trim().length() != 0) {
 					// serach for organisations based on the keyword and the
@@ -50,40 +53,40 @@ public class SearchOrganisation extends Action {
 				// get all organisations since keyword field is blank and org type field has 'ALL'.
 				col = DbUtil.getAmpOrganisations();
 			}
-			
+
 			if (col != null && col.size() > 0) {
 				List temp = (List) col;
 				Collections.sort(temp);
 				col = (Collection) temp;
-				
+
 				if (eaForm.getCurrentAlpha() != null) {
 					eaForm.setCurrentAlpha(null);
-					//eaForm.setStartAlphaFlag(true);  
+					//eaForm.setStartAlphaFlag(true);
 				}
 				eaForm.setStartAlphaFlag(true);
-	
+
 				String[] alphaArray = new String[26];
 				int i = 0;
-				for(char c = 'A'; c <= 'Z'; c++) { 
+				for(char c = 'A'; c <= 'Z'; c++) {
 					Iterator itr = col.iterator();
 					while(itr.hasNext()) {
 						AmpOrganisation org = (AmpOrganisation) itr.next();
 						if (org.getName().toUpperCase().indexOf(c) == 0) {
 							alphaArray[i++] = String.valueOf(c);
 							break;
-						} 
+						}
 					}
 				}
 				eaForm.setAlphaPages(alphaArray);
 			} else {
 				eaForm.setAlphaPages(null);
 			}
-			
+
 		} else {
 			col = eaForm.getCols();
 			eaForm.setCurrentAlpha(alpha);
 			if (!alpha.equals("viewAll")) {
-				eaForm.setStartAlphaFlag(false); 
+				eaForm.setStartAlphaFlag(false);
 				colAlpha = new ArrayList();
 				Iterator itr = col.iterator();
 				while(itr.hasNext()) {
@@ -95,21 +98,46 @@ public class SearchOrganisation extends Action {
 				eaForm.setColsAlpha(colAlpha);
 			}
 			else
-				eaForm.setStartAlphaFlag(true);  
+				eaForm.setStartAlphaFlag(true);
 		}
+
+        OrgProjectId hvOrgs[] = eaForm.getSelectedOrganizations();
+
+        Collection newCol = new ArrayList();
+        if(hvOrgs != null) {
+            Iterator itr = col.iterator();
+            boolean haveOrg=false;
+            while(itr.hasNext()) {
+                AmpOrganisation org = (AmpOrganisation) itr.next();
+                haveOrg=false;
+                for(int i = 0; i < hvOrgs.length; i++) {
+                    if(hvOrgs[i]!=null &&
+                       hvOrgs[i].getAmpOrgId().equals(org.getAmpOrgId()) ||
+                       hvOrgs[i].getName().equalsIgnoreCase((org.getName()))) {
+                        haveOrg=true;
+                        break;
+                    }
+                }
+                if(haveOrg==false) {
+                   newCol.add(org);
+                }
+            }
+        }else{
+            newCol=col;
+        }
 
 		int stIndex = 1;
 		int edIndex = eaForm.getNumResults();
 		Vector vect = new Vector();
 		int numPages;
-		
+
 		if (alpha == null || alpha.trim().length() == 0 || alpha.equals("viewAll")) {
-			if (edIndex > col.size()) {
-				edIndex = col.size();
+			if (edIndex > newCol.size()) {
+				edIndex = newCol.size();
 			}
-			vect.addAll(col);
-			numPages = col.size() / eaForm.getNumResults();
-			numPages += (col.size() % eaForm.getNumResults() != 0) ? 1 : 0;
+			vect.addAll(newCol);
+			numPages = newCol.size() / eaForm.getNumResults();
+			numPages += (newCol.size() % eaForm.getNumResults() != 0) ? 1 : 0;
 		} else {
 			if (edIndex > colAlpha.size()) {
 				edIndex = colAlpha.size();
@@ -119,7 +147,8 @@ public class SearchOrganisation extends Action {
 			numPages += (colAlpha.size() % eaForm.getNumResults() != 0) ? 1 : 0;
 		}
 
-		Collection tempCol = new ArrayList();
+
+        Collection tempCol = new ArrayList();
 		for (int i = (stIndex - 1); i < edIndex; i++) {
 			tempCol.add(vect.get(i));
 		}
@@ -133,12 +162,13 @@ public class SearchOrganisation extends Action {
 				pages.add(pageNum);
 			}
 		}
-		eaForm.setCols(col);
+
+        eaForm.setCols(newCol);
 		eaForm.setPagedCol(tempCol);
 		eaForm.setPages(pages);
-		eaForm.setCurrentPage(new Integer(1));	
-		
-		return mapping.findForward("forward");
+		eaForm.setCurrentPage(new Integer(1));
+
+    	return mapping.findForward("forward");
 	}
 }
 

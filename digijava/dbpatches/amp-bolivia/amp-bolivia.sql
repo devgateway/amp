5ae00a8b62f1c52cfc6d92393b53cb2d1f881482
gopinath <gopinath@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2006-11-16 07:20:19 +0000

    AMP-720 AMP Bolivia Admin
    git-svn-id: file:///svn/branches/amp-bolivia-nov06@914 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ digijava/dbpatches/amp-bolivia/amp-bolivia.sql
@@ -0,0 +1,6 @@
+ALTER TABLE amp_me_indicators DROP value_type;
+ALTER TABLE amp_me_indicators DROP creation_date;
+ALTER TABLE amp_me_indicators DROP type;
+ALTER TABLE amp_me_indicators DROP np_indicator;
+ALTER TABLE amp_me_indicators DROP category;
+ALTER TABLE amp_theme_indicators MODIFY np_indicator TINYINT(1);

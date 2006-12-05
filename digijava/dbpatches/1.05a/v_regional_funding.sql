262d4ab7610343b022eedf70e487dd216fe22beb
steve <steve@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2006-12-05 16:23:27 +0000

    ethiopia workshop phase 1
    git-svn-id: file:///svn/trunk@973 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- digijava/dbpatches/1.05a/v_regional_funding.sql
+++ digijava/dbpatches/1.05a/v_regional_funding.sql
@@ -7,6 +7,8 @@ CREATE OR REPLACE VIEW `v_regional_funding` AS select
 `f`.`adjustment_type` AS `adjustment_type`,
 date_format(`f`.`transaction_date`,_latin1'%Y-%m-%d') AS `transaction_date`,
 `f`.`transaction_amount` AS `transaction_amount`,`c`.`currency_code` AS `currency_code`,
-`amp_staging`.`getExchange`(`c`.`currency_code`,`f`.`transaction_date`) AS `exchange_rate` 
-from ((`amp_regional_funding` `f` join `amp_region` `r`) join `amp_currency` `c`) where 
+`getExchange`(`c`.`currency_code`,`f`.`transaction_date`) AS `exchange_rate`,
+p.code as perspective_code
+from amp_perspective p,((`amp_regional_funding` `f` join `amp_region` `r`) join `amp_currency` `c`) where 
+p.amp_perspective_id=f.perspective_id and
 ((`c`.`amp_currency_id` = `f`.`currency_id`) and (`f`.`region_id` = `r`.`amp_region_id`)) order by `f`.`activity_id`

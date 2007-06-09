aab6999d3d849ceac8aa0b606a49dd8978a69b8c
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-06-09 18:42:22 +0000

    AMP-1583
    git-svn-id: file:///svn/trunk@1933 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/dbpatches/1.05a/amp_field_update.sql
@@ -0,0 +1,7 @@
+INSERT INTO `amp_field` (`amp_field_id`,`field_name`) VALUES 
+ (2,'Objective Assumption'),
+ (3,'Objective Verification'),
+ (4,'Purpose Assumption'),
+ (5,'Purpose Verification'),
+ (6,'Results Assumption'),
+ (7,'Results Verification');

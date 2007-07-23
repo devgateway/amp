59493c24251075fc1c9706e97c9b773635704b45
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-07-23 17:25:22 +0000

    AMP-1601
    git-svn-id: file:///svn/trunk/amp@2428 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- dbpatches/1.09/patch_for_v_sectors.sql
+++ dbpatches/1.09/patch_for_v_sectors.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS `amp_local`.`getParentSectorId`$$
-CREATE DEFINER=`root`@`localhost` FUNCTION `getParentSectorId`(sectorId BIGINT(20)) RETURNS bigint(20)
+DROP FUNCTION IF EXISTS getParentSectorId$$
+CREATE FUNCTION getParentSectorId(sectorId BIGINT(20)) RETURNS bigint(20)
 BEGIN
      	declare sid bigint(20);
 	declare pid bigint(20);
@@ -18,4 +18,5 @@ END$$
 
 DELIMITER ;
 
-CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_sectors` AS select `sa`.`amp_activity_id` AS `amp_activity_id`,`s`.`name` AS `name`,`s`.`amp_sector_id` AS `amp_sector_id`,`sa`.`sector_percentage` AS `sector_percentage` from (`amp_activity_sector` `sa` join `amp_sector` `s` on((`amp_local`.`getParentSectorId`(`sa`.`amp_sector_id`) = `s`.`amp_sector_id`))) order by `sa`.`amp_activity_id`,`s`.`name`
+CREATE OR REPLACE VIEW `v_sectors` AS select `sa`.`amp_activity_id` AS `amp_activity_id`,`s`.`name` AS `name`,`s`.`amp_sector_id` AS `amp_sector_id`,`sa`.`sector_percentage` AS `sector_percentage` from (`amp_activity_sector` `sa` join `amp_sector` `s` on((getParentSectorId(`sa`.`amp_sector_id`) = `s`.`amp_sector_id`))) order by `sa`.`amp_activity_id`,`s`.`name`
+

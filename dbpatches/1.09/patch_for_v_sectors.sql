7f46a4bda64ca1f7acec543c572f5ad372cc8fe9
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-07-23 17:14:04 +0000

    AMP-1601
    git-svn-id: file:///svn/trunk/amp@2427 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ dbpatches/1.09/patch_for_v_sectors.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS `amp_local`.`getParentSectorId`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `getParentSectorId`(sectorId BIGINT(20)) RETURNS bigint(20)
+BEGIN
+     	declare sid bigint(20);
+	declare pid bigint(20);
+     	select parent_sector_id into pid from amp_sector where amp_sector_id=sectorId;
+	if pid is null then 
+ 		return sectorId;
+	end if;
+	while pid is not null do
+		set sid =pid;
+		select parent_sector_id into pid from amp_sector where amp_sector_id=sid;
+	end while;
+	return sid;
+END$$
+
+DELIMITER ;
+
+CREATE OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_sectors` AS select `sa`.`amp_activity_id` AS `amp_activity_id`,`s`.`name` AS `name`,`s`.`amp_sector_id` AS `amp_sector_id`,`sa`.`sector_percentage` AS `sector_percentage` from (`amp_activity_sector` `sa` join `amp_sector` `s` on((`amp_local`.`getParentSectorId`(`sa`.`amp_sector_id`) = `s`.`amp_sector_id`))) order by `sa`.`amp_activity_id`,`s`.`name`

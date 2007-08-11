1a550e19f1805bd302b52a2d52301afa243c5a24
ampromania <ampromania@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-08-11 11:31:22 +0000

    autopatcher jaxb
    git-svn-id: file:///svn/trunk@2658 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/WEB-INF/src/org/digijava/module/autopatcher/dbentity/AmpPatch.java
@@ -0,0 +1,10 @@
+package org.digijava.module.autopatcher.dbentity;
+
+import java.io.Serializable;
+
+public class AmpPatch implements Serializable {
+	private Long id;
+	private String keyName;
+	private String abstractFileName;
+	
+}

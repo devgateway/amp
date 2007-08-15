01271c189aa54063609e78966bb6d8e90cf6ba9a
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-08-15 11:15:23 +0000

    AMP-1743: country not reset
    git-svn-id: file:///svn/trunk/amp@2687 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/UpdateCurrency.java
+++ WEB-INF/src/org/digijava/module/aim/action/UpdateCurrency.java
@@ -28,105 +28,123 @@ import org.digijava.module.aim.dbentity.AmpCurrencyRate;
 import org.digijava.module.aim.form.CurrencyForm;
 import org.digijava.module.aim.helper.DateConversion;
 import org.digijava.module.aim.util.CurrencyUtil;
+import org.digijava.module.aim.util.DbUtil;
 
 public class UpdateCurrency extends Action {
 
-	private static Logger logger = Logger.getLogger(UpdateCurrency.class);
+        private static Logger logger = Logger.getLogger(UpdateCurrency.class);
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws java.lang.Exception {
+        public ActionForward execute(ActionMapping mapping, ActionForm form,
+                        HttpServletRequest request, HttpServletResponse response)
+                        throws java.lang.Exception {
 
-		HttpSession session = request.getSession();
-		if (session.getAttribute("ampAdmin") == null) {
-			return mapping.findForward("index");
-		} else {
-			String str = (String)session.getAttribute("ampAdmin");
-			if (str.equals("no")) {
-				return mapping.findForward("index");
-			}
-		}
+                HttpSession session = request.getSession();
+                if (session.getAttribute("ampAdmin") == null) {
+                        return mapping.findForward("index");
+                } else {
+                        String str = (String)session.getAttribute("ampAdmin");
+                        if (str.equals("no")) {
+                                return mapping.findForward("index");
+                        }
+                }
 
-		try {
+                try {
 
-		CurrencyForm crForm = (CurrencyForm) form;
+                CurrencyForm crForm = (CurrencyForm) form;
 
-		if (crForm.getDoAction() == null ||
-				crForm.getDoAction().equals("showCurrencies")) {
-			if (crForm.getCurrencyCode() != null ) {
-				Iterator itr = crForm.getAllCurrencies().iterator();
-				AmpCurrency curr = null;
-				while (itr.hasNext()) {
-					curr = (AmpCurrency) itr.next();
-					if (curr.getCurrencyCode().equals(crForm.getCurrencyCode())) {
-						crForm.setCurrencyName(curr.getCurrencyName());
-						crForm.setCountryName(curr.getCountryName());
-						crForm.setId(curr.getAmpCurrencyId());
-						break;
-					}
-				}
-			} else {
-				crForm.setId(new Long(-1));
-				crForm.setCountryName(null);
-				crForm.setCurrencyCode(null);
-				crForm.setCurrencyName(null);
-				crForm.setExchangeRate(null);
-				crForm.setExchangeRateDate(null);
-			}
-			if (crForm.getCountries() == null ||
-					crForm.getCountries().size() < 1) {
-				List countries = org.digijava.module.um.util.DbUtil.getCountries();
-				HashMap countriesMap = new HashMap();
-				Iterator iterator = TrnUtil.getCountries(
-						RequestUtils.getNavigationLanguage(request).getCode())
-						.iterator();
-				while (iterator.hasNext()) {
-					TrnCountry item = (TrnCountry) iterator.next();
-					countriesMap.put(item.getIso(), item);
-				}
-				//sort countries
-				List sortedCountries = new ArrayList();
-				iterator = countries.iterator();
-				while (iterator.hasNext()) {
-					Country item = (Country) iterator.next();
-					sortedCountries.add(countriesMap.get(item.getIso()));
-				}
-				Collections.sort(sortedCountries, TrnUtil.countryNameComparator);
-				crForm.setCountries(sortedCountries);
-			}
+                if (crForm.getDoAction() == null ||
+                                crForm.getDoAction().equals("showCurrencies")) {
+                        if (crForm.getCurrencyCode() != null ) {
+                                Iterator itr = crForm.getAllCurrencies().iterator();
+                                AmpCurrency curr = null;
+                                while (itr.hasNext()) {
+                                        curr = (AmpCurrency) itr.next();
+                                        if (curr.getCurrencyCode().equals(crForm.getCurrencyCode())) {
+                                                if (curr.getCountryName()!=null && !curr.getCountryName().equals("")){
+                                                        Country country=DbUtil.getCountryByName(curr.getCountryName());
+                                                        if (country!=null){
+                                                                crForm.setCountryIso(country.getIso());
+                                                        }else{
+                                                                crForm.setCountryIso(null);
+                                                                }
+                                                }else{
+                                                        crForm.setCountryIso(null);
+                                                }
+                                                crForm.setCountryName(curr.getCountryName());
+                                                crForm.setCurrencyName(curr.getCurrencyName());
+                                                crForm.setId(curr.getAmpCurrencyId());
+                                                break;
+                                        }
+                                }
+                        } else {
+                                crForm.setId(new Long(-1));
+                                crForm.setCountryName(null);
+                                crForm.setCountryIso("");
+                                crForm.setCurrencyCode(null);
+                                crForm.setCurrencyName(null);
+                                crForm.setExchangeRate(null);
+                                crForm.setExchangeRateDate(null);
+                        }
+                        if (crForm.getCountries() == null ||
+                                        crForm.getCountries().size() < 1) {
+                                List countries = org.digijava.module.um.util.DbUtil.getCountries();
+                                HashMap countriesMap = new HashMap();
+                                Iterator iterator = TrnUtil.getCountries(
+                                                RequestUtils.getNavigationLanguage(request).getCode())
+                                                .iterator();
+                                while (iterator.hasNext()) {
+                                        TrnCountry item = (TrnCountry) iterator.next();
+                                        countriesMap.put(item.getIso(), item);
+                                }
+                                //sort countries
+                                List sortedCountries = new ArrayList();
+                                iterator = countries.iterator();
+                                while (iterator.hasNext()) {
+                                        Country item = (Country) iterator.next();
+                                        sortedCountries.add(countriesMap.get(item.getIso()));
+                                }
+                                Collections.sort(sortedCountries, TrnUtil.countryNameComparator);
+                                crForm.setCountries(sortedCountries);
+                        }
 
-		} else {
-			AmpCurrency curr = new AmpCurrency();
-			curr.setCountryName(crForm.getCountryName());
-			curr.setCurrencyCode(crForm.getCurrencyCode());
-			curr.setCurrencyName(crForm.getCurrencyName());
-			curr.setAmpCurrencyId(crForm.getId());
-			curr.setActiveFlag(new Integer(1));
-			AmpCurrencyRate cRate = new AmpCurrencyRate();
-			if (crForm.getExchangeRate() != null &&
-					crForm.getExchangeRateDate() != null &&
-					crForm.getExchangeRateDate().trim().length() > 0) {
-				cRate.setExchangeRate(crForm.getExchangeRate());
-				cRate.setExchangeRateDate(
-						DateConversion.getDate(crForm.getExchangeRateDate()));
-				cRate.setToCurrencyCode(crForm.getCurrencyCode());
+                } else {
+                        AmpCurrency curr = new AmpCurrency();
+                        if (crForm.getCountryIso()!=null){
+                                String countryName=DbUtil.getDgCountry(crForm.getCountryIso()).getCountryName();
+                                curr.setCountryName(countryName);
+                        }else{
+                                curr.setCountryName(crForm.getCountryName());
+                        }
+                        curr.setCurrencyCode(crForm.getCurrencyCode());
+                        curr.setCurrencyName(crForm.getCurrencyName());
+                        curr.setAmpCurrencyId(crForm.getId());
+                        curr.setActiveFlag(new Integer(1));
+                        AmpCurrencyRate cRate = new AmpCurrencyRate();
+                        if (crForm.getExchangeRate() != null &&
+                                        crForm.getExchangeRateDate() != null &&
+                                        crForm.getExchangeRateDate().trim().length() > 0) {
+                                cRate.setExchangeRate(crForm.getExchangeRate());
+                                cRate.setExchangeRateDate(
+                                                DateConversion.getDate(crForm.getExchangeRateDate()));
+                                cRate.setToCurrencyCode(crForm.getCurrencyCode());
 
-			}
-			CurrencyUtil.saveCurrency(curr,cRate);
-			crForm.setCountryName(null);
-			crForm.setCurrencyCode(null);
-			crForm.setCurrencyName(null);
-			crForm.setExchangeRate(null);
-			crForm.setExchangeRateDate(null);
-			crForm.setAllCurrencies(null);
-			crForm.setDoAction(null);
-			crForm.setId(null);
-		}
-		} catch (Exception e) {
-			e.printStackTrace(System.out);
-		}
-		return mapping.findForward("forward");
-	}
+                        }
+                        CurrencyUtil.saveCurrency(curr,cRate);
+                        crForm.setCountryName(null);
+                        crForm.setCountryIso(null);
+                        crForm.setCurrencyCode(null);
+                        crForm.setCurrencyName(null);
+                        crForm.setExchangeRate(null);
+                        crForm.setExchangeRateDate(null);
+                        crForm.setAllCurrencies(null);
+                        crForm.setDoAction(null);
+                        crForm.setId(null);
+                }
+                } catch (Exception e) {
+                        e.printStackTrace(System.out);
+                }
+                return mapping.findForward("forward");
+        }
 
 }
 

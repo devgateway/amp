76a104159c0bbbdf440d3334a7fddd1d147c3912
vezugbaia <vezugbaia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-09-13 11:18:42 +0000

    AMP-1896
    git-svn-id: file:///svn/trunk/amp@2982 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/helper/YearlyInfoWorker.java
+++ WEB-INF/src/org/digijava/module/aim/helper/YearlyInfoWorker.java
@@ -4,46 +4,47 @@ import org.apache.log4j.Logger;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.ArrayList;
- 
+import org.apache.commons.lang.StringUtils;
+
 public class YearlyInfoWorker {
-	
+
 	private static Logger logger = Logger.getLogger(YearlyInfoWorker.class);
-	
+
 	public static Collection getYearlyInfo(FilterParams fp) {
-		
-		Collection yo = new ArrayList();											  	
-		Collection qo = QuarterlyInfoWorker.getQuarterlyInfo(fp); 
+
+		Collection yo = new ArrayList();
+		Collection qo = QuarterlyInfoWorker.getQuarterlyInfo(fp);
 		if ( qo.size() != 0 )
 			yo = getYearlySum(qo);
-				
-		if ( logger.isDebugEnabled() )	
-			logger.debug("GETYEARLYINFO() RETURNING A COLLECTION OF SIZE : " + yo.size());	
-															
+
+		if ( logger.isDebugEnabled() )
+			logger.debug("GETYEARLYINFO() RETURNING A COLLECTION OF SIZE : " + yo.size());
+
 		return yo;
 	}
-	
+
 	public static Collection getYearlySum(Collection qi) {
-		
+
 		if ( logger.isDebugEnabled() )
 			logger.debug("GETYEARLYSUM() PASSED A COLLECTION OF SIZE : " + qi.size());
-		
+
 		ArrayList yo = new ArrayList();
 		if ( qi.size() != 0 )	{
-		
+
 			ArrayList qo = new ArrayList(qi);
-			
+
 			double tempPlanned = 0.0;
 			double tempActual = 0.0;
 			QuarterlyInfo qf = null;
-			YearlyInfo yf = null;	
+			YearlyInfo yf = null;
 			String pa = "";
 			String aa = "";
-			
-			int i = 0;	
+
+			int i = 0;
 			qf = (QuarterlyInfo) qo.get(i);
-			
+
 			while ( i < qo.size() ) {
-				
+
 				yf = new YearlyInfo();
 				yf.setFiscalYear(qf.getFiscalYear());
 				if ( qf.getAggregate() == 0 )	{
@@ -53,7 +54,9 @@ public class YearlyInfoWorker {
 					}
 					if (qf.getActualAmount() != null && (!qf.getActualAmount().equals("0"))) {
 						aa = DecimalToText.removeCommas(qf.getActualAmount());
-						tempActual += Double.parseDouble(aa);
+                        if(StringUtils.isNumeric(aa)){
+                            tempActual += Double.parseDouble(aa);
+                        }
 					}
 				}
 				i++;
@@ -66,14 +69,14 @@ public class YearlyInfoWorker {
 					yo.add(yf);
 					break;
 				}
-	
+
 				while (qf.getFiscalYear()==yf.getFiscalYear()  ) {
 					if ( qf.getAggregate()==0)	{
 						if (qf.getPlannedAmount() != null &&  (!qf.getPlannedAmount().equals("0"))) {
 							pa = DecimalToText.removeCommas(qf.getPlannedAmount());
 							tempPlanned += Double.parseDouble(pa);
 						}
-	
+
 						if (qf.getActualAmount() != null && (!qf.getActualAmount().equals("0"))) {
 							aa = DecimalToText.removeCommas(qf.getActualAmount());
 							tempActual += Double.parseDouble(aa);
@@ -96,7 +99,7 @@ public class YearlyInfoWorker {
 			logger.debug("GETYEARLYSUM() RETURNING A COLLECTION OF SIZE : " + yo.size());
 		return yo;
 	}
-	
+
 	public static String getTotalYearly(Collection yearlyInfo, int type)	{
 		if ( logger.isDebugEnabled() )
 			logger.debug("getTotalYearly() passed a collection of size" + yearlyInfo.size() );
@@ -105,7 +108,7 @@ public class YearlyInfoWorker {
 		double total = 0.0 ;
 		String s1 = "" ;
 		String s2 = "" ;
-		
+
 		while ( iter.hasNext() )	{
 			y = (YearlyInfo) iter.next();
 			if ( type == Constants.PLANNED )	{
@@ -123,6 +126,6 @@ public class YearlyInfoWorker {
 		if ( logger.isDebugEnabled() )
 			logger.debug("getTotalYearly() returns total=" + strTotal );
 		return strTotal;
-	}	
-	
+	}
+
 }

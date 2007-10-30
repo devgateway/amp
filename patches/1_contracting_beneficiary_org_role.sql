52d7150905a4232f401c34710aa14bebe5f68ad7
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-10-30 12:49:16 +0000

    AMP-2085
    git-svn-id: file:///svn/trunk/amp@3380 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ patches/1_contracting_beneficiary_org_role.sql
@@ -0,0 +1,3 @@
+UPDATE amp_role SET role_code="BA", name="Beneficiary Agency"  WHERE name="Beneficiary Institution";
+UPDATE amp_role SET role_code="CA", name="Contracting Agency"  WHERE name="Contractor";
+

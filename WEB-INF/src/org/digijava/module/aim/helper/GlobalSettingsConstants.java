bdb6e577fe9da79874d18a461e28eebc8938fd92
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-11-30 13:01:36 +0000

    AMP-2192
    git-svn-id: file:///svn/trunk/amp@3571 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/helper/GlobalSettingsConstants.java
+++ WEB-INF/src/org/digijava/module/aim/helper/GlobalSettingsConstants.java
@@ -1,12 +1,12 @@
 package org.digijava.module.aim.helper;
 
 public class GlobalSettingsConstants {
-	public static final String DEFAULT_COUNTRY		= "Default Country";
-	public static final String PUBLIC_VIEW			= "Public View";
+	public static final String DEFAULT_COUNTRY			= "Default Country";
+	public static final String PUBLIC_VIEW				= "Public View";
 	public static final String FEATURE_TEMPLATE		= "Feature Template";
 	public static final String YEAR_RANGE_START		= "Year Range Start";
 	
-	public static final String NUMBER_OF_YEARS_IN_RANGE			= "Number of Years in Range";
+	public static final String NUMBER_OF_YEARS_IN_RANGE		= "Number of Years in Range";
 	
 	public static final String PUBLIC_VIEW_BIDGET_FILTER		= "Public View Budget Filter";
 	public static final String DEFAULT_SECTOR_SCHEME			= "Default Sector Scheme";

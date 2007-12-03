44cf9c0e6c3f562b770f0d5faf60890890361a76
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2007-12-03 21:09:53 +0000

    deleted bad patches + new rates exchange patch
    git-svn-id: file:///svn/trunk@3617 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/drop_fields.sql
+++
@@ -1,7 +0,0 @@
-delete from amp_fields_templates;
-delete from amp_fields_visibility;
-delete from amp_features_templates;
-delete from amp_features_visibility;
-delete from amp_modules_templates;
-delete from amp_modules_visibility;
-delete FROM dg_message where message_key like "viz:%";

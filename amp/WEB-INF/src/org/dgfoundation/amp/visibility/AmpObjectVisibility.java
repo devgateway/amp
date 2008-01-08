649c8b818f7d865d54388e65029157d203744234
dmihaila <dmihaila@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-01-08 17:31:13 +0000

    AMP-2231
    git-svn-id: file:///svn/trunk@3919 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/visibility/AmpObjectVisibility.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/visibility/AmpObjectVisibility.java
@@ -113,6 +113,7 @@ public abstract class AmpObjectVisibility  extends Permissible implements Serial
 	}
 
 	public String getProperName() throws IOException {
+		//System.out.println("-----------------------"+FeaturesUtil.makeProperString(this.getName()));
 		return FeaturesUtil.makeProperString(this.getName());
 	}
 

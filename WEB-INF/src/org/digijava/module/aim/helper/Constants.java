3ed2412a4848486ddf5d7c8773ec2fc1e13e8620
dmihaila <dmihaila@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-01-08 15:00:32 +0000

    AMP-2231
    git-svn-id: file:///svn/trunk/amp@3917 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/helper/Constants.java
+++ WEB-INF/src/org/digijava/module/aim/helper/Constants.java
@@ -295,5 +295,5 @@ public class Constants
 	public static final String MY_REPORTS_PER_PAGE = "myReportsPerPage";
 
        
-    public static final String   FEATURE_MANAGER_VISIBILITY_TREE_UPDATED="The visibility tree was updated." ;
+    public static final String   FEATURE_MANAGER_VISIBILITY_TREE_UPDATED="Your changes have been saved." ;
 }

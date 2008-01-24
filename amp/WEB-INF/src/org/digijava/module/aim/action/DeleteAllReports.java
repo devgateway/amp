218e8415c3e2301dc15dc56dcc667eb70a1dc300
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-01-24 16:10:53 +0000

    AMP-2529
    git-svn-id: file:///svn/trunk@4119 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/DeleteAllReports.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/DeleteAllReports.java
@@ -1,5 +1,8 @@
 package org.digijava.module.aim.action;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,8 +14,11 @@ import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.dgfoundation.amp.ar.AmpARFilter;
+import org.dgfoundation.amp.ar.ArConstants;
 import org.digijava.module.aim.dbentity.AmpReports;
 import org.digijava.module.aim.form.ReportsForm;
+import org.digijava.module.aim.helper.Constants;
 import org.digijava.module.aim.helper.TeamMember;
 import org.digijava.module.aim.util.AdvancedReportUtil;
 import org.digijava.module.aim.util.AuditLoggerUtil;
@@ -67,6 +73,7 @@ public class DeleteAllReports extends Action {
 								 //AmpReports aReportForLog=DbUtil.getAmpReport(ampReportForm.getReportId());
 								 AuditLoggerUtil.logObject(session,request,ampReport,"delete");
 								 DbUtil.updateAppSettingsReportDeleted(ampReportForm.getReportId());
+								 removeFromSession(request.getSession(), ampReportForm.getReportId());
 								 boolean deleted = AdvancedReportUtil.deleteReportsCompletely(ampReportForm.getReportId());										 
 										 if (deleted) {
 											errors.add("title", new ActionError(
@@ -84,6 +91,39 @@ public class DeleteAllReports extends Action {
 				 }
 				 
 				 return mapping.findForward("forward");				
+	  }
+	  
+	  public void removeFromSession(HttpSession session, Long reportId) {
+		 AmpReports defaultReport		= (AmpReports)session.getAttribute(Constants.DEFAULT_TEAM_REPORT);
+		 AmpReports	filterCurrentRep	= (AmpReports)session.getAttribute(Constants.FILTER_CURRENT_REPORT);
+		 List myReports					= (List)session.getAttribute(Constants.MY_REPORTS);
+		 TeamMember tm					= (TeamMember) session.getAttribute(Constants.CURRENT_MEMBER);
+		 
+		 
+		 if (defaultReport != null && defaultReport.getAmpReportId().equals(reportId)) {
+			 session.setAttribute(Constants.DEFAULT_TEAM_REPORT, null);
+		 }
+		 
+		 if ( filterCurrentRep != null && filterCurrentRep.getAmpReportId().equals(reportId) ) {
+			 session.setAttribute(Constants.FILTER_CURRENT_REPORT, null);
+		 }
+		 if ( tm!=null && tm.getAppSettings() != null) {
+			 AmpReports defTmReport	= tm.getAppSettings().getDefaultAmpReport();
+			 if ( defTmReport!=null && defTmReport.getAmpReportId().equals(reportId) ) {
+				 tm.getAppSettings().setDefaultAmpReport(null);
+			 }
+		 }
+		 
+		 
+		 if (myReports != null) {
+			 Iterator iter	= myReports.iterator();
+			 while (iter.hasNext()) {
+				 AmpReports rep		= (AmpReports) iter.next();
+				 if ( rep.getAmpReportId().equals(reportId) ) 
+					 	iter.remove();
+			 }
+		 }
+		 
 	  }
 }
 

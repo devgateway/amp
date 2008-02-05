0c13470f3b1aa1f2bc32fae31cd5ee3115a2ffc4
mcoria <mcoria@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-02-05 12:42:49 +0000

    AMP-2441
    git-svn-id: file:///svn/trunk@4240 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp-scripts/bolivia/sql/import_sisin.sql
@@ -0,0 +1,86 @@
+﻿use amp_bolivia;
+
+SET @commitment=0;
+SET @disbursment=1;
+SET @expenditure=2;
+
+SET @funding_adjusment_planned=0;
+SET @funding_adjusment_actual=1;
+
+SET @usd_currency_id=21;
+SET @mofed_perspective_id=2;
+
+SET @base_url_sisin='http://www.google.com?sisinCode=';
+
+
+ALTER TABLE amp_components ADD COLUMN CodigoSISIN VARCHAR(13) AFTER `type`;
+ALTER TABLE amp_components ADD INDEX `Index_CodigoSISIN` USING BTREE(`CodigoSISIN`);
+
+
+INSERT INTO amp_components (CodigoSISIN)
+SELECT distinct CodigoSISIN FROM sisin_db.seguimiento_financiero s
+where CodConvExt != '00000' and CodConvExt != '99999';
+
+UPDATE amp_components a,
+sisin_db.proyecto p
+SET a.title = p.NombreProyecto,
+a.description = p.DescripcionProyecto,
+a.code = p.CodigoSISIN,
+a.url = CONCAT(@base_url_sisin,p.CodigoSISIN)
+where a.CodigoSISIN = p.CodigoSISIN;
+
+INSERT INTO amp_activity_components(amp_activity_id, amp_component_id)
+SELECT
+a.amp_activity_id,
+c.amp_component_id
+FROM amp_components c
+join sisin_db.seguimiento_financiero sf on c.CodigoSISIN = sf.CodigoSisin
+join amp_activity a on a.amp_id = sf.CodConvExt
+group by a.amp_activity_id, c.amp_component_id;
+
+
+INSERT INTO amp_component_funding (transaction_type,adjustment_type,currency_id,perspective_id,amp_component_id,activity_id,transaction_amount,transaction_date,reporting_date)
+SELECT
+@commitment as transaction_type,
+@funding_adjusment_planned as adjustment_type,
+@usd_currency_id as currency_id,
+@mofed_perspective_id as perspective_id,
+c.amp_component_id,
+a.amp_activity_id,
+sf.MontoProgramado as transaction_amount,
+STR_TO_DATE(CONCAT(sf.Mes, '/01/', sf.Ano), '%m/%d/%Y') as transaction_date,
+FechaRegistro as reporting_date
+FROM amp_components c
+join sisin_db.seguimiento_financiero sf on c.CodigoSISIN = sf.CodigoSisin
+join amp_activity a on a.amp_id = sf.CodConvExt and sf.MontoProgramado != 0;
+
+INSERT INTO amp_component_funding (transaction_type,adjustment_type,currency_id,perspective_id,amp_component_id,activity_id,transaction_amount,transaction_date,reporting_date)
+SELECT
+@commitment as transaction_type,
+@funding_adjusment_actual as adjustment_type,
+@usd_currency_id as currency_id,
+@mofed_perspective_id as perspective_id,
+c.amp_component_id,
+a.amp_activity_id,
+sf.MontoReprogramado as transaction_amount,
+STR_TO_DATE(CONCAT(sf.Mes, '/01/', sf.Ano), '%m/%d/%Y') as transaction_date,
+FechaRegistro as reporting_date
+FROM amp_components c
+join sisin_db.seguimiento_financiero sf on c.CodigoSISIN = sf.CodigoSisin
+join amp_activity a on a.amp_id = sf.CodConvExt and sf.MontoReprogramado != 0;
+
+
+INSERT INTO amp_component_funding (transaction_type,adjustment_type,currency_id,perspective_id,amp_component_id,activity_id,transaction_amount,transaction_date,reporting_date)
+SELECT
+@expenditure,
+@funding_adjusment_actual,
+@usd_currency_id,
+@mofed_perspective_id,
+c.amp_component_id,
+a.amp_activity_id,
+sf.MontoEjecutado as transaction_amount,
+STR_TO_DATE(CONCAT(sf.Mes, '/01/', sf.Ano), '%m/%d/%Y') as transaction_date,
+FechaRegistro as reporting_date
+FROM amp_components c
+join sisin_db.seguimiento_financiero sf on c.CodigoSISIN = sf.CodigoSisin
+join amp_activity a on a.amp_id = sf.CodConvExt and sf.MontoEjecutado != 0;
---
+++ amp-scripts/bolivia/sql/undo_sisin.sql
@@ -0,0 +1,16 @@
+﻿use amp_bolivia;
+
+
+DELETE FROM  ac
+USING amp_activity_components as ac, amp_components as c
+where c.CodigoSISIN is not null and ac.amp_component_id = c.amp_component_id;
+
+
+DELETE FROM  acf
+USING amp_component_funding as acf, amp_components as c
+where c.CodigoSisin is not null and acf.amp_component_id = c.amp_component_id;
+
+DELETE FROM amp_components where CodigoSISIN is not null;
+
+ALTER TABLE amp_components DROP INDEX `Index_CodigoSISIN`;
+ALTER TABLE amp_components DROP COLUMN `CodigoSISIN`;

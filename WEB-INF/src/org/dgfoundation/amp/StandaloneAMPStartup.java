fe11bad1ad39bd500eb706985b49b5ee58e9423e
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-02-07 13:18:30 +0000

    importers and localNoDatasource
    git-svn-id: file:///svn/trunk/amp@4295 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/dgfoundation/amp/StandaloneAMPStartup.java
+++ WEB-INF/src/org/dgfoundation/amp/StandaloneAMPStartup.java
@@ -3,6 +3,12 @@
  */
 package org.dgfoundation.amp;
 
+import java.sql.SQLException;
+
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.Session;
+
+import org.dgfoundation.amp.importers.ImporterWorker;
 import org.digijava.kernel.exception.DgException;
 import org.digijava.kernel.persistence.PersistenceManager;
 import org.digijava.kernel.util.DigiConfigManager;
@@ -24,6 +30,24 @@ public class StandaloneAMPStartup {
 			DigiConfigManager
 					.initialize("repository");
 			PersistenceManager.initialize(false);
+			
+			
+			//BELOW THIS LINE, HIBERNATE IS AVAILABLE, ADD YOUR SCRIPT INVOCATION HERE			
+			try {
+				//EXAMPLE OF A WORKING HIBERNATE SESSION OBJECT:
+				Session session = PersistenceManager.getSession();
+			} catch (HibernateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//we import stuff using csv importer
+			//ImporterWorker iw=new ImporterWorker("/home/mihai/workspace/amp-testing/importers/");
+			//iw.start();
+			
+			
 		} catch (DgException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

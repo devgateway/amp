62b7e82ffa3c02f31e6704f1be5294434b38c9f5
mcoria <mcoria@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-02-19 14:29:28 +0000

    exprlogic test
    git-svn-id: file:///svn/trunk@4467 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/ar/cell/TotalCommitmentsAmountCell.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/ar/cell/TotalCommitmentsAmountCell.java
@@ -5,8 +5,9 @@ package org.dgfoundation.amp.ar.cell;
 
 import java.util.Iterator;
 
-import org.dgfoundation.amp.exprlogic.ExampleLogicalToken;
-import org.dgfoundation.amp.exprlogic.LogicalToken;
+import org.dgfoundation.amp.ar.ArConstants;
+import org.digijava.module.aim.logic.AmountCalculator;
+import org.digijava.module.aim.logic.Logic;
 
 /**
  * @author mihai
@@ -37,23 +38,9 @@ public class TotalCommitmentsAmountCell extends AmountCell {
          * @return Returns the amount.
          */
     public double getAmount() {
-    	double ret = 0;
 		if (id != null)
 			return convert();
-	
-		Iterator i = mergedCells.iterator();
-		while (i.hasNext()) {
-			CategAmountCell element = (CategAmountCell) i.next();
-		/*	if( ArConstants.DISBURSEMENT.equals(element.getMetaValueString(ArConstants.TRANSACTION_TYPE)) ) continue;
-			
-			 if( ArConstants.ACTUAL.equals(element.getMetaValueString(ArConstants.ADJUSTMENT_TYPE)) || 
-					 ArConstants.PLANNED.equals(element.getMetaValueString(ArConstants.ADJUSTMENT_TYPE)) )
-		*/
-			ret+=ExampleLogicalToken.totalCommitmentsLogicalToken.evaluate(element);
-		
-		}
-		return ret;
-
+		return Logic.getInstance().getCommitmentCalculator().calculateAmount(mergedCells);
 	}
 
     public Cell merge(Cell c) {
--- amp/WEB-INF/src/org/digijava/module/aim/logic/defaultimpl/DefaultCommitmentCalculator.java
+++ amp/WEB-INF/src/org/digijava/module/aim/logic/defaultimpl/DefaultCommitmentCalculator.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.dgfoundation.amp.ar.ArConstants;
 import org.dgfoundation.amp.ar.cell.CategAmountCell;
+import org.dgfoundation.amp.exprlogic.ExampleLogicalToken;
 import org.digijava.module.aim.logic.AmountCalculator;
 
 /**
@@ -15,14 +16,17 @@ public class DefaultCommitmentCalculator implements AmountCalculator{
 
 	public double calculateAmount(Set<CategAmountCell> mergedCells) {
 		double ret = 0;
+	
 		Iterator<CategAmountCell> i = mergedCells.iterator();
 		while (i.hasNext()) {
 			CategAmountCell element = (CategAmountCell) i.next();
-			if( ArConstants.DISBURSEMENT.equals(element.getMetaValueString(ArConstants.TRANSACTION_TYPE)) ) continue;
+		/*	if( ArConstants.DISBURSEMENT.equals(element.getMetaValueString(ArConstants.TRANSACTION_TYPE)) ) continue;
 			
 			 if( ArConstants.ACTUAL.equals(element.getMetaValueString(ArConstants.ADJUSTMENT_TYPE)) || 
 					 ArConstants.PLANNED.equals(element.getMetaValueString(ArConstants.ADJUSTMENT_TYPE)) )
-			ret += element.getAmount();
+		*/
+			ret+=ExampleLogicalToken.totalCommitmentsLogicalToken.evaluate(element);
+		
 		}
 		return ret;
 		

5cb88d05b5d8617ea71ad4a43375bf463bd984b9
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-02-20 17:05:00 +0000

    AMP-2669
    git-svn-id: file:///svn/trunk@4484 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/helper/CurrencyWorker.java
+++ amp/WEB-INF/src/org/digijava/module/aim/helper/CurrencyWorker.java
@@ -19,11 +19,12 @@ public class CurrencyWorker {
 	private static double exchangeRate = 0.0;
 
 	public static double convertToDouble(double amt, double fromExchangeRate,
-			double toExchangeRate) {
+			double toExchangeRate)
+	{
 		if (logger.isDebugEnabled())
 			logger.debug("convert passed amt=" + amt + " ,fromExchangeRate="
 					+ fromExchangeRate + ",toExchangeRate" + toExchangeRate);
-		if (fromExchangeRate != toExchangeRate && fromExchangeRate != 0) {
+		if (fromExchangeRate != toExchangeRate&&fromExchangeRate!=0) {
 			double inter = 1 / fromExchangeRate;
 			inter = inter * amt;
 			resultDbl = inter * toExchangeRate;
@@ -37,51 +38,52 @@ public class CurrencyWorker {
 	public static String convert(double amt, double fromExchangeRate,
 			double toExchangeRate) {
 
-		resultDbl = convertToDouble(amt, fromExchangeRate, toExchangeRate);
+		resultDbl	= convertToDouble(amt, fromExchangeRate, toExchangeRate);
 
-		// *** fix for AMP-1755
-		// DecimalFormat format = new DecimalFormat();
-		String inputString = FormatHelper.formatNumber(resultDbl);
-		// String inputString = String.valueOf(resultDbl);
-		// resultStr = CurrencyWorker.formatAmount(inputString);
+		//*** fix for AMP-1755
+//		DecimalFormat format = new DecimalFormat();
+		String inputString= FormatHelper.formatNumber(resultDbl);
+//		String inputString = String.valueOf(resultDbl);
+//		resultStr = CurrencyWorker.formatAmount(inputString);
 
-		// resultStr = mf.format(Math.round(resultDbl));
+//		resultStr = mf.format(Math.round(resultDbl));
 
 		if (logger.isDebugEnabled())
 			logger.debug("convert returns=" + resultStr);
-		// return resultStr;
+//		return resultStr;
 		return inputString;
 	}
 
-	public static double convertToUSD(double amnt, String fromCurrencyCode)
-			throws AimException {
+
+	public static double convertToUSD(double amnt,String fromCurrencyCode)  throws AimException{
 		exchangeRate = CurrencyUtil.getLatestExchangeRate(fromCurrencyCode);
-		return amnt * exchangeRate;
+		return amnt*exchangeRate;
 	}
+        public static double convertToDefaultCurr(double amnt,String fromCurrencyCode) throws AimException {
+          double amount=0;
+                try {
+                  exchangeRate = CurrencyUtil.getLatestExchangeRate(
+                      fromCurrencyCode);
+                }
+                catch (AimException ex) {
+                  throw ex;
+                }
+                amount=amnt/exchangeRate;
+                return  amount;
+        }
 
-	public static double convertToDefaultCurr(double amnt,
-			String fromCurrencyCode) throws AimException {
-		double amount = 0;
-		try {
-			exchangeRate = CurrencyUtil.getLatestExchangeRate(fromCurrencyCode);
-		} catch (AimException ex) {
-			throw ex;
-		}
-		amount = amnt / exchangeRate;
-		return amount;
-	}
 
-	public static double convertFromUSD(double amnt, String toCurrencyCode)
-			throws AimException {
+	public static double convertFromUSD(double amnt, String toCurrencyCode) throws AimException{
 		exchangeRate = CurrencyUtil.getLatestExchangeRate(toCurrencyCode);
-		return amnt / exchangeRate;
+		return amnt/exchangeRate;
 	}
 
-	public static double convertFromUSD(double amnt, Long toCurrencyId)
-			throws AimException {
+	public static double convertFromUSD(double amnt, Long toCurrencyId) throws AimException{
 		AmpCurrency ampcurrency = CurrencyUtil.getAmpcurrency(toCurrencyId);
-		return convertFromUSD(amnt, ampcurrency.getCurrencyCode());
+		return convertFromUSD(amnt,ampcurrency.getCurrencyCode());
 	}
+
+
 
 	public static double convert(double Amt, String currencyCode) {
 		if (logger.isDebugEnabled())
@@ -94,23 +96,26 @@ public class CurrencyWorker {
 
 	public static double convert1(double amt, double fromExchangeRate,
 			double toExchangeRate) {
+		
+		if (fromExchangeRate == 0) {
+			fromExchangeRate = 1;
+		}
 		if (fromExchangeRate != toExchangeRate) {
 			resultDbl = (amt / fromExchangeRate) * toExchangeRate;
+
 		} else {
 			resultDbl = amt;
 		}
-		// return Math.round(resultDbl);
 		return resultDbl;
 	}
 
 	/**
 	 * Formats the amount to include commas and decimal places Commas will be
 	 * inserted after every three digits
-	 * 
-	 * @param amt
-	 *            The aount value in String which is to be formatted
+	 *
+	 * @param amt The aount value in String which is to be formatted
 	 * @return The formatted ammount
-	 * 
+	 *
 	 * eg: The input will be a String like 25000000 and output will be a
 	 * formatted string in the form 25,000,000.00
 	 */
@@ -129,31 +134,40 @@ public class CurrencyWorker {
 
 	}
 
-	public static String removeCharsFromDouble(String s) {
-		String tmp = s;
-		String text = "";
-		if (tmp != null) {
-			char arr[] = tmp.toCharArray();
-			// text = "" ;
-			for (int i = 0; i < tmp.length(); i++) {
-				if (arr[i] >= '0' && arr[i] <= '9' || arr[i] == '.')
-					text += arr[i];
+
+	public static String removeCharsFromDouble(String s)
+	{
+		String tmp=s;
+		String text="";
+		if ( tmp != null )	{
+			char arr[] = tmp.toCharArray() ;
+			//text = "" ;
+			for (int i = 0 ; i < tmp.length() ; i++ )
+			{
+				if ( arr[i] >= '0' && arr[i]<='9' || arr[i] == '.')
+					text += arr[i] ;
 			}
-		} else
+		}
+		else
 			text += "0.0";
 		return text;
 	}
 
-	public static double formatToDouble(String amt) {
-		if (amt == null)
+
+	public static double formatToDouble(String amt)
+	{
+		if(amt==null)
 			return 0;
 		String fmt = amt.trim();
 		fmt = fmt.replaceAll(",", "");
-		fmt = removeCharsFromDouble(fmt);
+		fmt=removeCharsFromDouble(fmt);
 		double value = 0;
-		try {
+		try
+		{
 			value = Double.parseDouble(fmt);
-		} catch (NumberFormatException e) {
+		}
+		catch(NumberFormatException e)
+		{
 			logger.error("Trying to parse " + fmt + " to double :" + e);
 			return -1;
 		}
@@ -162,7 +176,7 @@ public class CurrencyWorker {
 
 	public static boolean validateAmount(String s) {
 		boolean valid = true;
-		// s=removeCharsFromDouble(s);
+		//s=removeCharsFromDouble(s);
 		try {
 			Double.parseDouble(s);
 		} catch (NumberFormatException e) {

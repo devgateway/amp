8eb80809c3c9d3f69a387ad9512fa21fd81e00f6
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-03-16 13:56:36 +0000

    filter by month refractor and repair
    git-svn-id: file:///svn/trunk@4723 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/logic/AmpARFilterHelper.java
+++ amp/WEB-INF/src/org/digijava/module/aim/logic/AmpARFilterHelper.java
@@ -6,8 +6,10 @@ public interface AmpARFilterHelper {
 
 	String createToYearQuery(Integer toYear);
 	
-	String createFromMonthQuery(Integer fromMonth);
-	
-	String createMonthYearQuery(Integer fromMonth, Integer fromYear, Integer toMonth, Integer toYear);
+	String createFromMonthQuery(Integer fromMonth, Integer fromYear);
+
+	String createToMonthQuery(Integer toMonth, Integer toYear);
+
+//	String createMonthYearQuery(Integer fromMonth, Integer fromYear, Integer toMonth, Integer toYear);
 
 }
--- amp/WEB-INF/src/org/digijava/module/aim/logic/defaultimpl/DefaulAmpARFilterHelper.java
+++ amp/WEB-INF/src/org/digijava/module/aim/logic/defaultimpl/DefaulAmpARFilterHelper.java
@@ -16,22 +16,24 @@ public class DefaulAmpARFilterHelper implements AmpARFilterHelper {
 				+ toYear + "'";
 	}
 	
-	public String createFromMonthQuery(Integer fromMonth) {
-		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
-				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND date_format(fd.transaction_date,_latin1'%m')>='"
-				+ fromMonth + "'";
-	}
-	
-	/*public String createToMonthQuery(Integer toMonth) {
-		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
-				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND date_format(fd.transaction_date,_latin1'%m')<='"
-				+ toMonth + "'";
-	}*/
-	
 	public String createMonthYearQuery(Integer fromMonth, Integer fromYear, Integer toMonth, Integer toYear) {
 		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
 				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND fd.transaction_date>=' "+fromYear+"-"+fromMonth+"" +
 						"-01' AND fd.transaction_date<='"+toYear+"-"+toMonth+"-31'";
+	}
+	
+	public String createFromMonthQuery(Integer fromMonth, Integer fromYear) {
+		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
+				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND ((date_format(fd.transaction_date,_latin1'%Y')='"
+				+ fromYear + "' AND date_format(fd.transaction_date, _latin1'%m')>='"+ fromMonth +"') OR " 
+				+ "(date_format(fd.transaction_date,_latin1'%Y') > '" + fromYear + "'))";
+	}
+	
+	public String createToMonthQuery(Integer toMonth, Integer toYear) {
+		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
+		+ "WHERE f.amp_funding_id=fd.amp_funding_id AND ((date_format(fd.transaction_date,_latin1'%Y')='"
+		+ toYear + "' AND date_format(fd.transaction_date, _latin1'%m')<='"+ toMonth +"') OR " 
+		+ "(date_format(fd.transaction_date,_latin1'%Y') < '" + toYear + "'))";
 	}
 
 }

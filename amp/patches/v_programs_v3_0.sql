ca9e7f1e1a81fd25ec86b813260f8e709dc644a5
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-03-19 11:10:17 +0000

    AMP-2784
    git-svn-id: file:///svn/trunk@4763 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/v_programs_v3_0.sql
+++ amp/patches/v_programs_v3_0.sql
@@ -2,15 +2,7 @@
  * Primary Program
  */
 
-DROP VIEW IF EXISTS `v_primaryprogram`;
-CREATE OR REPLACE VIEW `v_primaryprogram` AS 
-select `a`.`amp_activity_id` AS `amp_activity_id`,
-       `t`.`name` AS `name`,`ap`.`amp_program_id` AS `amp_program_id`,
-ap.program_percentage as program_percentage
-from ((`amp_activity` `a` 
-join `amp_activity_program` `ap` on(((`a`.`amp_activity_id` = `ap`.`amp_activity_id`) and (`ap`.`program_setting` = 2)))) 
-join `amp_theme` `t` on((`t`.`amp_theme_id` = `ap`.`amp_program_id`))) 
-order by `a`.`amp_activity_id`,`t`.`name`;
+CREATE OR REPLACE VIEW `v_primaryprogram` AS select `a`.`amp_activity_id` AS `amp_activity_id`,`t`.`name` AS `name`,`ap`.`amp_program_id` AS `amp_program_id`,ap.program_percentage as program_percentage from ((`amp_activity` `a` join `amp_activity_program` `ap` on(((`a`.`amp_activity_id` = `ap`.`amp_activity_id`) and (`ap`.`program_setting` = 2)))) join `amp_theme` `t` on((`t`.`amp_theme_id` = `ap`.`amp_program_id`))) order by `a`.`amp_activity_id`,`t`.`name`;
 
 
 
@@ -18,15 +10,7 @@ order by `a`.`amp_activity_id`,`t`.`name`;
  * Secondary Program
  */
 
-DROP VIEW IF EXISTS `v_secondaryprogram`;
-CREATE OR REPLACE VIEW `v_secondaryprogram` AS 
-select `a`.`amp_activity_id` AS `amp_activity_id`,
-       `t`.`name` AS `name`,`ap`.`amp_program_id` AS `amp_program_id`,
-ap.program_percentage as program_percentage
-from ((`amp_activity` `a` 
-join `amp_activity_program` `ap` on(((`a`.`amp_activity_id` = `ap`.`amp_activity_id`) and (`ap`.`program_setting` = 3)))) 
-join `amp_theme` `t` on((`t`.`amp_theme_id` = `ap`.`amp_program_id`))) 
-order by `a`.`amp_activity_id`,`t`.`name`;
+CREATE OR REPLACE VIEW `v_secondaryprogram` AS select `a`.`amp_activity_id` AS `amp_activity_id`,`t`.`name` AS `name`,`ap`.`amp_program_id` AS `amp_program_id`,ap.program_percentage as program_percentage from ((`amp_activity` `a` join `amp_activity_program` `ap` on(((`a`.`amp_activity_id` = `ap`.`amp_activity_id`) and (`ap`.`program_setting` = 3)))) join `amp_theme` `t` on((`t`.`amp_theme_id` = `ap`.`amp_program_id`))) order by `a`.`amp_activity_id`,`t`.`name`;
 
 /*
  * Change cell type to metaTextCell, so it will be able to hold metadata (percentage)

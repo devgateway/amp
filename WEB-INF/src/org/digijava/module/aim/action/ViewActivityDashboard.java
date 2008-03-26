1fd7817865215a1040d89383ef81b13c031b2d60
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-03-26 14:45:50 +0000

    AMP-3013, AMP-3015
    git-svn-id: file:///svn/trunk/amp@4856 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/ViewActivityDashboard.java
+++ WEB-INF/src/org/digijava/module/aim/action/ViewActivityDashboard.java
@@ -4,6 +4,11 @@
  */
 package org.digijava.module.aim.action;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,6 +19,12 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.DynaActionForm;
 import org.apache.struts.tiles.ComponentContext;
 import org.apache.struts.tiles.actions.TilesAction;
+import org.digijava.module.aim.dbentity.AmpActivity;
+import org.digijava.module.aim.dbentity.AmpIndicatorRiskRatings;
+import org.digijava.module.aim.dbentity.AmpIndicatorValue;
+import org.digijava.module.aim.dbentity.IndicatorActivity;
+import org.digijava.module.aim.util.ActivityUtil;
+import org.digijava.module.aim.util.IndicatorUtil;
 import org.digijava.module.aim.util.MEIndicatorsUtil;
 
 public class ViewActivityDashboard extends TilesAction {
@@ -31,9 +42,27 @@ public class ViewActivityDashboard extends TilesAction {
 		
 		if (request.getParameter("ampActivityId") != null) {
 			actId = new Long(Long.parseLong(
-					request.getParameter("ampActivityId")));
+					request.getParameter("ampActivityId")));			
 			
-			int risk = MEIndicatorsUtil.getOverallRisk(actId);
+			
+			ArrayList<AmpIndicatorRiskRatings> risks=new ArrayList<AmpIndicatorRiskRatings>();
+			Set<IndicatorActivity> valuesActivity=ActivityUtil.loadActivity(actId).getIndicators();
+			if(valuesActivity!=null && valuesActivity.size()>0){
+				Iterator<IndicatorActivity> it=valuesActivity.iterator();
+				while(it.hasNext()){
+					 IndicatorActivity indActivity=it.next();
+					 Set<AmpIndicatorValue> values=indActivity.getValues();					
+					 for(Iterator<AmpIndicatorValue> valuesIter=values.iterator();valuesIter.hasNext();){
+						 AmpIndicatorValue val=valuesIter.next();
+						 if(val.getRisk()!=null){
+							 risks.add(val.getRisk());
+							 break;//all values have same risk and this risk should go to connection.
+						 }					 					
+					}
+				}
+			}
+			
+			int risk = IndicatorUtil.getOverallRisk(risks);
 			String riskName = MEIndicatorsUtil.getRiskRatingName(risk);
 			String rskColor = MEIndicatorsUtil.getRiskColor(risk);
 			adForm.set("overallRisk",riskName);

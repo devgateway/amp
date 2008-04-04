9a8d576835a528724564eb9365b9edfd2ec18612
kobia <kobia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-04-04 15:00:22 +0000

    AMP-3023 fixing errors at startup for tanzania old db.
    git-svn-id: file:///svn/trunk@4932 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/7_amp_field_patch.sql
+++ amp/patches/7_amp_field_patch.sql
@@ -1,4 +1,4 @@
-INSERT INTO amp_field (amp_field_id,field_name) VALUES
+INSERT IGNORE INTO amp_field (amp_field_id,field_name) VALUES
  (8,"Results Objectively Verifiable Indicators"),
 (9,"Objective Objectively Verifiable Indicators"),
 (10,"Purpose Objectively Verifiable Indicators");

6ff5f981810b77806f924f3866cf50245d54c969
dmihaila <dmihaila@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-04-08 15:55:08 +0000

    AMP-3110
    git-svn-id: file:///svn/trunk/amp@4958 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/v_project_id.sql
+++
@@ -1,1 +0,0 @@
-create or replace view v_project_id as select i.amp_activity_id, concat(i.internal_id,' (',o.name,')') from amp_organisation o, amp_activity_internal_id i where o.amp_org_id=i.amp_org_id;

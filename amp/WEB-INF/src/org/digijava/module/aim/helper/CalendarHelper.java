b871168ea8782a2bf852fc3aea954d4bf04bf90b
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-04-14 15:03:02 +0000

    AMP-3171
    git-svn-id: file:///svn/trunk@5049 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/helper/CalendarHelper.java
+++ amp/WEB-INF/src/org/digijava/module/aim/helper/CalendarHelper.java
@@ -45,7 +45,15 @@ public class CalendarHelper {
 		return cm;
 	}
 	public int getQuarter() {
-		return Math.round( (gregCalendar.get(Calendar.MONTH)+1.0f) / 4 + 1);
+		switch(gregCalendar.get(Calendar.MONTH))
+		{
+		case 1:case 2: case 3: return 1;
+		case 4:case 5: case 6: return 2;
+		case 7:case 8: case 9: return 3;
+		case 10:case 11: case 12: return 4;
+		default: return 0;
+		}
+		//return Math.round( (gregCalendar.get(Calendar.MONTH)+1.0f) / 4 + 1);
 	}
 	public int getYear() {
 		return gregCalendar.get(Calendar.YEAR);

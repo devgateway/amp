bebe15f1697388dc545f6312e3ef2f02c004a772
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-04-16 12:41:32 +0000

    AMP-2998
    git-svn-id: file:///svn/trunk@5074 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/message/helper/AmpMessageWorker.java
+++ amp/WEB-INF/src/org/digijava/module/message/helper/AmpMessageWorker.java
@@ -1,6 +1,15 @@
 package org.digijava.module.message.helper;
 
 import java.util.Calendar;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.event.*;
 
 import org.digijava.module.aim.exception.AimException;
 import org.digijava.module.aim.util.TeamMemberUtil;
@@ -8,7 +17,7 @@ import org.digijava.module.message.dbentity.SystemMessage;
 import org.digijava.module.message.util.AmpMessageUtil;
 
 public class AmpMessageWorker {
-	public static void createAmpMessageForActivityCreation (Long ampActivityId,Long teamId) throws AimException {
+	public static void createAmpMessageForActivityCreation (Long ampActivityId,Long teamId) throws AimException,Exception {
 		Long teamLeaderId=TeamMemberUtil.getTeamHead(teamId).getAmpTeamMemId();
 		SystemMessage message=new SystemMessage();
 		message.setName("Created New Activity");
@@ -23,5 +32,34 @@ public class AmpMessageWorker {
 		Calendar cal=Calendar.getInstance();
 		message.setCreationDate(cal.getTime());
 		AmpMessageUtil.saveOrUpdateMessage(message);
+		sendMail();
 	}
+	
+	/* Bean Properties */
+	public static Properties props = null;
+	public static Session session = null;
+
+//	static {
+//		/*	Setting Properties for STMP host */
+//		props = System.getProperties();
+//		props.put("mail.smtp.host", "mail.yourisp.com");
+//		session = Session.getDefaultInstance(props, null);
+//	}
+
+	public static void sendMail() throws Exception {
+		props = System.getProperties();
+		props.put("mail.smtp.host", "mail.yahoo.com");
+		session = Session.getDefaultInstance(props, null);
+		try {				
+			MimeMessage message = new MimeMessage(session);
+			message.setRecipient(Message.RecipientType.TO, 
+				new InternetAddress("dareroinishvili@gmail.com"));
+			message.setFrom(new InternetAddress("skvisha@yahoo.com"));
+			message.setSubject("hola amigo");
+			message.setText("hello dear I");
+			Transport.send(message);
+		} catch (MessagingException e) {
+			throw new Exception(e.getMessage());
+		}
+	}	
 }

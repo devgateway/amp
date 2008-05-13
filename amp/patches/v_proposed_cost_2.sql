e055e525e7fada4474fdbaf5d6a2863097124556
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-05-13 15:47:26 +0000

    AMP-2793
    git-svn-id: file:///svn/trunk@5261 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/v_proposed_cost_2.sql
+++ amp/patches/v_proposed_cost_2.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE VIEW v_proposed_cost AS 
-SELECT amp_activity_id as amp_activity_id, amp_activity_id as object_id, amp_activity_id as object_id2, proj_cost_amount as transaction_amount, proj_cost_currcode as currency_code,proj_cost_date as transaction_date FROM amp_activity where proj_cost_amount is not null;
-UPDATE amp_columns set cellType='org.dgfoundation.amp.ar.cell.CategAmountCell' where columnName='Proposed Project Cost';
-INSERT INTO amp_measures (measureId,measureName,aliasName) values (9,'Uncommitted Balance','Uncommitted Balance','A');
+CREATE OR REPLACE VIEW v_proposed_cost AS SELECT amp_activity_id as amp_activity_id, amp_activity_id as object_id, amp_activity_id as object_id2, proj_cost_amount as transaction_amount, proj_cost_currcode as currency_code,proj_cost_date as transaction_date FROM amp_activity where proj_cost_amount is not null;
+DELETE FROM amp_report_column WHERE columnId IN (SELECT columnId FROM amp_columns WHERE extractorView='v_proposed_cost');
+DELETE FROM amp_columns WHERE extractorView='v_proposed_cost';
+INSERT INTO amp_columns (columnName,aliasName,cellType,extractorView) VALUES ('Proposed Project Cost','proposed_cost','org.dgfoundation.amp.ar.cell.CategAmountCell','v_proposed_cost');
+INSERT INTO amp_measures (measureName,aliasName) values ('Uncommitted Balance','Uncommitted Balance','A');

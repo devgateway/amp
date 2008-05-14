266998cf971cc3d1e5db141e8cad9e3d8fe4f958
vezugbaia <vezugbaia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-05-14 13:24:55 +0000

    AMP-3217
    git-svn-id: file:///svn/trunk/amp@5272 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/UpdateTeamMembers.java
+++ WEB-INF/src/org/digijava/module/aim/action/UpdateTeamMembers.java
@@ -31,150 +31,141 @@ import org.digijava.module.aim.util.TeamUtil;
 
 public class UpdateTeamMembers extends Action {
 
-	private static Logger logger = Logger.getLogger(UpdateTeamMembers.class);
+    private static Logger logger = Logger.getLogger(UpdateTeamMembers.class);
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws java.lang.Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+                                 HttpServletRequest request, HttpServletResponse response) throws java.lang.Exception {
 
-		logger.debug("In update teammembers");
-		boolean permitted = false;
-		HttpSession session = request.getSession();
-		if (session.getAttribute("ampAdmin") != null) {
-			String key = (String) session.getAttribute("ampAdmin");
-			if (key.equalsIgnoreCase("yes")) {
-				permitted = true;
-			} else {
-				if (session.getAttribute("teamLeadFlag") != null) {
-					key = (String) session.getAttribute("teamLeadFlag");
-					if (key.equalsIgnoreCase("true")) {
-						permitted = true;
-					}
-				}
-			}
-		}
-		if (!permitted) {
-			return mapping.findForward("index");
-		}
+        logger.debug("In update teammembers");
+        boolean permitted = false;
+        HttpSession session = request.getSession();
+        if (session.getAttribute("ampAdmin") != null) {
+            String key = (String) session.getAttribute("ampAdmin");
+            if (key.equalsIgnoreCase("yes")) {
+                permitted = true;
+            } else {
+                if (session.getAttribute("teamLeadFlag") != null) {
+                    key = (String) session.getAttribute("teamLeadFlag");
+                    if (key.equalsIgnoreCase("true")) {
+                        permitted = true;
+                    }
+                }
+            }
+        }
+        if (!permitted) {
+            return mapping.findForward("index");
+        }
 
+        TeamMemberForm upForm = (TeamMemberForm) form;
+        ActionErrors errors = new ActionErrors();
+        AmpTeam ampTeam = TeamUtil.getAmpTeam(upForm.getTeamId());
+        if (upForm.getAction() != null
+            && upForm.getAction().trim().equals("edit")) {
+            logger.debug("In edit team member");
+            AmpTeamMember ampMember = null;
 
-		try {
-			TeamMemberForm upForm = (TeamMemberForm) form;
-			ActionErrors errors = new ActionErrors();
-			AmpTeam ampTeam = TeamUtil.getAmpTeam(upForm.getTeamId());
-			if (upForm.getAction() != null
-					&& upForm.getAction().trim().equals("edit")) {
-				logger.debug("In edit team member");
-				AmpTeamMember ampMember=null;
+            if (upForm.getTeamMemberId() != null) {
+                ampMember = TeamUtil.getAmpTeamMember(upForm.getTeamMemberId());
+            } else {
+                ampMember = new AmpTeamMember();
+            }
 
-				if(upForm.getTeamMemberId()!=null){
-					ampMember=TeamUtil.getAmpTeamMember(upForm.getTeamMemberId());
-				}else {
-					ampMember = new AmpTeamMember();
-				}
-				 
-				//ampMember.setAmpTeamMemId(upForm.getTeamMemberId());
-				AmpTeamMemberRoles role = TeamMemberUtil.getAmpTeamMemberRole(upForm
-						.getRole());
-				AmpTeamMemberRoles teamLead = TeamMemberUtil.getAmpTeamHeadRole();
-				if (role.getRole().equals(teamLead.getRole())) {
-					ActionErrors error = new ActionErrors();
-					error.add(ActionErrors.GLOBAL_ERROR,
-											new ActionError(
-													"error.aim.addTeamMember.teamLeadRole"));
-				}
-				if (role.getRole().equals(teamLead.getRole())) {
-					logger.info("team name = " + ampTeam.getName());
-					logger.info(" team role = "+upForm.getRole());
-					logger.info(" this is the team Id = "+upForm.getTeamId() + " this is the member id = "+upForm.getTeamMemberId());
-					if ((ampTeam.getTeamLead() != null)&&(!ampTeam.getTeamLead().getAmpTeamMemId().equals(upForm.getTeamMemberId()))) {
-						upForm.setAmpRoles(TeamMemberUtil.getAllTeamMemberRoles());
-						
-						logger.info(" here");
-						String trnKey = "aim:teamLeadAlreadyExist";
-						String msg = CategoryManagerUtil.translate(trnKey, request, Constants.TEAM_LEAD_ALREADY_EXISTS);
-						errors.add(
-								ActionErrors.GLOBAL_ERROR,
-								new ActionError(
-										"error.aim.addTeamMember.teamLeadAlreadyExist",msg));
-						saveErrors(request, errors);
+            //ampMember.setAmpTeamMemId(upForm.getTeamMemberId());
+            AmpTeamMemberRoles role = TeamMemberUtil.getAmpTeamMemberRole(upForm
+                .getRole());
+            AmpTeamMemberRoles teamLead = TeamMemberUtil.getAmpTeamHeadRole();
+            if (role.getRole().equals(teamLead.getRole())) {
+                ActionErrors error = new ActionErrors();
+                error.add(ActionErrors.GLOBAL_ERROR,
+                          new ActionError(
+                              "error.aim.addTeamMember.teamLeadRole"));
+            }
+            if (role.getRole().equals(teamLead.getRole())) {
+                logger.info("team name = " + ampTeam.getName());
+                logger.info(" team role = " + upForm.getRole());
+                logger.info(" this is the team Id = " + upForm.getTeamId() + " this is the member id = " + upForm.getTeamMemberId());
+                if ( (ampTeam.getTeamLead() != null) && (!ampTeam.getTeamLead().getAmpTeamMemId().equals(upForm.getTeamMemberId()))) {
+                    upForm.setAmpRoles(TeamMemberUtil.getAllTeamMemberRoles());
 
-						return mapping.findForward("forward");
-						//return mapping.getInputForward();
+                    logger.info(" here");
+                    String trnKey = "aim:teamLeadAlreadyExist";
+                    String msg = CategoryManagerUtil.translate(trnKey, request, Constants.TEAM_LEAD_ALREADY_EXISTS);
+                    errors.add(
+                        ActionErrors.GLOBAL_ERROR,
+                        new ActionError(
+                            "error.aim.addTeamMember.teamLeadAlreadyExist", msg));
+                    saveErrors(request, errors);
 
-					}
+                    return mapping.findForward("forward");
+                    //return mapping.getInputForward();
 
-				}
-				logger.info(" this is the role.... "+role.getRole());
-				ampMember.setAmpMemberRole(role);
-				if (upForm.getReadPerms() != null
-						&& upForm.getReadPerms().equals("on")) {
-					ampMember.setReadPermission(new Boolean(true));
-				} else {
-					ampMember.setReadPermission(new Boolean(false));
+                }
 
-				}
+            }
+            logger.info(" this is the role.... " + role.getRole());
+            ampMember.setAmpMemberRole(role);
+            if (upForm.getReadPerms() != null
+                && upForm.getReadPerms().equals("on")) {
+                ampMember.setReadPermission(new Boolean(true));
+            } else {
+                ampMember.setReadPermission(new Boolean(false));
 
-				if (upForm.getWritePerms() != null
-						&& upForm.getWritePerms().equals("on")) {
-					ampMember.setWritePermission(new Boolean(true));
-				} else {
-					ampMember.setWritePermission(new Boolean(false));
-				}
-				if (upForm.getDeletePerms() != null
-						&& upForm.getDeletePerms().equals("on")) {
-					ampMember.setDeletePermission(new Boolean(true));
-				} else {
-					ampMember.setDeletePermission(new Boolean(false));
-				}
+            }
 
-				ampMember.setUser(UserUtils.getUser(upForm.getUserId()));
-				ampMember.setAmpTeam(ampTeam);
-				Collection col = TeamMemberUtil.getAllMemberAmpActivities(upForm
-						.getTeamMemberId());
-				Set temp = new HashSet();
-				temp.addAll(col);
-				ampMember.setActivities(temp);
-				logger.info(" updating the team member now....");
-				DbUtil.update(ampMember);
-				AmpTeamMember ampTeamHead = TeamMemberUtil.getTeamHead(ampTeam
-						.getAmpTeamId());
-				logger.info(" here finally");
+            if (upForm.getWritePerms() != null
+                && upForm.getWritePerms().equals("on")) {
+                ampMember.setWritePermission(new Boolean(true));
+            } else {
+                ampMember.setWritePermission(new Boolean(false));
+            }
+            if (upForm.getDeletePerms() != null
+                && upForm.getDeletePerms().equals("on")) {
+                ampMember.setDeletePermission(new Boolean(true));
+            } else {
+                ampMember.setDeletePermission(new Boolean(false));
+            }
 
-				if (ampTeam == null) {
-					logger.debug("ampTeam is null");
-				}
+            ampMember.setUser(UserUtils.getUser(upForm.getUserId()));
+            ampMember.setAmpTeam(ampTeam);
+            Collection col = TeamMemberUtil.getAllMemberAmpActivities(upForm
+                .getTeamMemberId());
+            Set temp = new HashSet();
+            temp.addAll(col);
+            ampMember.setActivities(temp);
+            logger.info(" updating the team member now....");
+            DbUtil.update(ampMember);
+            AmpTeamMember ampTeamHead = TeamMemberUtil.getTeamHead(ampTeam
+                .getAmpTeamId());
+            logger.info(" here finally");
 
-				if (ampTeamHead != null) {
-					ampTeam.setTeamLead(ampTeamHead);
-				} else {
-					ampTeam.setTeamLead(null);
-				}
-				DbUtil.update(ampTeam);
+            if (ampTeam == null) {
+                logger.debug("ampTeam is null");
+            }
 
-				if (ampTeam != null) {
-					request.setAttribute("teamId", ampTeam.getAmpTeamId());
-					return mapping.findForward("forward");
-				}
-			} else if (upForm.getAction() != null
-					&& upForm.getAction().trim().equals("delete")) {
-				logger.debug("In delete team member");
-				Long selMembers[] = new Long[1];
-				selMembers[0] = upForm.getTeamMemberId();
-				Site site = RequestUtils.getSite(request);
-				TeamMemberUtil.removeTeamMembers(selMembers,site.getId());
+            if (ampTeamHead != null) {
+                ampTeam.setTeamLead(ampTeamHead);
+            } else {
+                ampTeam.setTeamLead(null);
+            }
+            DbUtil.update(ampTeam);
 
-				if (ampTeam != null) {
-					request.setAttribute("teamId", ampTeam.getAmpTeamId());
-					return mapping.findForward("forward");
-				}
-			}
+            if (ampTeam != null) {
+                request.setAttribute("teamId", ampTeam.getAmpTeamId());
+                return mapping.findForward("forward");
+            }
+        } else if (upForm.getAction() != null
+                   && upForm.getAction().trim().equals("delete")) {
+            logger.debug("In delete team member");
+            Long selMembers[] = new Long[1];
+            selMembers[0] = upForm.getTeamMemberId();
+            Site site = RequestUtils.getSite(request);
+            TeamMemberUtil.removeTeamMembers(selMembers, site.getId());
 
-		} catch (Exception e) {
-			logger.error("Exception  :" + e);
-		}
-
-		return null;
-
-	}
+            if (ampTeam != null) {
+                request.setAttribute("teamId", ampTeam.getAmpTeamId());
+                return mapping.findForward("forward");
+            }
+        }
+        return mapping.findForward("forward");
+    }
 }

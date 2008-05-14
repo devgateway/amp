32b06edc131957ad51a1b8e50a440d2f1f80dc24
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-05-14 16:27:24 +0000

    AMP-3357
    git-svn-id: file:///svn/trunk/amp@5278 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/logic/boliviaimpl/BoliviaAmpARFilterHelper.java
+++ WEB-INF/src/org/digijava/module/aim/logic/boliviaimpl/BoliviaAmpARFilterHelper.java
@@ -3,17 +3,23 @@ package org.digijava.module.aim.logic.boliviaimpl;
 import org.digijava.module.aim.logic.AmpARFilterHelper;
 
 public class BoliviaAmpARFilterHelper implements AmpARFilterHelper {
-
+	//This is working the same way than the default filter, if we have a
 	public String createFromYearQuery(Integer fromYear) {
-		return "SELECT a.amp_activity_id " + "from amp_activity a "
-		+ "where date_format(a.convenio_date_filter,_latin1'%Y') >= '"
-		+ fromYear + "'";
+		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
+				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND date_format(fd.transaction_date,_latin1'%Y')>='"
+				+ fromYear + "'";
 	}
 
-	public String createToYearQuery(Integer toYear) {
-		return "SELECT a.amp_activity_id " + "from amp_activity a "
-		+ "where date_format(a.convenio_date_filter,_latin1'%Y') <= '"
-		+ toYear + "'";
+	public String createToYearQuery(Integer toYear) {		
+		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
+				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND date_format(fd.transaction_date,_latin1'%Y')<='"
+				+ toYear + "'";
+	}
+	
+	public String createMonthYearQuery(Integer fromMonth, Integer fromYear, Integer toMonth, Integer toYear) {
+		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
+				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND fd.transaction_date>=' "+fromYear+"-"+fromMonth+"" +
+						"-01' AND fd.transaction_date<='"+toYear+"-"+toMonth+"-31'";
 	}
 	
 	public String createFromMonthQuery(Integer fromMonth, Integer fromYear) {
@@ -23,21 +29,11 @@ public class BoliviaAmpARFilterHelper implements AmpARFilterHelper {
 				+ "(date_format(fd.transaction_date,_latin1'%Y') > '" + fromYear + "'))";
 	}
 	
-	
 	public String createToMonthQuery(Integer toMonth, Integer toYear) {
 		return "SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
 		+ "WHERE f.amp_funding_id=fd.amp_funding_id AND ((date_format(fd.transaction_date,_latin1'%Y')='"
 		+ toYear + "' AND date_format(fd.transaction_date, _latin1'%m')<='"+ toMonth +"') OR " 
 		+ "(date_format(fd.transaction_date,_latin1'%Y') < '" + toYear + "'))";
 	}
-	
-//	public String createMonthYearQuery(Integer fromMonth, Integer fromYear, Integer toMonth, Integer toYear) {
-//		return "QUERY NOT SAFE!";
-//		/*
-//		"SELECT DISTINCT(f.amp_activity_id) FROM amp_funding f, amp_funding_detail fd "
-//				+ "WHERE f.amp_funding_id=fd.amp_funding_id AND fd.transaction_date>=' "+fromYear+"-"+fromMonth+"" +
-//						"-01' AND fd.transaction_date<='"+toYear+"-"+toMonth+"-31'";
-//		*/
-//	}
 
 }

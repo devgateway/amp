9731a74cfe47c19201ef437e9ce3d8f69fcd413e
dmihaila <dmihaila@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-05-19 12:51:24 +0000

    AMP-3399
    git-svn-id: file:///svn/trunk@5312 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/ar/workers/MetaTextColWorker.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/ar/workers/MetaTextColWorker.java
@@ -6,6 +6,7 @@ package org.dgfoundation.amp.ar.workers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.dgfoundation.amp.ar.ARUtil;
 import org.dgfoundation.amp.ar.ArConstants;
 import org.dgfoundation.amp.ar.MetaInfo;
 import org.dgfoundation.amp.ar.ReportGenerator;
@@ -34,25 +35,27 @@ public class MetaTextColWorker extends TextColWorker {
 	protected Cell getCellFromRow(ResultSet rs) throws SQLException {
 		TextCell tc=(TextCell) super.getCellFromRow(rs);
 		MetaTextCell mtc=new MetaTextCell(tc);
+		
 		if(columnName.equals("Project Title")) 
 			mtc.getMetaData().add(new MetaInfo(ArConstants.DRAFT,rs.getBoolean(4))); else
 		if(columnName.equals("National Planning Objectives")) 
-			mtc.getMetaData().add(new MetaInfo(ArConstants.NPO_PERCENTAGE,rs.getDouble(4))); else
+			mtc.getMetaData().add(new MetaInfo(ArConstants.PERCENTAGE,rs.getDouble(4))); else
 		if(columnName.equals("Sector") || columnName.equals("Sub-Sector")) 
-			mtc.getMetaData().add(new MetaInfo(ArConstants.SECTOR_PERCENTAGE,rs.getDouble(4))); else
+			mtc.getMetaData().add(new MetaInfo(ArConstants.PERCENTAGE,rs.getDouble(4))); else
 			
 		if(columnName.equals("Executing Agency")) 
-			mtc.getMetaData().add(new MetaInfo(ArConstants.EXECUTING_AGENCY_PERCENTAGE,rs.getDouble(4))); else
+			mtc.getMetaData().add(new MetaInfo(ArConstants.PERCENTAGE,rs.getDouble(4))); else
 				
 		if(columnName.equals("Region") && generator.getReportMetadata().getType()==ArConstants.DONOR_TYPE)
-			mtc.getMetaData().add(new MetaInfo(ArConstants.LOCATION_PERCENTAGE,rs.getDouble(4))); else
+			mtc.getMetaData().add(new MetaInfo(ArConstants.PERCENTAGE,rs.getDouble(4))); else
 		if(columnName.equals("Componente") && generator.getReportMetadata().getType()==ArConstants.DONOR_TYPE) 
-			mtc.getMetaData().add(new MetaInfo(ArConstants.COMPONENTE_PERCENTAGE,rs.getDouble(4))); 
-		if(columnName.equals("Componente") && generator.getReportMetadata().getType()==ArConstants.DONOR_TYPE) 
-			mtc.getMetaData().add(new MetaInfo(ArConstants.COMPONENTE_PERCENTAGE,rs.getDouble(4))); 
+			mtc.getMetaData().add(new MetaInfo(ArConstants.PERCENTAGE,rs.getDouble(4))); 
 		if((columnName.equals("Primary Program") || columnName.equals("Secondary Program")) && generator.getReportMetadata().getType()==ArConstants.DONOR_TYPE) 
-			mtc.getMetaData().add(new MetaInfo(ArConstants.PROGRAM_PERCENTAGE,rs.getDouble(4))); 
-		
+			mtc.getMetaData().add(new MetaInfo(ArConstants.PERCENTAGE,rs.getDouble(4))); 
+		//if(mtc.getMetaData())
+		MetaInfo percentageMeta = MetaInfo.getMetaInfo(mtc.getMetaData(), ArConstants.PERCENTAGE);
+		if(percentageMeta!=null && ((Double)percentageMeta.getValue()).doubleValue()==0 ) 
+			mtc.setValue(ArConstants.UNALLOCATED);
 		return mtc;
 	}
 

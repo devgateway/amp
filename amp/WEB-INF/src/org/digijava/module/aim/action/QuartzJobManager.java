3416941e991fb011d98e12f404a59bcf52288b5d
vezugbaia <vezugbaia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-06-13 12:47:24 +0000

    AMP-3409
    git-svn-id: file:///svn/trunk@5613 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/WEB-INF/src/org/digijava/module/aim/action/QuartzJobManager.java
@@ -0,0 +1,41 @@
+package org.digijava.module.aim.action;
+
+import org.apache.struts.action.ActionForward;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionMapping;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.digijava.module.aim.form.QuartzJobManagerForm;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+import java.util.Date;
+import org.quartz.Scheduler;
+import org.quartz.TriggerUtils;
+import org.quartz.SchedulerFactory;
+import org.digijava.module.message.jobs.ActivityDisbursementsDatesJob;
+
+public class QuartzJobManager extends Action {
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws java.lang.Exception {
+
+            QuartzJobManagerForm qmform=(QuartzJobManagerForm)form;
+
+            SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
+            Scheduler sched = schedFact.getScheduler();
+            JobDetail ampJobDetail = new JobDetail("AmpActivityDisbursementsDatesJob", null, ActivityDisbursementsDatesJob.class);
+            Trigger ampJobTrigger = TriggerUtils.makeMinutelyTrigger(5);
+            ampJobTrigger.setName("ampActivityDisbursementsDatesJobTrigger");
+            ampJobTrigger.setStartTime(TriggerUtils.getEvenSecondDate(new Date()));
+            sched.scheduleJob(ampJobDetail, ampJobTrigger);
+            sched.start();
+
+            qmform.setMsText("Job Created");
+
+            return mapping.findForward("forward");
+        }
+            public QuartzJobManager() {
+    }
+}
--- amp/WEB-INF/src/org/digijava/module/message/jobs/ActivityDisbursementsDatesJob.java
+++ amp/WEB-INF/src/org/digijava/module/message/jobs/ActivityDisbursementsDatesJob.java
@@ -1,5 +1,33 @@
 package org.digijava.module.message.jobs;
 
-public class ActivityDisbursementsDatesJob {
+import org.quartz.StatefulJob;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.digijava.module.aim.util.ActivityUtil;
+import org.digijava.module.aim.dbentity.AmpActivity;
+import java.util.List;
+import java.util.Date;
+import java.util.Calendar;
+import org.digijava.module.aim.util.AmpDateUtils;
+import org.digijava.module.message.helper.ActivityDisbursementDateTrigger;
 
+public class ActivityDisbursementsDatesJob implements StatefulJob {
+    public void execute(JobExecutionContext context) throws JobExecutionException{
+
+        Date curDate=new Date();
+        Date dateBefore2Days=AmpDateUtils.getDateBeforeDays(curDate,2);
+
+        Calendar cl=Calendar.getInstance();
+        cl.setTime(curDate);
+
+        List<AmpActivity> actList=ActivityUtil.getAllActivitiesList();
+        for (AmpActivity act: actList){
+            if(act.getDisbursmentsDate()!=null){
+                if(act.getDisbursmentsDate().equals(dateBefore2Days) ||
+                   act.getDisbursmentsDate().after(dateBefore2Days)){
+                   new ActivityDisbursementDateTrigger(act);
+                }
+            }
+        }
+    }
 }

46ae5d63fc202b9230d3ec39032daee4b00dec7f
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-06-18 19:31:38 +0000

    AMP-2052
    git-svn-id: file:///svn/trunk@5664 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/form/TeamAuditForm.java
+++ amp/WEB-INF/src/org/digijava/module/aim/form/TeamAuditForm.java
@@ -11,9 +11,7 @@ public class TeamAuditForm extends ActionForm implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -2614366966871314200L;
-	/**
-	 * @author dan
-	 */
+	
 	private Collection logs;
 	private String sortBy;
 	private int pagesToShow;

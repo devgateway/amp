03b4bbbb4ca6d2a0e397b48334150dac059ecb25
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-06-19 12:24:10 +0000

    AMP-3454
    git-svn-id: file:///svn/trunk@5673 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/amp_columns_filters_all_columns.sql
+++
@@ -1,2 +0,0 @@
-TRUNCATE TABLE amp_columns_filters;
-INSERT INTO amp_columns_filters(column_id,bean_field_name,view_field_name) VALUES (64,'amp_sector_id','selectedSecondarySectors'),(41,'amp_program_id','selectedNatPlanObj'),(42,'amp_program_id','selectedPrimaryPrograms'),(43,'amp_program_id','selectedSecondaryPrograms'),(9,'region_id','regions'),(44,'amp_org_id','executingAgency'),(46,'amp_org_id','implementingAgency'),(48,'amp_org_id','beneficiaryAgency'),(10,'amp_modality_id','financingInstruments'),(5,'terms_assist_code','typeOfAssistance');

4a8a0bbe9266de0903695afa82fa9b9050cf2a07
vezugbaia <vezugbaia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-06-25 15:08:31 +0000

    AMP-3409
    git-svn-id: file:///svn/trunk@5742 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/QuartzJobManager.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/QuartzJobManager.java
@@ -1,41 +1,84 @@
 package org.digijava.module.aim.action;
 
-import org.apache.struts.action.ActionForward;
 import javax.servlet.http.HttpServletRequest;
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionMapping;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import org.digijava.module.aim.form.QuartzJobManagerForm;
-import org.quartz.JobDetail;
-import org.quartz.Trigger;
+import org.digijava.module.aim.helper.QuartzJobForm;
+import org.digijava.module.aim.util.QuartzJobUtils;
 import java.util.Date;
-import org.quartz.Scheduler;
-import org.quartz.TriggerUtils;
-import org.quartz.SchedulerFactory;
-import org.digijava.module.message.jobs.ActivityDisbursementsDatesJob;
+import java.text.SimpleDateFormat;
 
 public class QuartzJobManager extends Action {
+
     public ActionForward execute(ActionMapping mapping,
                                  ActionForm form,
                                  HttpServletRequest request,
                                  HttpServletResponse response) throws java.lang.Exception {
 
-            QuartzJobManagerForm qmform=(QuartzJobManagerForm)form;
+        QuartzJobManagerForm qmform = (QuartzJobManagerForm) form;
 
-            SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
-            Scheduler sched = schedFact.getScheduler();
-            JobDetail ampJobDetail = new JobDetail("ampActivityDisbursementsDatesJob", null, ActivityDisbursementsDatesJob.class);
-            Trigger ampJobTrigger = TriggerUtils.makeDailyTrigger(1,0);
-            ampJobTrigger.setName("ampActivityDisbursementsDatesJobTrigger");
-            ampJobTrigger.setStartTime(TriggerUtils.getEvenHourDate(new Date()));
-            sched.scheduleJob(ampJobDetail, ampJobTrigger);
-            sched.start();
+        if ("saveJob".equals(qmform.getAction())) {
+            QuartzJobForm job=new QuartzJobForm();
+            job.setClassFullname(qmform.getClassFullname());
+            job.setDayOfWeek(qmform.getSelectedDay());
 
-            qmform.setMsText("Job Created");
+            Date date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(qmform.getStartDateTime());
+            job.setStartDateTime(date);
 
+            job.setExeTime(qmform.getExeTime());
+            job.setName(qmform.getName());
+
+            if(!"".equals(qmform.getEndDateTime())){
+                date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse(qmform.getEndDateTime());
+                job.setEndDateTime(date);
+            }
+            job.setTriggerType(qmform.getTriggerType());
+
+            QuartzJobUtils.addJob(job);
+        } else if ("addJob".equals(qmform.getAction())) {
+            qmform.reset(mapping, request);
+            return mapping.findForward("addJob");
+
+        } else if ("editJob".equals(qmform.getAction())) {
+            for (QuartzJobForm exJob : qmform.getJobs()) {
+                if (exJob.getName().equals(qmform.getName())) {
+                    if (exJob.getEndDateTime() != null) {
+                        qmform.setEndDateTime(exJob.getEndDateTime().toString());
+                    }
+                    qmform.setStartDateTime(exJob.getStartDateTime().toString());
+                    qmform.setName(exJob.getName());
+                    qmform.setClassFullname(exJob.getClassFullname());
+                    qmform.setJob(exJob);
+                    break;
+                }
+            }
             return mapping.findForward("forward");
+
+        } else if ("deleteJob".equals(qmform.getAction())) {
+            QuartzJobUtils.deleteJob(qmform.getName());
+        } else if ("pauseAll".equals(qmform.getAction())) {
+            QuartzJobUtils.pauseAll();
+        } else if ("resumeAll".equals(qmform.getAction())) {
+            QuartzJobUtils.resumeAll();
+        } else if ("pauseJob".equals(qmform.getAction())) {
+            QuartzJobUtils.pauseJob(qmform.getName());
+        } else if ("resumeJob".equals(qmform.getAction())) {
+            QuartzJobUtils.resumeJob(qmform.getName());
+        } else if ("deleteJob".equals(qmform.getAction())) {
+            QuartzJobUtils.deleteJob(qmform.getName());
         }
-            public QuartzJobManager() {
+
+        qmform.setJobs(QuartzJobUtils.getAllJobs());
+        qmform.setAction(null);
+
+        return mapping.findForward("forward");
+    }
+
+    public QuartzJobManager() {
     }
 }
--- amp/WEB-INF/src/org/digijava/module/aim/form/QuartzJobManagerForm.java
+++ amp/WEB-INF/src/org/digijava/module/aim/form/QuartzJobManagerForm.java
@@ -1,18 +1,116 @@
 package org.digijava.module.aim.form;
 
+import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.digijava.module.aim.helper.QuartzJobForm;
 
 public class QuartzJobManagerForm extends ActionForm {
-    private String msText;
+    private String name;
+    private String classFullname;
+    private String startDateTime;
+    private String endDateTime;
+    private int triggerType;
+    private int selectedDay;
+    private String exeTime;
+    private QuartzJobForm job;
+    private Collection<QuartzJobForm> jobs;
+    private String action;
 
     public QuartzJobManagerForm() {
+
     }
 
-    public String getMsText() {
-        return msText;
+    public void reset(ActionMapping mapping, HttpServletRequest request){
+        name=null;
+        startDateTime=null;
+        endDateTime=null;
+        triggerType=0;
+        selectedDay=1;
+        exeTime=null;
+        jobs=null;
+        action=null;
     }
 
-    public void setMsText(String msText) {
-        this.msText = msText;
+    public Collection<QuartzJobForm> getJobs() {
+        return jobs;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public QuartzJobForm getJob() {
+        return job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEndDateTime() {
+        return endDateTime;
+    }
+
+    public String getExeTime() {
+        return exeTime;
+    }
+
+    public int getSelectedDay() {
+        return selectedDay;
+    }
+
+    public String getStartDateTime() {
+        return startDateTime;
+    }
+
+    public int getTriggerType() {
+        return triggerType;
+    }
+
+    public String getClassFullname() {
+        return classFullname;
+    }
+
+    public void setJobs(Collection<QuartzJobForm> jobs) {
+        this.jobs = jobs;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public void setJob(QuartzJobForm job) {
+        this.job = job;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setExeTime(String exeTime) {
+        this.exeTime = exeTime;
+    }
+
+    public void setEndDateTime(String endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public void setSelectedDay(int selectedDay) {
+        this.selectedDay = selectedDay;
+    }
+
+    public void setStartDateTime(String startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public void setTriggerType(int triggerType) {
+        this.triggerType = triggerType;
+    }
+
+    public void setClassFullname(String classFullname) {
+        this.classFullname = classFullname;
     }
 }

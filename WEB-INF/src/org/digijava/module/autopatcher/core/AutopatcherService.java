ec5282fedd63220f43ce572b3c39a9ed12324a0b
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-06-28 11:25:38 +0000

    AMPBURK-13
    git-svn-id: file:///svn/trunk/amp@5792 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/autopatcher/core/AutopatcherService.java
+++ WEB-INF/src/org/digijava/module/autopatcher/core/AutopatcherService.java
@@ -43,125 +43,127 @@ public class AutopatcherService extends AbstractServiceImpl {
 	private Collection appliedPatches;
 	private static Logger logger = Logger.getLogger(AutopatcherService.class);
 
-	
 	protected void processInitEvent(ServiceContext serviceContext)
 			throws ServiceException {
-		
-			Session session;
-			appliedPatches=new ArrayList();
-			try {
-				session = PersistenceManager.getSession();
-			String realRootPath=serviceContext.getRealPath("/"+patchesDir+"/");
+
+		Session session;
+		appliedPatches = new ArrayList();
+		try {
+			session = PersistenceManager.getSession();
+			String realRootPath = serviceContext.getRealPath("/" + patchesDir
+					+ "/");
 			logger.info("Applying patches...");
-			logger.info("Patch directory is "+realRootPath);
-			Collection<File> allPatchesFiles = PatcherUtil.getAllPatchesFiles(serviceContext.getRealPath(patchesDir));
+			logger.info("Patch directory is " + realRootPath);
+			Collection<File> allPatchesFiles = PatcherUtil
+					.getAllPatchesFiles(serviceContext.getRealPath(patchesDir));
 			Set allAppliedPatches = PatcherUtil.getAllAppliedPatches(session);
-			Iterator i=allPatchesFiles.iterator();
+			Iterator i = allPatchesFiles.iterator();
 			while (i.hasNext()) {
 				File element = (File) i.next();
-				String localPatchPath=element.getAbsolutePath().substring(realRootPath.length()+1,element.getAbsolutePath().length());
-				if(allAppliedPatches.contains(localPatchPath)) continue;
-				
-				try {
-				String delimiter=";";
-				boolean firstLine=true;
-				LineNumberReader bis=new LineNumberReader(new FileReader(element));
-				StringBuffer sb= new StringBuffer();
-				String s=bis.readLine();
-				while(s!=null) {
-				if(firstLine && s.length()>=11 && s.substring(0,9).equalsIgnoreCase("delimiter")) {
-					delimiter=s.substring(10, 11);
-					s = bis.readLine();
+				String localPatchPath = element.getAbsolutePath().substring(
+						realRootPath.length() + 1,
+						element.getAbsolutePath().length());
+				if (allAppliedPatches.contains(localPatchPath))
 					continue;
-				}
-					
-				 sb.append(s);
-				 firstLine=false;
-				 s = bis.readLine();					
-				}
-				bis.close();
-							
-				StringTokenizer stok=new StringTokenizer(sb.toString(),delimiter);
-				logger.info("Applying patch "+element.getAbsolutePath());
-				logger.info("Executing sql commands: "+sb.toString());
-				
-				
-				Connection connection = PersistenceManager.getSession().connection();
-				connection.setAutoCommit(false);
-				
-				try {				
-				Statement st=connection.createStatement();
-				
-				
-				
 
-				while(stok.hasMoreTokens()) {
-					
-				String sqlCommand=stok.nextToken();
-				if(sqlCommand.trim().equals("")) continue;
-			
-				st.addBatch(sqlCommand);
-				
-				
-				}
+				try {
+					String delimiter = ";";
+					boolean firstLine = true;
+					LineNumberReader bis = new LineNumberReader(new FileReader(
+							element));
+					StringBuffer sb = new StringBuffer();
+					String s = bis.readLine();
+					while (s != null) {
+						if (firstLine
+								&& s.length() >= 11
+								&& s.substring(0, 9).equalsIgnoreCase(
+										"delimiter")) {
+							delimiter = s.substring(10, 11);
+							s = bis.readLine();
+							continue;
+						}
 
-				st.executeBatch();
-				connection.commit();
-				st.close();
-				
-				PatchFile pf=new PatchFile();
-				pf.setAbsolutePatchName(localPatchPath);
-				pf.setInvoked(new Timestamp(System.currentTimeMillis()));
-				
-				
-				session.save(pf);
-				
-				appliedPatches.add(element.getAbsolutePath());
-			
-				
-				}
-				
-				catch (BatchUpdateException e) {
-					e.printStackTrace();
+						sb.append(s);
+						firstLine = false;
+						s = bis.readLine();
+					}
+					bis.close();
+
+					StringTokenizer stok = new StringTokenizer(sb.toString(),
+							delimiter);
+					logger.info("Applying patch " + element.getAbsolutePath());
+					logger.info("Executing sql commands: " + sb.toString());
+
+					Connection connection = PersistenceManager.getSession()
+							.connection();
+					connection.setAutoCommit(false);
+
+					try {
+						Statement st = connection.createStatement();
+
+						while (stok.hasMoreTokens()) {
+
+							String sqlCommand = stok.nextToken();
+							if (sqlCommand.trim().equals(""))
+								continue;
+
+							st.addBatch(sqlCommand);
+
+						}
+
+						st.executeBatch();
+						connection.commit();
+						st.close();
+
+						PatchFile pf = new PatchFile();
+						pf.setAbsolutePatchName(localPatchPath);
+						pf
+								.setInvoked(new Timestamp(System
+										.currentTimeMillis()));
+
+						session.save(pf);
+
+						appliedPatches.add(element.getAbsolutePath());
+
+					}
+
+					catch (BatchUpdateException e) {
+						e.printStackTrace();
+						logger.error(e);
+						connection.rollback();
+
+					} finally {
+						connection.setAutoCommit(true);
+						connection.close();
+					}
+
+				} catch (Exception e) {
 					logger.error(e);
-					connection.rollback();
-					
-				} finally {
-					connection.setAutoCommit(true);
-					connection.close();
-				}
-				
-				
-
-				
-				} catch (Exception e ) {
-					logger.error(e);
 					e.printStackTrace();
 				}
-		
+
 			}
 			PersistenceManager.releaseSession(session);
-			
-			} catch (HibernateException e1) {
-				// TODO Auto-generated catch block
-				throw new RuntimeException( "HibernateException Exception encountered", e1);
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				throw new RuntimeException( "SQLException Exception encountered", e1);
-			} catch (InvalidPatchRepositoryException e) {
-				// TODO Auto-generated catch block
-				throw new RuntimeException( "InvalidPatchRepositoryException Exception encountered", e);
-			}
-			
-			
-			logger.info(this.toString());
-			
-		
+
+		} catch (HibernateException e1) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(
+					"HibernateException Exception encountered", e1);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException("SQLException Exception encountered", e1);
+		} catch (InvalidPatchRepositoryException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(
+					"InvalidPatchRepositoryException Exception encountered", e);
+		}
+
+		logger.info(this.toString());
 
 	}
-	
+
 	public String toString() {
-		return "Autopatcher: Patches applied : "+appliedPatches;		
+		return "Autopatcher: Patches applied : " + appliedPatches;
 	}
 
 	public String getPatchesDir() {
@@ -171,5 +173,4 @@ public class AutopatcherService extends AbstractServiceImpl {
 	public void setPatchesDir(String patchesDir) {
 		this.patchesDir = patchesDir;
 	}
-	}
-
+}

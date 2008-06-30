c5cfb510f90777922e1bfbe56a7b171b91b9a98d
mkamara <mkamara@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-06-30 15:34:06 +0000

    AMP-3629
    git-svn-id: file:///svn/trunk/amp@5805 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ patches/insert_new_measures.sql
@@ -0,0 +1,2 @@
+REPLACE INTO amp_measures (measureid, measureName, aliasName, type) VALUES (9, "Actual Disbursement Orders", "Actual Disbursement Orders", "A");
+REPLACE INTO amp_measures (measureid, measureName, aliasName, type) VALUES (10, "Uncommitted Balance", "Uncommitted Balance", "A");

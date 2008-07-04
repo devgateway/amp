076132d867a334b2ed9faf36db55a189e8dfb3a5
ktha <ktha@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-07-04 11:41:29 +0000

    Removed unusused "templates" field (AMP-2220)
    git-svn-id: file:///svn/trunk@5887 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpModulesVisibility.java
+++ amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpModulesVisibility.java
@@ -7,7 +7,6 @@
 package org.digijava.module.aim.dbentity;
 
 import java.io.Serializable;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -15,8 +14,8 @@ import java.util.TreeSet;
 import org.dgfoundation.amp.visibility.AmpObjectVisibility;
 import org.digijava.module.aim.util.FeaturesUtil;
 
-public class AmpModulesVisibility extends AmpObjectVisibility implements
-		Serializable {
+public class AmpModulesVisibility extends AmpObjectVisibility implements Serializable {
+	
 	/**
 	 * 
 	 */
@@ -25,36 +24,35 @@ public class AmpModulesVisibility extends AmpObjectVisibility implements
 	/**
 	 * @author dan
 	 */
-	private Set templates;
-
 	private Set submodules;
 
-	public Set getTemplates() {
-		return templates;
-	}
-
-	public void setTemplates(Set templates) {
-		this.templates = templates;
-	}
-
+	/**
+	 * @see org.dgfoundation.amp.visibility.AmpObjectVisibility#getParent()
+	 */
 	public AmpObjectVisibility getParent() {
-		// TODO Auto-generated method stub
-		// templates.iterator().next();
 		return super.parent;
 	}
 
+	/**
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	public int compareTo(Object arg0) {
-		// TODO Auto-generated method stub
-
 		AmpObjectVisibility x = (AmpObjectVisibility) arg0;
 		return this.getId().compareTo(x.getId());
 
 	}
 
+	/**
+	 * @see org.dgfoundation.amp.visibility.AmpObjectVisibility#getVisible()
+	 */
 	public String getVisible() {
 		return getTemplate().getItems().contains(this) ? "true" : "false";
 	}
 
+	/**
+	 * @param id
+	 * @return
+	 */
 	public boolean isVisibleId(Long id) {
 		for (Iterator it = this.getTemplates().iterator(); it.hasNext();) {
 			AmpTemplatesVisibility x = (AmpTemplatesVisibility) it.next();
@@ -65,6 +63,10 @@ public class AmpModulesVisibility extends AmpObjectVisibility implements
 		return false;
 	}
 
+	/**
+	 * @param aObjVis
+	 * @return
+	 */
 	public boolean isVisibleTemplateObj(AmpObjectVisibility aObjVis) {
 		for (Iterator it = aObjVis.getItems().iterator(); it.hasNext();) {
 			AmpModulesVisibility x = (AmpModulesVisibility) it.next();
@@ -75,31 +77,48 @@ public class AmpModulesVisibility extends AmpObjectVisibility implements
 		return false;
 	}
 
+	/**
+	 * @see org.dgfoundation.amp.visibility.AmpObjectVisibility#getTemplate()
+	 */
 	public AmpTemplatesVisibility getTemplate() {
 		return (AmpTemplatesVisibility) parent;
 	}
 
+	/**
+	 * @return
+	 */
 	public TreeSet getSortedAlphaSubModules() {
 		TreeSet mySet = new TreeSet(FeaturesUtil.ALPHA_ORDER);
 		mySet.addAll(submodules);
 		return mySet;
 	}
 
+	/**
+	 * @return
+	 */
 	public Set getSubmodules() {
-
 		return submodules;
 	}
 
+	/**
+	 * @param submodules
+	 */
 	public void setSubmodules(Set submodules) {
 		this.submodules = submodules;
 	}
 
+	/**
+	 * @see org.digijava.module.gateperm.core.Permissible#getImplementedActions()
+	 */
 	@Override
 	public String[] getImplementedActions() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * @see org.digijava.module.gateperm.core.Permissible#getPermissibleCategory()
+	 */
 	@Override
 	public Class getPermissibleCategory() {
 		return AmpModulesVisibility.class;

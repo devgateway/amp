f146429b563f739db3e038c02d4bec297555fc55
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-07-04 15:33:39 +0000

    AMP-2898
    git-svn-id: file:///svn/trunk@5889 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/helper/IndicatorGridItem.java
+++ amp/WEB-INF/src/org/digijava/module/aim/helper/IndicatorGridItem.java
@@ -32,17 +32,32 @@ public class IndicatorGridItem {
 		this.year = year;
 		if (values != null && values.size() >0){
 			AmpIndicatorValue latestValue =null;
-			
+                        AmpIndicatorValue latestTarValue =null;
+			AmpIndicatorValue latestBaseValue =null;
 			//AmpThemeIndicatorValue latestValue = null;
 			int yearValue=Integer.valueOf(this.year).intValue();
 			if (values!=null){
 				for (AmpIndicatorValue value : values) {
 					//if this is target value
-					if (value.getValueType() == 0){
-						//currently there are no constraints when vales are entered.
-						//this means that in theory we may have several target values.
-						this.targetValue = value.getValue().toString();
-					}
+					if (value.getValueType() == 0) {
+                                        //currently there are no constraints when vales are entered.
+                                        //this means that in theory we may have several target values.
+                                        if (latestTarValue == null) {
+                                            latestTarValue = value;
+                                        }
+                                        Date targDate = value.getValueDate();
+                                        Calendar cal = Calendar.getInstance();
+                                        cal.setTime(targDate);
+                                        int tarYear = cal.get(Calendar.YEAR);
+                                            /*searching  target value.  
+                                              Its date must be equal or greater than current year 
+                                              and be the oldest for selected year. 
+                                              To recap if there is no target value for current year we will search in the following years targets values)
+                                             */
+                                        if (tarYear >= yearValue && latestTarValue.getValueDate().after(value.getValueDate())) {
+                                            latestTarValue = value;
+                                        }
+                                    }
 					//if this is actual value
 					if (value.getValueType() == 1){
 						Date creationDate = value.getValueDate();
@@ -61,17 +76,37 @@ public class IndicatorGridItem {
 							}
 						}
 					}
-					//base value
-					if (value.getValueType() == 2){
-						this.baseValue=value.getValue().toString();
-					}
-					
+                                    /*searching  base value.  
+                                    Its date must be equal or less than current year 
+                                    and be the newest for selected year. 
+                                    To recap if there is no base value for current year we will search in the previous years targets values)
+                                     */
+                                    if (value.getValueType() == 2) {
+                                        if (latestBaseValue == null) {
+                                            latestBaseValue = value;
+                                        }
+                                        Date baseDate = value.getValueDate();
+                                        Calendar cal = Calendar.getInstance();
+                                        cal.setTime(baseDate);
+                                        int baseYear = cal.get(Calendar.YEAR);
+                                        if (baseYear <= yearValue && latestBaseValue.getValueDate().before(value.getValueDate())) {
+                                            latestBaseValue = value;
+                                        }
+
+                                    }
+
 				}
 			}
 			
 			if (latestValue !=null){
 				this.actualValue = latestValue.getValue().toString();
 			}
+                        if(latestBaseValue!=null){
+                            this.baseValue=latestBaseValue.getValue().toString();
+                        }
+                         if(latestTarValue!=null){
+                            this.targetValue=latestTarValue.getValue().toString();
+                        }
 		}
 	}
 	

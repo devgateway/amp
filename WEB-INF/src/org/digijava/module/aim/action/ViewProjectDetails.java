a98c4cf864bf53b840a867036fc2852493894153
dmihaila <dmihaila@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-07-14 11:16:38 +0000

    AMP-3704
    git-svn-id: file:///svn/trunk/amp@5981 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/ViewProjectDetails.java
+++ WEB-INF/src/org/digijava/module/aim/action/ViewProjectDetails.java
@@ -9,6 +9,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.digijava.module.aim.form.MainProjectDetailsForm;
+import org.digijava.module.gateperm.core.GatePermConst;
 
 public class ViewProjectDetails extends Action {
     private static Logger logger = Logger.getLogger(ViewProjectDetails.class);
@@ -17,6 +18,7 @@ public class ViewProjectDetails extends Action {
             HttpServletRequest request,HttpServletResponse response) throws Exception {
         
         MainProjectDetailsForm mpForm = (MainProjectDetailsForm) form;
+        request.setAttribute(GatePermConst.ACTION_MODE, GatePermConst.Actions.VIEW);
         logger.debug("Description = " + mpForm.getDescription());
         logger.debug("Objectives = " + mpForm.getObjectives());
         

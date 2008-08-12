e74b8f7af286972b75b3f5e9fd1175fc3d05bacc
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-08-12 13:37:42 +0000

    AMP-3917
    git-svn-id: file:///svn/trunk@6300 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/helper/Constants.java
+++ amp/WEB-INF/src/org/digijava/module/aim/helper/Constants.java
@@ -11,8 +11,8 @@ public class Constants
 	public static class GlobalSettings {
 		public static final String YEAR_RANGE_START			= "Year Range Start";
 		public static final String NUMBER_OF_YEARS_IN_RANGE	= "Number of Years in Range";
-		public static final String START_YEAR_DEFAULT_VALUE	= "Start Year Default Value";
-		public static final String END_YEAR_DEFAULT_VALUE		= "End Year Default Value";
+		public static final String START_YEAR_DEFAULT_VALUE	= "Change Range Default Start Value";
+		public static final String END_YEAR_DEFAULT_VALUE		= "Change Range Default End Value";
 	};
 
 	public static final int COMMITMENT = 0 ;

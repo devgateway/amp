850e4c5e65538ed99b8590a5b82075e0290c7501
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-08-27 08:48:04 +0000

    AMP-4032
    git-svn-id: file:///svn/trunk/amp@6503 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/widget/table/filteredColumn/FilterItemProvider.java
+++ WEB-INF/src/org/digijava/module/widget/table/filteredColumn/FilterItemProvider.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface FilterItemProvider {
 	
 	static final int DONORS_FILTER = 1;
+        static final int ORG_GROUPS  = 2;
 	
 	List<FilterItem> getItems();
 	FilterItem getItem(Long id);

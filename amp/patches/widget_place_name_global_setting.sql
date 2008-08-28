458cd413e17e8968dca437e30b12112a68d57434
kobia <kobia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-08-28 10:05:35 +0000

    AMP-4061
    git-svn-id: file:///svn/trunk@6529 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/widget_place_name_global_setting.sql
+++ amp/patches/widget_place_name_global_setting.sql
@@ -1,4 +1,4 @@
 INSERT INTO amp_global_settings (settingsName,settingsValue,possibleValues,description,section) 
-VALUES  (41,'Show widget place names','false','t_Boolean','Displays widget place info if place is empty','general') 
+VALUES  ('Show widget place names','false','t_Boolean','Displays widget place info if place is empty','general') 
 
 

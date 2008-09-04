251326bb190b80c034cbaac9d8dfad9187fd4a7f
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-09-04 21:43:13 +0000

    AMP-3813
    git-svn-id: file:///svn/trunk/amp@6648 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/amp_columns_project_category.sql
+++ patches/amp_columns_project_category.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW v_project_category AS
-SELECT amp_activity_id, category_value AS name,
+ SELECT amp_activity_id, category_value AS name,
 	amp_categoryvalue_id AS amp_category_id FROM amp_category_value acv, amp_category_class acc, amp_activities_categoryvalues aac
 	WHERE  acv.id=aac.amp_categoryvalue_id AND acc.id=acv.amp_category_class_id AND acc.keyName='project_category';
 

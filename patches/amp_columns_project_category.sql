5846e65d02c7b94b2a5e4dd41c0948e5a9e9e07f
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-09-04 20:44:22 +0000

    AMP-3813
    git-svn-id: file:///svn/trunk/amp@6645 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/amp_columns_project_category.sql
+++ patches/amp_columns_project_category.sql
@@ -1,11 +1,8 @@
-INSERT INTO amp_category_class(category_name,is_multiselect,is_ordered, keyName)
-VALUES('Project Category', b'0', b'0', 'project_category');
+CREATE OR REPLACE VIEW v_project_category AS
+SELECT amp_activity_id, category_value AS name,
+	amp_categoryvalue_id AS amp_category_id FROM amp_category_value acv, amp_category_class acc, amp_activities_categoryvalues aac
+	WHERE  acv.id=aac.amp_categoryvalue_id AND acc.id=acv.amp_category_class_id AND acc.keyName='project_category';
 
-INSERT INTO amp_category_value(category_value, amp_category_class_id, index_column, field_type ) 
-	SELECT 'Government', cl.id, 0, 0 FROM amp_category_class cl WHERE cl.keyName='project_category';
-
-INSERT INTO amp_category_value(category_value, amp_category_class_id, index_column, field_type )
-	SELECT 'Independent Structure Under Government Control/Supervision', cl.id, 1, 0 FROM amp_category_class cl WHERE cl.keyName='project_category';
-
-INSERT INTO amp_category_value(category_value, amp_category_class_id, index_column, field_type )
-	SELECT 'Independent Executing Agency (not related to the Government)', cl.id, 2, 0 FROM amp_category_class cl WHERE cl.keyName='project_category';
+insert into amp_columns
+   	(columnName,cellType,extractorView)
+       values ('Project Category', 'org.dgfoundation.amp.ar.cell.TextCell','v_project_category');

f8e3bdf84b7517ace554f0a16e758ad8b0d3c153
vezugbaia <vezugbaia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-09-09 12:15:16 +0000

    AMP-3760
    git-svn-id: file:///svn/trunk/amp@6722 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/message/jobs/UserVerifiedDateJob.java
+++ WEB-INF/src/org/digijava/module/message/jobs/UserVerifiedDateJob.java
@@ -28,7 +28,7 @@ public class UserVerifiedDateJob implements StatefulJob {
             for (User user : userList) {
                 if (user.getCreationDate()!= null) {
                     String dt = sdf.format(user.getCreationDate());
-                    if (dt.equals(exDt) && !user.isEmailVerified()) {
+                    if (dt.equals(exDt)) {
                         AmpUserUtil.deleteUser(user.getId());
                     }
                 }

0087548d0adbb37cc170e23194a40e9b4e7475f5
msotero <msotero@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-01 13:40:36 +0000

    AMP-4569
    git-svn-id: file:///svn/trunk/amp@7227 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/fm_changes_amp_4569.sql
+++ patches/fm_changes_amp_4569.sql
@@ -1,11 +1,8 @@
-delete from amp_fields_templates where field in (select id from amp_fields_visibility where parent in (select b.id from amp_features_visibility b where b.name = 'Messages'));
-
-delete from amp_fields_visibility where parent in (select b.id from amp_features_visibility b where b.name = 'Messages');
-
-delete from amp_features_templates where feature in (select b.id from amp_features_visibility b where b.name = 'Messages');
-
+delete from amp_fields_templates where field in (select id from amp_fields_visibility where parent in (select id from amp_features_visibility b where name = 'Messages'));
+delete from amp_fields_visibility where parent in (select id from amp_features_visibility where name = 'Messages');
+delete from amp_features_templates where feature in (select id from amp_features_visibility where name = 'Messages');
 delete from amp_features_visibility  where name = 'Messages';
-
+delete from amp_modules_templates where module in (select id from amp_modules_visibility where name = 'My Messages');
+delete from amp_features_visibility where parent in ( select id from amp_modules_visibility where name = 'My Messages');
 delete from amp_modules_visibility where name = 'My Messages';
-
-insert into amp_features_visibility (name, parent, hasLevel) VALUES ('My Messages', (select b.id from amp_tanzania.amp_modules_visibility b where b.`name` = 'Messages'), true);
+insert into amp_features_visibility (name, parent, hasLevel) VALUES ('My Messages', (select id from amp_modules_visibility where name = 'Messages'), true);

ec44b65223ed7b70bcd65f32b4c3d16e9f4788ca
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-08 20:57:14 +0000

    Added named space "ordered" to geo info models
    git-svn-id: file:///svn/trunk@7333 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/models/location/district.rb
+++ odadata/app/models/location/district.rb
@@ -5,6 +5,8 @@ class District < ActiveRecord::Base
   has_many    :geo_relevances, :dependent => :delete_all
   has_many    :projects, :through => :geo_relevances
   
+  named_scope :ordered, :order => "name ASC"
+  
   def <=>(comp)
     self.id <=> comp.id
   end

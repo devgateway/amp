af2a0b368233ff858fd6d7a90d7d926f05e8e064
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-08 20:56:36 +0000

    Removed obselete styles from predifined report tables
    git-svn-id: file:///svn/trunk@7332 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/controllers/reports_controller.rb
+++ odadata/app/controllers/reports_controller.rb
@@ -1,25 +1,25 @@
 class ReportsController < ApplicationController
 
   def donors
-    @donors = Donor.main.ordered.all.select { |d| d.projects.published.size > 0 }
+    @donors = Donor.main.ordered.all.select { |d| d.projects.published.any? }
   end
   
   def sectors
-    @sectors = DacSector.ordered.all.select { |d| d.projects.published.size > 0 }
+    @sectors = DacSector.ordered.all.select { |d| d.projects.published.any? }
   end
   
   def mdgs
-    @mdgs = Mdg.ordered.all.select { |d| d.projects.published.size > 0 }
+    @mdgs = Mdg.ordered.all.select { |d| d.projects.published.any? }
   end
   
   def locations
     if params[:id]
       @province = Province.find(params[:id])
-      @districts = @province.districts.select { |d| d.projects.published.size > 0 }
+      @districts = @province.districts.ordered.select { |d| d.projects.published.any? }
       
       render :action => "location_detail"
     else
-      @provinces = Province.ordered.all.select { |d| d.projects.published.size > 0 }
+      @provinces = Province.ordered.all.select { |d| d.projects.published.any? }
       @national = Project.published.all
     end
   end

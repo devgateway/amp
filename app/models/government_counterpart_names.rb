e691c4a2fcac7558cd1279a9010f296b1281d5d7
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-09 13:36:39 +0000

    Removed obsolete GovernmentCounterpartNames model
    git-svn-id: file:///svn/trunk/odadata@7337 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- app/models/government_counterpart_names.rb
+++
@@ -1,7 +0,0 @@
-class GovernmentCounterpartNames < ActiveRecord::Base
-  def self.government_counterpart_name_list
-    self.find(:all, :select => "id, name", :order => "id ASC")
-  end
-  
-  validates_presence_of :name
-end

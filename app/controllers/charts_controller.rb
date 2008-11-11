9fdaa8bdf3c7430ccb0c91da4ccf3ecdfbe69d97
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-11 09:03:50 +0000

    Removed bluebook conntent from main app as this is in the analytics plugin now
    git-svn-id: file:///svn/trunk/odadata@7360 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- app/controllers/charts_controller.rb
+++
@@ -1,7 +0,0 @@
-class ChartsController < ApplicationController
-  
-  # Renders chart_name.xml.erb template from settings/ directory
-  def settings
-    render :action => File.join("settings", params[:id]), :layout => false
-  end
-end

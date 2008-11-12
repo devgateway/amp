a7b076ff2d32d23a13cf54167e2100d3d62690a4
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-12 20:14:07 +0000

    Added RedCloth gem dependency
    git-svn-id: file:///svn/trunk@7417 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/config/environment.rb
+++ odadata/config/environment.rb
@@ -17,6 +17,7 @@ Rails::Initializer.run do |config|
   # Gem Dependencies
   config.gem "rubyist-aasm", :source => 'http://gems.github.com', :lib => "aasm"
   config.gem "andand"
+  config.gem "RedCloth"
     
   # Add any subdirectories of app/models to the load path, so that we can use
   # sort of pseudo namespacing.

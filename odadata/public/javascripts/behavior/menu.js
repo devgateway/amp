43eec79de8c720cb30d409f1393e1f4613546bda
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-18 16:58:46 +0000

    Changes in menu and layout
    git-svn-id: file:///svn/trunk@7512 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/public/javascripts/behavior/menu.js
+++ odadata/public/javascripts/behavior/menu.js
@@ -1,7 +1,39 @@
-jQuery(function() {
-	jQuery("#nav li").hover(function() {
-		jQuery(this).addClass("over");
-	}, function() {
-		jQuery(this).removeClass("over");
-	});
+var timeout    = 500;
+var closetimer = 0;
+var ddmenuitem = 0;
+
+function mainMenu_open()
+{  
+	mainMenu_canceltimer();
+	mainMenu_close();
+	ddmenuitem = $(this).find('ul').css('visibility', 'visible');
+}
+
+function mainMenu_close()
+{
+	if(ddmenuitem) 
+    	ddmenuitem.css('visibility', 'hidden');
+}
+
+function mainMenu_timer()
+{
+	closetimer = window.setTimeout(mainMenu_close, timeout);
+}
+
+function mainMenu_canceltimer()
+{  
+	if(closetimer)
+	{
+    	window.clearTimeout(closetimer);
+		closetimer = null;
+    }
+}
+
+$(document).ready(
+function()
+{
+	$('#mainMenu > li').bind('mouseover', mainMenu_open)
+	$('#mainMenu > li').bind('mouseout',  mainMenu_timer)
 });
+
+document.onclick = mainMenu_close;

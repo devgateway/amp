df9241b60ffbaa6587cf7aa9bd26cd5b5e8f5518
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-11-23 23:06:49 +0000

    Changes in layout, styles for tables and main menu.
    git-svn-id: file:///svn/trunk@7541 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/public/javascripts/behavior/menu.js
+++ odadata/public/javascripts/behavior/menu.js
@@ -1,39 +1,34 @@
-var timeout    = 500;
-var closetimer = 0;
 var ddmenuitem = 0;
+var ddmenuimage = 0;
 
 function mainMenu_open()
 {  
-	mainMenu_canceltimer();
-	mainMenu_close();
-	ddmenuitem = $(this).find('ul').css('visibility', 'visible');
+	if($(this).get(0) != ddmenuimage)
+	{
+		mainMenu_close();
+		ddmenuitem = $(this).parent().find('ul').css('visibility', 'visible');
+		ddmenuimage = $(this).get(0);
+	}
+	else
+	{
+		mainMenu_close();
+	}
+	return false;
 }
 
 function mainMenu_close()
 {
+	if(ddmenuimage)
+		ddmenuimage = 0;
 	if(ddmenuitem) 
     	ddmenuitem.css('visibility', 'hidden');
-}
-
-function mainMenu_timer()
-{
-	closetimer = window.setTimeout(mainMenu_close, timeout);
-}
-
-function mainMenu_canceltimer()
-{  
-	if(closetimer)
-	{
-    	window.clearTimeout(closetimer);
-		closetimer = null;
-    }
 }
 
 $(document).ready(
 function()
 {
-	$('#mainMenu > li').bind('mouseover', mainMenu_open)
-	$('#mainMenu > li').bind('mouseout',  mainMenu_timer)
+	$('#mainMenu > li img').bind('click', mainMenu_open)
+//	$('#mainMenu > li ul').
 });
 
 document.onclick = mainMenu_close;

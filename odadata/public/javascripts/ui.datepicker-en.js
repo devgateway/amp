3076a15891df7d0c2fdac64812eeb0f02599fcd6
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-12-01 18:05:43 +0000

    ODA-25
    git-svn-id: file:///svn/trunk@7623 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ odadata/public/javascripts/ui.datepicker-en.js
@@ -0,0 +1,5 @@
+/* Inicialización en español para la extensión 'UI date picker' para jQuery. */
+/* Traducido por Vester (xvester@gmail.com). */
+jQuery(function($){
+	$.datepicker.setDefaults($.datepicker.regional['en']);
+});

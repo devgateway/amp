78c816d6ad25e180f23773fa33ef727137cd394b
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-12-03 21:27:17 +0000

    Flags and profile images for donors can now be uploaded by the MFP and are stored using the paperclip plugin.
    Flags are also shown in the header when logged in as focal point.
    
    
    git-svn-id: file:///svn/trunk@7645 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/models/donor.rb
+++ odadata/app/models/donor.rb
@@ -13,12 +13,15 @@ class Donor < ActiveRecord::Base
   has_many :accessible_fundings
   has_many :accessible_forecasts
   
+  has_attached_file :flag,            :styles => { :regular => "80x54", :avatar => "30x30" }
+  has_attached_file :profile_picture, :styles => { :regular => "200x200" }
+  
   ##
   # Validation
   validates_presence_of :name
   
   # Protect attributes that can only be modified by admin
-  attr_protected :name, :code, :currency, :cofunding_only
+  attr_protected :name, :code, :currency, :cofunding_only, :flag, :profile_picture
   
   # List of main donors (not cofunding only!)
   named_scope :main, :conditions => "cofunding_only IS DISTINCT FROM true"
--- odadata/db/migrate/20081202200839_reset_input_states.rb
+++
@@ -1,8 +0,0 @@
-class ResetInputStates < ActiveRecord::Migration
-  def self.up
-    execute "UPDATE projects SET input_state = 'profile_information'"
-  end
-
-  def self.down
-  end
-end

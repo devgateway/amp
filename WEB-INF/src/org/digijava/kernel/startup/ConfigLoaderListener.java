1db858f9c31d9fd91d81a093c4d9be2f91e6ca10
kobia <kobia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-12-16 11:56:28 +0000

    AMP-3477 Translations changes to hash code keys. Corrected version checks.
    git-svn-id: file:///svn/trunk/amp@7785 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/kernel/startup/ConfigLoaderListener.java
+++ WEB-INF/src/org/digijava/kernel/startup/ConfigLoaderListener.java
@@ -169,15 +169,15 @@ public class ConfigLoaderListener
 		int jdbcBugfixingVersion=Integer.parseInt((String)compat.get("jdbc.version.bugfixing"));
 		
 		
-//		if(metaData.getDatabaseMajorVersion()!=dbMajorVersion || 
-//				metaData.getDatabaseMinorVersion()!=dbMinorVersion || 
-//				dbBugfixingVersion>parseBugFixingVersion(metaData.getDatabaseProductVersion(), metaData.getDatabaseMajorVersion()+"."+metaData.getDatabaseMinorVersion())) 
-//			throw new IncompatibleEnvironmentException("Database version is incompatible. Database version needs to be "+dbMajorVersion+"."+dbMinorVersion+" and bugfixing version at least "+dbBugfixingVersion);
-//	
-//		if(metaData.getDriverMajorVersion()!=jdbcMajorVersion || 
-//				metaData.getDriverMinorVersion()!=jdbcMinorVersion || 
-//				jdbcBugfixingVersion>parseBugFixingVersion(metaData.getDriverVersion(), metaData.getDriverMajorVersion()+"."+metaData.getDriverMinorVersion())) 
-//			throw new IncompatibleEnvironmentException("JDBC driver version is incompatible. JDBC version needs to be "+jdbcMajorVersion+"."+jdbcMinorVersion+" and bugfixing version at least "+jdbcBugfixingVersion);
+		if(metaData.getDatabaseMajorVersion()!=dbMajorVersion || 
+				metaData.getDatabaseMinorVersion()!=dbMinorVersion || 
+				dbBugfixingVersion>parseBugFixingVersion(metaData.getDatabaseProductVersion(), metaData.getDatabaseMajorVersion()+"."+metaData.getDatabaseMinorVersion())) 
+			throw new IncompatibleEnvironmentException("Database version is incompatible. Database version needs to be "+dbMajorVersion+"."+dbMinorVersion+" and bugfixing version at least "+dbBugfixingVersion);
+	
+		if(metaData.getDriverMajorVersion()!=jdbcMajorVersion || 
+				metaData.getDriverMinorVersion()!=jdbcMinorVersion || 
+				jdbcBugfixingVersion>parseBugFixingVersion(metaData.getDriverVersion(), metaData.getDriverMajorVersion()+"."+metaData.getDriverMinorVersion())) 
+			throw new IncompatibleEnvironmentException("JDBC driver version is incompatible. JDBC version needs to be "+jdbcMajorVersion+"."+jdbcMinorVersion+" and bugfixing version at least "+jdbcBugfixingVersion);
 	
 		
 		logger.info("Database compatibility OK.");

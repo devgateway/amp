69d67b8a059300b8facb641f85091c61c1249945
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2008-12-17 13:57:07 +0000

    AMP-1681: documenting
    git-svn-id: file:///svn/trunk@7799 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/orgProfile/helper/Project.java
+++ amp/WEB-INF/src/org/digijava/module/orgProfile/helper/Project.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.digijava.module.orgProfile.helper;
 
@@ -9,7 +5,7 @@ import java.util.Set;
 import org.digijava.module.aim.dbentity.AmpActivitySector;
 
 /**
- *
+ * Project helper bean.
  * @author medea
  */
 public class Project {

fe283183acd3151b2a6d611f8c2cba0fcda6c37f
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-10 15:42:36 +0000

    Added admin module to change scrollbars
    git-svn-id: file:///svn/trunk/odadata@7981 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,6 @@
 class SettingsController < ApplicationController
+  access_rule 'admin'
+  
   def data_input_status
     @status = Prefs.data_input_open
   end
@@ -6,5 +8,19 @@ class SettingsController < ApplicationController
   def toggle_data_input
     Prefs.data_input_open = (params[:new_status] == "1") ? true : false
     redirect_to data_input_status_settings_path
+  end
+  
+  def scrollbars
+    if request.post?
+      Prefs.scroll_bar_public = params[:prefs][:scroll_bar_public]
+      Prefs.scroll_bar_admin  = params[:prefs][:scroll_bar_admin]
+      Prefs.scroll_bar_mfp    = params[:prefs][:scroll_bar_mfp]
+      
+      flash[:notice] = lc(:successful)
+    end
+  end
+  
+  def update_scrollbars
+    #
   end
 end

d5bba0d9f0f5fb41a8c9c96eb728998345b5ed44
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-11 14:39:15 +0000

    ODA-52
    git-svn-id: file:///svn/trunk@7991 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/models/mdgs/mdg.rb
+++ odadata/app/models/mdgs/mdg.rb
@@ -4,8 +4,9 @@ class Mdg < ActiveRecord::Base
   
   has_many  :targets
   has_many  :mdg_relevances
-  has_many  :projects, :through => :mdg_relevances, :uniq => true
-  
+  has_many  :projects, :through => :mdg_relevances, :uniq => true,
+    :select => "projects.*"
+
   named_scope :ordered, :order => "id ASC"
   
   def description_tooltip

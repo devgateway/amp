365b1b735084b830145382816b9613163c7aca09
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-13 13:51:22 +0000

    Removed unneeded plugins
    git-svn-id: file:///svn/trunk/odadata@8033 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- vendor/plugins/partial_validation/init.rb
+++
@@ -1,9 +0,0 @@
-require 'partial_validation'
-
-ActiveRecord::Base.class_eval do
-  include ActiveRecord::Validations::Partial
-end
-
-ActionView::Base.class_eval do
-  include PartialValidationHelper
-end

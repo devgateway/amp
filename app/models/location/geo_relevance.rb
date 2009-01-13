bd48454ca1b981c7c25d84f93ce3b3a3aa5bd3b3
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-13 15:21:51 +0000

    Updated fixtures and fixed location saving problem! Please do a "SOURCE="odanic_01052009" rake db:seed" before you continue.
    git-svn-id: file:///svn/trunk/odadata@8036 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- app/models/location/geo_relevance.rb
+++ app/models/location/geo_relevance.rb
@@ -8,7 +8,7 @@ class GeoRelevance < ActiveRecord::Base
   before_validation :update_province_id_for_district
   
 private
-  # If the inserted record is a target, also insert the id of the corresponding mdg
+  # If the inserted record is a district, also insert the id of the corresponding province
   def update_province_id_for_district
     if self.district
       self.province_id = self.district.province_id

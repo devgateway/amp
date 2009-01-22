f8b9c383d9ac02bbfab11bef719b2efd9d67ffd6
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-22 11:33:13 +0000

    AMP-3693
    git-svn-id: file:///svn/trunk@8152 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/WEB-INF/src/org/digijava/module/aim/form/DynLocationManagerForm.java
@@ -0,0 +1,86 @@
+package org.digijava.module.aim.form;
+
+import java.util.Collection;
+
+import org.apache.struts.action.ActionForm;
+import org.digijava.module.aim.dbentity.AmpCategoryValueLocations;
+import org.digijava.module.categorymanager.dbentity.AmpCategoryClass;
+
+public class DynLocationManagerForm extends ActionForm {
+	private Collection<AmpCategoryValueLocations> firstLevelLocations;
+	private Collection<AmpCategoryValueLocations> unorganizedLocations;
+	private AmpCategoryClass implementationLocation;
+	private int numOfLayers;
+	private Long firstLayerId;
+	private String treeStructure;
+	private String unorgLocations;
+	private Long deleteLocationId;
+
+	public Collection<AmpCategoryValueLocations> getFirstLevelLocations() {
+		return firstLevelLocations;
+	}
+
+	public void setFirstLevelLocations(
+			Collection<AmpCategoryValueLocations> firstLevelLocations) {
+		this.firstLevelLocations = firstLevelLocations;
+	}
+	
+	public Collection<AmpCategoryValueLocations> getUnorganizedLocations() {
+		return unorganizedLocations;
+	}
+
+	public void setUnorganizedLocations(
+			Collection<AmpCategoryValueLocations> unorganizedLocations) {
+		this.unorganizedLocations = unorganizedLocations;
+	}
+
+	public AmpCategoryClass getImplementationLocation() {
+		return implementationLocation;
+	}
+
+	public void setImplementationLocation(AmpCategoryClass implementationLocation) {
+		this.implementationLocation = implementationLocation;
+	}
+
+	public int getNumOfLayers() {
+		return numOfLayers;
+	}
+
+	public void setNumOfLayers(int numOfLayers) {
+		this.numOfLayers = numOfLayers;
+	}
+
+	public Long getFirstLayerId() {
+		return firstLayerId;
+	}
+
+	public void setFirstLayerId(Long firstLayerId) {
+		this.firstLayerId = firstLayerId;
+	}
+
+	public String getTreeStructure() {
+		return treeStructure;
+	}
+
+	public void setTreeStructure(String treeStructure) {
+		this.treeStructure = treeStructure;
+	}
+
+	public String getUnorgLocations() {
+		return unorgLocations;
+	}
+
+	public void setUnorgLocations(String unorgLocations) {
+		this.unorgLocations = unorgLocations;
+	}
+
+	public Long getDeleteLocationId() {
+		return deleteLocationId;
+	}
+
+	public void setDeleteLocationId(Long deleteLocationId) {
+		this.deleteLocationId = deleteLocationId;
+	}
+	
+	
+}

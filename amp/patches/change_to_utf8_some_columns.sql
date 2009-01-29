9cb0074ccebba3f412fa498d4f46a8b46e7b0a0a
kobia <kobia@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-29 16:14:32 +0000

    AMP-4715 some utf8 columns are latin!
    git-svn-id: file:///svn/trunk@8250 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/patches/change_to_utf8_some_columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE amp_message MODIFY COLUMN `name` text CHARACTER SET utf8;  
+
+ALTER TABLE amp_message MODIFY COLUMN `description` text CHARACTER SET utf8;  
+
+ALTER TABLE dg_message MODIFY COLUMN `message_utf8` text CHARACTER SET utf8;  

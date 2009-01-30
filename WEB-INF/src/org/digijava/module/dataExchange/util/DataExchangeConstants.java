e0314e8daba3e7ac8086ce0c9a3f15f0aad55fdc
mkalandadze <mkalandadze@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-01-30 15:40:50 +0000

    AMP-4664
    refactorig XSD
    
    
    git-svn-id: file:///svn/trunk/amp@8264 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/dataExchange/util/DataExchangeConstants.java
+++ WEB-INF/src/org/digijava/module/dataExchange/util/DataExchangeConstants.java
@@ -5,11 +5,18 @@ public class DataExchangeConstants {
 	public static String ADJUSTMENT_TYPE_ACTUAL = "Actual";
 	public static String ADJUSTMENT_TYPE_PLANNED = "Planned";
 	
-	public static String ORG_ROLE_RESPONSIBLE = "Responsible";
-	public static String ORG_ROLE_EXECUTING = "Executing";
-	public static String ORG_ROLE_IMPLEMENTING = "Implementing";
-	public static String ORG_ROLE_BENEFICIARY = "Beneficiary";
-	public static String ORG_ROLE_CONTRACTING = "Contracting";
+	public static String ORG_ROLE_REPORTING_AGENCY = "Reporting Agency";
+	public static String ORG_ROLE_FUNDING_AGENCY = "Funding Agency";
+	public static String ORG_ROLE_IMPLEMENTING_AGENCY = "Implementing Agency";
+	public static String ORG_ROLE_BENEFICIARY_AGENCY = "Beneficiary Agency";
+	public static String ORG_ROLE_CONTRACTING_AGENCY = "Contracting Agency";
+	public static String ORG_ROLE_REGIONAL_GROUP = "Regional Group";
+	public static String ORG_ROLE_SECTOR_GROUP = "Sector Group";
+	public static String ORG_ROLE_EXECUTING_AGENCY= "Executing Agency";
+	public static String ORG_ROLE_RESPONSIBLE_ORGANIZATION = "Responsible Organizaton";
+	public static String ORG_ROLE_CONTRACTOR = "Contractor";
+	public static String ORG_ROLE_RELEATED_INSTITUTIONS = "Releated Institutions";
+	
 	
 	public static String LOCATION_TYPE_COUNTRY = "Country";
 	public static String LOCATION_TYPE_REGION = "Region";

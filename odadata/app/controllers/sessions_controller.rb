eff1fe518cfc3ea3c1f20bf6dd1eed336c92c610
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-02-05 14:59:39 +0000

    Some more authentication testing
    git-svn-id: file:///svn/trunk@8334 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/controllers/sessions_controller.rb
+++ odadata/app/controllers/sessions_controller.rb
@@ -6,30 +6,28 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # TESTING ONLY!
-    session[:user_id] = 2
-    #logout_keeping_session!
+    logout_keeping_session!
     data = params[:session]
      
-    #if user = User.authenticate(data[:email], data[:password])
-    #  # Protects against session fixation attacks, causes request forgery
-    #  # protection if user resubmits an earlier form using back
-    #  # button. Uncomment if you understand the tradeoffs.
-    #  # TODO: It would certainly be good to use this but it would also reset the 
-    #  # output locale. It might make sense to store the locale in a separate cookie
-    #  # for that purpose. 
-    #  # reset_session
-    #  self.current_user = user
-    #  new_cookie_flag = (data[:remember_me] == "1")
-    #  handle_remember_cookie! new_cookie_flag
-    #  redirect_back_or_default('/')
-    #  flash[:notice] = "Logged in successfully"
-    #else
-    #  note_failed_signin
-    #  @email       = data[:email]
-    #  @remember_me = data[:remember_me]
-    #  render :action => 'new'
-    #end
+    if user = User.authenticate(data[:email], data[:password])
+      # Protects against session fixation attacks, causes request forgery
+      # protection if user resubmits an earlier form using back
+      # button. Uncomment if you understand the tradeoffs.
+      # TODO: It would certainly be good to use this but it would also reset the 
+      # output locale. It might make sense to store the locale in a separate cookie
+      # for that purpose. 
+      # reset_session
+      self.current_user = user
+      #new_cookie_flag = (data[:remember_me] == "1")
+      #handle_remember_cookie! new_cookie_flag
+      redirect_back_or_default('/')
+      flash[:notice] = "Logged in successfully"
+    else
+      note_failed_signin
+      @email       = data[:email]
+      @remember_me = data[:remember_me]
+      render :action => 'new'
+    end
   end
 
   def destroy

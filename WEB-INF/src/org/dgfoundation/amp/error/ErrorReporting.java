fe18c0ac82b092f7bb96e91f1a205be7e1eadcb5
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-02-10 19:27:51 +0000

    AMP-2401
    git-svn-id: file:///svn/trunk/amp@8385 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/dgfoundation/amp/error/ErrorReporting.java
+++ WEB-INF/src/org/dgfoundation/amp/error/ErrorReporting.java
@@ -1,6 +1,10 @@
 package org.dgfoundation.amp.error;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
+import org.digijava.kernel.user.User;
+import org.digijava.kernel.util.RequestUtils;
 
 public class ErrorReporting {
 	
@@ -14,9 +18,30 @@ public class ErrorReporting {
 	 */
 	private static void handle(Exception e){
 	}
-	
+
 	public static void handle(Exception e, Logger logger){
+		handle(e, logger, null);
+	}
+
+	public static void handle(Exception e, Logger logger, HttpServletRequest request){
 		logger.error(e.getMessage(), e);
+		User user = new User("unknown@amp.org", "Unknown", "Unknown");
+		if (request != null){
+			try{
+				User us = RequestUtils.getUser(request);
+				if (us != null)
+					user = us;
+			} catch (Exception shouldBeIgnored) {
+				logger.error("Can't get user", shouldBeIgnored);
+			}
+		}
+		
+		sendToKeeper(e, user);
+	}
+
+	private static void sendToKeeper(Exception e, User user){
+		ErrorKeeper ek = new ErrorKeeperRAM();
+		ek.store(e, user);
 	}
 	
 }

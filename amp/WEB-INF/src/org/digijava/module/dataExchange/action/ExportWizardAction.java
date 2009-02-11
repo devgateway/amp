01e41102c3b5cac018aed13ce792178642285b61
mkalandadze <mkalandadze@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-02-11 12:19:04 +0000

    AMP-4981
    changing GUI by Bindu requested
    
    
    git-svn-id: file:///svn/trunk@8394 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/dataExchange/action/ExportWizardAction.java
+++ amp/WEB-INF/src/org/digijava/module/dataExchange/action/ExportWizardAction.java
@@ -9,6 +9,12 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
+import org.dgfoundation.amp.ar.ARUtil;
+import org.digijava.module.aim.ar.util.ReportsUtil;
+import org.digijava.module.aim.dbentity.AmpClassificationConfiguration;
+import org.digijava.module.aim.helper.Constants;
+import org.digijava.module.aim.util.DbUtil;
+import org.digijava.module.aim.util.SectorUtil;
 import org.digijava.module.aim.util.TeamUtil;
 import org.digijava.module.dataExchange.form.ExportForm;
 import org.digijava.module.dataExchange.jaxb.ActivityType;
@@ -27,7 +33,15 @@ public class ExportWizardAction extends DispatchAction {
 		ExportForm eForm = (ExportForm)form;
 		
 		eForm.setActivityTree(ExportHelper.getActivityStruct("activity","activityTree","activity",ActivityType.class,true));
+
+		eForm.setDonorTypeList(DbUtil.getAllOrgTypesOfPortfolio());
+		eForm.setDonorGroupList(ARUtil.filterDonorGroups(DbUtil.getAllOrgGroupsOfPortfolio()));
+		eForm.setDonorAgencyList(ReportsUtil.getAllOrgByRoleOfPortfolio(Constants.ROLE_CODE_DONOR));
+		eForm.setPrimarySectorsList(SectorUtil.getAmpSectorsAndSubSectors(AmpClassificationConfiguration.PRIMARY_CLASSIFICATION_CONFIGURATION_NAME));
+		eForm.setSecondarySectorsList(SectorUtil.getAmpSectorsAndSubSectors(AmpClassificationConfiguration.SECONDARY_CLASSIFICATION_CONFIGURATION_NAME));
+		
 		eForm.setTeamList(TeamUtil.getAllTeams());
+		
 		
 		String forward = "default";
 		return mapping.findForward(forward);

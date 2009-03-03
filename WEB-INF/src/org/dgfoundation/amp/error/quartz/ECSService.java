2e7de6484c91c865c1f86038fdfabe0119ee0d2e
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-03-03 16:38:31 +0000

    AMP-5413
    git-svn-id: file:///svn/trunk/amp@8740 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/dgfoundation/amp/error/quartz/ECSService.java
+++ WEB-INF/src/org/dgfoundation/amp/error/quartz/ECSService.java
@@ -1,8 +1,11 @@
 package org.dgfoundation.amp.error.quartz;
 
+import java.util.Iterator;
+
 import org.apache.log4j.Logger;
 import org.digijava.kernel.service.AbstractServiceImpl;
 import org.digijava.kernel.service.ServiceContext;
+import org.digijava.module.aim.dbentity.AmpGlobalSettings;
 import org.digijava.module.aim.helper.GlobalSettingsConstants;
 import org.digijava.module.aim.util.FeaturesUtil;
 
@@ -13,6 +16,7 @@ import org.digijava.module.aim.util.FeaturesUtil;
  */
 
 public class ECSService extends AbstractServiceImpl {
+	private static Logger logger = Logger.getLogger(ECSService.class);
 	private static ECSRunner crummer = new ECSRunner();
 
 	protected void processInitEvent(ServiceContext serviceContext) {
@@ -22,6 +26,41 @@ public class ECSService extends AbstractServiceImpl {
 			if (!("-1").equalsIgnoreCase(cleanerEnabled)) {*/
 		/*	}
 		}*/
+
+		//ECS Checks
+		logger.info("Checking ECS Setup ...");
+		if (FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.ECS_ENABLED).equals("true")){
+			logger.info("   - ECS is enabled");
+			String policy = System.getProperties().getProperty("java.security.policy");
+			if (policy.indexOf("ecsClient.policy") < 0){
+				logger.info("   - policy NOT set!");
+				try{
+					Iterator iterator = FeaturesUtil.getGlobalSettingsCache().iterator();
+					while (iterator.hasNext()) {
+						AmpGlobalSettings ags = (AmpGlobalSettings) iterator.next();
+						if (ags.getGlobalSettingsName().equals(GlobalSettingsConstants.ECS_ENABLED)){
+							ags.setGlobalSettingsValue("false");
+							logger.info("ECS was Turned off!");
+							break;
+						}
+					}
+				} catch (Exception ignored) {logger.error(ignored);}
+				logger.error("#####################################################################");
+				logger.error("#                   Warning ECS policy not set!                     #");
+				logger.error("#                      ECS System turned OFF                        #");
+				logger.error("#                                                                   #");
+				logger.error("# To enable the system set the java security policy as instructed!  #");
+				logger.error("#                                                                   #");
+				logger.error("# Add this parameter to the jboss startup parameters:               #");
+				logger.error("#    -Djava.security.policy=\"<<Path to>>/ecsClient.policy\"          #");
+				logger.error("# <<Path to>> must be the absoulte path to ecsClient.policy file.   #");
+				logger.error("#                                                                   #");
+				logger.error("#####################################################################");
+			}
+		}
+		//
+
+		
 		String ecsEnabled = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.ECS_ENABLED);
 		if ("true".equalsIgnoreCase(ecsEnabled))
 			startService();

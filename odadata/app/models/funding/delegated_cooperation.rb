a627b6532c0dd724f76fc4b07f327f0bb4203aa3
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-03-08 19:49:07 +0000

    Fixed remaining issues of "Delegated Cooperation" and added translations
    git-svn-id: file:///svn/trunk@8817 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/models/funding/delegated_cooperation.rb
+++ odadata/app/models/funding/delegated_cooperation.rb
@@ -3,13 +3,23 @@ class DelegatedCooperation < ActiveRecord::Base
   belongs_to :delegating_donor, :class_name => "Donor"
   belongs_to :delegating_agency, :class_name => "DonorAgency"
   
+  validates_presence_of :delegating_donor_id
   validate :agency_belongs_to_donor
   
+  # Pretty output for fact sheets: "Donor[ - Agency]"
+  def to_s
+    out = delegating_donor.name
+    out += " - #{delegating_agency.name}" if delegating_agency
+    
+    out
+  end
   
 protected
   def agency_belongs_to_donor
+    return unless delegating_agency
+    
     unless delegating_agency.donor_id == delegating_donor.id
-      # TODO: Translation
+      # TODO: Translation (¡La agencia mandante seleccionada no pertenece al donante seleccionado!)
       errors.add_to_base "Selected delegating agency does not belong to selected donor!"
     end
   end

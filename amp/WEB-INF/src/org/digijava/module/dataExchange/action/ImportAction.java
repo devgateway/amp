df8d15f335649d5e2f21fcddce5c059dac9e70cd
dmihaila <dmihaila@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-03-27 11:44:03 +0000

    AMP-5649
    git-svn-id: file:///svn/trunk@8988 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/dataExchange/action/ImportAction.java
+++ amp/WEB-INF/src/org/digijava/module/dataExchange/action/ImportAction.java
@@ -15,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;

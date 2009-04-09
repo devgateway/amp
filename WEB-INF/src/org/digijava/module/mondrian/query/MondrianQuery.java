4f485649f9993f0823af4fb6f88acdcc22c537d4
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-04-09 18:39:45 +0000

    AMP-5509
    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@9081 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/mondrian/query/MondrianQuery.java
+++ WEB-INF/src/org/digijava/module/mondrian/query/MondrianQuery.java
@@ -1,7 +1,9 @@
 package org.digijava.module.mondrian.query;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.dgfoundation.amp.ar.AmpARFilter;
+import org.dgfoundation.amp.ar.FilterParam;
 import org.digijava.kernel.entity.Locale;
 import org.digijava.kernel.request.Site;
 import org.digijava.kernel.util.RequestUtils;
@@ -29,142 +32,26 @@ import org.digijava.module.aim.util.TeamUtil;
  */
 public class MondrianQuery {
 	protected static Logger logger = Logger.getLogger(MondrianQuery.class);
-
+	
+	private static String getformatDate(String dates) {
+		java.sql.Date date =  new java.sql.Date(FormatHelper.parseDate2(dates).getTime());
+		SimpleDateFormat fd = new SimpleDateFormat("yyyy-MM-dd");
+        return "'"+fd.format(date)+"'";
+    }
+	
+	private String generatedFilterQuery;
+	private void queryAppend(String filter) {
+		generatedFilterQuery += " AND amp_activity_id IN (" + filter + ")";
+	}
 	public void createQuery(HttpServletRequest request){
 		AmpARFilter filter = new AmpARFilter();
-		TeamMember tm = (TeamMember) request.getSession().getAttribute(
-				Constants.CURRENT_MEMBER);
-		filter.setAmpTeams(new TreeSet());
-		AmpApplicationSettings tempSettings = null;
-
-		if (tm != null) {
-			filter.setAccessType(tm.getTeamAccessType());
-			filter.setAmpTeams(TeamUtil.getRelatedTeamsForMember(tm));
-			Set teamAO = TeamUtil.getComputedOrgs(filter.getAmpTeams());
-
-			if (teamAO != null && teamAO.size() > 0)
-				filter.setTeamAssignedOrgs(teamAO);
-
-			tempSettings = DbUtil.getMemberAppSettings(tm.getMemberId());
-
-			if (tempSettings == null)
-				if (tm != null)
-					tempSettings = DbUtil.getTeamAppSettings(tm.getTeamId());
-
-			if (filter.getCurrency() == null)
-				filter.setCurrency(tempSettings.getCurrency());
-
-		}
-
-		if (filter.getCalendarType() == null) {
-			if (tempSettings != null) {
-				filter.setCalendarType(tempSettings.getFiscalCalendar());
-			}
-		}
-		String gvalue = FeaturesUtil
-				.getGlobalSettingValue(GlobalSettingsConstants.DEFAULT_CALENDAR);
-		if (filter.getCalendarType() == null) {
-			if (gvalue != null) {
-				Long fiscalCalId = Long.parseLong(gvalue);
-				filter.setCalendarType(DbUtil.getFiscalCalendar(fiscalCalId));
-			}
-		}
-
-		Long defaulCalenadarId = null;
-
-		if (tempSettings != null) {
-			if (tempSettings.getFiscalCalendar() != null) {
-				defaulCalenadarId = tempSettings.getFiscalCalendar()
-						.getAmpFiscalCalId();
-			} else {
-				defaulCalenadarId = Long
-						.parseLong(FeaturesUtil
-								.getGlobalSettingValue(GlobalSettingsConstants.DEFAULT_CALENDAR));
-			}
-		}
-
-		ICalendarWorker worker = null;
-		Date checkDate = null;
-		if (filter.getRenderStartYear() == null) {
-			if (tempSettings != null
-					&& tempSettings.getReportStartYear() != null
-					&& tempSettings.getReportStartYear().intValue() != 0) {
-				filter.setRenderStartYear(tempSettings.getReportStartYear());
-			} else {
-				gvalue = FeaturesUtil
-						.getGlobalSettingValue(org.digijava.module.aim.helper.Constants.GlobalSettings.START_YEAR_DEFAULT_VALUE);
-				if (gvalue != null && !"".equalsIgnoreCase(gvalue)
-						&& Integer.parseInt(gvalue) > 0) {
-					filter.setRenderStartYear(Integer.parseInt(gvalue));
-				}
-			}
-
-			if (filter.getRenderStartYear() != null
-					&& filter.getCalendarType() != null
-					&& defaulCalenadarId != filter.getCalendarType()
-							.getAmpFiscalCalId()) {
-				worker = filter.getCalendarType().getworker();
-				try {
-					checkDate = new SimpleDateFormat("dd/MM/yyyy")
-							.parse("01/01/" + filter.getRenderStartYear());
-					worker.setTime(checkDate);
-					filter.setRenderStartYear(worker.getYear());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		filter.setRenderStartYear((filter.getRenderStartYear() == null) ? -1
-				: filter.getRenderStartYear());
-
-		if (filter.getRenderEndYear() == null) {
-			if (tempSettings != null && tempSettings.getReportEndYear() != null
-					&& tempSettings.getReportEndYear().intValue() != 0) {
-				filter.setRenderEndYear(tempSettings.getReportEndYear());
-			} else {
-				gvalue = null;
-				gvalue = FeaturesUtil
-						.getGlobalSettingValue(org.digijava.module.aim.helper.Constants.GlobalSettings.END_YEAR_DEFAULT_VALUE);
-				if (gvalue != null && !"".equalsIgnoreCase(gvalue)
-						&& Integer.parseInt(gvalue) > 0) {
-					filter.setRenderEndYear(Integer.parseInt(gvalue));
-				}
-			}
-
-			if (filter.getRenderEndYear() != null
-					&& filter.getCalendarType() != null
-					&& defaulCalenadarId != filter.getCalendarType()
-							.getAmpFiscalCalId()) {
-				worker = filter.getCalendarType().getworker();
-				try {
-					checkDate = new SimpleDateFormat("dd/MM/yyyy")
-							.parse("01/01/" + filter.getRenderEndYear());
-					worker.setTime(checkDate);
-					filter.setRenderEndYear(worker.getYear());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		filter.setRenderEndYear((filter.getRenderEndYear() == null) ? -1
-				: filter.getRenderEndYear());
-
-		if (filter.getCurrentFormat() == null) {
-			filter.setCurrentFormat(FormatHelper.getDefaultFormat());
-			FormatHelper.tlocal.set(null);
-		} else {
-			FormatHelper.tlocal.set(filter.getCurrentFormat());
-		}
-
-		String widget = (String) request.getAttribute("widget");
-		if (widget != null)
-			filter.setWidget(new Boolean(widget).booleanValue());
-		
+		filter.readRequestData(request);
+	
 		filter.generateFilterQuery(request);
-		/*Prepare thread local to process the schema*/
+		generatedFilterQuery = filter.getGeneratedFilterQuery();
 		
-		QueryThread.setQuery(filter.getGeneratedFilterQuery());
+		/*Prepare thread local to process the schema*/
+		QueryThread.setQuery(generatedFilterQuery);
 		Site site = RequestUtils.getSite(request);
 		QueryThread.setSite(site);
 		Locale navigationLanguage = RequestUtils.getNavigationLanguage(request);

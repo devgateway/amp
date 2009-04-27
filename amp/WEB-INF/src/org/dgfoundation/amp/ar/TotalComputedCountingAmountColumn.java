7786b563c92961cd31856060388be8bfd090823a
sdimunzio <sdimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-04-27 19:21:28 +0000

    AMP-5585
    git-svn-id: file:///svn/trunk@9182 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/ar/TotalComputedCountingAmountColumn.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/ar/TotalComputedCountingAmountColumn.java
@@ -121,46 +121,43 @@ public class TotalComputedCountingAmountColumn extends TotalAmountColumn {
 			element.getAmount();
 
 			BigDecimal tmpValue = element.getValues().get(ArConstants.TOTAL_COMMITMENTS);
+			BigDecimal tmpMinValue;
 			trail_total_commitments = trail_total_commitments.add(tmpValue);
-			tmpValue=null;
-			
-			//ACTUAL_COMMITMENT
+			tmpValue = null;
+
+			// ACTUAL_COMMITMENT
 			tmpValue = element.getValues().get(ArConstants.ACTUAL_COMMITMENT);
-			// acutal_commitments
 			trail_acutal_commitments = trail_acutal_commitments.add(tmpValue);
-			// max_acutal_commitments compare to value if -1 max is less if 1
-			// max is greater
 			max_acutal_commitments = (max_acutal_commitments.compareTo(tmpValue) < 0) ? tmpValue : max_acutal_commitments;
-			// min_acutal_commitments compare to value if -1 min is less, if 1
-			// min is greater
+			if (min_acutal_commitments.intValue() == 0) {
+				min_acutal_commitments = max_acutal_commitments;
+			}
 			min_acutal_commitments = (min_acutal_commitments.compareTo(tmpValue) > 0) ? tmpValue : min_acutal_commitments;
-			tmpValue=null;
-			
-			//ACTUAL_DISBURSEMENT
+			tmpValue = null;
+
+			// ACTUAL_DISBURSEMENT
 			tmpValue = element.getValues().get(ArConstants.ACTUAL_DISBURSEMENT);
-			// acutal_disburments
 			trail_acutal_disburments = trail_acutal_disburments.add(tmpValue);
-			// max_acutal_commitments compare to value if -1 max is less if 1
-			// max is greater
 			max_acutal_disburments = (max_acutal_disburments.compareTo(tmpValue) < 0) ? tmpValue : max_acutal_disburments;
-			// min_acutal_commitments compare to value if -1 min is less, if 1
-			// min is greater
+			if (min_acutal_disburments.intValue() == 0) {
+				min_acutal_disburments = max_acutal_disburments;
+			}
 			min_acutal_disburments = (min_acutal_disburments.compareTo(tmpValue) > 0) ? tmpValue : min_acutal_disburments;
-			tmpValue=null;
-			
-			//PLANNED_COMMITMENT
+			tmpValue = null;
+
+			// PLANNED_COMMITMENT
 			tmpValue = element.getValues().get(ArConstants.PLANNED_COMMITMENT);
 			// planned_commitments
 			trail_planned_commitments = trail_planned_commitments.add(tmpValue);
-			// max_acutal_commitments compare to value if -1 max is less if 1
-			// max is greater
-			max_planned_disburments = (max_planned_disburments.compareTo(tmpValue) < 0) ? tmpValue : max_planned_disburments;
-			// min_acutal_commitments compare to value if -1 min is less, if 1
-			// min is greater
-			min_planned_disburments = (min_planned_disburments.compareTo(tmpValue) > 0) ? tmpValue : min_planned_disburments;
-			tmpValue=null;
-			
-			//PLANNED_DISBURSEMENT
+			max_planned_commitments = (max_planned_commitments.compareTo(tmpValue) < 0) ? tmpValue : max_planned_commitments;
+
+			if (min_planned_commitments.intValue() == 0) {
+				min_planned_commitments = max_planned_commitments;
+			}
+			min_planned_commitments = (min_planned_commitments.compareTo(tmpValue) > 0) ? tmpValue : min_planned_commitments;
+			tmpValue = null;
+
+			// PLANNED_DISBURSEMENT
 			tmpValue = element.getValues().get(ArConstants.PLANNED_DISBURSEMENT);
 			trail_planned_disburments = trail_planned_disburments.add(tmpValue);
 			// max_acutal_commitments compare to value if -1 max is less if 1
@@ -168,6 +165,9 @@ public class TotalComputedCountingAmountColumn extends TotalAmountColumn {
 			max_planned_disburments = (max_planned_disburments.compareTo(tmpValue) < 0) ? tmpValue : max_planned_disburments;
 			// min_acutal_commitments compare to value if -1 min is less, if 1
 			// min is greater
+			if (min_planned_disburments.intValue() == 0) {
+				min_planned_disburments = max_planned_disburments;
+			}
 			min_planned_disburments = (min_planned_disburments.compareTo(tmpValue) > 0) ? tmpValue : min_planned_disburments;
 
 		}
@@ -187,13 +187,13 @@ public class TotalComputedCountingAmountColumn extends TotalAmountColumn {
 		// MAX AND MIN VALUES
 		variables.put(ArConstants.MAX_ACTUAL_COMMITMENT, max_acutal_commitments);
 		variables.put(ArConstants.MIN_ACTUAL_COMMITMENT, min_acutal_commitments);
-		
+
 		variables.put(ArConstants.MAX_ACTUAL_DISBURSEMENT, max_acutal_disburments);
 		variables.put(ArConstants.MIN_ACTUAL_DISBURSEMENT, min_acutal_disburments);
 
 		variables.put(ArConstants.MAX_PLANNED_COMMITMENT, max_planned_commitments);
 		variables.put(ArConstants.MIN_PLANNED_COMMITMENT, min_planned_commitments);
-		
+
 		variables.put(ArConstants.MAX_PLANNED_DISBURSEMENT, max_planned_disburments);
 		variables.put(ArConstants.MIN_PLANNED_DISBURSEMENT, min_planned_disburments);
 

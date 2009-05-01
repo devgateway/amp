5e4fcf732e0454c9eec30c886fd572262414d48c
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-05-01 10:45:31 +0000

    AMP-3410
    git-svn-id: file:///svn/trunk@9233 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/widget/dbentity/AmpSectorTableWidget.java
+++ amp/WEB-INF/src/org/digijava/module/widget/dbentity/AmpSectorTableWidget.java
@@ -7,8 +7,6 @@ import org.digijava.module.widget.helper.WidgetVisitor;
 public class AmpSectorTableWidget extends AmpWidget {
 
     private Set<AmpSectorOrder> sectorsColumns;
-    private Set<AmpSectorTableYear> totalYears;
-    private Set<AmpSectorTableYear> percentYears;
     private Set<AmpSectorTableYear> years;
     private Long donorYear;
 
@@ -28,21 +26,6 @@ public class AmpSectorTableWidget extends AmpWidget {
         this.years = years;
     }
 
-    public Set<AmpSectorTableYear> getPercentYears() {
-        return percentYears;
-    }
-
-    public void setPercentYears(Set<AmpSectorTableYear> percentYears) {
-        this.percentYears = percentYears;
-    }
-
-    public Set<AmpSectorTableYear> getTotalYears() {
-        return totalYears;
-    }
-
-    public void setTotalYears(Set<AmpSectorTableYear> totalYears) {
-        this.totalYears = totalYears;
-    }
 
     public Set<AmpSectorOrder> getSectorsColumns() {
         return sectorsColumns;
@@ -54,17 +37,10 @@ public class AmpSectorTableWidget extends AmpWidget {
 
     public void addYear(AmpSectorTableYear year) {
         year.setWidget(this);
-        if (year.getType().equals(AmpSectorTableYear.PERCENT_TYPE_YEAR)) {
-            if (this.percentYears == null) {
-                this.percentYears = new HashSet<AmpSectorTableYear>();
-            }
-            this.percentYears.add(year);
-        } else {
-            if (this.totalYears == null) {
-                this.totalYears = new HashSet<AmpSectorTableYear>();
-            }
-            this.totalYears.add(year);
+        if(this.years==null){
+            this.years=new HashSet<AmpSectorTableYear>();
         }
+        this.years.add(year);
     }
 
     @Override

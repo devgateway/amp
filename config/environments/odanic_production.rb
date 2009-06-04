6cf6edd67f60e89878474e1f390029781f1ca2ea
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-06-04 18:45:08 +0000

    ODA-174
    git-svn-id: file:///svn/trunk/odadata@9526 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- config/environments/odanic_production.rb
+++ config/environments/odanic_production.rb
@@ -1,8 +1,25 @@
-config.cache_classes = false
+# Settings specified here will take precedence over those in config/environment.rb
+#require 'erubis/helpers/rails_helper'
 
-# Full error reports are enabled and caching is turned on
-config.action_controller.consider_all_requests_local = true
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+# Enable serving of images, stylesheets, and javascripts from an asset server
+#config.action_controller.asset_host                  = "http://nic_assets.odadata.eu"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# Use ActiveRecord session store
+# config.action_controller.session_store = :active_record_store
+
+# Suppress newline after erb statements
+# ActionView::Base.erb_trim_mode = '>'

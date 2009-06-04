04c7807a89102f2da40d155c14e1f0070a419478
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-06-04 16:22:38 +0000

    ODA-173
    git-svn-id: file:///svn/trunk/odadata@9520 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ config/environments/odanic_production.rb
@@ -0,0 +1,8 @@
+config.cache_classes = false
+
+# Full error reports are enabled and caching is turned on
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = true
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true

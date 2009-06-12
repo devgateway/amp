f9aae26e03ef7bc2a21138c8e3351e590dfad381
mkalandadze <mkalandadze@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-06-12 10:13:31 +0000

    AMP-6029
    changes in new fields visibility
    
    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@9596 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/helper/GlobalSettingsConstants.java
+++ WEB-INF/src/org/digijava/module/aim/helper/GlobalSettingsConstants.java
@@ -53,6 +53,6 @@ public class GlobalSettingsConstants {
     
     public static final String AMP_SERVER_NAME= "AMP Server Name";
     
-    
+    public static final String NEW_FIELDS_VISIBILITY= "New fields Visibility";    
 
 }

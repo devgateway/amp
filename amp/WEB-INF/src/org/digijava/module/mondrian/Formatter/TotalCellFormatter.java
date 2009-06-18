d7d7851fc3c02f5df0cbba7678bfaf1d88b9d51f
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-06-18 18:04:08 +0000

    AMP-5710
    git-svn-id: file:///svn/trunk@9697 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/mondrian/Formatter/TotalCellFormatter.java
+++ amp/WEB-INF/src/org/digijava/module/mondrian/Formatter/TotalCellFormatter.java
@@ -2,6 +2,8 @@ package org.digijava.module.mondrian.Formatter;
 
 import mondrian.olap.CellFormatter;
 import org.digijava.module.aim.helper.FormatHelper;
+import org.digijava.module.aim.helper.GlobalSettingsConstants;
+import org.digijava.module.aim.util.FeaturesUtil;
 
 /**
  * 
@@ -9,22 +11,24 @@ import org.digijava.module.aim.helper.FormatHelper;
  */
 
 /**
- *	A cell formatter modifies the behavior of Cell.getFormattedValue(). The class must implement the
- * 	mondrian.olap.CellFormatter interface, and is specified like this:
- *	<Measure name="name" formatter="com.acme.MyCellFormatter">
- *        <CalculatedMemberProperty />
- *	</Measure>
- *	For a calculated member that belongs to a cube or virtual cube, you can define a formatter by setting the 
- *	CELL_FORMATTER property of the member to the name of the formatter class:
- *	<CalculatedMember name="name" formatter="com.acme.MyCellFormatter">
- *		<CalculatedMemberProperty name="CELL_FORMATTER" value="com.acme.MyCellFormatter" />
- *	</CalculatedMember>
-*/
+ * A cell formatter modifies the behavior of Cell.getFormattedValue(). The class
+ * must implement the mondrian.olap.CellFormatter interface, and is specified
+ * like this: <Measure name="name" formatter="com.acme.MyCellFormatter">
+ * <CalculatedMemberProperty /> </Measure> For a calculated member that belongs
+ * to a cube or virtual cube, you can define a formatter by setting the
+ * CELL_FORMATTER property of the member to the name of the formatter class:
+ * <CalculatedMember name="name" formatter="com.acme.MyCellFormatter">
+ * <CalculatedMemberProperty name="CELL_FORMATTER"
+ * value="com.acme.MyCellFormatter" /> </CalculatedMember>
+ */
 
-public class TotalCellFormatter implements CellFormatter{
+public class TotalCellFormatter implements CellFormatter {
 
 	@Override
 	public String formatCell(Object value) {
-			return FormatHelper.formatNumber(new Double(value.toString()));
+		if (FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.AMOUNTS_IN_THOUSANDS).equalsIgnoreCase("true")) {
+			return FormatHelper.formatNumber(new Double(value.toString())/1000);
 		}
+		return FormatHelper.formatNumber(new Double(value.toString()));
+	}
 }

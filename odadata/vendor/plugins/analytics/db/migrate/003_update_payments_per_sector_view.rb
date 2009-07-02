9234063f35a487f6ec5f16b22372a521ec23a0ea
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-02 17:20:33 +0000

    ODANIC-72
    git-svn-id: file:///svn/trunk@9811 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/vendor/plugins/analytics/db/migrate/003_update_payments_per_sector_view.rb
+++ odadata/vendor/plugins/analytics/db/migrate/003_update_payments_per_sector_view.rb
@@ -2,16 +2,23 @@ class CreatePaymentsPerSectorView < ActiveRecord::Migration
   def self.up
     execute <<-END_SQL
       CREATE OR REPLACE VIEW sector_payments AS
-        SELECT project_dac_sector_shares.dac_sector_id, project_payment_totals."year", project_payment_totals."currency",
-          project_payment_totals.data_status, project_payment_totals.donor_id, 
-          (sum((project_payment_totals.total * ((100 / project_dac_sector_shares.amount))::double precision)) AS payments
-        FROM project_payment_totals, project_dac_sector_shares
-        WHERE project_dac_sector_shares.project_id = project_payment_totals.project_id 
-      	  AND project_dac_sector_shares.dac_sector_id IS DISTINCT FROM NULL 
-      		AND project_payment_totals.total IS DISTINCT FROM NULL
-        GROUP BY project_dac_sector_shares.dac_sector_id, project_payment_totals."year", project_payment_totals."currency", 
-          project_payment_totals.data_status, project_payment_totals.donor_id
-        ORDER BY payments DESC;
+      SELECT project_dac_sector_shares.dac_sector_id, project_payment_totals.year,
+          project_payment_totals.currency, project_payment_totals.data_status,
+          project_payment_totals.donor_id, sum((project_payment_totals.total * ((100
+          / project_dac_sector_shares.amount))::double precision)) AS payments
+      FROM project_payment_totals, project_dac_sector_shares
+      WHERE (((project_dac_sector_shares.project_id =
+          project_payment_totals.project_id) AND
+          (project_dac_sector_shares.dac_sector_id IS DISTINCT
+      FROM NULL::integer)) AND (project_payment_totals.total IS DISTINCT
+      FROM NULL::double precision))
+      GROUP BY project_payment_totals.donor_id, project_payment_totals.data_status,
+          project_payment_totals.year, project_payment_totals.currency,
+          project_dac_sector_shares.dac_sector_id
+      ORDER BY project_payment_totals.donor_id, project_payment_totals.data_status,
+          project_payment_totals.year, project_payment_totals.currency,
+          sum((project_payment_totals.total * ((100 /
+          project_dac_sector_shares.amount))::double precision)) DESC;
     END_SQL
   end
 

c9a1421c5cf9937cd3bb947047601f2eed3eaa45
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-09 09:00:05 +0000

    AMP-6153
    git-svn-id: file:///svn/trunk/amp@9832 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches_postgres/risk_category_values.sql
+++ patches_postgres/risk_category_values.sql
@@ -5,4 +5,3 @@ Insert into amp_category_value(category_value,amp_category_class_id,index_column
 Insert into amp_category_value(category_value,amp_category_class_id,index_column,field_type)(select "High",id,3,0 from amp_category_class where keyName="indicator_risk");
 Insert into amp_category_value(category_value,amp_category_class_id,index_column,field_type)(select "Very High",id,4,0 from amp_category_class where keyName="indicator_risk");
 Insert into amp_category_value(category_value,amp_category_class_id,index_column,field_type)(select "Critical",id,5,0 from amp_category_class where keyName="indicator_risk");
-UPDATE amp_indicator_values ind, amp_category_value val,amp_indicator_risk_ratings risk SET ind.risk_value = val.id WHERE ind.risk =risk.amp_ind_risk_ratings_id  and val.category_value=risk.rating_name;

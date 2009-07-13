7d403e8c0cdbb520f8747d6a647049f4068c62b5
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-13 19:03:48 +0000

    ODANIC-83
    git-svn-id: file:///svn/trunk/odadata@9855 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- vendor/plugins/analytics/app/controllers/bluebook/pages_controller.rb
+++ vendor/plugins/analytics/app/controllers/bluebook/pages_controller.rb
@@ -1,3 +1,9 @@
 class Bluebook::PagesController < BluebookController
+  def contents
+    render :file => "bluebook/pages/#{year}/contents", :layout => "bluebook"
+  end
 
+  def index
+    render :file => "bluebook/pages/#{year}/#{params[:page_name]}", :layout => "#{year}/bluebook"
+  end
 end

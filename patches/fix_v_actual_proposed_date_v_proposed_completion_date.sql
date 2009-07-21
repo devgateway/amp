55af64069d4f1483df5895ba7fbc5e2b7af90758
sdimunzio <sdimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-21 20:40:42 +0000

    AMP-6391
    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@9914 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ patches/fix_v_actual_proposed_date_v_proposed_completion_date.sql
@@ -0,0 +1,17 @@
+CREATE  OR REPLACE VIEW `v_actual_proposed_date` AS
+  select
+    `amp_malawi`.`amp_activity`.`amp_activity_id` AS `amp_activity_id`,
+    `amp_malawi`.`amp_activity`.`proposed_approval_date` AS `proposed_approval_date`
+  from
+    `amp_activity`
+  order by
+    `amp_malawi`.`amp_activity`.`amp_activity_id`;
+   
+ CREATE or replace view  `v_proposed_completion_date` AS
+  select
+    `amp_malawi`.`amp_activity`.`amp_activity_id` AS `amp_activity_id`,
+    `amp_malawi`.`amp_activity`.`proposed_completion_date` AS `proposed_completion_date`
+  from
+    `amp_activity`
+  order by
+    `amp_malawi`.`amp_activity`.`amp_activity_id`;

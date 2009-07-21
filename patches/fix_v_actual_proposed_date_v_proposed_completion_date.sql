04c971766c02af97405d8eff29552bd5590d1db9
sdimunzio <sdimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-21 20:55:55 +0000

    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@9915 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/fix_v_actual_proposed_date_v_proposed_completion_date.sql
+++ patches/fix_v_actual_proposed_date_v_proposed_completion_date.sql
@@ -1,17 +1,17 @@
 CREATE  OR REPLACE VIEW `v_actual_proposed_date` AS
   select
-    `amp_malawi`.`amp_activity`.`amp_activity_id` AS `amp_activity_id`,
-    `amp_malawi`.`amp_activity`.`proposed_approval_date` AS `proposed_approval_date`
+    `amp_activity`.`amp_activity_id` AS `amp_activity_id`,
+    `amp_activity`.`proposed_approval_date` AS `proposed_approval_date`
   from
     `amp_activity`
   order by
-    `amp_malawi`.`amp_activity`.`amp_activity_id`;
+    `amp_activity`.`amp_activity_id`;
    
  CREATE or replace view  `v_proposed_completion_date` AS
   select
-    `amp_malawi`.`amp_activity`.`amp_activity_id` AS `amp_activity_id`,
-    `amp_malawi`.`amp_activity`.`proposed_completion_date` AS `proposed_completion_date`
+    `amp_activity`.`amp_activity_id` AS `amp_activity_id`,
+    `amp_activity`.`proposed_completion_date` AS `proposed_completion_date`
   from
     `amp_activity`
   order by
-    `amp_malawi`.`amp_activity`.`amp_activity_id`;
+    `amp_activity`.`amp_activity_id`;

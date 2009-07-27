79ad017d6bcaf7b5a64b1ddf8a0ca69b2490c792
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-27 20:34:42 +0000

    ODANIC-79
    git-svn-id: file:///svn/trunk@9998 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/lib/settings.rb
+++ odadata/lib/settings.rb
@@ -1,4 +1,4 @@
 module Settings
   # ODAdata Color Scheme, mainly used for Graphs
-  COLOR_SCHEME = ["#000099", "#1A0AAC", "#2E69C7", "#40A3E1", "#00CCFF", "#33FFFF", "#99FFFF", "#CCFFFF", "#DDFFFF", "#FFFDAE", "#FFFD94", "#FFFF00", "#FBE90B", "#FFCD25", "#FF9D4D", "#FF7300", "#FF5100", "#CC0000", "#B60300"]
+  COLOR_SCHEME = ["#1A0AAC", "#2E69C7", "#40A3E1", "#00CCFF", "#33FFFF", "#99FFFF", "#CCFFFF", "#DDFFFF", "#FFFDAE", "#FFFD94", "#FFFF00", "#FBE90B", "#FFCD25", "#FF9D4D", "#FF7300", "#FF5100", "#CC0000", "#B60300","#000099" ]
 end  

4e8469d4f85b54796bb3680716c9e62f46a6ab96
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-07-30 14:33:18 +0000

    AMP-6432
    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@10038 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/message/triggers/ActivityCurrentCompletionDateTrigger.java
+++ WEB-INF/src/org/digijava/module/message/triggers/ActivityCurrentCompletionDateTrigger.java
@@ -23,8 +23,7 @@ public class ActivityCurrentCompletionDateTrigger extends Trigger {
     protected Event generateEvent() {
         Event e=new Event(ActivityCurrentCompletionDateTrigger.class);
         AmpActivity activity=(AmpActivity) source;
-        e.getParameters().put(PARAM_NAME,activity.getName());
-        e.getParameters().put(PARAM_TEAM_ID,activity.getTeam().getAmpTeamId());
+        e.getParameters().put(PARAM_NAME,activity.getName());        
         e.getParameters().put(PARAM_TRIGGER_SENDER,MessageConstants.SENDER_TYPE_SYSTEM);
         e.getParameters().put(PARAM_URL, "aim/selectActivityTabs.do~ampActivityId="+activity.getAmpActivityId());
         e.getParameters().put(PARAM_CREATED_BY, activity.getActivityCreator());
--- WEB-INF/src/org/digijava/module/message/triggers/ActivityDisbursementDateTrigger.java
+++ WEB-INF/src/org/digijava/module/message/triggers/ActivityDisbursementDateTrigger.java
@@ -24,7 +24,7 @@ public class ActivityDisbursementDateTrigger extends Trigger {
 		Event e=new Event(ActivityDisbursementDateTrigger.class);
 		AmpActivity activity=(AmpActivity) source;
 		e.getParameters().put(PARAM_NAME,activity.getName());
-        e.getParameters().put(PARAM_TEAM_ID,activity.getTeam().getAmpTeamId());
+        //e.getParameters().put(PARAM_TEAM_ID,activity.getTeam().getAmpTeamId());
 		e.getParameters().put(PARAM_TRIGGER_SENDER,MessageConstants.SENDER_TYPE_SYSTEM);
 		e.getParameters().put(PARAM_URL, "aim/selectActivityTabs.do~ampActivityId="+activity.getAmpActivityId());
 		e.getParameters().put(PARAM_CREATED_BY, activity.getActivityCreator());

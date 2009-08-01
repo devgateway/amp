b67851dac1cac7a877a7373afb89f36ce1f9d84a
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-08-01 22:34:22 +0000

    AMP-6018
    git-svn-id: file:///svn/trunk/amp@10058 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/parisindicator/helper/export/PIReport4Export.java
+++ WEB-INF/src/org/digijava/module/parisindicator/helper/export/PIReport4Export.java
@@ -9,23 +9,23 @@ import java.util.Map;
 import org.digijava.kernel.request.Site;
 import org.digijava.kernel.translator.TranslatorWorker;
 import org.digijava.module.aim.helper.FormatHelper;
-import org.digijava.module.parisindicator.helper.PIReport3Row;
+import org.digijava.module.parisindicator.helper.PIReport4Row;
 import org.digijava.module.parisindicator.helper.PIReportAbstractRow;
 import org.digijava.module.parisindicator.util.PIConstants;
 
-public class PIReport3Export extends PIAbstractExport {
+public class PIReport4Export extends PIAbstractExport {
 
-	public PIReport3Export(Site site, String langcode) {
+	public PIReport4Export(Site site, String langcode) {
 		super(site, langcode);
 	}
 
 	@Override
 	public Collection generateDataSource(Collection<PIReportAbstractRow> rows) throws Exception {
 		Iterator iter = rows.iterator();
-		ArrayList<AuxRow3> list = new ArrayList();
+		ArrayList<AuxRow4> list = new ArrayList();
 		while (iter.hasNext()) {
-			PIReport3Row row = (PIReport3Row) iter.next();
-			AuxRow3 auxRow = new AuxRow3();
+			PIReport4Row row = (PIReport4Row) iter.next();
+			AuxRow4 auxRow = new AuxRow4();
 			auxRow.setDonorGroup(row.getDonorGroup().getOrgGrpName());
 			auxRow.setYear(new Integer(row.getYear()).toString());
 			auxRow.setColumn1(FormatHelper.getDecimalFormat().format(row.getColumn1()));
@@ -42,25 +42,30 @@ public class PIReport3Export extends PIAbstractExport {
 		parameters.put("PI_REPORT_NUMBER", TranslatorWorker.translateText("Paris Indicator", this.getLangCode(), this
 				.getSite().getId())
 				+ " "
-				+ PIConstants.PARIS_INDICATOR_REPORT_3
+				+ PIConstants.PARIS_INDICATOR_REPORT_4
 				+ " "
 				+ TranslatorWorker.translateText("Report", this.getLangCode(), this.getSite().getId()));
 		parameters.put("PI_COL1", TranslatorWorker.translateText("Donors", this.getLangCode(), this.getSite().getId()));
 		parameters.put("PI_COL2", TranslatorWorker.translateText("Disbursement Year", this.getLangCode(), this
 				.getSite().getId()));
 		parameters.put("PI_COL3", TranslatorWorker.translateText(
-				"Aid flows to the government sector reported on the government's budget", this.getLangCode(), this
-						.getSite().getId()));
-		parameters.put("PI_COL4", TranslatorWorker.translateText("Total Aid flows disbursed to the government sector",
+				"Volume of technical co-operation for capacity development provided through co-ordinated programmes",
 				this.getLangCode(), this.getSite().getId()));
-		parameters.put("PI_COL5", TranslatorWorker.translateText(
-				"Proportion of aid flows to the government sector reported on government budget", this.getLangCode(),
-				this.getSite().getId()));
+		parameters.put("PI_COL4", TranslatorWorker.translateText(
+				"Total volume of technical co-operation provided", this.getLangCode(), this
+						.getSite().getId()));
+		parameters
+				.put(
+						"PI_COL5",
+						TranslatorWorker
+								.translateText(
+										"% of TC for capacity development provided through coordinated programmes consistent with national development strategies",
+										this.getLangCode(), this.getSite().getId()));
 		parameters.put("PI_LAST_YEAR", new Integer(year).toString());
 		return parameters;
 	}
 
-	public class AuxRow3 {
+	public class AuxRow4 {
 		private String donorGroup;
 		private String year;
 		private String column1;
@@ -107,7 +112,7 @@ public class PIReport3Export extends PIAbstractExport {
 			this.column3 = column3;
 		}
 
-		public AuxRow3() {
+		public AuxRow4() {
 		}
 	}
 }

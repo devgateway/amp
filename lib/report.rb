0b49af4e6e22d2b77c90d6918da1f4ad9cb41a4f
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-08-11 17:50:13 +0000

    ODANIC-82
    git-svn-id: file:///svn/trunk/odadata@10158 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- lib/report.rb
+++ lib/report.rb
@@ -20,25 +20,48 @@ module Report
       v.map { |name| "projects.#{name}_marker >= 1"}.join(" OR ") 
     },
     [:prj_status, :grant_loan, :aid_modality_id, :type_of_implementation] => 
-      lambda { |m, v| ["#{m} IN (?)", v] }
+      lambda { |m, v| ["#{m} IN (?)", v] },
+    [:on_off_budget] =>
+      lambda { |m, v| ["fundings.on_budget IN (?)", v] }
   }
   
   # Options for custom reports (fields available) => [field, caption, checked?]
   # TODO: Use translate_options
   def self.query_options    
-    [['donor', ll(:terms, :donor), true], ['agency', ll(:reports, :donor_agency), true], ['donor_project_number', ll(:reports, :project_number), true], 
-    ['oecd_number', ll(:reports, :oecd_id)], ['recipient_country_budget_nr', ll(:reports, :budget_number)], ['title', ll(:reports, :project_title), true], 
-    ['description', ll(:reports, :project_desc)], ['strategy_link', ll(:reports, :cs_link)], ['prj_status', ll(:reports, :project_status)],
-    ['start', ll(:reports, :start), true], ['end', ll(:reports, :end), true], 
-    ['total_commitments', ll(:reports, :commitments_td), true], ['total_payments', ll(:reports, :disbursements_td), true]] +
-    (Project::FIRST_YEAR_OF_RELEVANCE..Time.now.year+3).to_a.map { |y| ["funds_#{y}", "#{ll(:reports, :funds_for)} #{y}"] } +
-    [['total_cofunding', ll(:reports, :total_cofunding)], ['cofunding_donors', ll(:reports, :cofunding_donors)],
-    ['national_regional', ll(:reports, :national_regional)], ['type_of_implementation', ll(:reports, :toi)],
-    ['sectors', ll(:terms, :dac_sector)], ['aid_modality', ll(:reports, :toa)], ['grant_loan', "#{ll(:options, :grant)}/#{ll(:options, :loan)}"], 
-    ['impl_agencies', ll(:reports, :impl_agencies)], ['contr_agencies', ll(:reports, :contr_agencies)],
-    ['markers', ll(:terms, :markers)], ['mdg_goals', ll(:reports, :mdg_goals)], ['focal_regions', ll(:reports, :focal_regions)],
-    ['officer_responsible', ll(:reports, :officer_responsible)], ['website', ll(:terms, :internet_link)], ['comments', ll(:reports, :comments)],
-    ['updated_at', ll(:reports, :last_update)]]
+    [
+      ['donor', ll(:terms, :donor), true],
+      ['agency', ll(:reports, :donor_agency), true],
+      ['donor_project_number', ll(:reports, :project_number), true],
+      ['oecd_number', ll(:reports, :oecd_id)],
+      ['recipient_country_budget_nr', ll(:reports, :budget_number)],
+      ['title', ll(:reports, :project_title), true],
+      ['description', ll(:reports, :project_desc)],
+      ['strategy_link', ll(:reports, :cs_link)],
+      ['prj_status', ll(:reports, :project_status)],
+      ['start', ll(:reports, :start), true],
+      ['end', ll(:reports, :end), true],
+      ['total_commitments', ll(:reports, :commitments_td), true],
+      ['total_payments', ll(:reports, :disbursements_td), true]
+    ] +
+      (Project::FIRST_YEAR_OF_RELEVANCE..Time.now.year+3).to_a.map { |y| ["funds_#{y}", "#{ll(:reports, :funds_for)} #{y}"] } +
+    [
+      ['total_cofunding', ll(:reports, :total_cofunding)],
+      ['cofunding_donors', ll(:reports, :cofunding_donors)],
+      ['national_regional', ll(:reports, :national_regional)],
+      ['type_of_implementation', ll(:reports, :toi)],
+      ['sectors', ll(:terms, :dac_sector)],
+      ['aid_modality', ll(:reports, :toa)],
+      ['grant_loan', "#{ll(:options, :grant)}/#{ll(:options, :loan)}"],
+      ['impl_agencies', ll(:reports, :impl_agencies)],
+      ['contr_agencies', ll(:reports, :contr_agencies)],
+      ['markers', ll(:terms, :markers)],
+      ['mdg_goals', ll(:reports, :mdg_goals)],
+      ['focal_regions', ll(:reports, :focal_regions)],
+      ['officer_responsible', ll(:reports, :officer_responsible)],
+      ['website', ll(:terms, :internet_link)],
+      ['comments', ll(:reports, :comments)],
+      ['updated_at', ll(:reports, :last_update)]
+    ]
 
   #['gov_counterpart', ll(:reports, :gov_counterpart)], removed by 
   end

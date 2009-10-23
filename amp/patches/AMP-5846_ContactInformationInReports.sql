ef0b93d91df5eb2d9851f365be28281acd7b3781
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-10-23 13:16:15 +0000

    git-svn-id: file:///svn/trunk@10701 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/patches/AMP-5846_ContactInformationInReports.sql
+++ amp/patches/AMP-5846_ContactInformationInReports.sql
@@ -11,22 +11,22 @@ create view `v_contact_sector_ministry` AS (
 select
 	a.amp_activity_id,
 	concat_ws(_latin1' ',`c`.`lastname`,`c`.`name`) AS `contact`,
-    a.amp_activity_id AS `contact_id` 
+    a.amp_activity_id AS `contact_id`  
 from 
 	amp_contact c,
     amp_activity a,
-    amp_activity_contact ac 
+    amp_activity_contact ac  
 where
 	c.contact_id = ac.contact_id  
 and	ac.activity_id = a.amp_activity_id 
-and ac.contact_type LIKE 'SECTOR_MINISTRY_CONT');
+and ac.contact_type LIKE 'SECTOR_MINISTRY_CONT'); 
 
 
 create view `v_contact_project_coordinator` AS (
 select
 	a.amp_activity_id,
 	concat_ws(_latin1' ',`c`.`lastname`,`c`.`name`) AS `contact`,
-    a.amp_activity_id AS `contact_id`
+    a.amp_activity_id AS `contact_id` 
 from 
 	amp_contact c,
     amp_activity a,
@@ -56,12 +56,12 @@ create view `v_contact_donor` AS (
 select
 	a.amp_activity_id,
 	concat_ws(_latin1' ',`c`.`lastname`,`c`.`name`) AS `contact`,
-    a.amp_activity_id AS `contact_id`
+    a.amp_activity_id AS `contact_id`  
 from 
 	amp_contact c,
     amp_activity a,
-    amp_activity_contact ac
+    amp_activity_contact ac  
 where
-	c.contact_id = ac.contact_id
-and	ac.activity_id = a.amp_activity_id
+	c.contact_id = ac.contact_id  
+and	ac.activity_id = a.amp_activity_id  
 and ac.contact_type LIKE 'DONOR_CONT');

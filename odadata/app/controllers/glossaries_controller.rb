9e96a844cdd924fc73d1134171e204264150f0d4
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-11-02 18:02:10 +0000

    Squashed commit of the following:
    commit f86f63f0f0f6a14a85571fe13fadc834c1bc7677
    Author: Sven Völlmecke <sven.voellmecke@odadata.eu>
    Date:   Mon Nov 2 19:01:13 2009 +0100
    
        Removed old deprecated glossary stuff
    
    commit 993284280680a36695947479543f9d206c14874f
    Author: Sven Völlmecke <sven.voellmecke@odadata.eu>
    Date:   Fri Oct 30 21:12:48 2009 +0100
    
        Added missing acts_as_reportable gem to environment.rb
    
    commit 172f6ef80a9b2e0a72458d26d20b28f6886e82e2
    Author: Sven Völlmecke <sven.voellmecke@odadata.eu>
    Date:   Fri Oct 30 21:10:24 2009 +0100
    
        Removed default locale request from Prefs
    
    git-svn-id: file:///svn/trunk@10810 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/controllers/glossaries_controller.rb
+++
@@ -1,7 +0,0 @@
-class GlossariesController < ApplicationController
-  def show
-    glossary = I18n.translate("#{params[:model]}.#{params[:method]}", 
-                                :scope => [:glossary, :attributes])
-    render :text => textilize(glossary)
-  end
-end

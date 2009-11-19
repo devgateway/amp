70ed1f7dd6d5d59febf7249dea390aa5a9d679b0
msotero <msotero@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-11-19 22:42:18 +0000

    AMP-7391
    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@10946 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/SaveActivity.java
+++ WEB-INF/src/org/digijava/module/aim/action/SaveActivity.java
@@ -617,7 +617,7 @@ public class SaveActivity extends Action {
 							}
 						}
 						
-						if (isPrimarySectorEnabled()){
+						if (isPrimarySectorEnabled() && isInConfig(eaForm,"Primary")){
 							if(!hasPrimarySectorsAdded){
 								errors.add("noPrimarySectorsAdded",
 										new ActionError("error.aim.addActivity.noPrimarySectorsAdded", TranslatorWorker.translateText("please add primary sectors",locale,siteId)));
@@ -627,7 +627,7 @@ public class SaveActivity extends Action {
 						}
 
 						
-						if (isSecondarySectorEnabled()){
+						if (isSecondarySectorEnabled() && isInConfig(eaForm, "Secondary")){
 							if(!hasSecondarySectorsAdded){
 								errors.add("noSecondarySectorsAdded",
 										new ActionError("error.aim.addActivity.noSecondarySectorsAdded", TranslatorWorker.translateText("please add secondary sectors",locale,siteId)));								
@@ -2802,5 +2802,16 @@ public class SaveActivity extends Action {
 					}
 			return false;
 	  }
+		private boolean isInConfig(EditActivityForm eaForm, String sectorLevel) {
+			List <AmpClassificationConfiguration> classConfig = eaForm.getSectors().getClassificationConfigs();
+			for(AmpClassificationConfiguration cls : classConfig){
+				if(cls.getName().compareToIgnoreCase(sectorLevel)==0){
+					return true;
+				}
+			}
+			return false;
+		}
+
+
 
 }

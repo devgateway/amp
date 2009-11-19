44ee231604f5fa60acd37ac7646192d275cdf776
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-11-19 20:09:13 +0000

    AMP-7389
    git-svn-id: file:///svn/branches/AMP_1_13_RELEASE/amp@10945 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/um/util/AmpUserUtil.java
+++ WEB-INF/src/org/digijava/module/um/util/AmpUserUtil.java
@@ -37,12 +37,13 @@ public class AmpUserUtil {
 		try {
 			session = PersistenceManager.getRequestDBSession();
 			String queryString = "select u from " + User.class.getName() + " u"
-					+ " where u.banned=:banned order by u.email";
+					+ " where u.banned=:banned and u.globalAdmin=:gadmin order by u.email";
 			qry = session.createQuery(queryString);
 			if ( getBanned )
 				qry.setInteger("banned", 1);
 			else
 				qry.setInteger("banned", 0);
+				qry.setInteger("gadmin", 0);
 			users = qry.list();
 		} catch (Exception e) {
 			logger.error("Unable to get user");

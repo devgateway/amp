6f1548bc75afdf70f25e1c3ae07ad8a8b75b79df
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2009-12-21 15:41:23 +0000

    AMP-7511
    git-svn-id: file:///svn/trunk/amp@11136 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/amp-test/org/dgfoundation/amp/test/contacts/PrimaryContactCheckTest.java
+++ WEB-INF/amp-test/org/dgfoundation/amp/test/contacts/PrimaryContactCheckTest.java
@@ -1,6 +1,8 @@
 package org.dgfoundation.amp.test.contacts;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletContext;
 
@@ -8,6 +10,8 @@ import org.apache.log4j.Logger;
 import org.dgfoundation.amp.test.util.Configuration;
 import org.digijava.module.aim.action.AddressBookActions;
 import org.digijava.module.aim.dbentity.AmpContact;
+import org.digijava.module.aim.dbentity.AmpContactProperty;
+import org.digijava.module.aim.helper.Constants;
 import org.digijava.module.aim.util.ContactInfoUtil;
 
 import com.mockrunner.mock.web.MockHttpServletRequest;
@@ -43,16 +47,28 @@ public class PrimaryContactCheckTest extends BasicActionTestCaseAdapter{
 		List<AmpContact> contacts=ContactInfoUtil.getPagedContacts(1, 1, null, null, null);
 		if(contacts!=null){
 			for (AmpContact ampContact : contacts) {
-				//create contact with the same email
-				AmpContact newContact=createContact(ampContact.getEmail());	
-				assertTrue(ContactInfoUtil.getContactsCount(newContact.getEmail(),null)!=0);				
+				Set<AmpContactProperty> properties=ampContact.getProperties();
+				for (AmpContactProperty property : properties) {
+					if(property.getName().equals(Constants.CONTACT_PROPERTY_NAME_EMAIL)){
+						//create contact with the same email
+						AmpContact newContact=createContact(property.getValue());
+						Set<AmpContactProperty> props=newContact.getProperties();
+						for (AmpContactProperty ampContactProperty : props) {							
+							assertTrue(ContactInfoUtil.getContactsCount(ampContactProperty.getValue(),null)!=0);
+						}						
+					}
+				}								
 			}
 		}
 	}
 	
 	private AmpContact createContact(String email){
 		AmpContact contact= new AmpContact();
-		contact.setEmail(email);
+		contact.setProperties(new HashSet<AmpContactProperty> ());
+		AmpContactProperty property= new AmpContactProperty();
+		property.setName(Constants.CONTACT_PROPERTY_NAME_EMAIL);
+		property.setValue(email);		
+		contact.getProperties().add(property);
 		return contact;
 	}
 }

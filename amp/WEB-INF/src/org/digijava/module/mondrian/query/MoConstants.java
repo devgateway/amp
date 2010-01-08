6a505525f4ba0b6d28fe1fd3a0fd1979804bf4c4
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-01-08 15:48:26 +0000

    AMP-7529
    git-svn-id: file:///svn/trunk@11204 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/mondrian/query/MoConstants.java
+++ amp/WEB-INF/src/org/digijava/module/mondrian/query/MoConstants.java
@@ -14,14 +14,41 @@ public final class MoConstants {
 	public static String DONOR_DATES = "Donor Dates";
 	public static String REGIONS = "Regions";
 	public static String PRIMARY_PROGRAMS = "Primary Programs";
+	public static String PRIMARY_PROGRAM_LEVEL_1 = "Primary Program Level 1";
+	public static String PRIMARY_PROGRAM_LEVEL_2 = "Primary Program Level 2";
+	public static String PRIMARY_PROGRAM_LEVEL_3 = "Primary Program Level 3";
+	public static String PRIMARY_PROGRAM_LEVEL_4 = "Primary Program Level 4";
+	public static String PRIMARY_PROGRAM_LEVEL_5 = "Primary Program Level 5";
+	public static String PRIMARY_PROGRAM_LEVEL_6 = "Primary Program Level 6";
+	public static String PRIMARY_PROGRAM_LEVEL_7 = "Primary Program Level 7";
+	public static String PRIMARY_PROGRAM_LEVEL_8 = "Primary Program Level 8";
+	
 	public static String SECONDARY_PROGRAMS = "Secondary Program";
+	public static String SECONDARY_PROGRAMS_LEVEL_1 = "Secondary Program Level 1";
+	public static String SECONDARY_PROGRAMS_LEVEL_2 = "Secondary Program Level 2";
+	public static String SECONDARY_PROGRAMS_LEVEL_3 = "Secondary Program Level 3";
+	public static String SECONDARY_PROGRAMS_LEVEL_4 = "Secondary Program Level 4";
+	public static String SECONDARY_PROGRAMS_LEVEL_5 = "Secondary Program Level 5";
+	public static String SECONDARY_PROGRAMS_LEVEL_6 = "Secondary Program Level 6";
+	public static String SECONDARY_PROGRAMS_LEVEL_7 = "Secondary Program Level 7";
+	public static String SECONDARY_PROGRAMS_LEVEL_8 = "Secondary Program Level 8";
+	
+	public static  String NATIONAL_PROGRAM= "National Planning Objectives";
+	public static  String NATIONAL_PROGRAM_LEVEL_1= "National Planning Objectives Level 1";
+	public static  String NATIONAL_PROGRAM_LEVEL_2= "National Planning Objectives Level 2";
+	public static  String NATIONAL_PROGRAM_LEVEL_3= "National Planning Objectives Level 3";
+	public static  String NATIONAL_PROGRAM_LEVEL_4= "National Planning Objectives Level 4";
+	public static  String NATIONAL_PROGRAM_LEVEL_5= "National Planning Objectives Level 5";
+	public static  String NATIONAL_PROGRAM_LEVEL_6= "National Planning Objectives Level 6";
+	public static  String NATIONAL_PROGRAM_LEVEL_7= "National Planning Objectives Level 7";
+	public static  String NATIONAL_PROGRAM_LEVEL_8= "National Planning Objectives Level 8";
+	
 	public static String STATUS = "Status";
 	public static String DONOR = "Donor";
 	public static String DONOR_TYPES = "Donor Types";
 	public static String DONOR_GROUP = "Donor Group";
 	public static String FINANCING_INTRUMENT= "Financing Instrument";
 	public static  String TERMS_OF_ASSISTANCE= "Terms of Assistance";
-	public static  String NATIONAL_PROGRAM= "National Program";
 	public static String SECTORS= "Sectors";
 	public static String SUB_SECTORS = "Sub-Sectors";
 	public static String SEC_SUB_SECTORS = "Secondary Sector Sub-Sector";
--- amp/WEB-INF/src/org/digijava/module/mondrian/query/QueryThread.java
+++ amp/WEB-INF/src/org/digijava/module/mondrian/query/QueryThread.java
@@ -1,7 +1,12 @@
 package org.digijava.module.mondrian.query;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.digijava.kernel.request.Site;
 import org.digijava.kernel.entity.*;
+
+import com.sun.net.httpserver.HttpContext;
 
 /**
  * 
@@ -15,6 +20,7 @@ public class QueryThread {
 	private static ThreadLocal<String> tl = new ThreadLocal<String>();
 	private static ThreadLocal<Site> site = new ThreadLocal<Site>();
 	private static ThreadLocal<Locale> locale = new ThreadLocal<Locale>();
+	private static ThreadLocal<ServletContext> contex = new ThreadLocal<ServletContext>();
 	
 	public static String getQuery() {
 		return tl.get();
@@ -38,5 +44,13 @@ public class QueryThread {
 
 	public static void setLocale(Locale locale) {
 		QueryThread.locale.set(locale);
+	}
+	
+	public static ServletContext getContext() {
+		return contex.get();
+	}
+
+	public static void setcontext(ServletContext context) {
+		QueryThread.contex.set(context);
 	}
 }

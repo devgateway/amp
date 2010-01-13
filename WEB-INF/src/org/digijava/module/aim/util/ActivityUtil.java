fea81591fe0a5713ebae7f5aec907ffc402c5d2b
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-01-13 20:16:47 +0000

    AMP-5413 - ecs big update
    git-svn-id: file:///svn/trunk/amp@11230 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/util/ActivityUtil.java
+++ WEB-INF/src/org/digijava/module/aim/util/ActivityUtil.java
@@ -28,7 +28,7 @@ import org.apache.log4j.Logger;
 import org.dgfoundation.amp.Util;
 import org.dgfoundation.amp.error.AMPException;
 import org.dgfoundation.amp.error.ExceptionFactory;
-import org.dgfoundation.amp.error.keeper.ErrorReporting;
+import org.dgfoundation.amp.error.keeper.ErrorReportingPlugin;
 import org.dgfoundation.amp.utils.AmpCollectionUtils;
 import org.digijava.kernel.dbentity.Country;
 import org.digijava.kernel.exception.DgException;
@@ -1298,7 +1298,7 @@ public static Long saveActivity(RecoverySaveParameters rsp) throws Exception {
 				catch (HibernateException e) {
 					logger.error("Rollback failed", e);
 					AMPException ae = ExceptionFactory.newAMPException(Constants.AMP_ERROR_LEVEL_WARNING, false, e);
-					ErrorReporting.handle(ae, logger);
+					ErrorReportingPlugin.handle(ae, logger);
 					exceptionRaised = true;
 				}
 			}

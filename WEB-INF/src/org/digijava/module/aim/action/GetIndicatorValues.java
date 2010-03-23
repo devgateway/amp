340ca3ad44151095c5f6682c1cfa5dd9b36c8446
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-03-23 14:19:09 +0000

    AMP-8132
    git-svn-id: file:///svn/branches/AMP_1_14_RELEASE/amp@11829 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/GetIndicatorValues.java
+++ WEB-INF/src/org/digijava/module/aim/action/GetIndicatorValues.java
@@ -34,7 +34,7 @@ public class GetIndicatorValues extends Action {
 					Iterator itr = uIndValForm.getIndicators().iterator();
 					while (itr.hasNext()) {
 						ActivityIndicator actInd = (ActivityIndicator) itr.next();
-						if (actInd.getIndicatorValId().equals(indValId)) {
+						if (actInd.getIndicatorValId()!=null&&actInd.getIndicatorValId().equals(indValId)) {
 							uIndValForm.setBaseVal(actInd.getBaseVal());
 							uIndValForm.setBaseValDate(actInd.getBaseValDate());
 							uIndValForm.setTargetVal(actInd.getTargetVal());

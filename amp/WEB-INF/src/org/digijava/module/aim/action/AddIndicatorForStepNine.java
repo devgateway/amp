8a2eb3b0173e9df2759d532cf9d17e7f37397264
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-04-07 10:19:10 +0000

    AMP-8243
    git-svn-id: file:///svn/branches/AMP_1_14_RELEASE@11929 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/AddIndicatorForStepNine.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/AddIndicatorForStepNine.java
@@ -3,6 +3,7 @@ package org.digijava.module.aim.action;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.dgfoundation.amp.utils.AmpCollectionUtils;
 import org.digijava.module.aim.dbentity.AmpIndicator;
 import org.digijava.module.aim.form.EditActivityForm;
 import org.digijava.module.aim.helper.ActivityIndicator;
@@ -40,40 +42,56 @@ private static Logger logger = Logger.getLogger(AddIndicatorForStepNine.class);
 		if (tmpActivityIndicator==null){
 			tmpActivityIndicator=new ArrayList();
 		}
-
-
-	 if(colampMEIndValbox!=null && !colampMEIndValbox.isEmpty()){
-		Iterator<AmpMEIndicatorValue> itr = colampMEIndValbox.iterator();
-		while(itr.hasNext()){
-			AmpMEIndicatorValue tmpObj = (AmpMEIndicatorValue)itr.next();
-			ActivityIndicator actInd = new ActivityIndicator();
-			actInd.setActivityId(tmpObj.getActivityId().getAmpActivityId());
-			//actInd.setIndicatorValId(new Long(-1));
-			name = tmpObj.getIndicator().getName();
-			code = tmpObj.getIndicator().getCode();
-			actInd.setIndicatorName(name);
-			actInd.setIndicatorCode(code);
-			actInd.setBaseVal(null);
-			actInd.setBaseValDate(null);
-			actInd.setBaseValComments("");
-			actInd.setTargetVal(null);
-			actInd.setTargetValDate(null);
-			actInd.setTargetValComments("");
-			actInd.setRevisedTargetVal(null);
-			actInd.setRevisedTargetValDate(null);
-			actInd.setRevisedTargetValComments("");
-			actInd.setCurrentVal(null);
-			actInd.setCurrentValDate("");
-			actInd.setCurrentValComments("");
-			actInd.setRisk(new Long(-1));
-			AmpIndicator ind=tmpObj.getIndicator();
-			if(ind!=null){
-			actInd.setIndicatorId(ind.getIndicatorId());
+		
+		if(colampMEIndValbox!=null && !colampMEIndValbox.isEmpty()){
+			Map <Long,AmpMEIndicatorValue> ampMEIndMap = AmpCollectionUtils.createMap(colampMEIndValbox, 
+					new AmpCollectionUtils.KeyResolver<Long, AmpMEIndicatorValue>() {
+						@Override
+						public Long resolveKey(AmpMEIndicatorValue element) {						
+							return element.getIndicator().getIndicatorId();
+						}
+				
+			}) ;
+			
+			for (ActivityIndicator actInd : (Collection<ActivityIndicator>)tmpActivityIndicator) {
+				if(ampMEIndMap!=null && ampMEIndMap.get(actInd.getIndicatorId())!=null){
+					ampMEIndMap.remove(actInd.getIndicatorId());
+				}
 			}
-			tmpActivityIndicator.add(actInd);
-		}
-		eaForm.getIndicator().setIndicatorsME(tmpActivityIndicator);
-		}
+			
+			if(!ampMEIndMap.isEmpty()){
+				Iterator<AmpMEIndicatorValue> itr = ampMEIndMap.values().iterator();
+				while(itr.hasNext()){
+					AmpMEIndicatorValue tmpObj = (AmpMEIndicatorValue)itr.next();
+					ActivityIndicator actInd = new ActivityIndicator();
+					actInd.setActivityId(tmpObj.getActivityId().getAmpActivityId());
+					name = tmpObj.getIndicator().getName();
+					code = tmpObj.getIndicator().getCode();
+					actInd.setIndicatorName(name);
+					actInd.setIndicatorCode(code);
+					actInd.setBaseVal(null);
+					actInd.setBaseValDate(null);
+					actInd.setBaseValComments("");
+					actInd.setTargetVal(null);
+					actInd.setTargetValDate(null);
+					actInd.setTargetValComments("");
+					actInd.setRevisedTargetVal(null);
+					actInd.setRevisedTargetValDate(null);
+					actInd.setRevisedTargetValComments("");
+					actInd.setCurrentVal(null);
+					actInd.setCurrentValDate("");
+					actInd.setCurrentValComments("");
+					actInd.setRisk(new Long(-1));
+					AmpIndicator ind=tmpObj.getIndicator();
+					if(ind!=null){
+					actInd.setIndicatorId(ind.getIndicatorId());
+					}
+					tmpActivityIndicator.add(actInd);
+				}
+				eaForm.getIndicator().setIndicatorsME(tmpActivityIndicator);
+			}			
+			
+		}		
 
 		if(!eaForm.getIndicator().getIndicatorsME().isEmpty()){
             Collection<AmpCategoryValue> risks=CategoryManagerUtil.getAmpCategoryValueCollectionByKey(CategoryConstants.INDICATOR_RISK_TYPE_KEY);

d9dad8196fb3a4ea5f9872265008bb2e0e4f4c86
pehlert <pehlert@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-04-17 16:01:15 +0000

    Bye, bye nasty newstickers
    git-svn-id: file:///svn/trunk@12018 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/controllers/settings_controller.rb
+++ odadata/app/controllers/settings_controller.rb
@@ -9,18 +9,4 @@ class SettingsController < ApplicationController
     Prefs.data_input_open = (params[:new_status] == "1") ? true : false
     redirect_to :action => "data_input_status"
   end
-  
-  def scrollbars
-    if request.post?
-      Prefs.scroll_bar_public = params[:prefs][:scroll_bar_public]
-      Prefs.scroll_bar_admin  = params[:prefs][:scroll_bar_admin]
-      Prefs.scroll_bar_mfp    = params[:prefs][:scroll_bar_mfp]
-      
-      flash[:notice] = lc(:successful)
-    end
-  end
-  
-  def update_scrollbars
-    #
-  end
 end

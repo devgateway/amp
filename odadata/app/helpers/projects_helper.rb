16ea9c02a1f843052d2cb146aa18698820ae7add
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-04-20 00:58:50 +0000

    ODAMOZ-61
    git-svn-id: file:///svn/trunk@12034 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/helpers/projects_helper.rb
+++ odadata/app/helpers/projects_helper.rb
@@ -9,10 +9,14 @@ module ProjectsHelper
   def aid_modality_select(form)
     choices = AidModality.all.group_by(&:group_name)
     ungrouped = options_from_collection_for_select(choices.delete(nil), :id, :name, form.object.aid_modality_id)
-    grouped = choices.inject("") do |s, (k, values)|
-      s << content_tag(:optgroup, options_from_collection_for_select(values, :id, :name, form.object.aid_modality_id), :label => k)
+    groups = choices.inject("") do |s, (k, values)|
+      s << content_tag(:option, k, :value => k.gsub(/ +/,'_'), :group => "true")
     end
     blank_option = content_tag(:option,  ll(:options, :prompt))
-    form.label(:aid_modality_id) + select_tag("#{form.object_name}[aid_modality_id]", blank_option + ungrouped + grouped)
+    grouped = choices.inject("") do |s, (k, values)|
+      s << select_tag(k, blank_option + options_from_collection_for_select(values, :id, :name, form.object.aid_modality_id), :style => "display:none;")
+    end
+
+    form.label(:aid_modality_id) + hidden_field_tag("#{form.object_name}[aid_modality_id]", form.object.aid_modality_id) + select_tag("#{form.object_name}[dummy_aid_modality_id]", blank_option + ungrouped + groups) + grouped
   end
 end

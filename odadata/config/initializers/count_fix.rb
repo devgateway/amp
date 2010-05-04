b90e55cc5eebaf332ef8c766fddc7004a972b567
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-05-04 18:09:55 +0000

    ODAMOZ-109
    git-svn-id: file:///svn/trunk@12236 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/config/initializers/count_fix.rb
+++ odadata/config/initializers/count_fix.rb
@@ -10,6 +10,16 @@ class ActiveRecord::Base
         elsif name_and_options[0] =~ /\.\*$/
           name_and_options[0] = '*'
         end
+      elsif name_and_options[0].is_a?(Symbol)
+        if name_and_options[1].key?(:select)
+          if name_and_options[1][:select].include?('DISTINCT')
+            name_and_options[1][:select] = "DISTINCT #{quoted_table_name}.#{primary_key}"
+          elsif name_and_options[1][:select].include?("#{table_name}.*")
+            name_and_options[1][:select] = " #{quoted_table_name}.#{primary_key}"
+          elsif name_and_options[1][:select] =~ /\.\*$/
+            name_and_options[1][:select] = '*'
+          end
+        end
       end
       name_and_options
     end

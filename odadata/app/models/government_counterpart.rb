d02d2b9e3b9c032223c338c126306e2d078c7261
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-06-09 11:55:08 +0000

    ODAMOZ-197
    git-svn-id: file:///svn/trunk@12482 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/app/models/government_counterpart.rb
+++ odadata/app/models/government_counterpart.rb
@@ -2,7 +2,7 @@ class GovernmentCounterpart < ActiveRecord::Base
   validates_presence_of :name, :code
   has_many    :projects
 
-  named_scope :ordered, :order => "code ASC"
+  named_scope :ordered, :order => "name ASC"
   def name_with_code
     "#{code} - #{name}"
   end

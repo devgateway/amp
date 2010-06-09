5359bbd528d3ba421fe11a4d6c0d69cb62b3c45e
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-06-09 11:49:59 +0000

    AMP-8516
    git-svn-id: file:///svn/branches/AMP_1_14_RELEASE/amp@12481 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ WEB-INF/src/org/digijava/module/dataExchange/engine/SourceBuilder.java
@@ -0,0 +1,53 @@
+package org.digijava.module.dataExchange.engine;
+
+import java.io.InputStream;
+
+import org.digijava.module.dataExchange.dbentity.AmpSourceSetting;
+
+public abstract class SourceBuilder {
+	
+	protected AmpSourceSetting ampSourceSetting;
+	protected InputStream inputStream;
+
+
+	
+	public SourceBuilder(AmpSourceSetting ampSourceSetting,
+			InputStream inputStream) {
+		super();
+		this.ampSourceSetting = ampSourceSetting;
+		this.inputStream = inputStream;
+	}
+	
+	public SourceBuilder(AmpSourceSetting ampSourceSetting) {
+		super();
+		this.ampSourceSetting = ampSourceSetting;
+	}
+
+	public abstract void process() ;
+	/**
+	 * @return the ampSourceSetting
+	 */
+	public AmpSourceSetting getAmpSourceSetting() {
+		return ampSourceSetting;
+	}
+	/**
+	 * @param ampSourceSetting the ampSourceSetting to set
+	 */
+	public void setAmpSourceSetting(AmpSourceSetting ampSourceSetting) {
+		this.ampSourceSetting = ampSourceSetting;
+	}
+	/**
+	 * @return the inputStream
+	 */
+	public InputStream getInputStream() {
+		return inputStream;
+	}
+	/**
+	 * @param inputStream the inputStream to set
+	 */
+	public void setInputStream(InputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+	
+	
+}

e45243f29fef790ab50e14d91933dea52b0e5cfa
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-06-10 21:24:34 +0000

    AMP-8553 typo
    git-svn-id: file:///svn/branches/AMP_1_14_RELEASE/amp@12503 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/add_mode_of_payment_category.sql
+++ patches/add_mode_of_payment_category.sql
@@ -4,12 +4,12 @@ select 'Mode of Payment',"",False,False,'mode_of_payment' from DUAL where
 
 insert into amp_category_value (category_value,amp_category_class_id,index_column)
  select  'Direct Payment', c.id , 0 from  amp_category_class c
-   where c.keyName="mofed_of_payment"  and (select count(*) from amp_category_value where category_value='Direct Payment') <1;
+   where c.keyName="mode_of_payment"  and (select count(*) from amp_category_value where category_value='Direct Payment') <1;
 
 insert into amp_category_value (category_value,amp_category_class_id,index_column)
  select  'Direct Funding', c.id , 1 from  amp_category_class c
-   where c.keyName="mofed_of_payment"  and (select count(*) from amp_category_value where category_value='Direct Funding') <1;
+   where c.keyName="mode_of_payment"  and (select count(*) from amp_category_value where category_value='Direct Funding') <1;
 
 insert into amp_category_value (category_value,amp_category_class_id,index_column)
  select  'Reimbursable', c.id , 2 from  amp_category_class c
-   where c.keyName="mofed_of_payment"  and (select count(*) from amp_category_value where category_value='Reimbursable') <1;
+   where c.keyName="mode_of_payment"  and (select count(*) from amp_category_value where category_value='Reimbursable') <1;

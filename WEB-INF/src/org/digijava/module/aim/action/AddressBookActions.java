e9470a73da968cbf6dbb213d9b7927e1f06d773d
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-07-02 12:45:15 +0000

    AMP-8682
    git-svn-id: file:///svn/branches/AMP_1_14_RELEASE/amp@12719 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/aim/action/AddressBookActions.java
+++ WEB-INF/src/org/digijava/module/aim/action/AddressBookActions.java
@@ -2,7 +2,6 @@ package org.digijava.module.aim.action;
 
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -22,28 +21,21 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 import org.digijava.kernel.entity.Locale;
 import org.digijava.kernel.request.Site;
-import org.digijava.kernel.translator.TranslatorWorker;
-import org.digijava.kernel.user.User;
 import org.digijava.kernel.util.RequestUtils;
 import org.digijava.module.aim.dbentity.AmpActivityContact;
 import org.digijava.module.aim.dbentity.AmpContact;
 import org.digijava.module.aim.dbentity.AmpContactProperty;
 import org.digijava.module.aim.dbentity.AmpOrganisation;
 import org.digijava.module.aim.dbentity.AmpOrganisationContact;
-import org.digijava.module.aim.dbentity.AmpTeamMemberRoles;
 import org.digijava.module.aim.form.AddressBookForm;
 import org.digijava.module.aim.helper.AmpContactsWorker;
 import org.digijava.module.aim.helper.Constants;
 import org.digijava.module.aim.helper.ContactPropertyHelper;
 import org.digijava.module.aim.util.ContactInfoUtil;
 import org.digijava.module.aim.util.DbUtil;
-import org.digijava.module.aim.util.TeamMemberUtil;
 import org.digijava.module.categorymanager.util.CategoryManagerUtil;
 
 public class AddressBookActions extends DispatchAction {
-	
-	private Long siteId;
-	private String locale;
 	
 	public ActionForward viewAddressBook (ActionMapping mapping,ActionForm form, HttpServletRequest request,HttpServletResponse response) throws Exception {
     	AddressBookForm myForm=(AddressBookForm)form;
@@ -110,12 +102,6 @@ public class AddressBookActions extends DispatchAction {
 	
 	public ActionForward getContactsJSON (ActionMapping mapping,ActionForm form, HttpServletRequest request,HttpServletResponse response) throws Exception {
 		AddressBookForm myForm=(AddressBookForm)form;
-		//local and siteId
-		Site site = RequestUtils.getSite(request);
-		Long siteId=site.getId();
-		
-		Locale navigationLanguage = RequestUtils.getNavigationLanguage(request);
-		String locale=navigationLanguage.getCode();
 		
 		//total amount of contacts
 		String alpha=null;
@@ -639,6 +625,7 @@ public class AddressBookActions extends DispatchAction {
 		form.setLastname(null);
 		form.setName(null);
 		form.setOrganisationName(null);
+		form.setOrganizations(null);
 		form.setTitle(null);
 		form.setAlphaPages(null);
 		form.setCurrentAlpha(null);

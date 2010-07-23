334dc1af55ea2a53d3aa35c4b07fda93742c0746
droinishvili <droinishvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-07-23 09:33:55 +0000

    AMP-8761
    git-svn-id: file:///svn/branches/AMP_1_14_RELEASE/amp@12913 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- patches/activityContactsUpgradePatch.sql
+++ patches/activityContactsUpgradePatch.sql
@@ -116,7 +116,7 @@ where ac.contact_id=oc.contact_id and oc.amp_org_id=org.amp_org_id and ac.contac
 
 
 create or replace view `v_donor_cont_phone` AS (
-select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) 
+select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) as phone 
 from amp_activity_contact ac join amp_contact_properties cp where ac.contact_id=cp.contact_id AND cp.name='contact phone' and ac.contact_type='DONOR_CONT' 
 );
 
@@ -154,7 +154,7 @@ where ac.contact_id=oc.contact_id and oc.amp_org_id=org.amp_org_id and ac.contac
 
 
 create or replace view `v_mofed_cont_phone` AS (
-select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) 
+select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) as phone 
 from amp_activity_contact ac join amp_contact_properties cp where ac.contact_id=cp.contact_id AND cp.name='contact phone' and ac.contact_type='MOFED_CONT' 
 );
 
@@ -192,7 +192,7 @@ where ac.contact_id=oc.contact_id and oc.amp_org_id=org.amp_org_id and ac.contac
 
 
 create or replace view `v_sect_min_cont_phone` AS (
-select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) 
+select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) as phone 
 from amp_activity_contact ac join amp_contact_properties cp where ac.contact_id=cp.contact_id AND cp.name='contact phone' and ac.contact_type='SECTOR_MINISTRY_CONT' 
 );
 
@@ -229,7 +229,7 @@ where ac.contact_id=oc.contact_id and oc.amp_org_id=org.amp_org_id and ac.contac
 
 
 create or replace view `v_proj_coordr_cont_phone` AS (
-select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) 
+select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) as phone 
 from amp_activity_contact ac join amp_contact_properties cp where ac.contact_id=cp.contact_id AND cp.name='contact phone' and ac.contact_type='PROJ_COORDINATOR_CONT' 
 );
 
@@ -294,7 +294,7 @@ where ac.contact_id=oc.contact_id and oc.amp_org_id=org.amp_org_id and ac.contac
 
 
 create or replace view `v_impl_ex_cont_phone` AS ( 
-select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) 
+select ac.activity_id as amp_activity_id,COALESCE( concat((select category_value from amp_category_value where id=substr(cp.value,1,locate(' ', cp.value)) ),' ',substr(cp.value,locate(' ', cp.value)) )  , substr(cp.value,locate(' ', cp.value))) as phone 
 from amp_activity_contact ac join amp_contact_properties cp where ac.contact_id=cp.contact_id AND cp.name='contact phone' and ac.contact_type='IMPL_EXEC_AGENCY_CONT' 
 );
 

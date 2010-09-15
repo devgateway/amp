2c23874a0669d25b99abb9d21334178776d7509a
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-09-15 12:20:38 +0000

    git-svn-id: file:///svn/trunk@13281 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- wicket-test/src/main/java/org/dgfoundation/amp/onepager/web/pages/HelloWorld.java
+++ wicket-test/src/main/java/org/dgfoundation/amp/onepager/web/pages/HelloWorld.java
@@ -3,8 +3,12 @@
  */
 package org.dgfoundation.amp.onepager.web.pages;
 
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.PageLink;
 import org.dgfoundation.amp.onepager.components.RequiredTextField;
 
 /**
@@ -18,6 +22,7 @@ public class HelloWorld extends WebPage {
 	 */
 	public HelloWorld() {
 		add(new Label("message", "Hello World!"));
+        add(new BookmarkablePageLink<Void>("artyLink", ArtyPage.class));
 		RequiredTextField f = new RequiredTextField("reqtext");
 		add(f);
 	}

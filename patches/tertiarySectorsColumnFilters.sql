32f7cdb98c971c425470302d35b9c41b0b237187
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-09-15 09:38:02 +0000

    AMP-9181
    git-svn-id: file:///svn/branches/AMP_1_15_RELEASE/amp@13275 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ patches/tertiarySectorsColumnFilters.sql
@@ -0,0 +1,4 @@
+INSERT INTO `amp_columns_filters` (column_id,bean_field_name,view_field_name)VALUES
+(getReportColumnId('Tertiary Sector'),'tertiarySectorsAndAncestors','amp_sector_id'),
+(getReportColumnId('Tertiary Sector Sub-Sector'),'tertiarySectorsAndAncestors','amp_sector_id'),
+(getReportColumnId('Tertiary Sector Sub-Sub-Sector'),'tertiarySectorsAndAncestors','amp_sector_id');

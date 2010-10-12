ed2d1d0f80179a8c9eb314fbb2e248f7fc0373e8
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2010-10-12 16:52:59 +0000

    Removed unneeded files
    git-svn-id: file:///svn/trunk@13533 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- odadata/vendor/plugins/analytics/app/controllers/bluebook/pages_controller.rb
+++
@@ -1,9 +0,0 @@
-class Bluebook::PagesController < BluebookController
-  def contents
-    render :file => "bluebook/pages/#{year}/contents", :layout => "bluebook"
-  end
-
-  def index
-    render :file => "bluebook/pages/#{year}/#{params[:page_name]}", :layout => "bluebook"
-  end
-end
--- odadata/vendor/plugins/analytics/app/helpers/bluebook/donor_profiles_helper.rb
+++
@@ -1,5 +0,0 @@
-module Bluebook::DonorProfilesHelper
-  def in_millions(value, decimals = 2)
-    (value.base_value / 1_000_000).round_to(decimals)
-  end
-end

0a1d64bfb4cdb2149a22a78993e1d2061ce431e4
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-06-03 18:01:55 +0000

    AMP-10255 Issue with the Program affectation in the Activity form program percentage as float
    git-svn-id: file:///svn/branches/AMP_1_16_RELEASE/amp@15486 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- WEB-INF/src/org/digijava/module/dataExchange/engine/DEImportBuilder.java
+++ WEB-INF/src/org/digijava/module/dataExchange/engine/DEImportBuilder.java
@@ -618,7 +618,7 @@ public class DEImportBuilder {
 				
 				AmpActivityProgram ampActivityProgram = new AmpActivityProgram();
 				ampActivityProgram.setActivity(activity);
-				ampActivityProgram.setProgramPercentage((new Float(idmlProgram.getPercentage())).longValue());
+				ampActivityProgram.setProgramPercentage((new Float(idmlProgram.getPercentage())));
 				ampActivityProgram.setProgram(ampTheme);
 				
 				ArrayList<AmpActivityProgramSettings> allClassifConfigs = (ArrayList<AmpActivityProgramSettings>) getAllAmpActivityProgramSettings();

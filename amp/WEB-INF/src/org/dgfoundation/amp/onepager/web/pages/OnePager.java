c02826c81066ea45068b6a0f5ae20ae21ade6a8f
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-07-05 19:25:48 +0000

    AMP-10371
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@15677 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/web/pages/OnePager.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/web/pages/OnePager.java
@@ -4,6 +4,9 @@
  */
 package org.dgfoundation.amp.onepager.web.pages;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.PageParameters;
@@ -16,7 +19,10 @@ import org.dgfoundation.amp.onepager.components.features.AmpActivityFormFeature;
 import org.dgfoundation.amp.onepager.components.fields.AmpButtonField;
 import org.dgfoundation.amp.onepager.models.AmpActivityModel;
 import org.dgfoundation.amp.onepager.util.AmpFMTypes;
+import org.digijava.module.aim.dbentity.AmpActivityLocation;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
+import org.digijava.module.aim.dbentity.AmpLocation;
+import org.digijava.module.aim.util.LocationUtil;
 
 /**
  * @author mpostelnicu@dgateway.org since Sep 22, 2010
@@ -37,6 +43,16 @@ public class OnePager extends AmpHeaderFooter {
 		if ((activityId == null) || (activityId.compareTo("new") == 0)){
 			am = new AmpActivityModel();
 			newActivity = true;
+			
+			
+			if(parameters.get("lat") != null && parameters.get("lat") != null && parameters.get("geoId") != null && parameters.get("name") != null){
+				String activityName = (String)parameters.get("name");
+				String latitude = (String)parameters.get("lat");
+				String longitude = (String)parameters.get("lon");
+				String geoId = (String)parameters.get("geoId");
+				
+				initializeActivity(am.getObject(), activityName, latitude, longitude, geoId);
+			}
 		}
 		else{
 			am = new AmpActivityModel(Long.valueOf(activityId));
@@ -56,4 +72,37 @@ public class OnePager extends AmpHeaderFooter {
 		
 		
 	}
+
+	/**
+	 * Method used to initialize an Activity/ActivityVersion with
+	 * preset location information for the GIS module.
+	 * This allows to add activities by clicking on a map
+	 *
+	 * @param activity the activity
+	 * @param activityName the new activity name
+	 * @param latitude the latitude
+	 * @param longitude the longitude
+	 * @param geoId the geoId/geoCode for the location
+	 */
+
+	private void initializeActivity(AmpActivityVersion activity,
+			String activityName, String latitude, String longitude, String geoId) {
+
+		AmpActivityLocation actLoc = new AmpActivityLocation();
+		AmpLocation ampLoc = LocationUtil.getAmpLocationByGeoCode(geoId);
+		// This check is necessary to avoid an exception if the location doesn't
+		// have geoCode
+		if (ampLoc != null) {
+			Set<AmpActivityLocation> locations = new HashSet<AmpActivityLocation>();
+			activity.setName(activityName);
+			actLoc.setLatitude(latitude);
+			actLoc.setLongitude(longitude);
+			actLoc.setLocationPercentage(100f);
+			actLoc.setLocation(ampLoc);
+			locations.add(actLoc);
+			activity.setLocations(locations);
+		}
+
+	}
+
 }

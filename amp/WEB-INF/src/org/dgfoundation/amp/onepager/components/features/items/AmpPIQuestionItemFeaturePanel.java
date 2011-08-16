4683ce20b835879c4b78e43faa54a44be9175386
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-08-16 14:47:46 +0000

    AMP-10703
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@16052 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/items/AmpPIQuestionItemFeaturePanel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/items/AmpPIQuestionItemFeaturePanel.java
@@ -4,49 +4,28 @@
 package org.dgfoundation.amp.onepager.components.features.items;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.AbstractSingleSelectChoice;
-import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RadioChoice;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.LoadableDetachableModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-import org.dgfoundation.amp.onepager.OnePagerConst;
-import org.dgfoundation.amp.onepager.OnePagerUtil;
 import org.dgfoundation.amp.onepager.components.features.AmpFeaturePanel;
-import org.dgfoundation.amp.onepager.components.fields.AmpCategorySelectFieldPanel;
-import org.dgfoundation.amp.onepager.components.fields.AmpIndicatorGroupField;
-import org.dgfoundation.amp.onepager.models.ActivityBudgetModel;
+import org.dgfoundation.amp.onepager.models.AmpActivityModel;
 import org.dgfoundation.amp.onepager.models.PIYesNoAnswerModel;
 import org.dgfoundation.amp.onepager.models.PersistentObjectModel;
 import org.digijava.module.aim.dbentity.AmpAhsurvey;
 import org.digijava.module.aim.dbentity.AmpAhsurveyIndicator;
 import org.digijava.module.aim.dbentity.AmpAhsurveyQuestion;
 import org.digijava.module.aim.dbentity.AmpAhsurveyResponse;
-import org.digijava.module.aim.dbentity.AmpIndicator;
-import org.digijava.module.aim.dbentity.AmpIndicatorRiskRatings;
-import org.digijava.module.aim.dbentity.AmpIndicatorValue;
-import org.digijava.module.aim.dbentity.AmpOrganisation;
-import org.digijava.module.aim.dbentity.IndicatorActivity;
-import org.digijava.module.aim.util.DbUtil;
-import org.digijava.module.aim.util.MEIndicatorsUtil;
-import org.digijava.module.categorymanager.dbentity.AmpCategoryValue;
-import org.digijava.module.categorymanager.util.CategoryConstants;
+import org.hibernate.Session;
 
 import com.rc.retroweaver.runtime.Arrays;
 
@@ -65,11 +44,22 @@ public class AmpPIQuestionItemFeaturePanel extends AmpFeaturePanel<AmpAhsurveyIn
 	public AmpPIQuestionItemFeaturePanel(String id, String fmName, final IModel<AmpAhsurveyIndicator> surveyIndicator, final IModel<AmpAhsurvey> survey){
 		super(id, surveyIndicator, fmName, true);
 		
+		if (surveyIndicator.getObject() != null){
+			Iterator<AmpAhsurveyQuestion> it = surveyIndicator.getObject().getQuestions().iterator();
+			while (it.hasNext()) {
+				AmpAhsurveyQuestion qq = (AmpAhsurveyQuestion) it
+						.next();
+				qq.getAmpTypeId().getName();
+			}
+		}
+		
+		
 		final AbstractReadOnlyModel<List<AmpAhsurveyQuestion>> listModel = new AbstractReadOnlyModel<List<AmpAhsurveyQuestion>>() {
 			private static final long serialVersionUID = 3706184421459839210L;
 			@Override
 			public List<AmpAhsurveyQuestion> getObject() {
-				return new ArrayList<AmpAhsurveyQuestion>(surveyIndicator.getObject().getQuestions());
+				Set<AmpAhsurveyQuestion> set = (Set<AmpAhsurveyQuestion>)surveyIndicator.getObject().getQuestions();
+				return new ArrayList<AmpAhsurveyQuestion>(set);
 			}
 		};
 		

7e17919db147e1fcedfffdef0c8e97560ebf44b3
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-08-26 15:10:11 +0000

    AMP-10823
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@16207 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/subsections/subsectionSlideToggle.js
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/subsections/subsectionSlideToggle.js
@@ -3,4 +3,24 @@ $(document).ready(function(){
 		$(this).siblings("div:first").slideToggle();
 		return false;
 		});
-})
+	var mainContentTop = $('#mainContent').offset().top - 23;
+	var mainContentLeft = $('#mainContent').offset().left + 800;
+	$('#rightMenu').css('top', mainContentTop + "px");
+	$('#rightMenu').css('left', mainContentLeft + "px");
+	$('#rightMenu').css('display', 'block');
+});
+
+$(window).resize(function() {
+	var mainContentTop = $('#mainContent').offset().top - 23;
+	var mainContentLeft = $('#mainContent').offset().left;
+	mainContentLeft = mainContentLeft + 800;
+	$('#rightMenu').css('top', mainContentTop + "px");
+	$('#rightMenu').css('left', mainContentLeft + "px");
+});
+
+$(window).scroll(function() {
+	var mainContentLeft = $('#mainContent').offset().left;
+	var currentScrollLeft = $(window).scrollLeft();
+	mainContentLeft = mainContentLeft + 800 - currentScrollLeft;
+	$('#rightMenu').css('left', mainContentLeft + "px");
+});

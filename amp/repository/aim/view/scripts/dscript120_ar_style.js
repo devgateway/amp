76aad7c7f32b3d94948e5fb09a22f774dbd8a210
ashahinyan <ashahinyan@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-08-31 09:41:18 +0000

    AMP-10870
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@16258 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/repository/aim/view/scripts/dscript120_ar_style.js
+++ amp/repository/aim/view/scripts/dscript120_ar_style.js
@@ -4,7 +4,7 @@ Please refer to readme.html for full Instructions
 Text[...]=[title,text]
 
 Style[...]=[TitleColor,TextColor,TitleBgColor,TextBgColor,TitleBgImag,TextBgImag,TitleTextAlign,TextTextAlign, TitleFontFace, TextFontFace, TipPosition, StickyStyle, TitleFontSize, TextFontSize, Width, Height, BorderSize, PadTextArea, CoordinateX , CoordinateY, TransitionNumber, TransitionDuration, TransparencyLevel ,ShadowType, ShadowColor]
-*/
+*/ 
 
 var FiltersEnabled = 1 // if your not going to use transitions or filters in any of the tips set this to 0
 

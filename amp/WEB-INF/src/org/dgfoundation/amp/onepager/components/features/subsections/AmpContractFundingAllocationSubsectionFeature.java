bc3d559110568f9b09654948b5934798bdbbf855
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-10-08 17:13:02 +0000

    AMP-9203 move contracting EU ammounts into one section so that they can be hidden
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@16761 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/subsections/AmpContractFundingAllocationSubsectionFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/subsections/AmpContractFundingAllocationSubsectionFeature.java
@@ -11,6 +11,7 @@ import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
+import org.dgfoundation.amp.onepager.components.AmpEUAmountsComponent;
 import org.dgfoundation.amp.onepager.components.fields.AmpDatePickerFieldPanel;
 import org.dgfoundation.amp.onepager.components.fields.AmpSelectFieldPanel;
 import org.dgfoundation.amp.onepager.components.fields.AmpTextFieldPanel;
@@ -27,26 +28,6 @@ public class AmpContractFundingAllocationSubsectionFeature extends
 	
 	private static Logger logger = Logger.getLogger(AmpContractFundingAllocationSubsectionFeature.class);
 
-	private void calculateTotalAmount(IModel<IPAContract> model){
-		IPAContract c = model.getObject();
-		Double total = (double) 0;
-		if (c.getTotalECContribIBAmount() != null)
-			total += c.getTotalECContribIBAmount();
-		if (c.getTotalECContribINVAmount() != null)
-			total += c.getTotalECContribINVAmount();
-		
-		if (c.getTotalNationalContribCentralAmount() != null)
-			total += c.getTotalNationalContribCentralAmount();
-		if (c.getTotalNationalContribIFIAmount() != null)
-			total += c.getTotalNationalContribIFIAmount();
-		if (c.getTotalNationalContribRegionalAmount() != null)
-			total += c.getTotalNationalContribRegionalAmount();
-		
-		if (c.getTotalPrivateContribAmount() != null)
-			total += c.getTotalPrivateContribAmount();		
-		model.getObject().setTotalAmount(total);
-	}
-	
 	public AmpContractFundingAllocationSubsectionFeature(String id,
 			final IModel<IPAContract> model, String fmName){
 		super(id, fmName, model);
@@ -67,83 +48,9 @@ public class AmpContractFundingAllocationSubsectionFeature extends
 				currencyList,
 				"Currency", true, false);
 		add(valueCurrency);
+		
+		add(new AmpEUAmountsComponent("EUAmounts", model, "EU Amounts"));
 
-		final AmpTextFieldPanel<Double> totalAmount = new AmpTextFieldPanel<Double>("totalAmount", new PropertyModel<Double>(model, "totalAmount"), "Contract Total Amount");
-		totalAmount.getTextContainer().setEnabled(false);
-		add(totalAmount);
-		
-		AmpTextFieldPanel<Double> ibAmount = new AmpTextFieldPanel<Double>("ibAmount", new PropertyModel<Double>(model, "totalECContribIBAmount"), "IB Amount");
-		ibAmount.getTextContainer().add(new AjaxFormComponentUpdatingBehavior("onblur") {
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				calculateTotalAmount(model);
-				target.addComponent(totalAmount);
-			}
-		});
-		add(ibAmount);
-		AmpDatePickerFieldPanel ibDate = new AmpDatePickerFieldPanel("ibDate", new PropertyModel(model, "totalECContribIBAmountDate"), "IB Date");
-		add(ibDate);
-		
-		AmpTextFieldPanel<Double> invAmount = new AmpTextFieldPanel<Double>("invAmount", new PropertyModel<Double>(model, "totalECContribINVAmount"), "INV Amount");
-		invAmount.getTextContainer().add(new AjaxFormComponentUpdatingBehavior("onblur") {
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				calculateTotalAmount(model);
-				target.addComponent(totalAmount);
-			}
-		});
-		add(invAmount);
-		AmpDatePickerFieldPanel invDate = new AmpDatePickerFieldPanel("invDate", new PropertyModel(model, "totalECContribINVAmountDate"), "INV Date");
-		add(invDate);
-		
-		AmpTextFieldPanel<Double> centralAmount = new AmpTextFieldPanel<Double>("centralAmount", new PropertyModel<Double>(model, "totalNationalContribCentralAmount"), "Central Amount");
-		centralAmount.getTextContainer().add(new AjaxFormComponentUpdatingBehavior("onblur") {
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				calculateTotalAmount(model);
-				target.addComponent(totalAmount);
-			}
-		});
-		add(centralAmount);
-		AmpDatePickerFieldPanel centralDate = new AmpDatePickerFieldPanel("centralDate", new PropertyModel(model, "totalNationalContribCentralAmountDate"), "Central Date");
-		add(centralDate);
-		
-		AmpTextFieldPanel<Double> ifiAmount = new AmpTextFieldPanel<Double>("ifiAmount", new PropertyModel<Double>(model, "totalNationalContribIFIAmount"), "IFI Amount");
-		ifiAmount.getTextContainer().add(new AjaxFormComponentUpdatingBehavior("onblur") {
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				calculateTotalAmount(model);
-				target.addComponent(totalAmount);
-			}
-		});
-		add(ifiAmount);
-		AmpDatePickerFieldPanel ifiDate = new AmpDatePickerFieldPanel("ifiDate", new PropertyModel(model, "totalNationalContribIFIAmountDate"), "IFI Date");
-		add(ifiDate);
-		
-		AmpTextFieldPanel<Double> regionalAmount = new AmpTextFieldPanel<Double>("regionalAmount", new PropertyModel<Double>(model, "totalNationalContribRegionalAmount"), "Regional Amount");
-		regionalAmount.getTextContainer().add(new AjaxFormComponentUpdatingBehavior("onblur") {
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				calculateTotalAmount(model);
-				target.addComponent(totalAmount);
-			}
-		});
-		add(regionalAmount);
-		AmpDatePickerFieldPanel regionalDate = new AmpDatePickerFieldPanel("regionalDate", new PropertyModel(model, "totalNationalContribRegionalAmountDate"), "Regional Date");
-		add(regionalDate);
-		
-		AmpTextFieldPanel<Double> privIbAmount = new AmpTextFieldPanel<Double>("privIbAmount", new PropertyModel<Double>(model, "totalPrivateContribAmount"), "IB Amount");
-		privIbAmount.getTextContainer().add(new AjaxFormComponentUpdatingBehavior("onblur") {
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				calculateTotalAmount(model);
-				target.addComponent(totalAmount);
-			}
-		});
-		add(privIbAmount);
-		AmpDatePickerFieldPanel privIbDate = new AmpDatePickerFieldPanel("privIbDate", new PropertyModel(model, "totalPrivateContribAmountDate"), "IB Date");
-		add(privIbDate);
-		
 	}
 
 }

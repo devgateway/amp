2b1f31d9a999ed20b02e172831a87ed4ff3cb647
mpostelnicu <mpostelnicu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-11-16 02:20:35 +0000

    AMP-11439 Need to add proposed project cost and fixed exchange rate functionality to the new form
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@16969 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpDonorCommitmentsFormTableFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpDonorCommitmentsFormTableFeature.java
@@ -4,21 +4,29 @@
  */
 package org.dgfoundation.amp.onepager.components.features.tables;
 
+import java.text.NumberFormat;
 import java.util.List;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.convert.converters.DoubleConverter;
+import org.apache.wicket.validation.validator.MinimumValidator;
 import org.dgfoundation.amp.onepager.OnePagerUtil;
 import org.dgfoundation.amp.onepager.components.features.subsections.AmpDonorCommitmentsSubsectionFeature;
 import org.dgfoundation.amp.onepager.components.fields.AmpSelectFieldPanel;
+import org.dgfoundation.amp.onepager.components.fields.AmpTextFieldPanel;
 import org.digijava.module.aim.dbentity.AmpFunding;
 import org.digijava.module.aim.dbentity.AmpFundingDetail;
 import org.digijava.module.aim.helper.Constants;
+import org.digijava.module.aim.helper.FormatHelper;
 import org.digijava.module.fundingpledges.dbentity.FundingPledges;
 import org.digijava.module.fundingpledges.dbentity.PledgesEntityHelper;
 
@@ -57,6 +65,14 @@ public class AmpDonorCommitmentsFormTableFeature extends
 								.getAmpDonorOrgId().getAmpOrgId()); 
 					};
 				};
+				
+				
+				AmpTextFieldPanel<Double> exchangeRate = new AmpTextFieldPanel<Double>("exchangeRate",
+						new PropertyModel<Double>(item.getModel(), "fixedExchangeRate"), "exchangeRate",true);
+	
+				exchangeRate.getTextContainer().add(new MinimumValidator<Double>(0.001d));
+				exchangeRate.getTextContainer().add(new AttributeModifier("size", true, new Model<String>("6")));
+				item.add(exchangeRate);
 				
 				item.add(new AmpSelectFieldPanel<FundingPledges>("pledge",
 						new PropertyModel<FundingPledges>(item.getModel(),

f6877ae656f64b41c3cfba31ea230087589b6241
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2011-11-16 15:40:07 +0000

    AMP-11484
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@16982 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/sections/AmpDonorFundingFormSectionFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/sections/AmpDonorFundingFormSectionFeature.java
@@ -4,30 +4,44 @@
  */
 package org.dgfoundation.amp.onepager.components.features.sections;
 
+import java.text.NumberFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.lang.Double;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.util.convert.IConverter;
+import org.apache.wicket.util.convert.converters.DoubleConverter;
 import org.dgfoundation.amp.onepager.OnePagerUtil;
+import org.dgfoundation.amp.onepager.components.AmpFundingAmountComponent;
 import org.dgfoundation.amp.onepager.components.features.items.AmpFundingItemFeaturePanel;
+import org.dgfoundation.amp.onepager.components.fields.AmpDatePickerFieldPanel;
 import org.dgfoundation.amp.onepager.components.fields.AmpDeleteLinkField;
 import org.dgfoundation.amp.onepager.components.fields.AmpMinSizeCollectionValidationField;
+import org.dgfoundation.amp.onepager.components.fields.AmpSelectFieldPanel;
+import org.dgfoundation.amp.onepager.components.fields.AmpTextFieldPanel;
 import org.dgfoundation.amp.onepager.models.AmpOrganisationSearchModel;
 import org.dgfoundation.amp.onepager.translation.TranslatorUtil;
 import org.dgfoundation.amp.onepager.yui.AmpAutocompleteFieldPanel;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
+import org.digijava.module.aim.dbentity.AmpCurrency;
 import org.digijava.module.aim.dbentity.AmpFunding;
 import org.digijava.module.aim.dbentity.AmpFundingDetail;
 import org.digijava.module.aim.dbentity.AmpFundingMTEFProjection;
 import org.digijava.module.aim.dbentity.AmpOrganisation;
+import org.digijava.module.aim.helper.FormatHelper;
+import org.digijava.module.aim.util.CurrencyUtil;
 
 /**
  * The donor funding section of the activity form. Includes selecting an org,
@@ -61,10 +75,49 @@ public class AmpDonorFundingFormSectionFeature extends
 		super(id, fmName, am);
 		setModel = new PropertyModel<Set<AmpFunding>>(
 				am, "funding");
-		
 		if (setModel.getObject() == null)
 			setModel.setObject(new LinkedHashSet<AmpFunding>());
+
+		AmpFundingAmountComponent<AmpActivityVersion> funding = new AmpFundingAmountComponent<AmpActivityVersion>("proposedAmount",
+				am, "Amount", "funAmount", "Currency",
+				"currencyCode", "Date", "funDate");
+		add(funding);
+		/*
+		add(new Label("proposedAmount", "tset"));
+		add(new AmpTextFieldPanel("proposedCurrency", new Model("asda"), "Curr"));
+		add(new Label("proposedDate", "tset"));
+		AmpTextFieldPanel<Double> amount = new AmpTextFieldPanel<Double>("proposedAmount",
+				new PropertyModel<Double>(am, "funAmount"), "Amount",true) {
+			public IConverter getInternalConverter(java.lang.Class<?> type) {
+				DoubleConverter converter = (DoubleConverter) DoubleConverter.INSTANCE;
+				NumberFormat formatter = FormatHelper.getDecimalFormat(true);
+				
+//				formatter.setMinimumFractionDigits(0);
+				converter.setNumberFormat(getLocale(), formatter);
+				return converter; 
+			}
+		};
+		amount.getTextContainer().add(new AttributeModifier("size", true, new Model<String>("12")));
+		add(amount);
+		*/
 		
+		/*
+		AbstractReadOnlyModel<List<AmpCurrency>> currencyList = new AbstractReadOnlyModel<List<AmpCurrency>>() {
+			@Override
+			public List<AmpCurrency> getObject() {
+				return (List<AmpCurrency>) CurrencyUtil.getAllCurrencies(CurrencyUtil.ORDER_BY_CURRENCY_CODE);
+			}
+		};
+		
+		AmpSelectFieldPanel<AmpCurrency> currency = new AmpSelectFieldPanel<AmpCurrency>("proposedCurrency",
+				new PropertyModel<AmpCurrency>(am, "currencyCode"),
+				currencyList,
+				"Currency", true, false);
+		add(currency);
+		AmpDatePickerFieldPanel date = new AmpDatePickerFieldPanel("proposedDate", new PropertyModel<Date>(
+				am, "funDate"), "Date",true);
+		add(date);
+		*/
 		listModel = OnePagerUtil
 				.getReadOnlyListModelFromSetModel(setModel);
 

a9e98e4040df0ab2044ca65f947f09ca86285dc9
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-01-31 17:30:42 +0000

    AMP-12106
    git-svn-id: file:///svn/branches/AMP_1_17_RELEASE@17456 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/WEB-INF/src/org/digijava/module/budgetexport/form/BudgetExportForm.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.digijava.module.budgetexport.form;
+
+import org.apache.struts.action.ActionForm;
+
+/**
+ * @author Alex
+ *
+ */
+public class BudgetExportForm extends ActionForm {
+
+}

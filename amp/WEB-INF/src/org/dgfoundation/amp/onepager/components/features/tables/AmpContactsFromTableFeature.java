8c314f6a402bc111a11746a6ff6ce8deba498708
mgugushvili <mgugushvili@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-02-09 10:05:05 +0000

    AMP-12068
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@17561 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpContactsFromTableFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpContactsFromTableFeature.java
@@ -172,7 +172,7 @@ public class AmpContactsFromTableFeature extends AmpFormTableFeaturePanel<AmpAct
                     AmpContactDetailFeaturePanel detailFax=new AmpContactDetailFeaturePanel("addContactFax", contactModel,"Add Contact Fax",true,Constants.CONTACT_PROPERTY_NAME_FAX);  
                     item.add(detailFax);
 
-                    item.add(new AmpTextAreaFieldPanel<String>("officeaddress",new PropertyModel<String>(contactModel,"officeaddress"),"contact office address",false, false, false));
+                    item.add(new AmpTextAreaFieldPanel<String>("officeaddress",new PropertyModel<String>(contactModel,"officeaddress"),"contact office address",false, true, true));
                 } catch (Exception ex) {
 
                 }

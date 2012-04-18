4740cf9d6d989d890b4e373ab082a515f01a45c3
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-04-18 16:00:44 +0000

    AMP-12857
    git-svn-id: file:///svn/branches/AMP_2_1_RELEASE@18102 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpThemeSearchModel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpThemeSearchModel.java
@@ -54,7 +54,7 @@ public class AmpThemeSearchModel extends AbstractAmpAutoCompleteModel<AmpTheme> 
 				Criteria crit = session.createCriteria(AmpTheme.class);
 				crit.setCacheable(true);
 				//The following line was commented out because it added only the parent hierarchy in the list.
-				//getParams().put(AbstractAmpAutoCompleteModel.PARAM.EXACT_MATCH, false);
+				getParams().put(AbstractAmpAutoCompleteModel.PARAM.EXACT_MATCH, false);
 				if (input.trim().length() > 0){
 					Object o = getTextCriterion("name", input);
 					if (o instanceof SimpleExpression){

9a472a3042f3cd22cc683b4167dcf45b4343c909
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-06-06 19:01:24 +0000

    AMP-13413
    git-svn-id: file:///svn/branches/AMP_2_0_RELEASE@18568 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/validators/AmpUniqueComponentTitleValidator.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/validators/AmpUniqueComponentTitleValidator.java
@@ -3,9 +3,11 @@
  */
 package org.dgfoundation.amp.onepager.validators;
 
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.validation.IValidatable;
 import org.apache.wicket.validation.IValidator;
 import org.apache.wicket.validation.ValidationError;
+import org.digijava.module.aim.dbentity.AmpActivityGroup;
 import org.digijava.module.aim.util.ComponentsUtil;
 
 /**
@@ -14,13 +16,16 @@ import org.digijava.module.aim.util.ComponentsUtil;
  *
  */
 
-public class AmpUniqueComponentTitleValidator implements IValidator<String> {
+public class AmpUniqueComponentTitleValidator  implements IValidator<String> {
+
+	private final PropertyModel<AmpActivityGroup> ampActivityGroupModel;
 
 	/**
 	 * @param propertyModel 
 	 * 
 	 */
-	public AmpUniqueComponentTitleValidator() {
+	public AmpUniqueComponentTitleValidator(PropertyModel<AmpActivityGroup> propertyModel) {
+		this.ampActivityGroupModel = propertyModel;
 	}
 
 	/* (non-Javadoc)
@@ -31,7 +36,7 @@ public class AmpUniqueComponentTitleValidator implements IValidator<String> {
 		if(validatable.getValue().trim().length()==0) 
 			return;
 		
-		boolean exists = ComponentsUtil.checkComponentNameExists(validatable.getValue());
+		boolean exists = ComponentsUtil.checkComponentNameExistsExcludingGroup(validatable.getValue(), ampActivityGroupModel.getObject());
 		if(exists){
 			ValidationError error = new ValidationError();
 			error.addMessageKey("AmpUniqueComponentTitleValidator");

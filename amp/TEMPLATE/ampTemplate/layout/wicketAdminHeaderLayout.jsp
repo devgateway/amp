c134858f1dfa6f0303e9e466eb89e5fa4f96e314
dan <dan@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-08-17 16:27:49 +0000

    AMP-13782
    git-svn-id: file:///svn/branches/AMP_2_2_RELEASE@19039 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/layout/wicketAdminHeaderLayout.jsp
+++ amp/TEMPLATE/ampTemplate/layout/wicketAdminHeaderLayout.jsp
@@ -10,23 +10,24 @@
 <%@ taglib uri="/taglib/moduleVisibility" prefix="module" %>
 <%@ page import="org.dgfoundation.amp.ar.AmpARFilter"%>
 
-	<jsp:include page="headerTop_2.jsp"/>
-    <center>
-	<div class="main_menu" id="userHomeMenu" >
-    	<table cellpadding="0"cellspacing="0" width="1000">
-        	<tr>
-            	<td style="width:900px;" valign="top"><jsp:include page="headerMiddle.jsp"/></td>
-                <td>
-                	<digi:secure authenticated="true">
-	        			<div class="workspace_info"> <!-- I think this class should be renamed to correspong the logout item -->   						
-	   					<digi:link styleClass="loginWidget" href="/j_spring_logout" module="aim">
-							<digi:trn key="aim:logout">LOGOUT</digi:trn>
-						</digi:link>
-						</div>	
-					</digi:secure>
-				</td>
-            </tr>
-        </table>
-    	
-	</div>
- </center>
+	<div id="whHeader">
+		<jsp:include page="headerTop_2.jsp"/>
+	    <center>
+		<div class="main_menu" id="userHomeMenu" >
+	    	<table cellpadding="0"cellspacing="0" width="1000">
+	        	<tr>
+	            	<td style="width:900px;" valign="top"><jsp:include page="headerMiddle.jsp"/></td>
+	                <td>
+	                	<digi:secure authenticated="true">
+		        			<div class="workspace_info" style="border-left: 0px"> <!-- I think this class should be renamed to correspong the logout item -->   						
+		   					<digi:link styleClass="loginWidget" href="/j_spring_logout" module="aim">
+								<digi:trn key="aim:logout">LOGOUT</digi:trn>
+							</digi:link>
+							</div>	
+						</digi:secure>
+					</td>
+	            </tr>
+	        </table>
+ 		</div>
+	 	</center>
+ 	</div>
--- amp/WEB-INF/src/org/dgfoundation/amp/permissionmanager/web/pages/AmpPMHeaderFooter.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/permissionmanager/web/pages/AmpPMHeaderFooter.java
@@ -9,17 +9,27 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.apache.wicket.Session;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
+import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
 import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
+import org.apache.wicket.request.resource.PackageResourceReference;
 import org.dgfoundation.amp.onepager.AmpAuthWebSession;
+import org.dgfoundation.amp.onepager.behaviors.DocumentReadyBehavior;
 import org.dgfoundation.amp.onepager.behaviors.JQueryBehavior;
+import org.dgfoundation.amp.onepager.components.features.sections.AmpStructuresFormSectionFeature;
 import org.dgfoundation.amp.onepager.translation.AmpAjaxBehavior;
+import org.dgfoundation.amp.onepager.util.UrlEmbederComponent;
 
 /**
  * @author dan
@@ -48,10 +58,11 @@ public class AmpPMHeaderFooter extends WebPage {
         }
 		
 		add(new JQueryBehavior());
+		add(new DocumentReadyBehavior());
 		AmpAjaxBehavior ampajax = new AmpAjaxBehavior();
 		add(ampajax);
-		final CharSequence callBackUrl = ampajax.getCallbackUrl();
-
+		//final CharSequence callBackUrl = ampajax.getCallbackUrl();
+/*
 		IModel variablesModel = new AbstractReadOnlyModel() {
 			public Map getObject() {
 				Map<String, CharSequence> variables = new HashMap<String, CharSequence>(2);
@@ -62,6 +73,7 @@ public class AmpPMHeaderFooter extends WebPage {
 			}
 		};
 		
+ */
 		   add(new IndicatingAjaxLink("fmmode", new Model("FM Mode")) {
 			    @Override
 			    public void onClick(AjaxRequestTarget arg0) {
@@ -73,6 +85,35 @@ public class AmpPMHeaderFooter extends WebPage {
 			     setResponsePage(PermissionManager.class);
 			    }
 			   });
+			add(new UrlEmbederComponent("wHeader", "/showLayout.do?layout=wicketAdminHeaderLayout", "$(\"#switchTranslationMode\").attr('href', 'javascript:wicketSwitchTranslationMode()');$(\"#switchFMMode\").css(\"display\", \"block\");"));
+			add(new UrlEmbederComponent("wFooter", "/showLayout.do?layout=wicketFooter"));
 		
 	}
+	
+	public HttpServletRequest getServletRequest(){
+		ServletWebRequest servletWebRequest = (ServletWebRequest) getRequest();
+		HttpServletRequest request = servletWebRequest.getContainerRequest();
+		return request;
+	}
+	
+	public HttpSession getHttpSession(){
+		HttpSession session = getServletRequest().getSession();
+		return session;
+	}
+	
+	@Override
+	protected void setHeaders(WebResponse response) {
+		super.setHeaders(response);
+		response.setHeader("Cache-Control", "no-cache, max-age=0, must-revalidate, no-store");
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		//TODO:1.5
+		//response.renderJavaScriptReference(new PackageResourceReference("/ckeditor/ckeditor.js"));
+		response.renderJavaScriptReference("/ckeditor/ckeditor.js");
+		response.renderJavaScriptReference(new PackageResourceReference(AmpStructuresFormSectionFeature.class, "gisPopup.js"));
+	}
+	
 }

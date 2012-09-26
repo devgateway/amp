b1dbaaed86206952497041b7ac5d405e8b592b80
george <george@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-09-26 15:05:19 +0000

    git-svn-id: file:///svn/branches/AMP_2_2_RELEASE@19290 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/repository/mondrian/sql/refresh_mondrian_cache.sql
+++ amp/repository/mondrian/sql/refresh_mondrian_cache.sql
@@ -576,5 +576,5 @@ CREATE INDEX idx_nac_prog_name ON cached_v_m_donor_funding (national_program_nam
 drop table if EXISTS cached_v_results;
 create table cached_v_results as SELECT amp_activity.amp_activity_id, btrim(dg_editor.body) AS ebody FROM amp_activity, dg_editor WHERE (amp_activity.results = (dg_editor.editor_key)::text) ORDER BY amp_activity.amp_activity_id;
 
-drop table if EXISTS cached_v_proposed_start_date
+drop table if EXISTS cached_v_proposed_start_date;
 create table cached_v_proposed_start_date as SELECT amp_activity.amp_activity_id, amp_activity.proposed_start_date FROM amp_activity ORDER BY amp_activity.amp_activity_id;

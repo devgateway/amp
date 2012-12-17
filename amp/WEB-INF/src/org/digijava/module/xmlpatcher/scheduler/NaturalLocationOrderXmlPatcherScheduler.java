ed1b72d1f999cbd680b3e33c4f0bed5ca0238605
apopescu <apopescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2012-12-17 10:08:11 +0000

    AMP-14377
    git-svn-id: file:///svn/branches/AMP_2_2_RELEASE@19644 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/WEB-INF/src/org/digijava/module/xmlpatcher/scheduler/NaturalLocationOrderXmlPatcherScheduler.java
@@ -0,0 +1,60 @@
+/**
+ * @author Adrian Popescu
+ */
+package org.digijava.module.xmlpatcher.scheduler;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.digijava.module.xmlpatcher.dbentity.AmpXmlPatch;
+
+/**
+ * @author Adrian Popescu - Sample scheduler,
+ *         returns patches ordered by their location name + file name. 
+ * @see org.digijava.module.xmlpatcher.core.XmlPatcherService#processInitEvent(org.digijava.kernel.service.ServiceContext)
+ */
+public class NaturalLocationOrderXmlPatcherScheduler extends XmlPatcherScheduler {
+
+	
+	public static class NaturalLocationOrderPatchComparator implements
+			Comparator<AmpXmlPatch> {
+		@Override
+		public int compare(AmpXmlPatch arg0, AmpXmlPatch arg1) {
+			if(arg0.getLocation().compareTo(arg1.getLocation())==0){
+				return arg0.getPatchId().compareTo(arg1.getPatchId());
+			}
+			return arg0.getLocation().compareTo(arg1.getLocation());
+		}
+
+	}
+
+	/**
+	 * @see XmlPatcherScheduler#XmlPatcherScheduler(Map, Set)
+	 * @param properties
+	 * @param patches
+	 */
+	public NaturalLocationOrderXmlPatcherScheduler(Map<String, Object> properties,
+			List<AmpXmlPatch> patches) {
+		super(properties, patches);
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.digijava.module.xmlpatcher.scheduler.XmlPatcherScheduler#
+	 * getScheduledPatchCollection()
+	 */
+	@Override
+	public Collection<AmpXmlPatch> getScheduledPatchCollection() {
+		Collection<AmpXmlPatch> ret = new TreeSet<AmpXmlPatch>(
+				new NaturalLocationOrderPatchComparator());
+		ret.addAll(patches);
+		return ret;
+	}
+
+}

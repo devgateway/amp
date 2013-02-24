74429e32b298d7429086ff83943009959ffeeb4d
agartner <agartner@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2013-02-24 20:37:12 +0000

    AMP-14827
    git-svn-id: file:///svn/branches/AMP_2_3_RELEASE@20051 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/UpdateWorkspace.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/UpdateWorkspace.java
@@ -235,7 +235,7 @@ public class UpdateWorkspace extends Action {
 					uwForm.getDeletedChildWorkspaces().clear();
 				}
 
-				if(!uwForm.getUseFilter() ){
+				if( uwForm.getUseFilter() == null || !uwForm.getUseFilter() ){
 					if (uwForm.getOrganizations() != null) {
 						TreeSet s = new TreeSet();
 						s.addAll(uwForm.getOrganizations());

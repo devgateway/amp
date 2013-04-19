13aa3f4a6f7928c8d4c54b8cbb2a1c7fb9376f17
aartimon <aartimon@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2013-04-19 08:52:17 +0000

    AMP-15268
    git-svn-id: file:///svn/branches/AMP_2_3_RELEASE@20396 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/AmpFundingAmountComponent.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/AmpFundingAmountComponent.java
@@ -7,6 +7,8 @@ package org.dgfoundation.amp.onepager.components;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
@@ -22,6 +24,7 @@ import org.dgfoundation.amp.onepager.components.fields.*;
 import org.dgfoundation.amp.onepager.models.MTEFYearsModel;
 import org.digijava.module.aim.action.AddFunding;
 import org.digijava.module.aim.dbentity.AmpCurrency;
+import org.digijava.module.aim.dbentity.AmpOrgGroup;
 import org.digijava.module.aim.helper.FormatHelper;
 import org.digijava.module.aim.helper.KeyValue;
 import org.digijava.module.aim.util.CurrencyUtil;
@@ -40,6 +43,29 @@ public class AmpFundingAmountComponent<T> extends Panel {
 	private AmpTextFieldPanel<Double> amount;
 	private AmpSelectFieldPanel<AmpCurrency> currency;
 	private Component date;
+    private final IModel<List<KeyValue>> mtefYearsChoices = new AbstractReadOnlyModel<List<KeyValue>>() {
+        private List<KeyValue> list = null;
+        @Override
+        public List<KeyValue> getObject() {
+            if (list != null)
+                return list;
+
+            list = new ArrayList<KeyValue>(21);
+            boolean fiscal = MTEFYearsModel.getFiscal();
+            Calendar calendar = Calendar.getInstance();
+            int currentYear = calendar.get(Calendar.YEAR);
+            calendar.set(Calendar.DAY_OF_YEAR, 1);
+            //go back 10 years for historical data
+            currentYear -= 10;
+            //10 years back + current year + 10 years forward
+            for (int i = 0; i < 21; i++){
+                calendar.set(Calendar.YEAR, currentYear);
+                list.add(MTEFYearsModel.convert(calendar.getTime(), fiscal));
+                currentYear++;
+            }
+            return list;
+        }
+    };
  
 	private Collection<AmpCollectionValidatorField> validationFields = new ArrayList<AmpCollectionValidatorField>();
 	/**
@@ -107,24 +133,16 @@ public class AmpFundingAmountComponent<T> extends Panel {
 			date = datetmp;
 		}
 		else{
-			IModel<List<String>> mtefYearsChoices = new AbstractReadOnlyModel<List<String>>() {
-				@Override
-				public List<String> getObject() {
-					List<KeyValue> years = AddFunding.generateAvailableMTEFProjectionYears(null);
-					List<String> ret = new ArrayList<String>();
-					Iterator<KeyValue> it = years.iterator();
-					while (it.hasNext()) {
-						KeyValue keyValue = (KeyValue) it.next();
-						ret.add(keyValue.getKey());
-					}
-					return ret;
-				}
-			};
+
 			MTEFYearsModel yearModel = new MTEFYearsModel(new PropertyModel<Date>(model, propertyDate));
-			AmpTextFieldPanel<String> datetmp = new AmpTextFieldPanel<String>("date", yearModel, fmDate, true, true);
-			datetmp.getTextContainer().setEnabled(false);
-			datetmp.getTextContainer().add(new AttributeModifier("size", new Model<String>("10")));
-			date = datetmp;
+//			AmpTextFieldPanel<String> datetmp = new AmpTextFieldPanel<String>("date", yearModel, fmDate, true, true);
+//			datetmp.getTextContainer().setEnabled(false);
+//			datetmp.getTextContainer().add(new AttributeModifier("size", new Model<String>("10")));
+//			date = datetmp;
+
+
+            AmpSelectFieldPanel<KeyValue> datetmp = new AmpSelectFieldPanel<KeyValue>("date", yearModel, mtefYearsChoices, fmDate, true, true, new ChoiceRenderer<KeyValue>("value", "key"));
+            date = datetmp;
 		}
 		add(date);
 		setRenderBodyOnly(true);
--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/MTEFYearsModel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/MTEFYearsModel.java
@@ -1,28 +1,76 @@
 package org.dgfoundation.amp.onepager.models;
 import java.util.Calendar;
 import java.util.Date;
-import org.apache.wicket.model.IModel;
 
-public class MTEFYearsModel implements IModel<String> {
+import org.apache.wicket.Session;
+import org.apache.wicket.model.IModel;
+import org.dgfoundation.amp.onepager.AmpAuthWebSession;
+import org.digijava.module.aim.dbentity.AmpApplicationSettings;
+import org.digijava.module.aim.dbentity.AmpFiscalCalendar;
+import org.digijava.module.aim.dbentity.AmpTeamMember;
+import org.digijava.module.aim.helper.GlobalSettingsConstants;
+import org.digijava.module.aim.helper.KeyValue;
+import org.digijava.module.aim.helper.TeamMember;
+import org.digijava.module.aim.util.DbUtil;
+import org.digijava.module.aim.util.FeaturesUtil;
+import org.digijava.module.aim.util.FiscalCalendarUtil;
+
+public class MTEFYearsModel implements IModel<KeyValue> {
 	private static final long serialVersionUID = 1L;
 	private IModel<Date> sourceModel;
-	
+    private boolean fiscal;
+
+
+    public static boolean getFiscal(){
+        boolean fiscal = true;
+
+        TeamMember tm = ((AmpAuthWebSession) Session.get()).getCurrentMember();
+        if (tm != null){
+            AmpApplicationSettings tempSettings = DbUtil.getMemberAppSettings(tm.getMemberId());
+            if (tempSettings == null)
+                tempSettings = DbUtil.getTeamAppSettings(tm.getTeamId());
+            Long defaultCalendarId=null;
+            if (tempSettings!=null && tempSettings.getFiscalCalendar()!=null){
+                defaultCalendarId=tempSettings.getFiscalCalendar().getAmpFiscalCalId();
+            }else{
+                defaultCalendarId=Long.parseLong(FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.DEFAULT_CALENDAR));
+            }
+            AmpFiscalCalendar ampFiscalCalendar = FiscalCalendarUtil.getAmpFiscalCalendar(defaultCalendarId);
+            if (!ampFiscalCalendar.getIsFiscal())
+                fiscal = false;
+        }
+        return fiscal;
+    }
+
+    public static KeyValue convert(Date date, boolean fiscal){
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int year = c.get(Calendar.YEAR);
+
+        String key = Integer.toString(year);
+        String value = Integer.toString(year);
+        if (fiscal)
+            value += "/"+Integer.toString(year+1);
+        return new KeyValue(key, value);
+    }
+
 	public MTEFYearsModel(IModel<Date> sourceModel) {
 		this.sourceModel = sourceModel;
+        this.fiscal = getFiscal();
 	}
 	
 	@Override
-	public void setObject(String object) {
-		//value is not modifiable 
+	public void setObject(KeyValue object) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_YEAR, 1);
+        calendar.set(Calendar.YEAR, Integer.parseInt(object.getKey()));
+        sourceModel.setObject(calendar.getTime());
 	}
 	
 	@Override
-	public String getObject() {
-		Date date = sourceModel.getObject();
-		Calendar c = Calendar.getInstance();
-		c.setTime(date);
-		int year = c.get(Calendar.YEAR);	
-		return Integer.toString(year)+"/"+Integer.toString(year+1);
+	public KeyValue getObject() {
+        Date date = sourceModel.getObject();
+        return convert(date, fiscal);
 	}
 
 	@Override

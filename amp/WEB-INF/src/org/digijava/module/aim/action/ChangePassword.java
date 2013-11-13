3178419788aba0994a46cba9c0f50f1a9b09c1e8
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2013-11-13 14:15:29 +0000

    AMP-16106. Fix change password. Code refactoring
    git-svn-id: file:///svn/branches/AMP_2_6_RELEASE@21351 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/ChangePassword.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/ChangePassword.java
@@ -1,4 +1,4 @@
-package org.digijava.module.aim.action ;
+package org.digijava.module.aim.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,46 +15,45 @@ import org.digijava.module.um.util.DbUtil;
 
 public class ChangePassword extends Action {
 
-		  private static Logger logger = Logger.getLogger(ChangePassword.class);
+    private static Logger logger = Logger.getLogger(ChangePassword.class);
 
-		  public ActionForward execute(ActionMapping mapping,
-								ActionForm form,
-								HttpServletRequest request,
-								HttpServletResponse response) throws java.lang.Exception {
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) throws java.lang.Exception {
 
 
-					 ChangePasswordForm cpForm = (ChangePasswordForm) form;
-					 
-					 ActionMessages errors = new ActionMessages();
+        ChangePasswordForm cpForm = (ChangePasswordForm) form;
+        ActionMessages errors = new ActionMessages();
 
-					 logger.debug("In change password");
 
-					 if (cpForm.getUserId() != null && cpForm.getOldPassword() != null && cpForm.getNewPassword() != null) {
-								try {
-										  if (DbUtil.isRegisteredEmail(cpForm.getUserId()) != true) {
-													 errors.add(ActionMessages.GLOBAL_MESSAGE,new ActionMessage("error.aim.invalidUserId"));
-													 saveErrors(request,errors);
-													 return mapping.getInputForward();
-										  }
+        if (cpForm.getUserId() != null && cpForm.getOldPassword() != null && cpForm.getNewPassword() != null) {
+            try {
+                if (! DbUtil.isRegisteredEmail(cpForm.getUserId())) {
+                    errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("error.aim.invalidUserId"));
+                    saveErrors(request, errors);
+                    return mapping.getInputForward();
+                }
 
-										  if (DbUtil.isCorrectPassword(cpForm.getUserId(),cpForm.getOldPassword()) != true) {
-													 errors.add(ActionMessages.GLOBAL_MESSAGE,new ActionMessage("error.aim.invalidPassword"));
-													 saveErrors(request,errors);
-													 return mapping.getInputForward();
-										  }
-										  
-										  DbUtil.updatePassword(cpForm.getUserId(),
-																cpForm.getOldPassword(),
-																cpForm.getNewPassword());
-								} catch (Exception e) {
-										  errors.add(ActionMessages.GLOBAL_MESSAGE,
-																new ActionMessage("error.aim.cannotChangePassword"));
-										  saveErrors(request,errors);
-										  return mapping.getInputForward();
-								}
-								return mapping.findForward("success");
-					 } else {
-								return mapping.findForward("changePassword");
-					 }
-		  }
+                if (! DbUtil.isCorrectPassword(cpForm.getUserId(), cpForm.getOldPassword())) {
+                    errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("error.aim.invalidPassword"));
+                    saveErrors(request, errors);
+                    return mapping.getInputForward();
+                }
+
+                DbUtil.updatePassword(cpForm.getUserId(),
+                        cpForm.getOldPassword(),
+                        cpForm.getNewPassword());
+
+
+            } catch (Exception e) {
+                errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("error.aim.cannotChangePassword"));
+                saveErrors(request, errors);
+                return mapping.getInputForward();
+            }
+            return mapping.findForward("success");
+        } else {
+            return mapping.findForward("changePassword");
+        }
+    }
 }

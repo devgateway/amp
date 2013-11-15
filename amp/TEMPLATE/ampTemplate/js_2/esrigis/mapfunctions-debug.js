92abcdf2b01b90faacdf612e836325abe6356155
ddimunzio <ddimunzio@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2013-11-15 20:42:00 +0000

    AMP-16311
    git-svn-id: file:///svn/branches/AMP_2_4_RELEASE@21389 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/js_2/esrigis/mapfunctions-debug.js
+++ amp/TEMPLATE/ampTemplate/js_2/esrigis/mapfunctions-debug.js
@@ -433,7 +433,7 @@ function findbydistance(evt) {
  */
 function showStInfoWindow() {
 	searchactive = false;
-	alert('Ion!');
+	//alert('Ion!');
 	hideTooltip();
 	var content =
 	    "<table border='0' width='100%' cellpadding='0' cellspacing='0' style='border: 1px solid gray;font-size: 10px;'>"

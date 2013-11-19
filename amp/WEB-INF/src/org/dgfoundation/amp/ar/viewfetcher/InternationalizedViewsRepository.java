392c27a572d82c156c3ee8dac8f33b9c4c82680d
cdolghier <cdolghier@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2013-11-19 13:03:36 +0000

    AMP-15571
    added support for pledges views' translations
    
    
    git-svn-id: file:///svn/branches/AMP_2_7_RELEASE@21412 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/ar/viewfetcher/InternationalizedViewsRepository.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/ar/viewfetcher/InternationalizedViewsRepository.java
@@ -248,9 +248,17 @@ public class InternationalizedViewsRepository {
 				addViewDef(this, new I18nViewDescription("v_pledges_donor_type").
 						addColumnDef(new I18nViewColumnDescription("org_type", "org_type_id", AmpOrgType.class, "orgType")));
 				
-				// !!! V_PLEDGES_FUNDING !!! \\
-				// !!! ?? V_PLEDGES_FUNDING_ST ?? !!! \\
+				addViewDef(this, new I18nViewDescription("v_pledges_funding").
+						addColumnDef(new I18nViewColumnDescription("org_name", "org_id", AmpOrganisation.class, "name")).
+						addColumnDef(new I18nViewColumnDescription("p_sectorname", "p_sector_id", AmpSector.class, "name")).
+						addColumnDef(new I18nViewColumnDescription("region", "region_id", AmpCategoryValueLocations.class, "name"))
+						);
 
+				addViewDef(this, new I18nViewDescription("v_pledges_funding_st").
+						addColumnDef(new I18nViewColumnDescription("org_grp_name", "amp_org_grp_id", AmpOrgGroup.class, "orgGrpName")).
+						addColumnDef(new I18nViewColumnDescription("donor_type_name", "org_type_id", AmpOrgType.class, "orgType")));
+
+				
 				addViewDef(this, new I18nViewDescription("v_pledges_programs").
 						addColumnDef(new I18nViewColumnDescription("name", "amp_program_id", AmpTheme.class, "name")));
 

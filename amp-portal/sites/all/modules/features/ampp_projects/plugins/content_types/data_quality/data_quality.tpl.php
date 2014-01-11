d058e0cc3e90787ad1aab789008df001d2ba42c0
vamirbekyan <vamirbekyan@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-01-11 22:47:07 +0000

    adding to the trunk
    git-svn-id: file:///svn/branches/AMP_2_4_RELEASE@21808 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp-portal/sites/all/modules/features/ampp_projects/plugins/content_types/data_quality/data_quality.tpl.php
@@ -0,0 +1,18 @@
+<?php if(!empty($quality_records)): ?>
+<div class="data-quality"> 
+  <div class="data-quality-header">
+    <div class="data-quality-column-1"><?php print t('Donor');?></div>
+    <div class="data-quality-column-2"><?php print t('Last update');?></div>
+  </div>
+  <?php $index = 1;?>
+  <?php foreach ($quality_records as $name => $date): ?>
+  <?php $class = ($index % 2 == 0) ? 'even' : 'odd';?>
+  <div class="data-quality-row-<?php print $class?>">
+    <div class="data-quality-column-1"><?php print $name;?></div>
+    <div class="data-quality-column-2"><?php print $date;?></div>
+  </div>
+  <?php $index++;?>
+  <?php endforeach;?>
+</table>
+ 
+<?php endif;?>

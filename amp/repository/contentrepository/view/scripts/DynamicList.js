8473657ea0e73012b8a2b173e53b99218b184a7a
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-01-15 18:34:56 +0000

    AMP-16798 Reset filters button broken in documents/resources
    git-svn-id: file:///svn/branches/AMP_2_7_RELEASE@21896 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/repository/contentrepository/view/scripts/DynamicList.js
+++ amp/repository/contentrepository/view/scripts/DynamicList.js
@@ -244,7 +244,19 @@ AbstractDynamicList.prototype.resetFilterData		= function (shouldRetrieveFilters
 	    
 		
 		}
-	    
+	
+	var unfiltered = document.getElementsByTagName("input"),
+        i = unfiltered.length,
+        input;
+    while(i--)
+    {
+        input = unfiltered[i];
+        if (!input.type || input.type === 'text')
+        {
+            input.value="";
+        }
+    }
+	
 	
 };
 

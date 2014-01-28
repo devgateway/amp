9c4b284c9843625ed5417a3a4ea49dba6be23ea2
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-01-28 20:26:47 +0000

    AMP-16580. Do not add a location if it already exists. Optimize existing code
    git-svn-id: file:///svn/branches/AMP_2_7_RELEASE@22023 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/fundingpledges/action/PledgeLocationSelected.java
+++ amp/WEB-INF/src/org/digijava/module/fundingpledges/action/PledgeLocationSelected.java
@@ -3,10 +3,7 @@
  */
 package org.digijava.module.fundingpledges.action;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import org.apache.log4j.Logger;
 import org.apache.struts.action.Action;
@@ -14,11 +11,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.digijava.module.aim.dbentity.AmpCategoryValueLocations;
-import org.digijava.module.aim.dbentity.AmpLocation;
-import org.digijava.module.aim.form.EditActivityForm;
-import org.digijava.module.aim.helper.Location;
 import org.digijava.module.aim.util.DynLocationManagerUtil;
-import org.digijava.module.categorymanager.util.CategoryConstants;
 import org.digijava.module.fundingpledges.dbentity.FundingPledgesLocation;
 import org.digijava.module.fundingpledges.form.PledgeForm;
 
@@ -30,27 +23,38 @@ public class PledgeLocationSelected extends Action {
 			javax.servlet.http.HttpServletRequest request,
 			javax.servlet.http.HttpServletResponse response) throws Exception {
 		
-		PledgeForm plForm 	= (PledgeForm) form;
-		Long [] userSelectedLocs	= plForm.getUserSelectedLocs(); 
-		if ( userSelectedLocs != null ) {
-			for (int i=0; i<userSelectedLocs.length; i++) {
-				AmpCategoryValueLocations ampCVLocation		= DynLocationManagerUtil.getLocation( userSelectedLocs[i], false);
-				FundingPledgesLocation fpl = new FundingPledgesLocation();
-				fpl.setLocation(ampCVLocation);
-				if ( plForm.getSelectedLocs() == null ){
-					plForm.setSelectedLocs( new ArrayList<FundingPledgesLocation>() );
-				}
-				if ( !plForm.getSelectedLocs().contains(fpl) ){
-					if (plForm.getSelectedLocs().size()==0) {
-						fpl.setLocationpercentage(100f);
-					} else {
-						fpl.setLocationpercentage(0f);
-					}
-					plForm.getSelectedLocs().add(fpl);
-				}
-			}
+		PledgeForm plForm = (PledgeForm) form;
+		Long[] userSelectedLocs = plForm.getUserSelectedLocs();
+		if (userSelectedLocs != null) {
+            List<Long> locsIds = getSelectedLocIds(plForm);
+            for (Long userSelectedLoc : userSelectedLocs) {
+                if (! locsIds.contains(userSelectedLoc)) {
+                    AmpCategoryValueLocations ampCVLocation = DynLocationManagerUtil.getLocation(userSelectedLoc, false);
+                    FundingPledgesLocation fpl = new FundingPledgesLocation();
+                    fpl.setLocation(ampCVLocation);
+                    if (plForm.getSelectedLocs() == null) {
+                        plForm.setSelectedLocs(new ArrayList<FundingPledgesLocation>());
+                    }
+                    if (plForm.getSelectedLocs().size() == 0) {
+                        fpl.setLocationpercentage(100f);
+                    } else {
+                        fpl.setLocationpercentage(0f);
+                    }
+                    plForm.getSelectedLocs().add(fpl);
+                }
+            }
 		}
 		
 		return mapping.findForward("forward");
 	}
+
+    private List<Long> getSelectedLocIds(PledgeForm plForm) {
+        List<Long> selectedLocs = new ArrayList<Long>();
+        if (plForm.getSelectedLocs() != null) {
+            for (FundingPledgesLocation location : plForm.getSelectedLocs()) {
+                selectedLocs.add(location.getLocation().getId());
+            }
+        }
+        return selectedLocs;
+    }
 }

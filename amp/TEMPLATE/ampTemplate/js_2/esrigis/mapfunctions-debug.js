25fae9fd158e5c37b85144b421d66df5d22e3b8e
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-02-20 23:03:03 +0000

    AMP-16862
    git-svn-id: file:///svn/branches/AMP_2_8_RELEASE@22291 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/js_2/esrigis/mapfunctions-debug.js
+++ amp/TEMPLATE/ampTemplate/js_2/esrigis/mapfunctions-debug.js
@@ -643,6 +643,31 @@ function createPeaceBuildingFeatureLayer() {
  */
 var indicatoractive = false;
 function toggleindicatormap(id) {
+	//modifications start
+	if (highlightson){
+		closeHide("highlightLegend");
+		highlightson =false;
+	}
+	if(nationalactive)
+	{
+		togglenational();
+	}
+	if(id==7 && map.getLayer("indicator_8").visible)
+	{
+		var functionalayer = map.getLayer('countrymap');
+		map.getLayer("indicator_8").hide();
+		$('#indicatorLegend > .legendContent').hide('slow');
+		functionalayer.show();
+		indicatoractive = false;
+	}
+	else if(id==8 && map.getLayer("indicator_7").visible){
+		var functionalayer = map.getLayer('countrymap');
+		map.getLayer("indicator_7").hide();
+		$('#indicatorLegend > .legendContent').hide('slow');
+		functionalayer.show();
+		indicatoractive = false;
+	}
+//modifications end	
 	var layer = map.getLayer("indicator_" + id);
 	var functionalayer = map.getLayer('countrymap');
 	if (layer.visible) {
@@ -980,6 +1005,19 @@ function getActivities(clear) {
  */
 
 function getNationalActivities() {
+	//update starts here
+	if(map.getLayer("indicator_8").visible){
+		toggleindicatormap('8');
+	}
+	if(map.getLayer("indicator_7").visible){
+		toggleindicatormap('7');
+	}
+	
+	if (highlightson){
+		closeHide("highlightLegend");
+		highlightson =false;
+	}
+//and ends here	
 	if(!nationalactive){
 		showLoading();
 		var xhrArgs = {
@@ -1275,6 +1313,19 @@ var implementationLevel = [ {
 } ];
 
 function getHighlights(level) {
+	//modifications start
+	if(map.getLayer("indicator_8").visible){
+			toggleindicatormap('8');
+		}
+		if(map.getLayer("indicator_7").visible){
+			toggleindicatormap('7');
+		}
+		if(nationalactive)
+		{
+			togglenational();
+		}
+		
+	//modifications end	
 	if (highlightson){
 		closeHide("highlightLegend");
 		highlightson =false;

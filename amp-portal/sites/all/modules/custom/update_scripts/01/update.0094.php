617f52b6b39957bf53e5dfbbc81d41000fa2cacc
wschneider <wschneider@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-03-07 17:06:50 +0000

    AMP-17098: Disable devel.module
    git-svn-id: file:///svn/branches/AMP_2_7_RELEASE@22461 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp-portal/sites/all/modules/custom/update_scripts/01/update.0094.php
@@ -0,0 +1,10 @@
+<?php
+
+// Provide a list of modules to be disabled and uninstalled.
+$modules = array(
+  'devel',
+    'devel_generate',
+    'devel_node_access',
+);
+// Uninstall modules
+_us_install_modules($modules);

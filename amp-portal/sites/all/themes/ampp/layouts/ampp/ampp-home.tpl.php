71f8b576bd8392d83601d899c6b387fa2be8c5bc
wschneider <wschneider@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-04-08 14:27:47 +0000

    AMP-15286: Fix homepage html template to prevent style issues.
    git-svn-id: file:///svn/branches/AMP_2_7_RELEASE@22794 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp-portal/sites/all/themes/ampp/layouts/ampp/ampp-home.tpl.php
+++ amp-portal/sites/all/themes/ampp/layouts/ampp/ampp-home.tpl.php
@@ -1,27 +1,30 @@
 <?php
-// $Id: ampp-home.tpl.php,v 1.4 2012/02/06 19:17:31 vamirbekyan Exp $
 ?>
 <div class="ampphome-layout" <?php if (!empty($css_id)) { print ' id="' . $css_id . '"'; } ?>>
-	<div class="ampphome-slideshow">
-		<div class="ampphome-slideshow-container"><?php print $content['slideshow']; ?></div>
-		<div class="clear"></div>
-	</div>
-  
-	<div id="col1">
-		<div class="ampphome-search"><div class="ampphome-search-container"><?php print $content['search']; ?></div><div class="clear"></div></div>
-		<div class="ampphome-donor-map">
-			<div class="ampphome-donor-container"><?php print $content['donor_profile']; ?></div>
-			<div class="ampphome-map-container"><?php print $content['map']; ?></div>
-		</div>
-		<div class="ampphome-contact-social">
-			<div class="ampphome-contact-container"><?php print $content['quick_contact']; ?></div>
-		</div>
-	</div>
+  <div class="ampphome-slideshow clearfix">
+    <div class="ampphome-slideshow-container"><?php print $content['slideshow']; ?></div>
+  </div>
 
-	<div id="col2"> 
-		<div class="ampphome-news">
-			<div class="ampphome-news-slideshow-container"><?php print $content['news_slideshow']; ?></div>
-		</div>
-	</div>
+  <div class="clearfix">
+    <div id="col1">
+      <div class="ampphome-search clearfix">
+        <div class="ampphome-search-container"><?php print $content['search']; ?></div>
+      </div>
+
+      <div class="ampphome-donor-map clearfix">
+        <div class="ampphome-donor-container"><?php print $content['donor_profile']; ?></div>
+        <div class="ampphome-map-container"><?php print $content['map']; ?></div>
+      </div>
+
+      <div class="ampphome-contact-social clearfix">
+        <div class="ampphome-contact-container"><?php print $content['quick_contact']; ?></div>
+      </div>
+    </div>
+
+    <div id="col2">
+      <div class="ampphome-news">
+        <div class="ampphome-news-slideshow-container"><?php print $content['news_slideshow']; ?></div>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="clear"></div>

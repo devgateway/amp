d6ded694b71086373fc4bd39eecaa6054dbbd142
wschneider <wschneider@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-04-15 10:32:33 +0000

    AMP-17298: Fix the public activities filter and the date filter for the commitments_report pane and change the base table in order to optimize the query. Translate organization names.
    git-svn-id: file:///svn/branches/AMP_2_7_RELEASE@22868 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp-portal/sites/all/modules/features/ampp_projects/plugins/content_types/commitments_report/commitments_report.tpl.php
+++ amp-portal/sites/all/modules/features/ampp_projects/plugins/content_types/commitments_report/commitments_report.tpl.php
@@ -1,23 +1,27 @@
 <?php if(!empty($commitments_report)): ?>
-<table class="commitments_report">
-  <tr class="commitments_report-header">
-    <td class="commitments_report-column-1"><?php print t('Donor Group');?></td>
-    <td class="commitments_report-column-2"><?php print t('Total Costs');?></td>
-  </tr>
-  <?php $class = 'even'; ?>
-  <?php for($i = 0; $i < sizeof($commitments_report); $i++):?>
-  <?php
-    $class = ($i % 2 == 0) ? 'even' : 'odd';
-  ?>
-  <tr class="commitments_report-row-<?php print $class?>">
-    <td class="commitments_report-column-1"><?php print "{$commitments_report[$i]['name']} ({$commitments_report[$i]['count']}) ";?></td>
-    <td class="commitments_report-column-2"><?php print "{$commitments_report[$i]['amount']}" . $curr_symbol;?></td>
-  </tr>
+<table class="commitments-report-table">
+  <thead>
+    <tr>
+      <th class="column-one"><?php print t('Donor Group'); ?></th>
+      <th class="column-two"><?php print t('Total Costs'); ?></th>
+    </tr>
+  </thead>
 
-  <?php endfor;?>
-  <tr class="commitments_report-row-<?php print $class?>">
-    <td class="commitments_report-column-1"><?php print t('TOTAL (!total)', array('!total' => $total_org_count))?></td>
-    <td class="commitments_report-column-2"><?php print $total_amount . $curr_symbol;?></td>
-  </tr>
+  <tbody>
+    <?php for($i = 0; $i < sizeof($commitments_report); $i++): ?>
+      <?php $class = ($i % 2 == 0) ? 'even' : 'odd'; ?>
+      <tr class="row row-<?php print $class?>">
+        <td class="column-one"><?php print "{$commitments_report[$i]['name']} ({$commitments_report[$i]['count']}) "; ?></td>
+        <td class="column-two"><?php print "{$commitments_report[$i]['amount']}" . $currency_symbol; ?></td>
+      </tr>
+    <?php endfor; ?>
+  </tbody>
+
+  <tfoot>
+    <tr>
+      <th class="column-one"><?php print t('TOTAL (!total)', array('!total' => $total_org_count)); ?></th>
+      <th class="column-two"><?php print $total_amount . $currency_symbol; ?></th>
+    </tr>
+  </tfoot>
 </table>
 <?php endif;?>

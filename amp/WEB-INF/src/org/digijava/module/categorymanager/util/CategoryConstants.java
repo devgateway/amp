71be2c952daf5423d36e98bcd3e9ff815ff2eaab
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-05-21 16:34:49 +0000

    AMP-17374 Budget filtering issue in Gis map
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@23233 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/categorymanager/util/CategoryConstants.java
+++ amp/WEB-INF/src/org/digijava/module/categorymanager/util/CategoryConstants.java
@@ -184,6 +184,9 @@ public class CategoryConstants {
 		new CategoryConstants.HardCodedCategoryValue("implementation_location", "Zone", true);
 	public static final HardCodedCategoryValue IMPLEMENTATION_LOCATION_DISTRICT	= 
 		new CategoryConstants.HardCodedCategoryValue("implementation_location", "District", true);
+    public static final HardCodedCategoryValue IMPLEMENTATION_LOCATION_ALL	=
+            new CategoryConstants.HardCodedCategoryValue("implementation_location", "All", true);
+
 	public static final HardCodedCategoryValue IMPLEMENTATION_LOCATION_SECTOR	= 
 			new CategoryConstants.HardCodedCategoryValue("implementation_location", "Sector Location", true);
 	
@@ -193,11 +196,13 @@ public class CategoryConstants {
 		 new CategoryConstants.HardCodedCategoryValue("implementation_level", "National", true);
 	public static final HardCodedCategoryValue IMPLEMENTATION_LEVEL_REGIONAL = 
 		 new CategoryConstants.HardCodedCategoryValue("implementation_level", "Regional", true);
-	 
+
 	 public static final HardCodedCategoryValue ACTIVITY_BUDGET_ON = 
 		 new CategoryConstants.HardCodedCategoryValue("activity_budget", "On Budget", true);
 	 public static final HardCodedCategoryValue ACTIVITY_BUDGET_OFF = 
 		 new CategoryConstants.HardCodedCategoryValue("activity_budget", "Off Budget", true);
+	 public static final HardCodedCategoryValue ACTIVITY_TREASURY = 
+			 new CategoryConstants.HardCodedCategoryValue("activity_budget", "Treasury", true);
 
 	 public static final HardCodedCategoryValue ADJUSTMENT_TYPE_ACTUAL = 
 		 new CategoryConstants.HardCodedCategoryValue("adjustment_type", "Actual", true);

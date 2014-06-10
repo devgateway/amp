cd7cf2cd245645d64fa88061268ce70865a9c3db
nmandrescu <nmandrescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-06-10 14:53:57 +0000

    AMP-17617 added a method to retrieve import configurations list non limited by pagination and now it will be used during import so that the list is not truncated
    git-svn-id: file:///svn/branches/AMP_2_8_RELEASE@23553 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/dataExchange/util/SourceSettingDAO.java
+++ amp/WEB-INF/src/org/digijava/module/dataExchange/util/SourceSettingDAO.java
@@ -53,6 +53,10 @@ public class SourceSettingDAO {
 	}
 	
 	public List<DESourceSetting> getPagedAmpSourceSettingsObjects(int fromIndex, String sortBy) {
+		return getAmpSourceSettingsObjects(fromIndex, sortBy, true);
+	}
+	
+	public List<DESourceSetting> getAmpSourceSettingsObjects(int fromIndex, String sortBy, boolean isPaged) {
 		try{
 			String queryString 	= "select ss from "	+ DESourceSetting.class.getName() + " ss";
 			//sort
@@ -75,7 +79,8 @@ public class SourceSettingDAO {
 			}
 			Query query			= hbSession.createQuery(queryString);
 			query.setFirstResult(fromIndex);
-			query.setMaxResults(DEConstants.RECORDS_AMOUNT_PER_PAGE);
+			if( isPaged )
+				query.setMaxResults(DEConstants.RECORDS_AMOUNT_PER_PAGE);
 //			if(resultNum!=-1){
 //				query.setMaxResults(resultNum);
 //			}

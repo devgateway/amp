fd7b772992ec047b06d3886db288b2f09d23a339
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-06-17 18:48:00 +0000

    AMP-17579: Start mocking out a little of Filter Model and more template HTML in views.
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@23653 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/tools/Filters/views/FilterView.js
@@ -0,0 +1,26 @@
+define(
+  [
+    "underscore",
+    "backbone",
+    'js/amp/tools/Filters/models/Model.js'
+  ],
+  function (_, Backbone, FilterModel) {
+    'use strict';
+
+    var View = Backbone.View.extend({
+
+      baseTemplate: _.template(BaseTemplate),
+      template: _.template(Template),
+
+      initialize: function () {
+        this.model = new FilterModel();
+      },
+
+      render: function () {
+
+      }
+    });
+
+    return View;
+  }
+);
--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/tools/Filters/views/FiltersView.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/tools/Filters/views/FiltersView.js
@@ -3,20 +3,36 @@ define(
     "underscore",
     "backbone",
     "text!" + APP_ROOT + "/amp/tools/BaseTool/template.html",
-    "text!" + APP_ROOT + "/amp/tools/Filters/templates/template.html"
+    "text!" + APP_ROOT + "/amp/tools/Filters/templates/template.html",
+    'js/amp/tools/Filters/models/Filter.js'
   ],
-  function (_, Backbone, BaseTemplate, Template) {
+  function (_, Backbone, BaseTemplate, Template, FilterModel) {
     'use strict';
 
-    var LayersView = Backbone.View.extend({
+    var View = Backbone.View.extend({
 
       baseTemplate: _.template(BaseTemplate),
       template: _.template(Template),
       title: "Filters",
       iconClass: "ampicon-filters",
       description: "Apply filters to the map.",
+ 
+      //Intentionally not a 'collection' because it won't be populated by the API, and it holds views not models.
+      filterViews:[],
 
       initialize: function () {
+
+        //TODO: Create all the custom views we need, and push them into filterViews:
+        //      These views should inherit from a parent view.
+        //        organizationsFilter
+        //        locationsFilter
+        //        yearsFilter
+        //        etc...
+
+
+        //TODO: register listener for FILTER_CHANGED event, then iterate over 
+        //      filterViews and call createFilterJSON on each model
+        //      create master filter object and pass it to the map. to call api and re-render.
 
       },
 
@@ -24,7 +40,6 @@ define(
       render: function () {
 
           //render base
-          // TODO: abstract some of this into a new BaseTemplate view:
           this.$el.html(this.baseTemplate({
             title: this.title, 
             titleID:this.title.replace(/ /g,''), 
@@ -35,9 +50,11 @@ define(
           // add content.
           this.$el.find('.content').html(this.template({title: this.title}));
 
+          // TODO: Iterate over filterViews and render them all.
+
       }
     });
 
-    return LayersView;
+    return View;
   }
 );

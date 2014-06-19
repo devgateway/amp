0cfb819e04fbe9021ff372865741f9244fafbf8d
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-06-19 17:37:43 +0000

    AMP-17579: Added mock logic to data uality refactor less a little
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@23714 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/dataquality/models/dataquality-model.js
@@ -0,0 +1,31 @@
+define(
+  [
+    'underscore',
+    'backbone'
+  ],
+  function (_, backbone ) {
+    'use strict';
+
+
+    var Model = Backbone.Model.extend({      
+        defaults: {
+        },
+        
+        initialize: function(){            
+        },
+
+        // returns a data quality score from 1-100
+        getScore: function(){
+          //TODO: emit an event, or some other way of finding all relevant models.
+          //      and use responses to calculate an aproximate dataQuality score
+
+          //temporary stub, random score:
+          return Math.floor(Math.random()*100);
+        },
+        
+
+    });
+
+    return Model;
+  });
+
--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/dataquality/views/dataquality-view.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/dataquality/views/dataquality-view.js
@@ -2,23 +2,51 @@ define(
   [
     'underscore',
     'backbone',
-    'text!' + APP_ROOT + '/amp/dataquality/templates/dataquality-template.html'
+    'text!' + APP_ROOT + '/amp/dataquality/templates/dataquality-template.html',
+    APP_ROOT + '/amp/dataquality/models/dataquality-model.js'
   ],
-  function (_, Backbone, Template) {
+  function (_, Backbone, Template, DataQualityModel) {
     'use strict';
 
     var DataQuality = Backbone.View.extend({
+      minGoodScore: 66,
+      minWarningScore: 33,
+      currentScore: -1, // Should be between 0 and 100 for 'real' values
 
       template: _.template(Template),
 
       initialize: function () {
-
+        this.model = new DataQualityModel();
       },
 
       render: function () {
-        console.log('dataquality render');
+
         this.$el.html(this.template({}));
-        console.log('dataquality render done');
+        this.updateScore();
+      },
+
+      // TODO make sure updateScore is triggered by map filter changes.
+      updateScore: function(){
+        this.currentScore = this.model.getScore();
+        console.log('score: ' + this.currentScore);
+        var styleClass = 'info';
+
+
+        if(this.currentScore >= this.minGoodScore && this.currentScore <= 100){
+          styleClass = 'success';
+
+        } else if(this.currentScore >= this.minWarningScore && this.currentScore < this.minGoodScore){
+          styleClass ='warning';
+
+        } else if(this.currentScore >= 0 && this.currentScore < this.minWarningScore){
+          styleClass = 'danger';
+
+        } else {
+          console.warn("data quality score is outside of acceptable range.");
+        }
+
+        this.$('.label').removeClass().addClass('label label-' + styleClass);
+        this.$('.glyphicon').removeClass().addClass('glyphicon glyphicon-record text-' + styleClass);
       }
     });
 

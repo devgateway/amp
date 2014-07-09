b4c65b7a2183570785996281f7f37b814ff74b02
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-07-09 17:34:45 +0000

    AMP-17675
    git-svn-id: file:///svn/branches/AMP_2_8_RELEASE@23999 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/fields/AmpTextAreaFieldPanel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/fields/AmpTextAreaFieldPanel.java
@@ -110,7 +110,6 @@ public class AmpTextAreaFieldPanel extends AmpFieldPanel<String> {
                     "var ua = window.navigator.userAgent;"+
                     "var msie = ua.indexOf('MSIE ');"+
                     "if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\\:11\\./)  || window.ActiveXObject) {  "+
-                    "console.log('IE');"+
                     "$('#"+ closeLink.getMarkupId() +"').click();"+
 				     "setTimeout(function(){showRichEditor()"+
 				     "},1500);}" +

720f694f02ff49d301d6bb7db2ec8021498bc12e
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-07-15 15:15:44 +0000

    AMP-17877: remove 'frame' code for now since we're only doing unit tests
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@24042 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/entry.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/entry.js
@@ -1,29 +1,5 @@
 var QUnit = require('qunitjs');
-var runner$ = require('jquery');
-var frame = require('./util/frame.js');
-
-// Frame code currently not working sinze switch from dojo. 
-// Probably not needed for unit testing.
-  // register all of the available test iframes
-  var $iFrames = runner$('.test-frame');
-  $iFrames.each(function(i, frameEl) {
-    var $iFrame = runner$(frameEl);
-    frame.registerIFrame($iFrame);
-  });
-
-  // // register the progress indicators
-  var table = runner$('#test-frames-status');
-  frame.registerStatus(function(statusObj) {
-    runner$('.tests-queued', table).text(statusObj.queued);
-    runner$('.tests-running', table).text(statusObj.running);
-    runner$('.tests-finished', table).text(statusObj.finished);
-  });
-
-
 
 //Run tests:
-//require('./scripts/basemap.js');
-require('./scripts/map.js');
+require('./scripts/basemap.js');
 
-// start testing!
- frame.runTests();

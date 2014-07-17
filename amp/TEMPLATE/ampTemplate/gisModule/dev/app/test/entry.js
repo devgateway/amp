0547a4dcf270f2a187c116b83a8d0e058dc129a3
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-07-17 20:46:53 +0000

    AMP-17877: First qUnit tests. Test Basemap collection
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@24115 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/entry.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/entry.js
@@ -1,4 +1,3 @@
-//var QUnit = require('qunitjs');
 
 //Run tests:
 require('./scripts/test-basemap.js');
--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/scripts/test-basemap.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/scripts/test-basemap.js
@@ -1,9 +1,47 @@
 
-// This doesn't work...
-var basemapCollection = require('../../js/amp/map/models/basemap-collection');
+require('../../mock-api/fakeServer.js');
 
-var basemapModel = require('../../js/amp/map/models/basemap-model');
 
-QUnit.test( "Basemap test", function( assert ) {
-  assert.ok( 2 == "2", "2s" ); 
+var BasemapCollection = require('../../js/amp/map/collections/basemap-collection');
+
+
+
+QUnit.test( 'Basemap Collection test', function( assert ) {
+  var basemapCollection =  new BasemapCollection();
+
+  assert.ok( basemapCollection.length > 5, 'Has atleast 6 maps' );
+
+
+  var defaultMap = basemapCollection.getBasemap();
+  console.log('defaultMap',defaultMap);
+  assert.ok( defaultMap, 'Loaded defaultMap' ); 
+  assert.ok( defaultMap.get('esriId') === 'Gray' 
+        &&  defaultMap.get('source') === 'esri' 
+        &&  defaultMap.get('selected') == true, 'defaultMap is selected and gray' ); 
+
+
+  var grayMap = basemapCollection.getBasemap('Gray');
+  assert.ok( grayMap, 'Loaded Gray map' ); 
+  assert.ok( grayMap.get('esriId') === 'Gray' &&  grayMap.get('source') === 'esri', 'Gray map loaded correct values' ); 
+
+
+  var emptyBasemap = basemapCollection.getBasemap('Empty Basemap');
+  console.log(emptyBasemap);
+  assert.ok( emptyBasemap, 'Loaded  Empty Basemap' ); 
+  assert.ok( emptyBasemap.get('id') == 'Empty Basemap' 
+          &&  emptyBasemap.get('source') == null  
+          &&  !emptyBasemap.get('selected'), 'Empty Basemap has correct values' ); 
+
+
+  var badBasemap = basemapCollection.getBasemap('fake basemap name');
+  assert.ok( !badBasemap , 'bad Basemap request returns something falsey' ); 
+
+
+  // test select
+  basemapCollection.selectBasemap('Empty Basemap');
+  var newDefaultMap = basemapCollection.getBasemap();
+  assert.ok( newDefaultMap.get('id') === 'Empty Basemap'
+        &&  newDefaultMap.get('selected') == true, 'select empty basemap' ); 
+
+
 });

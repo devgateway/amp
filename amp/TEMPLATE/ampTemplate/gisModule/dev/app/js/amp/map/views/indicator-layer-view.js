c94cb1e50dde06b0d5206e80125afe8fecbe81ab
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-07-21 21:37:14 +0000

    AMP-17575 Add geoJSON type indicator layer.
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@24170 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/views/indicator-layer-view.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/views/indicator-layer-view.js
@@ -32,38 +32,81 @@ module.exports = Backbone.View.extend({
 
     if(indicator){
       this.visible = true;
-      var property = indicator.get('property');
-      var maxValue = 0;
 
-
-      this.indicatorLayer = L.esri.featureLayer(indicator.get('featurePath'),{
-        simplifyFactor: 0.9,
-        style:  function (feature) {
-            maxValue = Math.max(maxValue, feature.properties[property]);
-            return {color: 'blue',  weight: 1, opacity:0.4 };
-          },
-      }).addTo(self.map);
-
-
-      // set all styles after they are on page and we know the max
-      this.indicatorLayer.on('load', function(){
-        self._updateIndicatorStyles(maxValue, property);
-      });
+      if (indicator.get('type') === 'arcgis'){
+        this._loadArcGISLayer(indicator);
+      } else if(indicator.get('type') === 'geoJSON'){
+        this._loadGeoJSONLayer(indicator);        
+      } else{
+        console.warn('No type matches this indicator', indicator);
+      }
     }
   },
 
+  _loadArcGISLayer: function(indicator){
+    var self = this;
+    var property = indicator.get('property');
+    var maxValue = 0;
 
+    this.indicatorLayer = L.esri.Layers.featureLayer(indicator.get('featurePath'),{
+      simplifyFactor: 0.9,
+      style:  function (feature) {
+          maxValue = Math.max(maxValue, feature.properties[property]);
+          return {color: 'blue',  weight: 1, opacity:0.4 };
+        },
+    }).addTo(self.map);
 
-  // TODO: moveindicator layer should be own view under map.
-  _updateIndicatorStyles: function(maxValue, property){
+    // load is triggered after every pan, because on pan it asks for more feature info from server.
+    // set all styles after they are on page and we know the max
+    this.indicatorLayer.on('load', function(){
+      self._updateESRIIndicatorStyles(maxValue, property);
+    }); 
+  },
+
+  _loadGeoJSONLayer: function(indicator) {
+    var self = this;
+    var property = indicator.get('property');
+    var maxValue = 0;
+
+    //TODO: may need to apply API base url to begining of path
+    $.get(indicator.get('featurePath')).then(function(geoJsonData){
+
+      // create layer
+      self.indicatorLayer = L.geoJson(
+        geoJsonData, {
+        style:  function (feature) {
+            maxValue = Math.max(maxValue, feature.properties[property]);         
+            return {color: 'blue',  weight: 1,  opacity:0.4, fillOpacity:0.5 };
+          }
+      }).addTo(self.map);
+      self._updateIndicatorStyles(maxValue, property);
+    });
+
+  },
+
+  // for esri indicator groups..
+  _updateESRIIndicatorStyles: function(maxValue, property){
     var self = this;
     this.indicatorLayer.eachFeature(function(layer){
       var id = layer.feature.id;
-      var rgb = 'rgb(' + Math.round(Math.min(255,layer.feature.properties[property]*255/maxValue)+60) + ' ,' +
-                          Math.round(Math.min(255,(layer.feature.properties[property]*255/maxValue))) + ',' +
-                          Math.round(Math.min(255,(layer.feature.properties[property]*255/maxValue))) + ')';
+      var colourInt = Math.round(Math.min(255,layer.feature.properties[property]*255/maxValue));
+      var rgb = 'rgb(' + (colourInt + 60) + ' ,' +
+                colourInt + ',' +
+                colourInt + ')';
       self.indicatorLayer.setFeatureStyle(id, {color: rgb, fillColor:rgb, weight: 1, fillOpacity:0.5 });
     });
   },
+
+  // For normal Leaflet feature group
+  _updateIndicatorStyles: function(maxValue, property){
+    this.indicatorLayer.setStyle(function(feature){
+
+      var colourInt = Math.round(Math.min(255, feature.properties[property]*255/maxValue));
+      var rgb = 'rgb(' + (colourInt + 60) + ' ,' +
+                Math.max(colourInt-60,0) + ',' +
+                Math.max(colourInt-60,0) + ')'; 
+      return {color: rgb, fillColor:rgb, weight: 1, fillOpacity:0.5 };
+    });
+  },  
 
 });

171600c1254b40cc15a915f542f7f63783f65e1c
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-08-19 19:53:52 +0000

    AMP-17567: Removed unneeded test.
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@24577 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/entry.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/test/entry.js
@@ -3,4 +3,3 @@ var Qunit = require('qunitjs');
 // Run tests (they execute immediately on require)
 require('./tests/test-basemap.js');
 require('./tests/test-state.js');
-require('./tests/test-api-helper.js');

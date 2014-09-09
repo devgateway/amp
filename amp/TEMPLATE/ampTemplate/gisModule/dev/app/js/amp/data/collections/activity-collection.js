51b2538535063c9c08319e95700003b3be9f323e
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-09-09 16:18:21 +0000

    AMP-18043: fix regression in structures
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@24798 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/collections/activity-collection.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/collections/activity-collection.js
@@ -14,36 +14,36 @@ module.exports = Backbone.Collection
 
   //smart ID fetching, load locally, and only fetch if we don't have the activity.
   getActivites: function (aryOfIDs) {
-  	var deferred = $.Deferred();
+    var deferred = $.Deferred();
 
-  	// check which IDs we have locally.
-  	var matches = [];
-  	this.each(function(activity){
-  		var index = _.indexOf(aryOfIDs, activity.id);
-  		if(index > -1){
-  			matches.push(activity.toJSON());	 // add activity to array
-  			aryOfIDs.splice(index,1);          // remove id from array
-  		}
-  	});
+    // check which IDs we have locally.
+    var matches = [];
+    this.each(function(activity){
+      var index = _.indexOf(aryOfIDs, activity.id);
+      if(index > -1){
+        matches.push(activity.toJSON());   // add activity to array
+        aryOfIDs.splice(index,1);          // remove id from array
+      }
+    });
 
-  	// if there are activities we don't have locally.
-  	if( !_.isEmpty(aryOfIDs)){
-	  	// do an api request to get remaining ones
-			this.url = '/rest/gis/activities/' + aryOfIDs.join(',');			
-		  this.fetch({remove: false}).then(function(newData){
-		  	matches = _.union(matches, newData);
-		  	this.url = '/rest/gis/activities';	// reset url
-		  	deferred.resolve(matches);
-		  }).fail(function(err){
-		  		console.error('failed to get ' + this.url, err);
-		  		this.url = '/rest/gis/activities';	// reset url
-		  		deferred.resolve(matches);
-		  }); 		
-  	} else{
-  		deferred.resolve(matches);
-  	}
+    // if there are activities we don't have locally.
+    if( !_.isEmpty(aryOfIDs)){
+      // do an api request to get remaining ones
+      this.url = '/rest/gis/activities/' + aryOfIDs.join(',');      
+      this.fetch({remove: false}).then(function(newData){
+        matches = _.union(matches, newData);
+        this.url = '/rest/gis/activities';  // reset url
+        deferred.resolve(matches);
+      }).fail(function(err){
+          console.error('failed to get ' + this.url, err);
+          this.url = '/rest/gis/activities';  // reset url
+          deferred.resolve(matches);
+      });     
+    } else{
+      deferred.resolve(matches);
+    }
 
-	  return deferred;
+    return deferred;
   }
 
 });
--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/structures-project-sites-shell-collection-model.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/structures-project-sites-shell-collection-model.js
@@ -18,6 +18,14 @@ var ONAMES = {  // TODO: temp hack until filters have a proper data structure
   73: 'Joint United Nations Programme on HIV/AIDS (UNAIDS)',
   74: 'UNICEF',
   75: 'World Bank',
+  3267: 'Ministère du Plan',
+  3204:  'World Bank',
+  3253: 'Global Partnership for Education',
+  3351: 'CARE International UK',
+  3856:  'Icelandic International Development Agency (ICEIDA)',
+  4192: 'UNICEF',
+  4211: 'Canadian International Development Agency (CIDA)',
+  3570: 'African Development Fund (AFDF)',
   105045712: 'Deutsche Gesellschaft fur Internationale Zusammenarbeit (GIZ)',
   40279598: 'India EXIM Bank',
   24: 'International Fund for Agricultural Development (IFAD)',
@@ -67,26 +75,48 @@ module.exports = Backbone.Model
     return Backbone.Model.prototype.fetch.call(this, options);
   },
 
+  // David replacec this with: _loadActivities
   load: function() {
-    this.activities.load();  // not needed right away, but start it as early as possible
-    return LoadOnceMixin.load.apply(this);
+    //this.activities.load();  // not needed right away, but start it as early as possible
+    
+    return LoadOnceMixin.load.apply(this).then(this._loadActivities);
   },
 
   parse: function(data) {
     // TODO: don't keep data.features around
     data.sites = new Structures(data.features);
+
     return data;
   },
 
+  // Loads all activities for these structures/sites.
+  _loadActivities: function(){
+    var allActivityIds = [];
+    // join all activity ids
+    this.get('sites').each(function(site){
+      allActivityIds = _(allActivityIds).union(allActivityIds,site.get('properties').activity);
+    });
+
+    return this.activities.getActivites(allActivityIds);
+  },
+
   updatePaletteSet: function() {
-    this.activities.load().done(_.bind(function(activities) {
+    var self = this;
+
+    //load the necessary activities.
+    this._loadActivities().done(_.bind(function(activities) {
       var activity;
 
       var orgSites = this.get('sites')
         .chain()
         .groupsBy(function(site) {
-          activity = activities.get(site.get('properties').projectId);
-          return activity.get('matchesFilters').org;
+          // TODO: only grabs first activity...does't handle multiple activities, 
+          // which may be introduced in the future..
+          activity = self.activities.get(site.get('properties').activity[0]); 
+          //TODO: / Phil: organizations is a map of arrays, don't think this code does what we want...
+          // I think we want just organizations[1]  for donor.
+          // I asked Julian about API, returning funny values for org role..
+          return activity.get('matchesFilters').organizations; 
         })
         .map(function(sites, orgId) {
           return {
@@ -100,7 +130,6 @@ module.exports = Backbone.Model
         })
         .reverse()
         .value();
-
       this.palette.set('elements', orgSites);
 
     }, this));

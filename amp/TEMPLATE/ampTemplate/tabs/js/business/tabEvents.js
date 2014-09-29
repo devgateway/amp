ca516f84f2828e46ea0f612138ee79ce85cc4c63
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-09-29 00:05:37 +0000

    AMP-18022 - Added view to show legends region.
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25033 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/tabs/js/business/tabEvents.js
+++ amp/TEMPLATE/ampTemplate/tabs/js/business/tabEvents.js
@@ -2,290 +2,305 @@
 /*https://github.com/icereval/backbone-documentmodel*/
 define([ 'marionette', 'collections/contents', 'models/content', 'views/dynamicContentView', 'text!views/html/filtersWrapperTemplate.html',
 		'text!views/html/filtersItemTemplate.html', 'models/filter', 'collections/filters', 'models/tab',
-		'text!views/html/invisibleTabLinkTemplate.html', 'jqgrid', 'util/columnsMapping' ], function(Marionette, Contents, Content,
-		DynamicContentView, filtersTemplate, filtersItemTemplate, Filter, Filters, Tab, invisibleTabLinkTemplate, jqGrid, columnsMapping) {
+		'text!views/html/invisibleTabLinkTemplate.html', 'jqgrid', 'util/columnsMapping', 'text!views/html/legendsTemplate.html' ],
+		function(Marionette, Contents, Content, DynamicContentView, filtersTemplate, filtersItemTemplate, Filter, Filters, Tab,
+				invisibleTabLinkTemplate, jqGrid, columnsMapping, legendsTemplate) {
 
-	"use strict";
+			"use strict";
 
-	function TabEvents() {
-		// Constructor
-		function TabEvents() {
-		}
-	}
-
-	// Some private method.
-	function putAnimation() {
-		return '<span><img src="/TEMPLATE/ampTemplate/tabs/css/images/ajax-loader.gif"/></span>';
-	}
-
-	function extractMetadata(content) {
-		var Metadata = Backbone.DocumentModel.extend({
-			defausts : {
-				columns : [],
-				measures : [],
-				hierarchies : []
+			function TabEvents() {
+				// Constructor
+				function TabEvents() {
+				}
 			}
-		});
-		var metadata = new Metadata();
-		var metadataJson = content.get('reportMetadata').get('reportSpec');
-		metadata.columns = metadataJson.get('columns');
-		metadata.hierarchies = metadataJson.get('hierarchies');
-		metadata.measures = metadataJson.get('measures');
-		return metadata;
-	}
 
-	function extractFilters(content) {
-		var filters = new Filters();
-		var filtersJson = content.get('reportMetadata').get('reportSpec').get('filters').get('filterRules');
-		$(filtersJson.keys()).each(function(i, item) {
-			var subElement = filtersJson.get(item);
-			if (subElement instanceof Backbone.Collection) {
-				if (item.indexOf('ElementType = ENTITY') > -1) {
-					var name = item.substring(item.indexOf('[') + 1, item.indexOf(']'));
-					var element = subElement.models[0];
-					var content = [];
-					if (element.get('value') != null) {
-						content = element.get('value');
-					} else if (element.get('values') != null) {
-						_.each(element.get('values').models, function(item, i) {
-							content.push(item.get('value'));
+			// Some private method.
+			function putAnimation() {
+				return '<span><img src="/TEMPLATE/ampTemplate/tabs/css/images/ajax-loader.gif"/></span>';
+			}
+
+			function extractMetadata(content) {
+				var Metadata = Backbone.DocumentModel.extend({
+					defausts : {
+						columns : [],
+						measures : [],
+						hierarchies : []
+					}
+				});
+				var metadata = new Metadata();
+				var metadataJson = content.get('reportMetadata').get('reportSpec');
+				metadata.columns = metadataJson.get('columns');
+				metadata.hierarchies = metadataJson.get('hierarchies');
+				metadata.measures = metadataJson.get('measures');
+				return metadata;
+			}
+
+			function extractFilters(content) {
+				var filters = new Filters();
+				var filtersJson = content.get('reportMetadata').get('reportSpec').get('filters').get('filterRules');
+				$(filtersJson.keys()).each(function(i, item) {
+					var subElement = filtersJson.get(item);
+					if (subElement instanceof Backbone.Collection) {
+						if (item.indexOf('ElementType = ENTITY') > -1) {
+							var name = item.substring(item.indexOf('[') + 1, item.indexOf(']'));
+							var element = subElement.models[0];
+							var content = [];
+							if (element.get('value') != null) {
+								content = element.get('value');
+							} else if (element.get('values') != null) {
+								_.each(element.get('values').models, function(item, i) {
+									content.push(item.get('value'));
+								});
+							}
+
+							var auxFilter = new Filter({
+								name : name,
+								values : content
+							});
+							filters.add(auxFilter);
+						} else if (item.indexOf('ElementType = DATE') > -1) {
+
+						} else if (item.indexOf('ElementType = YEAR') > -1) {
+
+						}
+					}
+				});
+				return filters;
+			}
+
+			function populateGrid(id, dynamicLayoutView, firstContent) {
+				// TODO: Move grid section elsewhere.
+				var TableSectionView = Marionette.ItemView.extend({
+					template : '#grid-template'
+				});
+				var tableSectionView = new TableSectionView();
+				dynamicLayoutView.results.show(tableSectionView);
+
+				// Define grid structure.
+				var tableStructure = extractMetadata(firstContent);
+				var grouping = (tableStructure.hierarchies.length > 0) ? true : false;
+				var grid = $("#tab_grid");
+				$(grid).attr("id", "tab_grid_" + id);
+				var pager = $("#tab_grid_pager");
+				$(pager).attr("id", "tab_grid_pager_" + id);
+
+				$(grid).jqGrid({
+					caption : false,
+					url : '/rest/data/report/' + id + '/result/',
+					/* url : '/rest/data/report/' + id + '/result/jqGrid', */
+					datatype : 'json',
+					jsonReader : {
+						repeatitems : false,
+						root : function(obj) {
+							return transformData(obj);
+						},
+						page : function(obj) {
+							return obj.currentPageNumber;
+						},
+						total : function(obj) {
+							return obj.totalPageCount;
+						},
+						records : function(obj) {
+							return obj.totalRecords;
+						}
+					},
+					colNames : columnsMapping.createJQGridColumnNames(tableStructure, grouping),
+					colModel : columnsMapping.createJQGridColumnModel(tableStructure),
+					height : 250,
+					autowidth : true,
+					shrinkToFit : true,
+					forceFit : false,
+					viewrecords : true,
+					loadtext : 'Loading...',
+					headertitles : true,
+					gridview : true,
+					rownumbers : false,
+					rowNum : 100,
+					pager : "#tab_grid_pager_" + id,
+					emptyrecords : 'No records to view',
+					grouping : grouping,
+					groupingView : columnsMapping.createJQGridGroupingModel(tableStructure, grouping),
+					gridComplete : function() {
+					}
+				});
+			}
+
+			function transformData(data) {
+				// console.log(data);
+				var rows = [];
+				getContentRecursively(data.reportContents /* data.pageArea */, rows, null);
+				return rows;
+			}
+
+			function getContentRecursively(obj, rows, parent) {
+				if (obj != undefined && obj != null) {
+					if (obj.children == null || obj.children.length == 0) {
+						// console.log(obj.contents);
+						var row = {
+							id : 0
+						};
+						$.each(obj.contents, function(key, element) {
+							var colName = columnsMapping.getMap()[key];
+							if (colName != undefined && colName != null) {
+								if (element.displayedValue != null && element.displayedValue != "") {
+									row[colName] = element.displayedValue;
+								} else {
+									// TODO: This section uses data from the
+									// parent node
+									// just because now some hierarchical
+									// reports
+									// come with empty columns after the 1st
+									// children
+									// node. Remove it when that problem is
+									// fixed.
+									if (parent != undefined) {
+										if (parent[key].displayedValue.indexOf(' Totals') > 0) {
+											row[colName] = parent[key].displayedValue.substring(0, parent[key].displayedValue
+													.indexOf(' Totals'));
+										} else {
+											row[colName] = parent[key].displayedValue;
+										}
+									} else {
+										// TODO: If the data where ok this
+										// section wount
+										// be needed either.
+										row[colName] = 'Undefined';
+									}
+								}
+								row['id'] = Math.random();
+							}
+						});
+						// console.log(row);
+						rows.push(row);
+					} else {
+						$(obj.children).each(function(i, item) {
+							getContentRecursively(item, rows, obj.contents);
 						});
 					}
+				}
+			}
 
-					var auxFilter = new Filter({
-						name : name,
-						values : content
+			function retrieveTabContent(selectedTabIndex) {
+				var id = app.TabsApp.tabItemsView.collection.models[selectedTabIndex].get('id');
+
+				// Create a region where the dynamic content will be rendered
+				// inside
+				// the tab.
+				var regionsName = '#main-dynamic-content-region_' + id;
+				app.TabsApp.addRegions({
+					'filtersRegion' : regionsName
+				});
+
+				if (id >= 0) {
+					// Get collection with data we will use to render the tab
+					// content.
+					var firstContent = new Content({
+						id : id
 					});
-					filters.add(auxFilter);
-				} else if (item.indexOf('ElementType = DATE') > -1) {
 
-				} else if (item.indexOf('ElementType = YEAR') > -1) {
+					// --------------------------------------------------------------------------------------//
+					// TODO: Move filters section elsewhere.
+					// Create collection of Filters.
+					var filters = extractFilters(firstContent);
 
-				}
-			}
-		});
-		return filters;
-	}
-
-	function populateGrid(id, dynamicLayoutView, firstContent) {
-		// TODO: Move grid section elsewhere.
-		var TableSectionView = Marionette.ItemView.extend({
-			template : '#grid-template'
-		});
-		var tableSectionView = new TableSectionView();
-		dynamicLayoutView.results.show(tableSectionView);
-
-		// Define grid structure.
-		var tableStructure = extractMetadata(firstContent);
-		var grouping = (tableStructure.hierarchies.length > 0) ? true : false;
-		var grid = $("#tab_grid");
-		$(grid).attr("id", "tab_grid_" + id);
-		var pager = $("#tab_grid_pager");
-		$(pager).attr("id", "tab_grid_pager_" + id);
-
-		$(grid).jqGrid({
-			caption : false,
-			url : '/rest/data/report/' + id + '/result/',
-			/* url : '/rest/data/report/' + id + '/result/jqGrid', */
-			datatype : 'json',
-			jsonReader : {
-				repeatitems : false,
-				root : function(obj) {
-					return transformData(obj);
-				},
-				page : function(obj) {
-					return obj.currentPageNumber;
-				},
-				total : function(obj) {
-					return obj.totalPageCount;
-				},
-				records : function(obj) {
-					return obj.totalRecords;
-				}
-			},
-			colNames : columnsMapping.createJQGridColumnNames(tableStructure, grouping),
-			colModel : columnsMapping.createJQGridColumnModel(tableStructure),
-			height : 250,
-			autowidth : true,
-			shrinkToFit : true,
-			forceFit : false,
-			viewrecords : true,
-			loadtext : 'Loading...',
-			headertitles : true,
-			gridview : true,
-			rownumbers : false,
-			rowNum : 100, /* TODO: get this parameter from global settings. */
-			pager : "#tab_grid_pager_" + id,
-			emptyrecords : 'No records to view',
-			grouping : grouping,
-			groupingView : columnsMapping.createJQGridGroupingModel(tableStructure, grouping),
-			gridComplete : function() {
-			}
-		});
-	}
-
-	function transformData(data) {
-		// console.log(data);
-		var rows = [];
-		getContentRecursively(data.reportContents /* data.pageArea */, rows, null);
-		return rows;
-	}
-
-	function getContentRecursively(obj, rows, parent) {
-		if (obj != undefined && obj != null) {
-			if (obj.children == null || obj.children.length == 0) {
-				// console.log(obj.contents);
-				var row = {
-					id : 0
-				};
-				$.each(obj.contents, function(key, element) {
-					var colName = columnsMapping.getMap()[key];
-					if (colName != undefined && colName != null) {
-						if (element.displayedValue != null && element.displayedValue != "") {
-							row[colName] = element.displayedValue;
-						} else {
-							// TODO: This section uses data from the parent node
-							// just because now some hierarchical reports
-							// come with empty columns after the 1st children
-							// node. Remove it when that problem is fixed.
-							if (parent != undefined) {
-								if (parent[key].displayedValue.indexOf(' Totals') > 0) {
-									row[colName] = parent[key].displayedValue.substring(0, parent[key].displayedValue.indexOf(' Totals'));
-								} else {
-									row[colName] = parent[key].displayedValue;
-								}
-							} else {
-								// TODO: If the data where ok this section wount
-								// be needed either.
-								row[colName] = 'Undefined';
-							}
+					// Define the views.
+					var FilterItemView = Marionette.ItemView.extend({
+						tagName : 'div',
+						className : 'round-filter',
+						template : $(filtersItemTemplate, '#template-filters').html(),
+						events : {
+							'click' : "testclick"
+						},
+						testclick : function() {
+							console.log('testclick');
 						}
-						row['id'] = Math.random();
-					}
-				});
-				// console.log(row);
-				rows.push(row);
-			} else {
-				$(obj.children).each(function(i, item) {
-					getContentRecursively(item, rows, obj.contents);
-				});
-			}
-		}
-	}
+					});
+					var CompositeItemView = Marionette.CompositeView.extend({
+						template : $(filtersTemplate, '#template-table-filters').html(),
+						childView : FilterItemView
+					});
+					var compositeView = new CompositeItemView({
+						collection : filters
+					});
 
-	function retrieveTabContent(selectedTabIndex) {
-		var id = app.TabsApp.tabItemsView.collection.models[selectedTabIndex].get('id');
+					// Render views.
+					var dynamicLayoutView = new DynamicContentView();
+					app.TabsApp.filtersRegion.show(dynamicLayoutView);
+					dynamicLayoutView.filters.show(compositeView);
 
-		// Create a region where the dynamic content will be rendered inside
-		// the tab.
-		var regionsName = '#main-dynamic-content-region_' + id;
-		app.TabsApp.addRegions({
-			'filtersRegion' : regionsName
-		});
+					// Create accordion for filters area.
+					$("#main-dynamic-content-region_" + id + " #filters-collapsible-area").accordion({
+						collapsible : true,
+						active : false
+					});
+					// Create jQuery buttons.
+					$("#main-dynamic-content-region_" + id + " .buttonify").button();
 
-		if (id >= 0) {
-			// Get collection with data we will use to render the tab content.
-			var firstContent = new Content({
-				id : id
-			});
+					// --------------------------------------------------------------------------------------//
+					// TODO: make complex view for adding more info in this
+					// section.
+					var Legend = Backbone.Model.extend({});
+					var LegendView = Marionette.ItemView.extend({
+						template : _.template(legendsTemplate),
+						className : 'legends-container'
+					});
+					var legend = new Legend({
+						currencyCode : firstContent.get('reportMetadata').get('reportSpec').get('settings').get('currencyCode')
+					});
+					var legendView = new LegendView({
+						model : legend
+					});
+					dynamicLayoutView.legends.show(legendView);
 
-			// --------------------------------------------------------------------------------------//
-			// TODO: Move filters section elsewhere.
-			// Create collection of Filters.
-			var filters = extractFilters(firstContent);
+					// --------------------------------------------------------------------------------------//
+					populateGrid(id, dynamicLayoutView, firstContent);
 
-			// Define the views.
-			var FilterItemView = Marionette.ItemView.extend({
-				tagName : 'div',
-				className : 'round-filter',
-				template : $(filtersItemTemplate, '#template-filters').html(),
-				events : {
-					'click' : "testclick"
-				},
-				testclick : function() {
-					console.log('testclick');
+				} else if (id == -1) {
+					// "More Tabs..." tab.
+					var ItemView = Marionette.ItemView.extend({
+						model : Tab,
+						template : $(invisibleTabLinkTemplate, '#invisibleTabLink').html()
+					});
+
+					var InvisibleTabsCollectionView = Marionette.CollectionView.extend({
+						childView : ItemView,
+						tagName : 'ul'
+					});
+
+					app.TabsApp.filtersRegion.show(new InvisibleTabsCollectionView({
+						collection : app.TabsApp.tabsCollection
+					}));
 				}
-			});
-			var CompositeItemView = Marionette.CompositeView.extend({
-				template : $(filtersTemplate, '#template-table-filters').html(),
-				childView : FilterItemView
-			});
-			var compositeView = new CompositeItemView({
-				collection : filters
-			});
-
-			// Render views.
-			var dynamicLayoutView = new DynamicContentView();
-			app.TabsApp.filtersRegion.show(dynamicLayoutView);
-			dynamicLayoutView.filters.show(compositeView);
-
-			// Create accordion for filters area.
-			$("#main-dynamic-content-region_" + id + " #filters-collapsible-area").accordion({
-				collapsible : true,
-				active : false
-			});
-			// Create jQuery buttons.
-			$("#main-dynamic-content-region_" + id + " .buttonify").button();
-
-			// --------------------------------------------------------------------------------------//
-			// TODO: make complex view for adding more info in this section.
-			var legendsText = '<p>' + firstContent.get('reportMetadata').get('reportSpec').get('settings').get('currencyCode')
-					+ '</p> - <a>Show Legends...</a>';
-			var LegendView = Marionette.ItemView.extend({
-				template : legendsText
-			});
-			var legendView = new LegendView();
-			dynamicLayoutView.legends.show(legendView);
-
-			// --------------------------------------------------------------------------------------//
-			populateGrid(id, dynamicLayoutView, firstContent);
-
-		} else if (id == -1) {
-			// "More Tabs..." tab.
-			var ItemView = Marionette.ItemView.extend({
-				model : Tab,
-				template : $(invisibleTabLinkTemplate, '#invisibleTabLink').html()
-			});
-
-			var InvisibleTabsCollectionView = Marionette.CollectionView.extend({
-				childView : ItemView,
-				tagName : 'ul'
-			});
-
-			app.TabsApp.filtersRegion.show(new InvisibleTabsCollectionView({
-				collection : app.TabsApp.tabsCollection
-			}));
-		}
-	}
-
-	// "Class" methods definition here.
-	TabEvents.prototype = {
-		constructor : TabEvents,
-		onCreateTab : function(event, ui) {
-			console.log('create tab');
-			this.onActivateTab(event, ui);
-		},
-		onActivateTab : function(event, ui) {
-			console.log('activate tab');
-
-			// TODO: move this logic elsewhere.
-			var panel = null;
-			var selectedTabIndex = 0;
-			if (ui.panel != undefined) {
-				panel = ui.panel;
-				selectedTabIndex = ui.tab.index();
-			} else {
-				panel = ui.newPanel;
-				selectedTabIndex = ui.newTab.index();
 			}
-			// Put loading animation.
-			// $(panel).html(putAnimation());
-			// Simulate time consuming content.
-			setTimeout(function() {
-				retrieveTabContent(selectedTabIndex);
-			}, 10);
-		}
-	};
 
-	return TabEvents;
-});
+			// "Class" methods definition here.
+			TabEvents.prototype = {
+				constructor : TabEvents,
+				onCreateTab : function(event, ui) {
+					console.log('create tab');
+					this.onActivateTab(event, ui);
+				},
+				onActivateTab : function(event, ui) {
+					console.log('activate tab');
+
+					// TODO: move this logic elsewhere.
+					var panel = null;
+					var selectedTabIndex = 0;
+					if (ui.panel != undefined) {
+						panel = ui.panel;
+						selectedTabIndex = ui.tab.index();
+					} else {
+						panel = ui.newPanel;
+						selectedTabIndex = ui.newTab.index();
+					}
+					// Put loading animation.
+					// $(panel).html(putAnimation());
+					// Simulate time consuming content.
+					setTimeout(function() {
+						retrieveTabContent(selectedTabIndex);
+					}, 10);
+				}
+			};
+
+			return TabEvents;
+		});

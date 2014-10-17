9cb56e641599566019e638641ef213924efe4d7f
nmandrescu <nmandrescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-10-17 11:04:36 +0000

    AMP-17695 adding Public Portal [Period] (last X months) configuration in Global Settings
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25256 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/helper/GlobalSettingsConstants.java
+++ amp/WEB-INF/src/org/digijava/module/aim/helper/GlobalSettingsConstants.java
@@ -3,8 +3,6 @@ package org.digijava.module.aim.helper;
 public class GlobalSettingsConstants {
     public static final String DEFAULT_COUNTRY = "Default Country";
    
-    public static final String PUBLIC_VIEW = "Public View";
-    
     public static final String FEATURE_TEMPLATE = "Feature Template";
     
     public static final String YEAR_RANGE_START = "Year Range Start";
@@ -13,7 +11,11 @@ public class GlobalSettingsConstants {
 
     public static final String NUMBER_OF_YEARS_IN_RANGE = "Number of Years in Range";
 
+    public static final String PUBLIC_VIEW = "Public View";
+    
     public static final String PUBLIC_VIEW_BIDGET_FILTER = "Public View Budget Filter";
+    
+    public static final String PUBLIC_VIEW_LAST_PERIOD = "Public View Last Period (months)";
     
     public static final String DEFAULT_SECTOR_SCHEME = "Default Sector Scheme";
 

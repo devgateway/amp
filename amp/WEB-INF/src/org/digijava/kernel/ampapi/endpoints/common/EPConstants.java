f39404bf9a77811ede59664cd606868b2af8d96c
nmandrescu <nmandrescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-10-17 14:29:14 +0000

    AMP-17695 adding language requested identification for public users
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25258 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/EPConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.digijava.kernel.ampapi.endpoints.common;
+
+/**
+ * Endpoint related constants
+ * @author Nadejda Mandrescu
+ *
+ */
+public class EPConstants {
+	public static final String LANGUAGE = "language";
+}

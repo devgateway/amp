caa3e0bff306d3db4de215ffc5d57fba407f29bd
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-10-23 14:18:48 +0000

    AMP-18425
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25334 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/items/AmpAgreementItemPanel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/items/AmpAgreementItemPanel.java
@@ -198,6 +198,8 @@ public class AmpAgreementItemPanel extends AmpFieldPanel<AmpFunding>{
         AmpAjaxLinkField cancel = new AmpAjaxLinkField("cancel", "Cancel", "Cancel") {
             @Override
             protected void onClick(AjaxRequestTarget target) {
+                model.getObject().setAgreement(null);                
+
                 newAgreementForm.setVisibilityAllowed(false);
                 AmpAgreementItemPanel.this.isFormOpen=false;
                 target.add(newAgreementForm.getParent());

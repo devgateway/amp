467db7eccf36cde206fd0f34dbe61463f39f4202
nmandrescu <nmandrescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-11-03 22:22:59 +0000

    removing hardcoded path
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25474 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/mondrian/util/MondrianUtils.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/mondrian/util/MondrianUtils.java
@@ -35,7 +35,7 @@ import org.olap4j.metadata.Datatype;
 public class MondrianUtils {
 	protected static final Logger logger = Logger.getLogger(MondrianUtils.class);
 	
-	public static String PRINT_PATH = "/home/nadia/Documents/AMP/work/Mondrian/reports";
+	public static String PRINT_PATH = null;
 	
 	/**
 	 * Gets full OlapException error details

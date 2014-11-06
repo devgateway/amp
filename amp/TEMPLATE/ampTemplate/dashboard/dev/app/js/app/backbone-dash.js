86178eb3e4191153446c608f41d8ada1b216514e
pschleihauf <pschleihauf@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-11-06 10:34:00 +0000

    AMP-18276: cache chart data requests
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25551 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/dashboard/dev/app/js/app/backbone-dash.js
+++ amp/TEMPLATE/ampTemplate/dashboard/dev/app/js/app/backbone-dash.js
@@ -6,33 +6,57 @@ var Backbone = require('backbone');
 var IS_PHILS_CORS = window && (window.location.host === 'localhost:3000');
 
 
+var CACHE = ['/rest/dashboard'];
+
+
 function InitError(instance) {
   this.instance = instance;
   this.toString = function() { return 'Module initialization error'; };
 }
 
 
-// we have to alias the old backbone sync here.
-// otherwise things get messed up in filters init.
-var bs = Backbone.sync;
+var syncOverride = (function(bs) {
+  var cache = {};
 
+  function _doSync(url, method, model, options) {
+    _(options).extend({
+      // maybe use phil's DRC CORS dev server
+      url: (IS_PHILS_CORS ? 'http://66.207.103.134' : '') + url,
+      headers: {
+        // jscs:disable disallowQuotedKeysInObjects
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        // jscs:enable disallowQuotedKeysInObjects
+      }
+    });
+    return bs.call(this, method, model, options);
+  }
 
-function syncOverride(method, model, options) {
-  var url = _.has(options, 'url') ? options.url : _.result(model, 'url');
+  function _cacheSync(url, method, model, options) {
+    var key = url + '+POST:' + options.data;
+    if (cache[key]) { return cache[key](); }
 
-  _(options).extend({
-    // maybe use phil's DRC CORS dev server
-    url: (IS_PHILS_CORS ? 'http://66.207.103.134' : '') + url,
-    headers: {
-      // jscs:disable disallowQuotedKeysInObjects
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-      // jscs:enable disallowQuotedKeysInObjects
-    }
-  });
+    var xhr = _doSync(url, method, model, options);
 
-  return bs.call(this, method, model, options);
-}
+    cache[key] = function() {
+      // calling this function will make the xhr re-call its callbacks
+      return xhr
+        .done(options.success)
+        .always(options.complete);
+    };
+
+    // do not cache failed requests
+    xhr.fail(function() { delete cache[key]; });
+
+    return xhr;
+  }
+
+  return function(method, model, options) {
+    var url = _.has(options, 'url') ? options.url : _.result(model, 'url'),
+        cacheable = _.some(CACHE, function(i) { return url.indexOf(i) === 0; });
+    return (cacheable ? _cacheSync : _doSync)(url, method, model, options);
+  };
+})(Backbone.sync);
 
 
 // mixed into all models, collections and views to ensure we have a convenient

254d3de09996dfdb0bb96028850e69af95376f5c
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-11-10 01:58:06 +0000

    AMP-18311 Rollback as per discusion with Fernando since changing the endpoint names breaks Aid predictability dashboard
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@25637 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/dashboards/EndPoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/dashboards/EndPoints.java
@@ -65,7 +65,7 @@ public class EndPoints {
 	 */
 	
 	@POST 
-	@Path("/aid-predictability")
+	@Path("/aidPredictability")
 	@Produces(MediaType.APPLICATION_JSON)
 	@ApiMethod(ui = false, id = "aidPredictability")
 	//TODO: Implement Filters

ae8af2cb38a6982823b94c576cdcba48b5a96ec9
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-11-27 15:53:15 +0000

    AMP-18547
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@26000 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp/ckeditor_4.4.6/config.js
@@ -0,0 +1,11 @@
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+		config.toolbar = [
+	['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','PasteFromWord', 'Image','FontSize']
+	];
+		//config.skin = 'kama';
+		config.width = 340;
+};

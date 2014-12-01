9ee0b70927149c8f0c4a2c69bf21efba24979914
drust-smith <drust-smith@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-12-01 15:14:07 +0000

    AMP-18893: Project site link now opens in new tab. Also fixed minr ui improvements.
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@26024 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/views/structures-view.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/views/structures-view.js
@@ -202,7 +202,7 @@ module.exports = Backbone.View
   _hilightProject: function(projectId) {
     this.featureGroup.eachLayer(function(layer) {
       if (layer.feature.properties.activity.id === projectId && layer.setStyle) {
-        layer.setStyle({color: '#222', stroke: true});
+        layer.setStyle({color: '#222', stroke: true, weight:2});
       }
     });
   },

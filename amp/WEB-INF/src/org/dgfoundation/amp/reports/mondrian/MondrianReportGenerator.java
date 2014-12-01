760d57d1ef4d28f79b90f9d125eed2eab60a7607
nmandrescu <nmandrescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-12-01 11:59:39 +0000

    AMP-18818 removing the workaround to be aware if the issue is reintroduced to apply a proper fix/prevent wrong changes
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@26021 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/reports/mondrian/MondrianReportGenerator.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/reports/mondrian/MondrianReportGenerator.java
@@ -622,24 +622,21 @@ public class MondrianReportGenerator implements ReportExecutor {
 				currentLevel--; 
 			}
 			if (column != null) { //must not be null actually
-				try {
-					int year = CalendarUtil.parseYear(spec.getSettings().getCalendar(), column.columnName); 
-					boolean isAllowed = yearSet.contains(year); //first check if it is in the set
-					if (!isAllowed)
-						for (Integer[] range : yearRanges)
-							if (range[0] <= year && year <= range[1]) { //check if the year is within any [min, max] allowed range from the list of configured ranges
-								isAllowed = true;
-								break;
-							}
-					
-					if (!isAllowed) {
-						leafColumnsNumberToRemove.add(pos);
-						iter.remove();//remove also the leaf header
-					}
-				}
-				catch(Exception e) {
-					//TODO: Added Exception when value comes "Undefined" it gives an exception. Happens in reports that have "Allow empty funding columns for year, quarter and month" activated
-					logger.error("Year couldn't be parsed. Column Name: " + column.columnName);
+				int year = CalendarUtil.parseYear(spec.getSettings().getCalendar(), column.columnName);
+				// first check if it is in the set
+				boolean isAllowed = yearSet.contains(year); 
+				if (!isAllowed)
+					for (Integer[] range : yearRanges)
+						// check if the year is within any [min, max] allowed range from the list of configured ranges
+						if (range[0] <= year && year <= range[1]) { 
+							isAllowed = true;
+							break;
+						}
+				
+				if (!isAllowed) {
+					leafColumnsNumberToRemove.add(pos);
+					// remove also the leaf header
+					iter.remove();
 				}
 			}
 			pos ++;

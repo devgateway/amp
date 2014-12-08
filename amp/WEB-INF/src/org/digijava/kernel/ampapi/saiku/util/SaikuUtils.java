5a76b943e61b3ec10dc7a8060fe9952d8c46bdcb
fferreyra <fferreyra@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2014-12-08 21:15:24 +0000

    AMP-18614 Initial Implementation of Saiku reports save
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@26098 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/saiku/util/SaikuUtils.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/saiku/util/SaikuUtils.java
@@ -20,10 +20,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.SortedSet;
-import java.util.TreeSet;
 
 import org.apache.commons.lang.StringUtils;
 import org.digijava.kernel.ampapi.mondrian.util.MoConstants;
+import org.digijava.kernel.persistence.PersistenceManager;
+import org.digijava.module.aim.dbentity.AmpAnalyticalReport;
+import org.digijava.module.aim.dbentity.AmpTeamMember;
+import org.digijava.module.aim.helper.Constants;
+import org.digijava.module.aim.helper.TeamMember;
+import org.digijava.module.aim.util.TeamUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.olap4j.Axis;
 import org.olap4j.CellSet;
 import org.olap4j.CellSetAxis;
@@ -35,12 +42,16 @@ import org.saiku.olap.dto.resultset.CellDataSet;
 import org.saiku.olap.dto.resultset.DataCell;
 import org.saiku.olap.dto.resultset.MemberCell;
 import org.saiku.olap.util.SaikuProperties;
+import org.saiku.repository.IRepositoryObject;
+import org.saiku.repository.RepositoryFileObject;
 import org.saiku.service.olap.totals.AxisInfo;
 import org.saiku.service.olap.totals.TotalNode;
 import org.saiku.service.olap.totals.TotalsListsBuilder;
 import org.saiku.service.olap.totals.aggregators.TotalAggregator;
 import org.saiku.service.util.exception.SaikuServiceException;
 import org.saiku.web.rest.objects.resultset.Total;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * Stores any utility methods that are Saiku specific (e.g. use Saiku API)
@@ -467,5 +478,74 @@ public class SaikuUtils {
 		    return new byte[ 0 ];
 		  }
 
+	  /*
+	   * 
+	   */
+	public static List<IRepositoryObject> getReports() {
+		
+		Session session = PersistenceManager.getCurrentSession().getSessionFactory().openSession();
+		AmpTeamMember ampTeamMember = getLoggedUser();
+        Query query = session.createQuery("from AmpAnalyticalReport where owner = :owner ");
+        query.setParameter("owner", ampTeamMember);
 
+        List<AmpAnalyticalReport> list = query.list();
+        List<IRepositoryObject> listFiles = new ArrayList<IRepositoryObject>();
+        for(AmpAnalyticalReport report : list) {
+        	listFiles.add(new RepositoryFileObject(report.getName(), report.getName(), "", report.getName(), null));
+        }
+		return listFiles;
+	}
+
+	public static AmpAnalyticalReport getReport(int id) {
+		
+		Session session = PersistenceManager.getSession();
+		AmpAnalyticalReport report = (AmpAnalyticalReport)session.get(AmpAnalyticalReport.class, id);
+        return report;
+	}
+
+	public static void saveReport(AmpAnalyticalReport report) {
+		Session session = PersistenceManager.getSession();
+		AmpAnalyticalReport existingReport = getReports(report.getName());
+		if(existingReport != null) {
+			existingReport.setData(report.getData());
+			report = existingReport;
+		}
+		
+		if(report.getId() != null) {
+	    	session.merge(report);
+		}
+		else
+		{
+			session.save(report);
+		}
+    	session.flush();
+	}
+
+	public static AmpAnalyticalReport getReports(String name) {
+		Session session = PersistenceManager.getSession();
+        Query query = session.createQuery("from AmpAnalyticalReport where name = :name and owner = :owner ");
+        query.setParameter("name", name);
+        query.setParameter("owner", getLoggedUser());
+        if(!query.list().isEmpty()) {
+            AmpAnalyticalReport report = (AmpAnalyticalReport)query.list().get(0);
+            return report;
+        }
+        return null;
+	}
+
+	public static AmpAnalyticalReport createReport(String name, String content) {
+		AmpAnalyticalReport report = new AmpAnalyticalReport();
+    	report.setName(name);
+    	report.setData(content);
+		report.setOwner(getLoggedUser());
+		return report;
+	}
+
+	private static AmpTeamMember getLoggedUser() {
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+		TeamMember tm = (TeamMember) attr.getRequest().getSession().getAttribute(Constants.CURRENT_MEMBER);
+		AmpTeamMember ampTeamMember = TeamUtil.getAmpTeamMember(tm.getMemberId());
+		return ampTeamMember;
+	}
 }

7589fb9fb9e89d9db133b3a9c1240c568b7e255d
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-01-15 20:48:23 +0000

    AMP-19278
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26350 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpSectorSearchModel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpSectorSearchModel.java
@@ -46,7 +46,8 @@ public class AmpSectorSearchModel extends
 		try {
 			ret = new ArrayList<AmpSector>();
 			session = PersistenceManager.getRequestDBSession();
-			
+			session.enableFilter("isDeletedFilter").setParameter("deleted", Boolean.FALSE);
+
 //			BOZO, CONSTANTIN			
 //			debug / research code: please leave it here until I remove it later
 //			String myQuery = "select sec from " + AmpSector.class.getName() + " sec where sec.name like '%у%'";
@@ -80,7 +81,9 @@ public class AmpSectorSearchModel extends
 		} catch (DgException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} finally {
+			session.disableFilter("isDeletedFilter");		
+			}
 		return ret;
 	}
 

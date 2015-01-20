3bc49620f4898be8643371443ca7e873370286b2
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-01-20 19:13:50 +0000

    AMP-17741 Change to show currency name instead of currency code
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26404 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/ExportActivityToPDF.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/ExportActivityToPDF.java
@@ -1245,8 +1245,8 @@ public class ExportActivityToPDF extends Action {
 					if(myForm.getFunding().getProProjCost().getFunAmount()!=null){
 						costOutput+=" "+myForm.getFunding().getProProjCost().getFunAmount();
 					}
-					if(myForm.getFunding().getProProjCost().getCurrencyCode()!=null){
-						costOutput+=" "+myForm.getFunding().getProProjCost().getCurrencyCode();
+					if(myForm.getFunding().getProProjCost().getCurrencyName()!=null){
+						costOutput+=" "+myForm.getFunding().getProProjCost().getCurrencyName();
 					}
 					if(myForm.getFunding().getProProjCost().getFunDate()!=null){
 						costOutput+="\n"+TranslatorWorker.translateText("Date")+ "\t: "+myForm.getFunding().getProProjCost().getFunDate();
@@ -3085,7 +3085,7 @@ public class ExportActivityToPDF extends Action {
 				ProposedProjCost ppc = it.next();
 				innerCell = new PdfPCell();
 				innerCell.setBorder(0);
-				innerCell = new PdfPCell(new Paragraph(ppc.getFunAmount() + " " + ppc.getCurrencyCode(), plainFont));
+				innerCell = new PdfPCell(new Paragraph(ppc.getFunAmount() + " " + ppc.getCurrencyName(), plainFont));
 				dataTable.addCell(innerCell);
 				innerCell = new PdfPCell();
 				innerCell.setBorder(0);

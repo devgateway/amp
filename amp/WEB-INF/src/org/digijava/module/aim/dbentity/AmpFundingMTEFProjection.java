f32068b754addbef168a6b5f9dc292d5fb9b7be7
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-02-03 04:34:40 +0000

    AMP-19354 MTEFs are being rounded in activity preview and exports and thus showing 0s when amounts are in millions
    git-svn-id: file:///svn/branches/AMP_2_8_RELEASE@26528 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpFundingMTEFProjection.java
+++ amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpFundingMTEFProjection.java
@@ -119,7 +119,7 @@ public class AmpFundingMTEFProjection implements Cloneable, Serializable, Compar
 		
 		public Double getTransactionAmount()
 		{
-			return FeaturesUtil.applyThousandsForVisibility(this.getAmount());
+			return this.getAmount();
 		}
 		
 		public Double getAbsoluteTransactionAmount()

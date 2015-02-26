f5ed736303055e1901ca4fe4a56944f460dd6527
cdolghier <cdolghier@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-02-26 14:44:10 +0000

    AMP-19551
    1. also run component ETL in case of incremental ETL
    2. implement incremental ETL for Components
    
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26697 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/mondrian/EtlConfiguration.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/mondrian/EtlConfiguration.java
@@ -22,6 +22,11 @@ public class EtlConfiguration {
 	public final Set<Long> pledgeIds;
 	
 	/**
+	 * components to add/remove from all the relevant tables
+	 */
+	public final Set<Long> componentIds;
+	
+	/**
 	 * Julian date codes to add/remove from the relevant tables. null means "all"
 	 */
 	public final Set<Long> dateCodes;
@@ -29,9 +34,10 @@ public class EtlConfiguration {
 	public final boolean fullEtl;
 
 	
-	public EtlConfiguration(Set<Long> activityIds, Set<Long> pledgeIds, Set<Long> dateCodes, boolean fullEtl) {
+	public EtlConfiguration(Set<Long> activityIds, Set<Long> pledgeIds, Set<Long> componentIds, Set<Long> dateCodes, boolean fullEtl) {
 		this.activityIds = activityIds;
 		this.pledgeIds = pledgeIds;
+		this.componentIds = componentIds;
 		this.dateCodes = dateCodes;
 		this.fullEtl = fullEtl;
 	}
@@ -64,6 +70,20 @@ public class EtlConfiguration {
 		return prefix + pledgeIdsInQuery;
 	}
 	
+	protected String componentIdsInQuery;
+	
+	/**
+	 * builds an SQL subfragment which filters based on the pledge IDs
+	 * @param prefix
+	 * @return
+	 */
+	public String componentIdsIn(String prefix) {
+		//if (fullEtl) return "1=1"; 
+		if (componentIdsInQuery == null)
+			componentIdsInQuery = " IN (" + Util.toCSStringForIN(componentIds) + ")";
+		return prefix + componentIdsInQuery;
+	}
+	
 	protected String entityIdsInQuery;
 	/**
 	 * builds an SQL subfragment which filters based on the entity IDs (activity IDs + shifted pledge IDs)
@@ -76,6 +96,7 @@ public class EtlConfiguration {
 			entityIdsInQuery = " IN (" + Util.toCSStringForIN(getAllEntityIds()) + ")";
 		return prefix + entityIdsInQuery;
 	}
+	
 	public Set<Long> getAllEntityIds() {
 		Set<Long> res = new java.util.HashSet<>(this.activityIds);
 		for(long pledgeId:this.pledgeIds)
@@ -85,9 +106,10 @@ public class EtlConfiguration {
 	
 	
 	@Override public String toString() {
-		return String.format("activities: <%s>; pledges: <%s>; dates: <%s>", 
+		return String.format("activities: <%s>; pledges: <%s>; components: <%s>; dates: <%s>", 
 				activityIds.size() > 20 ? String.format("[%d] activities", activityIds.size()) : activityIds.toString(),
 				pledgeIds.size() > 20 ? String.format("[%d] pledges", pledgeIds.size()) : pledgeIds.toString(),
+				componentIds.size() > 20 ? String.format("[%d] components", componentIds.size()) : componentIds.toString(),
 				dateCodes == null ? "(all)" : dateCodes.toString());
 	}
 }

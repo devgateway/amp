d6a249172397c9cbd5a69373a45ce43999991194
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-03-03 14:02:02 +0000

    AMP-19384. Implement versioning methods
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26727 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpAidEffectivenessIndicatorOption.java
+++ amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpAidEffectivenessIndicatorOption.java
@@ -1,12 +1,15 @@
 package org.digijava.module.aim.dbentity;
 
+import org.digijava.module.aim.util.Output;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * The amp aid effectiveness indicator option domain class
  * An option could be anything, not just predefined
  */
-public class AmpAidEffectivenessIndicatorOption implements Serializable {
+public class AmpAidEffectivenessIndicatorOption implements Serializable, Versionable, Cloneable {
 
     private Long ampIndicatorOptionId;
     private String ampIndicatorOptionName;
@@ -55,5 +58,38 @@ public class AmpAidEffectivenessIndicatorOption implements Serializable {
         int result = ampIndicatorOptionId != null ? ampIndicatorOptionId.hashCode() : 0;
         result = 31 * result + (indicator != null ? indicator.hashCode() : 0);
         return result;
+    }
+
+    @Override
+    public boolean equalsForVersioning(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AmpAidEffectivenessIndicatorOption)) return false;
+
+        AmpAidEffectivenessIndicatorOption that = (AmpAidEffectivenessIndicatorOption) o;
+
+        return !(ampIndicatorOptionId != null ? !ampIndicatorOptionId.equals(that.ampIndicatorOptionId) : that.ampIndicatorOptionId != null);
+    }
+
+    @Override
+    public Object getValue() {
+        return this.ampIndicatorOptionName;
+    }
+
+    @Override
+    public Output getOutput() {
+        Output out = new Output();
+        out.setOutputs(new ArrayList<Output>());
+        out.getOutputs().add(new Output(null, new String[] { getIndicator().getAmpIndicatorName() }, new Object[] { this.ampIndicatorOptionName}));
+        return out;
+    }
+
+    @Override
+    public Object prepareMerge(AmpActivityVersion newActivity) throws Exception {
+        return (AmpAidEffectivenessIndicatorOption) clone();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }

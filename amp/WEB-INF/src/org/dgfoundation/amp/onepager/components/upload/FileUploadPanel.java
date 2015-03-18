f78da658a661299dc607312fa3bba8fa782c8bab
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-03-18 15:41:59 +0000

    AMP-19681 Activity Form Translations
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26840 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/upload/FileUploadPanel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/upload/FileUploadPanel.java
@@ -1,9 +1,10 @@
 package org.dgfoundation.amp.onepager.components.upload;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.util.upload.FileItem;
+import org.digijava.kernel.translator.TranslatorWorker;
 
 /**
  *
@@ -14,6 +15,8 @@ public class FileUploadPanel extends Panel {
 
     public FileUploadPanel(String id, String activityId, IModel<FileItem> fileItemModel) {
         super(id);
+        add (new Label("chooseFileLabel", TranslatorWorker.translateText("Choose file")));
+        add (new Label("noFileLabel", TranslatorWorker.translateText("No file chosen")));
         add(new FileUploadBehavior(activityId, fileItemModel));
     }
 }

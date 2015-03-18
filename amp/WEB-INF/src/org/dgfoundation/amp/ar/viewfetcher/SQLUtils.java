b99aa7b3e40901178cc57546f54f1b63a9eaaf15
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-03-18 18:23:01 +0000

    AMP-19698 Merge 2.11
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26844 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/ar/viewfetcher/SQLUtils.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/ar/viewfetcher/SQLUtils.java
@@ -51,11 +51,21 @@ public class SQLUtils {
 	 */
 	public static LinkedHashMap<String, String> getTableColumnsWithTypes(final String tableName, boolean crashOnDuplicates){
 		String query = String.format("SELECT c.column_name, c.data_type FROM information_schema.columns As c WHERE table_schema='public' AND table_name = '%s' ORDER BY c.ordinal_position", tableName.toLowerCase());
-		try (Connection jdbcConnection = PersistenceManager.getJdbcConnection()) {
+		Connection jdbcConnection = null;
+		try  {
+			jdbcConnection=PersistenceManager.getJdbcConnection();
 			return getTableColumnsWithTypes(jdbcConnection, tableName, query, crashOnDuplicates);
 		}
 		catch(SQLException ex) {
 			throw new RuntimeException(ex);
+		}finally{
+			try {
+				if(!jdbcConnection.getAutoCommit())
+					jdbcConnection.commit();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			PersistenceManager.closeQuietly(jdbcConnection);
 		}
 	}
 	

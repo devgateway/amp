e13e2bad8312061f050077fc7a16a7721ba50c0b
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-03-18 14:19:56 +0000

    AMP-19765: Filter Widget doesnt load saved filters for any Tab if language is other than English
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@26837 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/tabs/js/models/filter.js
+++ amp/TEMPLATE/ampTemplate/tabs/js/models/filter.js
@@ -2,6 +2,7 @@ define([ 'underscore', 'backbone' ], function (_, Backbone) {
     var Filter = Backbone.Model.extend({
         defaults: {
             name: '',
+            trnName: '',
             values: [],
             entityType: null
         }

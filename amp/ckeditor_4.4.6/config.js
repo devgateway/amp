0ba7379ace99b7b1fb71adc56aef91822d7af01c
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-03-25 20:06:38 +0000

    AMP-19742
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@26917 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/ckeditor_4.4.6/config.js
+++ amp/ckeditor_4.4.6/config.js
@@ -1,6 +1,6 @@
 
 CKEDITOR.editorConfig = function( config ) {
-	config.extraPlugins = 'removeformat'
+	config.extraPlugins = 'removeformat';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';

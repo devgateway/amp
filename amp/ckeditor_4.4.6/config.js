6b1441818bf0038a87d2cf1b796d117cd9bada2d
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-03-25 18:50:16 +0000

    AMP-19742 Add remove formatting button to AF CKEditors
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@26914 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/ckeditor_4.4.6/config.js
+++ amp/ckeditor_4.4.6/config.js
@@ -1,11 +1,13 @@
 
 CKEDITOR.editorConfig = function( config ) {
+	config.extraPlugins = 'removeformat'
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 		config.toolbar = [
-	['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','PasteFromWord', 'Image','FontSize']
+	['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','PasteFromWord', 'RemoveFormat','FontSize']
 	];
 		//config.skin = 'kama';
+	
 		config.width = 340;
 };

27569662959d8b7463f191989eb37e1b357bb5a3
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-04-06 14:32:56 +0000

    AMP-19667: Aid predictability chart doesn't tell you what it shows
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27025 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/dashboard/dev/app/js/app/views/chart-aid-predictability.js
+++ amp/TEMPLATE/ampTemplate/dashboard/dev/app/js/app/views/chart-aid-predictability.js
@@ -26,9 +26,17 @@ module.exports = ChartViewBase.extend({
   getTTContent: function(context) {
 	var of = app.translator.translateSync("amp.dashboard:of","of");
 	var total = app.translator.translateSync("amp.dashboard:total","total");
-	/*var header = context.x.raw + " " + app.translator.translateSync("amp.dashboard:aid-predictability-" 
-			+ context.data[context.series.index].originalKey + "-" + this.model.get('measure'), "");*/
-    var otherSeries = context.data[1 - context.series.index],  // WARNING: assumes only 2 series
+	
+	// IMPORTANT: We assume this chart will ALWAYS show 2 data series.
+	var index = -1;
+	if (context.data[0].disabled === true) {
+		index = 1;
+	} else {
+		index = context.series.index;
+	}	
+	var header = context.x.raw + " " + app.translator.translateSync("amp.dashboard:aid-predictability-" 
+			+ context.data[index].originalKey + "-" + this.model.get('measure'), "");
+    var otherSeries = context.data[1 - index],  // WARNING: assumes only 2 series
         otherHere = otherSeries.values[context.x.index],
         line2;
     if (otherHere.y > 0) {
@@ -37,7 +45,7 @@ module.exports = ChartViewBase.extend({
       line2 = context.x.raw + ' ' + otherSeries.key + ': <b>' + otherHere.y + '</b>';
     }
     return {tt: {
-      heading: context.x.raw + ' ' + context.series.key,
+      heading: header,
       bodyText: '<b>' + context.y.fmt + '</b> ' + this.model.get('currency'),
       footerText: line2
     }};

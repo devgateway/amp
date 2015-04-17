46b36f514fe46e2e05d325a87b813f7d5bc88fad
pschleihauf <pschleihauf@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-04-17 14:50:41 +0000

    AMP-19895: Aid Predictability only show % of YEAR total
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27132 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/dashboard/dev/app/js/app/views/chart-aid-predictability.js
+++ amp/TEMPLATE/ampTemplate/dashboard/dev/app/js/app/views/chart-aid-predictability.js
@@ -24,26 +24,26 @@ module.exports = ChartViewBase.extend({
   },
 
   getTTContent: function(context) {
-	var of = app.translator.translateSync("amp.dashboard:of","of");
-	var total = app.translator.translateSync("amp.dashboard:total","total");
-	
-	// IMPORTANT: We assume this chart will ALWAYS show 2 data series.
-	var index = -1;
-	if (context.data[0].disabled === true) {
-		index = 1;
-	} else {
-		index = context.series.index;
-	}	
-	var header = context.x.raw + " " + app.translator.translateSync("amp.dashboard:aid-predictability-" 
-			+ context.data[index].originalKey + "-" + this.model.get('measure'), "");
-    var otherSeries = context.data[1 - index],  // WARNING: assumes only 2 series
-        otherHere = otherSeries.values[context.x.index],
-        line2;
-    if (otherHere.y > 0) {
-      line2 = '<b>' + d3.format('%')(context.y.raw / otherHere.y) + '</b>&nbsp<span>' + of + '</span>&nbsp' + context.x.raw + '&nbsp<span>' + total + '</span>';
-    } else {
-      line2 = context.x.raw + ' ' + otherSeries.key + ': <b>' + otherHere.y + '</b>';
-    }
+  var of = app.translator.translateSync("amp.dashboard:of","of");
+  var total = app.translator.translateSync("amp.dashboard:total","total");
+
+  // IMPORTANT: We assume this chart will ALWAYS show 2 data series.
+  var index = -1;
+  if (context.data[0].disabled === true) {
+    index = 1;
+  } else {
+    index = context.series.index;
+  }
+  var header = context.x.raw + " " +
+        app.translator.translateSync("amp.dashboard:aid-predictability-" +
+        context.data[index].originalKey + "-" + this.model.get('measure'), "");
+
+  var otherSeries = context.data[1 - index],  // WARNING: assumes only 2 series
+      otherHere = otherSeries.values[context.x.index],
+      line2 = '<b>' + d3.format('%')(context.y.raw / otherHere.y) +
+        '</b>&nbsp<span>' + of + '</span>&nbsp' + context.x.raw +
+        '&nbsp<span>' + total + '</span>';
+
     return {tt: {
       heading: header,
       bodyText: '<b>' + context.y.fmt + '</b> ' + this.model.get('currency'),

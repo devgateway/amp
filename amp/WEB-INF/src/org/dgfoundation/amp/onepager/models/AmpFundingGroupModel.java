cf77773589d0f57a8991e4da55c73c07ff1c6ec1
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-04-29 19:44:40 +0000

    AMP-19620 Show funding items in the order they were added
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27283 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpFundingGroupModel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpFundingGroupModel.java
@@ -1,8 +1,13 @@
 package org.dgfoundation.amp.onepager.models;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+
 import org.apache.wicket.model.IModel;
 import org.digijava.module.aim.dbentity.AmpFunding;
 import org.digijava.module.aim.dbentity.AmpOrganisation;
@@ -14,7 +19,7 @@ public class AmpFundingGroupModel implements IModel<Set<AmpOrganisation>> {
 	public AmpFundingGroupModel(IModel<Set<AmpFunding>> model) {
 		this.model = model;
 	}
-	
+
 	@Override
 	public void detach() {
 		model.detach();
@@ -22,11 +27,31 @@ public class AmpFundingGroupModel implements IModel<Set<AmpOrganisation>> {
 
 	@Override
 	public Set<AmpOrganisation> getObject() {
-		HashSet<AmpOrganisation> orgsSet = new HashSet<AmpOrganisation>();
+		Set<AmpOrganisation> orgsSet = new LinkedHashSet<AmpOrganisation>();
 		Set<AmpFunding> fSet = model.getObject();
-		if(fSet == null)
+		if (fSet == null)
 			return orgsSet;
-		Iterator<AmpFunding> fSetIt = fSet.iterator();		
+
+		List<AmpFunding> auxAmpFunding = new ArrayList(fSet);
+		Collections.sort(auxAmpFunding, new Comparator<AmpFunding>() {
+
+			@Override
+			public int compare(AmpFunding o1, AmpFunding o2) {
+
+				if (o1.getAmpFundingId() == null ^ o2.getAmpFundingId() == null) {
+					return (o1.getAmpFundingId() == null) ? -1 : 1;
+				}
+
+				if (o1.getAmpFundingId() == null && o2.getAmpFundingId() == null) {
+					return 0;
+				}
+				return o1.getAmpFundingId().compareTo(o2.getAmpFundingId());
+
+			}
+
+		});
+
+		Iterator<AmpFunding> fSetIt = auxAmpFunding.iterator();
 		while (fSetIt.hasNext()) {
 			AmpFunding funding = (AmpFunding) fSetIt.next();
 			orgsSet.add(funding.getAmpDonorOrgId());
@@ -36,7 +61,7 @@ public class AmpFundingGroupModel implements IModel<Set<AmpOrganisation>> {
 
 	@Override
 	public void setObject(Set<AmpOrganisation> object) {
-		//do nothing
+		// do nothing
 	}
-	
+
 }

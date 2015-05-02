a21ae9d373a3fa9dc59c38e9fe83ee0bef7ba0ff
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-05-02 12:20:33 +0000

    AMP-19972. Number of daysIn Month fix. Code clean up
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27301 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/GlobalSettings.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/GlobalSettings.java
@@ -36,7 +36,6 @@ import org.digijava.module.aim.helper.CountryBean;
 import org.digijava.module.aim.helper.GlobalSettingsConstants;
 import org.digijava.module.aim.helper.KeyValue;
 import org.digijava.module.aim.services.auditcleaner.AuditCleaner;
-import org.digijava.module.aim.util.DbUtil;
 import org.digijava.module.aim.util.FeaturesUtil;
 import org.digijava.module.common.util.DateTimeUtil;
 import org.digijava.module.currencyrates.CurrencyRatesService;
@@ -52,12 +51,12 @@ public class GlobalSettings extends Action {
 		// probably should flush all RCD from all sessions
 		//session.removeAttribute(ArConstants.REPORTS_Z_FILTER);
 		// TODO-CONSTANTIN should we:
-		// 1) clean data for ALL sessions? (correct but disruptive) 
+		// 1) clean data for ALL sessions? (correct but disruptive)
 		// 2) clean data for current session? (kinda useless for an admin)
 		// 3) do nothing?
 		ReportContextData.clearSession();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 	HttpServletRequest request, HttpServletResponse response) throws java.lang.Exception
@@ -72,13 +71,13 @@ public class GlobalSettings extends Action {
 					return mapping.findForward("index");
 				}
 			}
-		
+
 		GlobalSettingsForm gsForm = (GlobalSettingsForm) form;
 		if(request.getParameter("save")!=null){
 			String save = request.getParameter("save");
 			logger.info(" this is the action "+save);
 			flushSessionObjects(session);
-	
+
 			logger.info(" id is "+gsForm.getGlobalId()+"   name is "+gsForm.getGlobalSettingsName()+ "  value is... "+gsForm.getGsfValue());
 			dailyCurrencyRatesChanges(gsForm);
 			this.updateGlobalSetting(gsForm.getGlobalId(), gsForm.getGsfValue());
@@ -86,8 +85,8 @@ public class GlobalSettings extends Action {
 			auditTrialCleanerChanges(gsForm);
 			refreshGlobalSettingsCache	= true;
 		}
-		
-		
+
+
 		if (request.getParameter("saveAll") != null) {
 			flushSessionObjects(session);
 			String allValues = gsForm.getAllValues();
@@ -95,7 +94,7 @@ public class GlobalSettings extends Action {
 			AmpGlobalSettings projectValidationSetting = FeaturesUtil.getGlobalSettingsCache().get(GlobalSettingsConstants.PROJECTS_VALIDATION);
 			while (token.hasMoreTokens()) {
 				String element = token.nextToken();
-				String[] nameValue = element.split("=");				
+				String[] nameValue = element.split("=");
 				Long id = getLongOrNull(nameValue[0]);
 				String newValue = nameValue.length < 2 ? "" : nameValue[1];
 				if (projectValidationSetting.getGlobalId().equals(id) && !newValue.equals(projectValidationSetting.getGlobalSettingsValue())) {
@@ -104,7 +103,7 @@ public class GlobalSettings extends Action {
 				// allow empty fields, like Public Portal URL when Public Portal = false
 				this.updateGlobalSetting(id, newValue);
 			}
-			
+
 			//this.updateGlobalSetting(gsForm.getGlobalId(), gsForm.getGsfValue());
 			//ActionMessages errors = new ActionMessages();
 			refreshGlobalSettingsCache	= true;
@@ -112,22 +111,22 @@ public class GlobalSettings extends Action {
 			auditTrialCleanerChanges();
 			DigiCacheManager.getInstance().getCache(ArConstants.EXCHANGE_RATES_CACHE).clear();
 		}
-		
+
 		List<AmpGlobalSettings> col = FeaturesUtil.getGlobalSettings();
 		if (refreshGlobalSettingsCache) {
 			FeaturesUtil.buildGlobalSettingsCache(col);
 			//FeaturesUtil.logGlobalSettingsCache();
 			org.digijava.module.aim.helper.GlobalSettings globalSettings = (org.digijava.module.aim.helper.GlobalSettings) getServlet().getServletContext().getAttribute(Constants.GLOBAL_SETTINGS);
 	    	globalSettings.setShowComponentFundingByYear(FeaturesUtil.isShowComponentFundingByYear());
-	    	FeaturesUtil.switchLogicInstance();	    	
-	     	
+	    	FeaturesUtil.switchLogicInstance();
+
 	    	ServletContext ampContext = this.getServlet().getServletContext();
-	    	
+
 			AmpTreeVisibility ampTreeVisibility=new AmpTreeVisibility();
 			AmpTemplatesVisibility currentTemplate = FeaturesUtil.getDefaultAmpTemplateVisibility();
 	    	ampTreeVisibility.buildAmpTreeVisibility(currentTemplate);
 	    	FeaturesUtil.setAmpTreeVisibility(ampContext, session,ampTreeVisibility);
-	    	
+
 		}
 		gsForm.setGsfCol(col);
 		for (AmpGlobalSettings ampGS:col)
@@ -137,9 +136,9 @@ public class GlobalSettings extends Action {
 	 	 	{
 	 	 		ampGS.setListOfValues(ampGS.getGlobalSettingsValue().split(";"));
 	 	 	}
-	 	 	
+
 			gsForm.setGlobalSettingType(ampGS.getGlobalSettingsName(), ampGS.getGlobalSettingsPossibleValues());
-			
+
 			/**
 			 *  Getting the name of the criteria for possible values:
 			 *  if v_view_name => the values are taken from the specified view
@@ -164,7 +163,7 @@ public class GlobalSettings extends Action {
 		saveErrors(request, errors);
 		return mapping.findForward("viewGS");
 	}
-	
+
 	/**
 	 * updates workspaces's validation depending whether the GlobalSettings has changed to "off" or  "on"
 	 * @param gsNewValue
@@ -173,7 +172,7 @@ public class GlobalSettings extends Action {
     	String query ="UPDATE " + AmpApplicationSettings.class.getName();
     	if (gsNewValue.toLowerCase().equals("on")) {
     		query+=" SET validation='allEdits' WHERE validation='validationOff'";
-    	} 
+    	}
     	else if (gsNewValue.toLowerCase().equals("off")) {
     		query+=" SET validation='validationOff'";
     	}
@@ -181,7 +180,7 @@ public class GlobalSettings extends Action {
 			.createQuery(query)
 			.executeUpdate();
     }
-    
+
 	/**
 	 * parses a String as a Long. Returns null in case it fails to do so
 	 * @param s
@@ -196,10 +195,10 @@ public class GlobalSettings extends Action {
 
 	@SuppressWarnings("unchecked")
 	private void dailyCurrencyRatesChanges() {
-		
+
 		String value = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.DAILY_CURRENCY_RATES_UPDATE_ENALBLED);
 		boolean update = (value.compareToIgnoreCase("On") == 0);
-		
+
 		String hour = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.DAILY_CURRENCY_RATES_UPDATE_HOUR);
 		String timeout = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.DAILY_CURRENCY_RATES_UPDATE_TIMEOUT);
 
@@ -210,9 +209,9 @@ public class GlobalSettings extends Action {
 			CurrencyRatesService.stopCurrencyRatesService();
 		}
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private void auditTrialCleanerChanges() {
 		String value = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.AUTOMATIC_AUDIT_LOGGER_CLEANUP);
@@ -226,9 +225,9 @@ public class GlobalSettings extends Action {
 			}
 		}
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param gsForm
 	 */
 	private void auditTrialCleanerChanges(GlobalSettingsForm gsForm){
@@ -244,7 +243,7 @@ public class GlobalSettings extends Action {
 			}
 		}
 	}
-	
+
 	/**
 	 * @param gsForm
 	 */
@@ -261,7 +260,7 @@ public class GlobalSettings extends Action {
 					String timeout=null;
 					for(AmpGlobalSettings ampGS : ampGSCollection)
 					{
-						
+
 						if(ampGS.getGlobalSettingsName().compareTo(GlobalSettingsConstants.DAILY_CURRENCY_RATES_UPDATE_HOUR)==0){
 							hour=ampGS.getGlobalSettingsValue();
 						}
@@ -270,7 +269,7 @@ public class GlobalSettings extends Action {
 						}
 					}
 					CurrencyRatesService.startCurrencyRatesService(hour, timeout );
-					
+
 				}else{
 					CurrencyRatesService.stopCurrencyRatesService();
 				}
@@ -295,7 +294,7 @@ public class GlobalSettings extends Action {
 	private Collection<KeyValue> getPossibleValues(String tableName)
 	{
 		Collection<KeyValue> ret 	= new Vector<KeyValue>();
-	
+
 		if (tableName == null || tableName.length() == 0)
 			return ret;
 
@@ -313,7 +312,7 @@ public class GlobalSettings extends Action {
 		String qryStr 		= null;
 		Query qry 			= null;
 		Transaction tx		= null;
-		try{
+		try {
 				session					= PersistenceManager.getSession();
 //beginTransaction();
 
@@ -332,14 +331,7 @@ public class GlobalSettings extends Action {
 		catch (Exception ex) {
 			logger.error("Exception : " + ex.getMessage());
 			ex.printStackTrace(System.out);
-			if (tx != null) {
-				try {
-					tx.rollback();
-				} catch (Exception rbf) {
-					logger.error("Rollback failed !");
-				}
-			}
-		}
+        }
 	}
 	/**
 	 *
@@ -351,11 +343,10 @@ public class GlobalSettings extends Action {
 		String criterion		= ags.getGlobalSettingsPossibleValues();
 		if ( criterion!=null && criterion.startsWith("t_")  ) {
 			if (criterion.equals("t_Integer") || criterion.equals("t_static_range") ){
-				try{
+				try {
 					Integer.parseInt(value);
 					return true;
-				}
-				catch(Exception E) { // value is not an integer
+				} catch(Exception E) { // value is not an integer
 					ActionMessage ae	= new ActionMessage("error.aim.globalSettings.valueIsNotOfType", criterion.substring(2));
 					errors.add("title", ae);
 					return false;
@@ -365,10 +356,8 @@ public class GlobalSettings extends Action {
 					criterion.equals("t_year_default_start")||criterion.equals("t_year_default_end")){
 				try{
 					int intValue	= Integer.parseInt(value);
-					if (intValue!=-1 && (intValue < 1000 || intValue > 2999  ))
-						return false;
-					return true;
-				}
+                    return !(intValue != -1 && (intValue < 1000 || intValue > 2999));
+                }
 				catch(Exception E) { // value is not a year
 					ActionMessage ae	= new ActionMessage("error.aim.globalSettings.valueIsNotOfType", criterion.substring(2));
 					errors.add("title", ae);
@@ -390,32 +379,52 @@ public class GlobalSettings extends Action {
 		}
 		return true;
 	}
-	/*POTENTIAL_BUG: this doesn't take into account leap years*/
-	public static int numOfDaysInMonth(int month) {
-		if (month > 0 && month < 13) {
-			int maxDays	= 0;
-			switch (month) {
-				case 1: ;
-				case 3: ;
-				case 5: ;
-				case 7: ;
-				case 8: ;
-				case 10: ;
-				case 12:
-					maxDays	= 31;
-					break;
-				case 4: ;
-				case 6: ;
-				case 9: ;
-				case 11: 
-					maxDays = 30;
-					break;
-				case 2: 
-					maxDays = 28;
-					break;
-			}
-			return maxDays;
-		}
-		return 0;
-	}
+
+    /**
+     * This method is potentially dangerous since does not consider leap years
+     * Do not use java.util.Calendar since it will be much slower
+     * @param month number of month starting from 1
+     * @return
+     */
+    public static int numOfDaysInMonth(int month) {
+        return numOfDaysInMonth(month, -1);
+    }
+
+    /**
+     * Do not use java.util.Calendar since it will be much slower
+     * @param month number of month starting from 1
+     * @param year the year (leap year matters)
+     * @return
+     */
+    public static int numOfDaysInMonth(int month, int year) {
+        if (month > 0 && month < 13) {
+            int maxDays	= 0;
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    maxDays	= 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    maxDays = 30;
+                    break;
+                case 2:
+                    if (year > 0) {
+                        maxDays = year % 4 == 0 ? 29 : 28;
+                    } else {
+                        maxDays = 28;
+                    }
+                    break;
+            }
+            return maxDays;
+        }
+        return 0;
+    }
 }

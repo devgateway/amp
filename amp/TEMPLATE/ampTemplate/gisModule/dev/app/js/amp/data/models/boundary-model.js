43fd2992cda9f061aaa5959340cfe905d67761d4
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-05-14 20:43:33 +0000

    AMP-19917 Generate static boundary file endpoints for active AMP country on tomcat startup
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27416 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/boundary-model.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/boundary-model.js
@@ -6,7 +6,7 @@ module.exports = Backbone.Model
 .extend(LoadOnceMixin).extend({
   // temporary so we get path to static files..
   url: function() {
-    return '/TEMPLATE/ampTemplate/gisModule/dev/app/mock-api/data/boundaries/' + this.id + '.json';
+    return '/TEMPLATE/ampTemplate/gisModule/dev/app/mock-api/data/boundaries/'+ this.get("country") + '/' + this.id + '.json';
   }
 
 });

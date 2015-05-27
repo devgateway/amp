8133da026618cf4baa9479fa9c214164472c137a
vchihai <vchihai@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-05-27 14:21:44 +0000

    AMP-18973
    Fixed percentage (setting null value) when percentage field is disabled on form.
    Fixed the issue regarding the possibility to not add more organizations when editing an activity
    
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27497 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpRelatedOrganizationsBaseTableFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpRelatedOrganizationsBaseTableFeature.java
@@ -84,7 +84,8 @@ public class AmpRelatedOrganizationsBaseTableFeature extends AmpFormTableFeature
 	 */
 	public void roleAdded(AjaxRequestTarget target, AmpOrgRole ampOrgRole) {
 		orgAddedOrRemoved = true;
-        changeSearchVisibility(target);
+		updateSearchVisibility();
+		target.add(searchOrganization);
         addFundingAutomatically(target, ampOrgRole);
 	}
 
@@ -94,7 +95,8 @@ public class AmpRelatedOrganizationsBaseTableFeature extends AmpFormTableFeature
      */
     public void roleRemoved(AjaxRequestTarget target,AmpOrgRole ampOrgRole) {
     	orgAddedOrRemoved = true;
-        changeSearchVisibility(target);
+    	updateSearchVisibility();
+    	target.add(searchOrganization);
         removeFundingAutomatically(target, ampOrgRole);
     }
 
@@ -132,7 +134,7 @@ public class AmpRelatedOrganizationsBaseTableFeature extends AmpFormTableFeature
         }
     }
 
-    private void changeSearchVisibility(AjaxRequestTarget target) {
+    private void updateSearchVisibility() {
         if (maxSizeCollectionValidationField.isVisible()){
             List<AmpOrgRole> tmpList = listModel.getObject();
             if (tmpList != null && tmpList.size() > 0){
@@ -141,7 +143,6 @@ public class AmpRelatedOrganizationsBaseTableFeature extends AmpFormTableFeature
             else{
                 searchOrganization.setVisibilityAllowed(true);
             }
-            target.add(searchOrganization);
         }
     }
 
@@ -286,10 +287,13 @@ public class AmpRelatedOrganizationsBaseTableFeature extends AmpFormTableFeature
 				ampOrgRole.setActivity(am.getObject());
 				ampOrgRole.setRole(specificRole);
 				//ampOrgRole.setAmpOrgRoleId(choice.getAmpOrgId());
-				if(list.getObject().size()>0)
-					ampOrgRole.setPercentage(0f);
-				else
-					ampOrgRole.setPercentage(100f);
+				if (percentageValidationField.isVisible()) {
+					if(list.getObject().size()>0)
+						ampOrgRole.setPercentage(0f);
+					else
+						ampOrgRole.setPercentage(100f);
+				}
+				
 				if (setModel.getObject() == null)
                     setModel.setObject(new HashSet<AmpOrgRole>());
                 
@@ -317,6 +321,9 @@ public class AmpRelatedOrganizationsBaseTableFeature extends AmpFormTableFeature
 		};
 		searchOrgs.add(UpdateEventBehavior.of(FundingOrgListUpdateEvent.class));
     	searchOrganization = new AmpSearchOrganizationComponent<String>("search", new Model<String> (), "Search Organizations", searchOrgs, availableOrgGroupChoices);
+    	
+    	updateSearchVisibility();
+    	
 		add(searchOrganization);
 	}
 //	private boolean existOrganization(Set<AmpOrgRole> set, AmpOrgRole ampOrgRole) {

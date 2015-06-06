162638f1b9f8bf8fd381f9112aa27d7426a26b4e
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-06 22:02:10 +0000

    AMP-20292 Projects list based on user right to edit
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27585 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/interchange/InterchangeEndpoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/interchange/InterchangeEndpoints.java
@@ -1,13 +1,20 @@
 package org.digijava.kernel.ampapi.endpoints.interchange;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
+import org.digijava.kernel.request.TLSUtils;
+import org.digijava.module.aim.helper.Constants;
+import org.digijava.module.aim.helper.TeamMember;
 
 
 /**
@@ -18,6 +25,10 @@ import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
 
 @Path("interchange")
 public class InterchangeEndpoints {
+	
+	@Context
+	private HttpServletRequest httpRequest;
+
 	
 	@GET
 	@Path("/fields")
@@ -31,7 +42,14 @@ public class InterchangeEndpoints {
 	@GET
 	@Path("/projects")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public List<JsonBean> getProjects() {
-		return InterchangeUtils.getActivityList();
+	public Collection<JsonBean> getProjects() {
+		TeamMember tm = (TeamMember) TLSUtils.getRequest().getSession().getAttribute(Constants.CURRENT_MEMBER);
+		String ampAdmin = (String) httpRequest.getSession().getAttribute("ampAdmin");
+		boolean isAdmin = ampAdmin!=null && ampAdmin.equals("yes");
+		//Admin users or users that didn't select a workspace get an empty list
+		if (isAdmin || tm == null) {
+			return new ArrayList<JsonBean>();
+		}
+		return InterchangeUtils.getActivityList(tm,TLSUtils.getRequest().getSession());
 	}
 }

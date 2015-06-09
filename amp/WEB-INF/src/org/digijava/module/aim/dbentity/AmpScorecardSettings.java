f57092a5b8352dcd5a5b88c6420a310f2b7fce70
vchihai <vchihai@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-09 12:01:55 +0000

    AMP-20313
    The collection is instantiated in AmpScorecardSettings to avoid null
    
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27598 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpScorecardSettings.java
+++ amp/WEB-INF/src/org/digijava/module/aim/dbentity/AmpScorecardSettings.java
@@ -1,9 +1,10 @@
 package org.digijava.module.aim.dbentity;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
-public class AmpScorecardSettings implements Serializable{
+public class AmpScorecardSettings implements Serializable {
 
 	/**
 	 * 
@@ -13,35 +14,44 @@ public class AmpScorecardSettings implements Serializable{
 	private Boolean validationPeriod;
 	private Integer validationTime;
 	private Double percentageThreshold;
-	private Set<AmpScorecardSettingsCategoryValue> closedStatuses;
-	
+	private Set<AmpScorecardSettingsCategoryValue> closedStatuses = new HashSet<AmpScorecardSettingsCategoryValue>();
+
 	public Long getAmpScorecardSettingsId() {
 		return ampScorecardSettingsId;
 	}
+
 	public void setAmpScorecardSettingsId(Long ampScorecardSettingsId) {
 		this.ampScorecardSettingsId = ampScorecardSettingsId;
 	}
+
 	public Boolean getValidationPeriod() {
 		return validationPeriod;
 	}
+
 	public void setValidationPeriod(Boolean validationPeriod) {
 		this.validationPeriod = validationPeriod;
 	}
+
 	public Integer getValidationTime() {
 		return validationTime;
 	}
+
 	public void setValidationTime(Integer validationTime) {
 		this.validationTime = validationTime;
 	}
+
 	public Set<AmpScorecardSettingsCategoryValue> getClosedStatuses() {
 		return closedStatuses;
 	}
+
 	public void setClosedStatuses(Set<AmpScorecardSettingsCategoryValue> closedStatuses) {
 		this.closedStatuses = closedStatuses;
 	}
+
 	public Double getPercentageThreshold() {
 		return percentageThreshold;
 	}
+
 	public void setPercentageThreshold(Double percentageThreshold) {
 		this.percentageThreshold = percentageThreshold;
 	}

a95750ea0ab1b66e43e3054b5d702e8743c513dc
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-15 14:33:55 +0000

    AMP-20292 Projects list based on user right to edit
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27647 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/interchange/InterchangeEndpoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/interchange/InterchangeEndpoints.java
@@ -39,21 +39,37 @@ public class InterchangeEndpoints {
 		return InterchangeUtils.getAllAvailableFields();
 	}
 	
-	
+	/**
+	 * Returns a Json object with the list of all projects on the system, including its view and edit status for the current logged user.
+	 * If the user can view the project, the 'view' property of the project is set to true. False otherwise.
+	 * If the user can edit the project, the 'edit' property of the project on the json is set to true. False otherwise.
+	 * Pagination can be used if the parameters are sent on the request. If not parameters are sent, the full list
+	 * of projects is returned. 
+	 * 
+	 * 
+	 * @param pid  current pagination request reference (random id). It acts as a key for a LRU caching mechanism that holds the 
+	 * full list of projects for the current user. If it is not provided no caching is used
+	 * @param offset, Integer used for pagination. It represents which is the first project to return. It helps to skip the unnecessary 
+	 * records.
+	 * @param size, Integer used for pagination. It tells how many projects to return
+	 * @return list of JsonBean with all the projects on the system
+	 */
 	@GET
 	@Path("/projects")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	public Collection<JsonBean> getProjects(@QueryParam ("pid") String pid,@QueryParam("offset") Integer startFrom, @QueryParam("count") Integer size) {
+	public Collection<JsonBean> getProjects(@QueryParam ("pid") String pid,@QueryParam("offset") Integer offset, @QueryParam("count") Integer count) {
 		TeamMember tm = (TeamMember) TLSUtils.getRequest().getSession().getAttribute(Constants.CURRENT_MEMBER);
 		Collection<JsonBean> activityCollection = InterchangeUtils.getActivityList(pid, tm);
 		int start = 0;
 		int end = activityCollection.size() - 1;
-		if (startFrom != null && size != null && startFrom < activityCollection.size()) {
-			start = startFrom.intValue();
-			if (activityCollection.size() > (startFrom + size)) {
-				end = startFrom + size;
+		if (offset != null && count != null && offset < activityCollection.size()) {
+			start = offset.intValue();
+			if (activityCollection.size() > (offset + count)) {
+				end = offset + count;
 			}
 		}
 		return new ArrayList(activityCollection).subList(start, end);
 	}
+	
+	
 }

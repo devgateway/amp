8cbe5a56dc6ac51229e6f0cb85fb654d991cbbff
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-16 22:46:37 +0000

    AMP-20388 Issue_in case if proejct is deleted , scorecard calculation will be wrong
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27659 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/scorecard/service/ScorecardService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/scorecard/service/ScorecardService.java
@@ -376,7 +376,7 @@ public class ScorecardService {
 						threshold = DEFAULT_THRESHOLD;
 					}
 					String quarterEndDate = new SimpleDateFormat("yyyy-MM-dd").format(quarter.getQuarterEndDate());
-
+					//Get total (not completed nor deleted) activities  by donor at the end of a given quarter
 					String query = "select count (distinct (a.amp_id)) as total_activities,r.organisation as donor_id "
 							+ "from amp_activity_version a, amp_org_role r,amp_organisation o  WHERE  r.activity=a.amp_activity_id ";
 
@@ -390,6 +390,7 @@ public class ScorecardService {
 							+ " AND    (( af.source_role_id IS NULL) "
 							+ " OR     af.source_role_id =( SELECT amp_role_id         FROM   amp_role "
 							+ " WHERE  role_code='DN')))) " + " and date_created <= '" + quarterEndDate + "' "
+							+ " AND a.deleted is false " 
 							+ " group by r.organisation; ";
 
 					try (RsInfo rsi = SQLUtils.rawRunQuery(conn, query, null)) {

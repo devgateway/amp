5e9af8c246dc2f297e0110620e70802a987620c4
vchihai <vchihai@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-22 15:30:39 +0000

    AMP-20323
    Made some refactoring
    
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27729 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/errors/ApiError.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/errors/ApiError.java
@@ -1,7 +1,6 @@
 package org.digijava.kernel.ampapi.endpoints.errors;
 
 import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,15 +15,15 @@ import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
 
 public class ApiError {
 	
-	public final static String GENERAL_ERROR_CODE = "0";
-	public final static String UNHANDLED_ERROR_CODE = "1";
+	public final static String GENERAL_ERROR_CODE = "0000";
+	public final static String UNHANDLED_ERROR_CODE = "0100";
+	public final static String JSON_ERROR_CODE = "error";
 	
 	/**  Will store the mapping between the component and it's Id (C). */
-	public static final Map<String, Integer> COMPONENT_ID_CLASS_MAP = new HashMap<String, Integer>();
-	
-	static {
-		COMPONENT_ID_CLASS_MAP.put(InterchangeEndpoints.class.getName(), 1);
-	}
+	public static final Map<String, Integer> COMPONENT_ID_CLASS_MAP = new HashMap<String, Integer>() {{
+		put(InterchangeEndpoints.class.getName(), 1);
+		put(ApiError.class.getName(), 2);
+	}};
 	
 	/**
 	 * Returns a JSON object with a single error message  => generic 0 error code with one error in the list.
@@ -35,7 +34,7 @@ public class ApiError {
 		JsonBean errorBean = new JsonBean();
 		errorBean.set(GENERAL_ERROR_CODE, new String[] {errorMessage});
 		
-		return errorBean;
+		return getResultErrorBean(errorBean);
 	};
 	
 	/**
@@ -46,33 +45,33 @@ public class ApiError {
 	 * @return the JSON object of the error. E.g.: {0: [“Generic error 1”, “Generic error 2”], 135: [“Forbidden fields have been configured: sector_id”], 123: [“Generic error 1”]}
 	 */
 	public static JsonBean toError(List<?> errorMessages) {
-		JsonBean errorBean = new JsonBean();
 		Map<String, List<String>> errors = new HashMap<String, List<String>>();
 		
-		for(Object errorMessage : errorMessages) {
-			if (errorMessage instanceof String) {
-				if (errors.get(GENERAL_ERROR_CODE) == null) {
-					errors.put(GENERAL_ERROR_CODE, new ArrayList<String>());
+		if (errorMessages != null && errorMessages.size() > 0) {
+			if (errorMessages.get(0) instanceof String) {
+				for(Object errorMessage : errorMessages) {
+					if (errors.get(GENERAL_ERROR_CODE) == null) {
+						errors.put(GENERAL_ERROR_CODE, new ArrayList<String>());
+					}
+					
+					errors.get(GENERAL_ERROR_CODE).add((String) errorMessage);
 				}
-				
-				errors.get(GENERAL_ERROR_CODE).add((String) errorMessage);
-			} else if (errorMessage instanceof ApiErrorMessage) {
-				ApiErrorMessage apiError = (ApiErrorMessage) errorMessage;
-				String errorId = getErrorId(apiError);
-				
-				if (errors.get(errorId) == null) {
-					errors.put(errorId, new ArrayList<String>());
+			} else if (errorMessages.get(0) instanceof ApiErrorMessage) {
+				int componentId = getErrorComponentId((ApiErrorMessage) errorMessages.get(0));
+				for(Object errorMessage : errorMessages) {
+					ApiErrorMessage apiError = (ApiErrorMessage) errorMessage;
+					String errorId = getErrorId(componentId, apiError.id);
+					
+					if (errors.get(errorId) == null) {
+						errors.put(errorId, new ArrayList<String>());
+					}
+					
+					errors.get(errorId).add(getErrorText(apiError));
 				}
-				
-				errors.get(errorId).add(getErrorText(apiError));
 			}
 		}
 		
-		for (String errorId : errors.keySet()) {
-			errorBean.set(errorId, errors.get(errorId).toArray());
-		}
-		
-		return errorBean;
+		return getResultErrorBean(errors);
 	};
 	
 	/**
@@ -82,33 +81,43 @@ public class ApiError {
 	 */
 	public static JsonBean toError(ApiErrorMessage apiErrorMessage) {
 		JsonBean errorBean = new JsonBean();
+		errorBean.set(getErrorId(getErrorComponentId(apiErrorMessage), apiErrorMessage.id), new String[] {getErrorText(apiErrorMessage)});
 		
-		errorBean.set(getErrorId(apiErrorMessage), new String[] {getErrorText(apiErrorMessage)});
-		
-		return errorBean;
+		return getResultErrorBean(errorBean);
 	};
+	
+	private static JsonBean getResultErrorBean(Object errorBean) {
+		JsonBean resultErrorBean = new JsonBean();
+		resultErrorBean.set(JSON_ERROR_CODE, errorBean);
+		
+		return resultErrorBean;
+	}
 	
 	/**
 	 * Returns the id of the ApiErrorMessage object. A lookup for the class code will be made on the stacktrace . 
 	 * @param ApiErrorMessage object
 	 * @return the id of the error
 	 */
-	private static String getErrorId(ApiErrorMessage apiErrorMessage) {
+	private static String getErrorId(int componentId, int errorId) {
+		if (componentId != 0) {
+			return String.format("%02d%02d", componentId, errorId);
+		} else {
+			return GENERAL_ERROR_CODE;
+		}
+	}
+	
+	private static Integer getErrorComponentId(ApiErrorMessage apiErrorMessage) {
 		StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-		StringBuilder sb = new StringBuilder(); 
 		
 		for (StackTraceElement st : stackTrace) {
-			sb.append(st.toString());
-		}
-		
-		for (String componentName : COMPONENT_ID_CLASS_MAP.keySet()) {
-			if (sb.toString().indexOf((componentName)) > 0) {
-				String errorId = "" +  COMPONENT_ID_CLASS_MAP.get(componentName) + apiErrorMessage.id;
-				return errorId;
+			for (String componentName : COMPONENT_ID_CLASS_MAP.keySet()) {
+				if (st.toString().indexOf((componentName)) >= 0) {
+					return COMPONENT_ID_CLASS_MAP.get(componentName);
+				}
 			}
 		}
 		
-		return GENERAL_ERROR_CODE + apiErrorMessage.id;
+		return 0;
 	}
 	
 	/**
@@ -118,14 +127,17 @@ public class ApiError {
 	 * @return the message of the error
 	 */
 	private static String getErrorText(ApiErrorMessage apiErrorMessage) {
-		
-		StringBuilder sb = new StringBuilder();
-		Formatter formatter = new Formatter(sb);
-		formatter.format(apiErrorMessage.prefix, apiErrorMessage.value);
-		formatter.close();
-		
-		String errorText = sb + " (" + apiErrorMessage.description + ")";
-		
-		return errorText;
+		if (apiErrorMessage.prefix != null && apiErrorMessage.value != null) {
+			String errorText = "";
+			try {
+				errorText = String.format(apiErrorMessage.prefix, apiErrorMessage.value) + " (" + apiErrorMessage.description + ")";
+			} catch(Exception e) {
+				errorText = apiErrorMessage.description + " (Exception during formatting the error message - " + e.getMessage() + ")";
+			}
+			
+			return errorText;
+		} 
+			
+		return apiErrorMessage.description;
 	}
 }

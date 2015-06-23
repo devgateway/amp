3d073ab410ea288ce5d3e840219f789b704e9bbb
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-23 13:55:10 +0000

    AMP-20374: Issue - Pagination - When applied filters are reset, page starts at 0 page and not 1, thus it doesn't show results
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27751 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/saikuui/js/saiku/plugins/AMPFilters/plugin.js
+++ amp/TEMPLATE/ampTemplate/saikuui/js/saiku/plugins/AMPFilters/plugin.js
@@ -24,6 +24,7 @@ var AMPFilters = Backbone.View.extend({
 
 				window.currentFilter = new ampFilter({el:$('#filters-container'), draggable:true, caller: 'REPORTS'});
 				window.currentFilter.showFilters();
+				window.currentFilter.converted = false;
 				$('#filters-container').hide();
 
 				Saiku.events.listenTo(window.currentFilter, 'cancel', function() {
@@ -35,10 +36,10 @@ var AMPFilters = Backbone.View.extend({
 					$('#filters-container').hide();
 				});
 				Saiku.events.listenTo(window.currentFilter, 'apply', function(data) {
-					if(data.columnFilters || data.otherFilters) {
+					//if(data.columnFilters || data.otherFilters) {
 						self.workspace.query.run_query(window.currentFilter.serialize(), null);
 						self.filters_button.removeClass('on');
-					}
+					//}
 					$('#filters-container').hide();
 				});
 			},
@@ -65,7 +66,7 @@ var AMPFilters = Backbone.View.extend({
 				if ($(event.target).hasClass('on')) {
 					// AMP-18921: workaround to the filters until they will be properly initialized, 
 					// that should be done as part of filters widget improvement as a whole
-					this.workspace.query.initFilters();
+					//this.workspace.query.initFilters();
 					$('#filters-container').show();
 				} else {
 					$('#filters-container').hide();
@@ -184,6 +185,114 @@ FilterUtils.convertJavaFiltersToJS = function(data) {
 	return blob;
 };*/
 
+FilterUtils.getDateIntervalType = function(element) {
+	var min = element.min;
+	var max = element.max;
+	if (min === undefined)
+		return "max";
+	if (max === undefined)
+		return "min";
+	if (element.valueToName.min === undefined) {
+		return "max";
+	}
+	if (element.valueToName.max === undefined) {
+		return "min";
+	}		
+	return "both";
+};
+
+FilterUtils.extractFilters = function(content) {
+	var filters = new Array();
+	var filtersColumnsJson = content.columnFilterRules;
+	var keys = [];
+	for(var k in filtersColumnsJson) keys.push(k);
+	$(keys).each(function(i, item) {
+		var subElement = filtersColumnsJson[item];
+		if (subElement instanceof Array) {
+			var element = subElement[0];
+			var content = [];
+			if (element.name === 'DATE') {
+				var dateIntervalType = FilterUtils.getDateIntervalType(element, item, i);
+			}				
+			if (element.value !== null) {
+				var auxItem = {};
+				auxItem.id = element.value;
+				auxItem.name = element.valueToName[element.value];
+				content.push(auxItem);
+			} else if (element.valueToName !== null) {
+				// This should be .models but the way the endpoint returns
+				// the data breaks backbone.
+				_.each(element.valueToName, function(item_, i) {
+					// Need to do this because of how js parses these data
+					// and adds an extra element.
+					if (i !== undefined && item_ !== undefined) {
+						var item = {};
+						item.id = i;
+						item.name = item_;
+						if (dateIntervalType !== undefined)
+							item.dateIntervalType = dateIntervalType;
+						content.push(item);
+					}
+				});
+			}
+			//translate filter values
+			_.each(content,function(item, i) {
+				//for now only true or false were asked to be translated. 
+				//Avoid doing a ajax call for all values if we only need 2.
+				if (item.name === "true" || item.name === "false") {
+					item.trnName = item.name/*TranslationManager.getTranslated(item.name)*/;
+				 }
+				else {
+					item.trnName = item.name;
+				}
+			});
+			var auxFilter = {
+				trnName : item/*TranslationManager.getTranslated(item)*/,
+				name: item,
+				values : content
+			};
+			filters.push(auxFilter);
+		}
+	});
+
+	var filtersDateColumnsJson = content.columnDateFilterRules;
+	var keys = [];
+	for(var k in filtersDateColumnsJson) keys.push(k);
+	$(keys).each(function(i, item) {
+		var subElement = filtersDateColumnsJson[item];
+		if (subElement instanceof Array) {
+			var element = subElement.models[0];
+			var content = [];	
+			if (element.value !== null) {
+				var auxItem = {};
+				auxItem.id = element.value;
+				auxItem.name = element.valueToName[element.value];
+				content.push(auxItem);
+			} else if (element.valueToName !== null) {
+				// This should be .models but the way the endpoint returns
+				// the data breaks backbone.
+				_.each(element.valueToName, function(j, item2) {
+					// Need to do this because of how js parses these data
+					// and adds an extra element.
+					if (j !== undefined) {
+						var item_ = {};
+						item_.id = i;
+						item_.name = j;
+						content.push(item_);
+					}
+				});
+			}
+			var auxFilter = {
+				trnName : item/*TranslationManager.getTranslated(item)*/,
+				name: item,
+				values : content
+			};
+			filters.push(auxFilter);
+		}
+	});
+	return filters;
+};
+
 FilterUtils.convertJavaFiltersToJS = function(data) {
 	// Define some basic defaults needed in the widget filter.
 	var blob = {
@@ -197,40 +306,8 @@ FilterUtils.convertJavaFiltersToJS = function(data) {
 			"Donor Id" : []
 		}
 	};
-	_.each(data.columnDateFilterRules, function(item, i) {
-		switch (i) {
-		case 'Actual Completion Date':
-			var newDate = {};
-			var i = -1;
-			_.map(item[0]['valueToName'], function(item_) {
-				i++;
-				if(i == 0) {
-					newDate['start'] = item_;
-				} else if(i == 1) {
-					newDate['end'] = item_;
-				}
-				return newDate;
-			});
-			blob.otherFilters['Actual Completion Date'] = newDate;
-			break;
-		case 'Actual Start Date':
-			var newDate = {};
-			var i = -1;
-			_.map(item[0]['valueToName'], function(item_) {
-				i++;
-				if(i == 0) {
-					newDate['start'] = item_;
-				} else if(i == 1) {
-					newDate['end'] = item_;
-				}
-				return newDate;
-			});
-			blob.otherFilters['Actual Start Date'] = newDate;
-			break;
-		}
-	});
-	_.each(data.columnFilterRules, function(item, i) {
-		switch (i) {
+	_.each(data, function(item, i) {
+		switch (item.name) {
 
 		// cases where columnFilter matches item name
 		case 'Responsible Organization':
@@ -244,8 +321,9 @@ FilterUtils.convertJavaFiltersToJS = function(data) {
 		case 'On/Off/Treasury Budget':
 		case 'Zone':
 		case 'Region':
-			blob.columnFilters[i] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+		case 'District':
+			blob.columnFilters[item.name] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;
 
@@ -254,42 +332,46 @@ FilterUtils.convertJavaFiltersToJS = function(data) {
 		case 'Executing Agency':
 		case 'Implementing Agency':
 		case 'Beneficiary Agency':
-			blob.columnFilters[i+ ' Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters[item.name + ' Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;
 		case 'National Planning Objectives':
-			blob.columnFilters['National Planning Objectives Level 1 Id'] = _.map(item[0]['values'], function(
+			blob.columnFilters['National Planning Objectives Level 1 Id'] = _.map(item.values, function(
 					item_) {
-				return parseInt(item_);
+				return parseInt(item_.id);
 			});
 			blob.columnFilters['National Planning Objectives Level 2 Id'] = blob.columnFilters['National Planning Objectives Level 1 Id'];
 			break;
 		case 'Primary Program':
-			blob.columnFilters['Primary Program Level 1 Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Primary Program Level 1 Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;
 		case 'Secondary Program':
-			blob.columnFilters['Secondary Program Level 3 Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Secondary Program Level 3 Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;
 		case 'Donor Agency':
-			blob.columnFilters['Donor Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Donor Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;
+		case 'Funding Organization':
+			blob.columnFilters['Donor Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
+			});
 		/*case 'Contracting Agency Groups':
-			blob.columnFilters['Contracting Agency Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Contracting Agency Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;*/
 		case 'Primary Sector':
 			// NOTE: Since the filter widget (arbitrarily) uses 3 different fields for Primary Sectors we
 			// triplicate the values coming from the endpoint.
-			blob.columnFilters['Primary Sector Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Primary Sector Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			blob.columnFilters['Primary Sector Sub-Sector Id'] = blob.columnFilters['Primary Sector Id'];
 			blob.columnFilters['Primary Sector Sub-Sub-Sector Id'] = blob.columnFilters['Primary Sector Id'];
@@ -297,8 +379,8 @@ FilterUtils.convertJavaFiltersToJS = function(data) {
 		case 'Secondary Sector':
 			// NOTE: Since the filter widget (arbitrarily) uses 3 different fields for Secondary Sectors we
 			// triplicate the values coming from the endpoint.
-			blob.columnFilters['Secondary Sector Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Secondary Sector Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			blob.columnFilters['Secondary Sector Sub-Sector Id'] = blob.columnFilters['Secondary Sector Id'];
 			blob.columnFilters['Secondary Sector Sub-Sub-Sector Id'] = blob.columnFilters['Secondary Sector Id'];
@@ -315,47 +397,43 @@ FilterUtils.convertJavaFiltersToJS = function(data) {
 		case 'DATE':
 			//FilterUtils.fillDateBlob(blob.otherFilters.date, item.attributes);
 			var newDate = {};
-			var i = -1;
-			_.map(item[0]['valueToName'], function(item_) {
-				i++;
-				if(i == 0) {
-					newDate['start'] = item_;
-				} else if(i == 1) {
-					newDate['end'] = item_;
+			_.map(item.values, function(item_, i) {						
+				if(i === 0) {
+					newDate['start'] = item_.name;
+				} else if(i === 1) {
+					newDate['end'] = item_.name;
 				}
 				return newDate;
 			});
 			blob.otherFilters['date'] = newDate;
 			break;
 		case 'Tertiary Sector':
-			blob.columnFilters['Tertiary Program Level 1 Id'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Tertiary Program Level 1 Id'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;					
 		case 'Team':
-			blob.columnFilters['Workspaces'] = _.map(item[0]['values'], function(item_) {
-				return parseInt(item_);
+			blob.columnFilters['Workspaces'] = _.map(item.values, function(item_) {
+				return parseInt(item_.id);
 			});
 			break;
 		case 'Actual Start Date':
 		case 'Actual Completion Date':
 			var newDate = {};
-			_.map(item[0]['values'], function(item_, i) {						
-				if(i == 0) {
+			_.map(item.values, function(item_, i) {						
+				if(i === 0) {
 					newDate['start'] = item_.name;
-				} else if(i == 1) {
+				} else if(i === 1) {
 					newDate['end'] = item_.name;
 				}
 				return newDate;
 			});
-			blob.otherFilters[i] = newDate;
+			blob.otherFilters[item.name] = newDate;
 			break;
 		default:
-			console.error(i + "-"+item);
+			console.error(item);
 			break;
 		}
 	});
-	console.log("use blob");
-	console.log(blob);
 	return blob;
 };

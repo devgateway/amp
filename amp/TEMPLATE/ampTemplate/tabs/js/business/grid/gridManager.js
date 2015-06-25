f42f89efdce98f734b4479846ee7cc98b334df51
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-25 18:38:54 +0000

    AMP-20478: Long text in tabs should be truncated and ignore style of editor
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27808 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/tabs/js/business/grid/gridManager.js
+++ amp/TEMPLATE/ampTemplate/tabs/js/business/grid/gridManager.js
@@ -187,6 +187,14 @@ define([ 'business/grid/columnsMapping', 'business/translations/translationManag
 								row.className = className + ' status_1';
 								jQuery(row.cells[0]).html(link);
 								
+								// Shorten texts.
+								for (var j = 0; j < colModel.length; j++) {
+									var cell = row.cells[j];
+									if (jQuery(cell).css('display') !== 'none') {
+										cell.textContent = shortenTexts(cell.textContent);
+									}
+								}
+								
 								//check public view - no team present
 								if(!teamid) continue;
 								
@@ -294,7 +302,7 @@ define([ 'business/grid/columnsMapping', 'business/translations/translationManag
 							
 							TranslationManager.searchAndTranslate();
 						},
-						loadComplete : function() {
+						loadComplete : function() {						
 							// Calculate footer totals row for this page.
 							var colData = {};
 							var totalColumnIndex = colModel.length - 1;
@@ -406,8 +414,8 @@ define([ 'business/grid/columnsMapping', 'business/translations/translationManag
 				postProcessHierarchies(rows, hierarchies);
 			}
 		}
-		// console.log(rows);
-		// console.warn(partialTotals);
+		//console.log(rows);
+		//console.warn(partialTotals);
 		return rows;
 	}
 
@@ -523,6 +531,14 @@ define([ 'business/grid/columnsMapping', 'business/translations/translationManag
 			}
 		});
 		return ret;
+	}
+	
+	function shortenTexts(text) {
+		var max = 100;
+		if (text !== undefined && text !== null && text.length > max) {
+			return text.substring(0, max) + "...";
+		}
+		return text;
 	}
 
 	return GridManager;

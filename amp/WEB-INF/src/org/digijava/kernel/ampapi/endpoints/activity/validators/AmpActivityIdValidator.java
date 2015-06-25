35c898b136909408b8f4372002e668686a1526e8
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-25 20:15:26 +0000

    AMP-20466	Implement AmpActivityIdValidator
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27810 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/AmpActivityIdValidator.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/AmpActivityIdValidator.java
@@ -5,11 +5,12 @@ package org.digijava.kernel.ampapi.endpoints.activity.validators;
 
 import org.digijava.kernel.ampapi.endpoints.activity.ActivityEPConstants;
 import org.digijava.kernel.ampapi.endpoints.activity.ActivityErrors;
+import org.digijava.kernel.ampapi.endpoints.activity.InterchangeUtils;
 import org.digijava.kernel.ampapi.endpoints.errors.ApiErrorMessage;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
 import org.digijava.module.aim.annotations.interchange.ActivityFieldsConstants;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
-import org.digijava.module.aim.util.ActivityUtil;
+import org.digijava.module.aim.util.ActivityVersionUtil;
 
 /**
  * Validates amp_activity_id and amp_id to be valid
@@ -33,21 +34,29 @@ public class AmpActivityIdValidator extends InputValidator {
 		boolean isValid = true;
 		String ampId = (String) newFieldParent.get(ActivityFieldsConstants.AMP_ID);
 		String ampActivityId = (String) newFieldParent.get(ActivityFieldsConstants.AMP_ACTIVITY_ID);
-		if (update) {
-			Long latestAmpActivityId = ActivityUtil.getLastActivityId(ampId);
-		
-			//if oldActivity's amp_id doesn't match the one received or it is not the latest amp_activity_id for that activity 
-			// -> then it is invalid
-			if (!oldActivity.getAmpId().equals(ampId)
-					|| !latestAmpActivityId.equals(oldActivity.getAmpActivityId())) {
-				isValid = false;
-			}
+		String fieldName = (String) fieldDescription.get(ActivityEPConstants.FIELD_NAME);
+		if (InterchangeUtils.underscorify(ActivityFieldsConstants.AMP_ACTIVITY_ID).equals(fieldName)) {
+			if (update) {
+				Long latestAmpActivityId = ActivityVersionUtil.getLastVersionForVersion(Long.valueOf(ampActivityId));
+				if (!latestAmpActivityId.equals(oldActivity.getAmpActivityId())) {
+					isValid = false;
+				}
 
-		} else {
-			
-			//On insert,if amp_id was sent OR amp_activity_id was sent and it was not 0  - > them it is invalid
-			if ((ampActivityId != null && Long.valueOf(ampActivityId).longValue() != 0l) || ampId != null) {
-				isValid = false;
+			} else {
+				if ((ampActivityId != null && Long.valueOf(ampActivityId).longValue() != 0l)) {
+					isValid = false;
+				}
+			}
+		} else if (InterchangeUtils.underscorify(ActivityFieldsConstants.AMP_ID).equals(fieldName)) {
+			if (update) {
+				if (!oldActivity.getAmpId().equals(ampId)) {
+					isValid = false;
+				}
+
+			} else {
+				if (ampId != null) {
+					isValid = false;
+				}
 			}
 		}
 		return isValid;

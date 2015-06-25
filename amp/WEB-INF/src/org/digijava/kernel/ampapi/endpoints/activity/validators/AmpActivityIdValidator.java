8c5aec8633c60eb77a60fed8dc5d606ed35a8084
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-25 20:15:57 +0000

    AMP-20466	Implement AmpActivityIdValidator
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27811 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/AmpActivityIdValidator.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/AmpActivityIdValidator.java
@@ -3,17 +3,22 @@
  */
 package org.digijava.kernel.ampapi.endpoints.activity.validators;
 
+import org.digijava.kernel.ampapi.endpoints.activity.ActivityEPConstants;
 import org.digijava.kernel.ampapi.endpoints.activity.ActivityErrors;
+import org.digijava.kernel.ampapi.endpoints.activity.InterchangeUtils;
 import org.digijava.kernel.ampapi.endpoints.errors.ApiErrorMessage;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
+import org.digijava.module.aim.annotations.interchange.ActivityFieldsConstants;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
+import org.digijava.module.aim.util.ActivityVersionUtil;
 
 /**
  * Validates amp_activity_id and amp_id to be valid
+ * 
  * @author Nadejda Mandrescu
  */
 public class AmpActivityIdValidator extends InputValidator {
-	
+
 	public AmpActivityIdValidator() {
 		this.continueOnSuccess = false;
 	}
@@ -26,8 +31,34 @@ public class AmpActivityIdValidator extends InputValidator {
 	@Override
 	public boolean isValid(AmpActivityVersion oldActivity, JsonBean newFieldParent, JsonBean oldFieldParent,
 			JsonBean fieldDescription, String fieldPath, boolean update) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		boolean isValid = true;
+		String ampId = (String) newFieldParent.get(ActivityFieldsConstants.AMP_ID);
+		String ampActivityId = (String) newFieldParent.get(ActivityFieldsConstants.AMP_ACTIVITY_ID);
+		String fieldName = (String) fieldDescription.get(ActivityEPConstants.FIELD_NAME);
+		if (InterchangeUtils.underscorify(ActivityFieldsConstants.AMP_ACTIVITY_ID).equals(fieldName)) {
+			if (update) {
+				Long latestAmpActivityId = ActivityVersionUtil.getLastVersionForVersion(Long.valueOf(ampActivityId));
+				if (!latestAmpActivityId.equals(oldActivity.getAmpActivityId())) {
+					isValid = false;
+				}
 
+			} else {
+				if ((ampActivityId != null && Long.valueOf(ampActivityId).longValue() != 0l)) {
+					isValid = false;
+				}
+			}
+		} else if (InterchangeUtils.underscorify(ActivityFieldsConstants.AMP_ID).equals(fieldName)) {
+			if (update) {
+				if (!oldActivity.getAmpId().equals(ampId)) {
+					isValid = false;
+				}
+
+			} else {
+				if (ampId != null) {
+					isValid = false;
+				}
+			}
+		}
+		return isValid;
+	}
 }

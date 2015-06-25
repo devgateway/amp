f03b82c14b58c2fb080607faa6cf68a2c8e5ec9c
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-25 18:22:35 +0000

    AMP-20468	Implement MultipleEntriesValidator
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27806 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/MultipleEntriesValidator.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/MultipleEntriesValidator.java
@@ -3,6 +3,9 @@
  */
 package org.digijava.kernel.ampapi.endpoints.activity.validators;
 
+import java.util.Collection;
+
+import org.digijava.kernel.ampapi.endpoints.activity.ActivityEPConstants;
 import org.digijava.kernel.ampapi.endpoints.activity.ActivityErrors;
 import org.digijava.kernel.ampapi.endpoints.errors.ApiErrorMessage;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
@@ -10,7 +13,8 @@ import org.digijava.module.aim.dbentity.AmpActivityVersion;
 
 /**
  * Validates that multiple values are provided only when it is allowed
- * @author Nadejda Mandrescu
+ * 
+ * @author Nadejda Mandrescu, Emanuel Perez
  */
 public class MultipleEntriesValidator extends InputValidator {
 
@@ -22,8 +26,24 @@ public class MultipleEntriesValidator extends InputValidator {
 	@Override
 	public boolean isValid(AmpActivityVersion oldActivity, JsonBean newFieldParent, JsonBean oldFieldParent,
 			JsonBean fieldDescription, String fieldPath, boolean update) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean isValid = true;
+		boolean multipleValuesAllowed = Boolean.valueOf(fieldDescription.getString(ActivityEPConstants.MULTIPLE_VALUES));
+		String fieldName = fieldDescription.getString(ActivityEPConstants.FIELD_NAME);
+		Object fieldValue = newFieldParent.get(fieldName);
+		if (!multipleValuesAllowed && hasManyElements(fieldValue)) {
+			isValid = false;
+		}
+		return isValid;
+	}
+
+	/**
+	 * Checks if an Object is a Collection with more than one element
+	 * 
+	 * @param fieldValue, the Object to check
+	 * @return true if the Object is a Collection with many elements, false otherwise
+	 */
+	private boolean hasManyElements(Object fieldValue) {
+		return fieldValue != null && fieldValue instanceof Collection && ((Collection) fieldValue).size() > 1;
 	}
 
 }

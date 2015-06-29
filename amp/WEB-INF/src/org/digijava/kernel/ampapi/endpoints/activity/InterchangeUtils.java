8aaf872328e8e0279d34c2ac1db9fe7948d474b1
vchihai <vchihai@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-29 07:21:38 +0000

    AMP-20521
    A dedicated class for Full Project Details created (ActivityExport)
    
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@27845 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/InterchangeUtils.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/InterchangeUtils.java
@@ -1,14 +1,8 @@
 package org.digijava.kernel.ampapi.endpoints.activity;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -16,33 +10,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.dgfoundation.amp.ar.viewfetcher.RsInfo;
-import org.dgfoundation.amp.ar.viewfetcher.SQLUtils;
 import org.digijava.kernel.ampapi.endpoints.activity.visibility.FMVisibility;
 import org.digijava.kernel.ampapi.endpoints.errors.ApiError;
 import org.digijava.kernel.ampapi.endpoints.errors.ApiErrorMessage;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
-import org.digijava.kernel.entity.Message;
-import org.digijava.kernel.persistence.PersistenceManager;
-import org.digijava.kernel.persistence.WorkerException;
-import org.digijava.kernel.translator.TranslatorWorker;
 import org.digijava.module.aim.annotations.interchange.Interchangeable;
 import org.digijava.module.aim.annotations.interchange.InterchangeableDiscriminator;
 import org.digijava.module.aim.annotations.interchange.Validators;
 import org.digijava.module.aim.dbentity.AmpActivityFields;
-import org.digijava.module.aim.dbentity.AmpActivityProgram;
-import org.digijava.module.aim.dbentity.AmpActivitySector;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
-import org.digijava.module.aim.dbentity.AmpComponentFunding;
-import org.digijava.module.aim.dbentity.AmpSector;
-import org.digijava.module.aim.dbentity.AmpTheme;
-import org.digijava.module.aim.util.ActivityUtil;
-import org.digijava.module.aim.util.time.StopWatch;
-import org.digijava.module.categorymanager.dbentity.AmpCategoryValue;
-import org.digijava.module.translation.util.ContentTranslationUtil;
-import org.hibernate.jdbc.Work;
-
-import clover.org.apache.commons.lang.StringUtils;
 
 /**
  * Activity Import/Export Utility methods 
@@ -149,9 +125,6 @@ public class InterchangeUtils {
 		return ((Class<?>) genericTypes[0]);
 	}
 
-
-
-
 	/**
 	 * converts the uppercase letters of a string to underscore + lowercase (except for first one)
 	 * 
@@ -196,7 +169,6 @@ public class InterchangeUtils {
 	public static boolean isCompositeField(Field field) {
 		return field.getAnnotation(InterchangeableDiscriminator.class) != null;
 	}
-
 	
 	/**
 	 * Activity Export as JSON
@@ -217,229 +189,14 @@ public class InterchangeUtils {
 	 */
 	public static JsonBean getActivity(Long projectId, JsonBean filter) {
 		try {
-			AmpActivityVersion activity = ActivityUtil.loadActivity(projectId);
-			return getActivity(activity, filter);
+			ActivityExporter exporter = new ActivityExporter();
+			return exporter.getActivity(projectId, filter);
 		} catch (Exception e) {
 			LOGGER.error("Coudn't load activity with id: " + projectId + ". " + e.getMessage());
 			throw new RuntimeException(e);
 		}
 	}
 	
-	/**
-	 * Activity Export as JSON 
-	 * 
-	 * @param activity actual activity to export
-	 * @param filter is the JSON with a list of fields
-	 * @return
-	 */
-	public static JsonBean getActivity(AmpActivityVersion activity, JsonBean filter) {
-		JsonBean activityJson = new JsonBean();
-			
-		activityJson.set("amp_activity_id", activity.getAmpActivityId());
-		activityJson.set("amp_id", activity.getAmpId());
-		
-		List<String> filteredFields = filter != null ? (List<String>) filter.get(ActivityEPConstants.FILTER_FIELDS) 
-				: new ArrayList<String>();
-		
-		Field[] fields = activity.getClass().getSuperclass().getDeclaredFields();
-
-		for (Field field : fields) {
-			try {
-				readFieldValue(field, activity, activityJson, filteredFields, null);
-			} catch (IllegalArgumentException | IllegalAccessException
-					| NoSuchMethodException | SecurityException
-					| InvocationTargetException e) {
-				LOGGER.error("Coudn't read activity fields with id: " + activity.getAmpActivityId() + ". " 
-					+ e.getMessage());
-				throw new RuntimeException(e);
-			}
-		}
-		
-		return activityJson;
-	}
-	
-	public static String getGetterMethodName(String fieldName) {
-		
-		if (fieldName.length() == 1)
-			return "get" + Character.toUpperCase(fieldName.charAt(0));
-		return "get" + Character.toUpperCase(fieldName.charAt(0)) + 
-				((fieldName.length() > 1) ? fieldName.substring(1) : "");
-	}	
-	public static Long  getId(Object obj) throws NoSuchMethodException, 
-	SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		if (InterchangeUtils.isSimpleType(obj.getClass())) {
-			return null;
-		} else {
-			for (Field field : obj.getClass().getDeclaredFields()) {
-				Interchangeable ant = field.getAnnotation(Interchangeable.class);
-				if (ant != null) {
-					if (ant.id()) {
-						Method meth = obj.getClass().getMethod(getGetterMethodName(field.getName()));
-						if (Long.class.isAssignableFrom(field.getType())) {
-							return (Long) meth.invoke(obj);
-						} else {
-							/*we need to go deeper*/
-							return getId(meth.invoke(obj));
-						}
-					}
-				}
-			}
-		}
-		return null;
-	}	
-	/**
-	 * 
-	 * @param field
-	 * @param fieldInstance
-	 * @param resultJson
-	 * @param filteredFields
-	 * @param fieldPath
-	 * @return
-	 */
-	private static void readFieldValue(Field field, Object fieldInstance, JsonBean resultJson, List<String> filteredFields, String fieldPath) throws IllegalArgumentException, 
-	IllegalAccessException, NoSuchMethodException, SecurityException, InvocationTargetException {
-		
-		Interchangeable interchangeable = field.getAnnotation(Interchangeable.class);
-		
-		if (interchangeable != null && FMVisibility.isVisible(field)) {
-			field.setAccessible(true);
-			String fieldTitle = underscorify(interchangeable.fieldTitle());
-			
-			String filteredFieldPath = fieldPath == null ? fieldTitle : fieldPath + "~" + fieldTitle;
-			Object object = field.get(fieldInstance);
-			
-			if (!interchangeable.pickIdOnly()) {
-				// check if the member is a collection
-				
-				if (isCompositeField(field)) {
-					generateCompositeCollection(field, object, resultJson, filteredFields, fieldPath);
-				} if (isFiltered(filteredFieldPath, filteredFields)) {
-					if (isCollection(field)) {
-						Collection<Object> collectionItems = (Collection<Object>) object;
-						// if the collection is not empty, it will be parsed and a JSON with member details will be generated
-						List<JsonBean> collectionJson = new ArrayList<JsonBean>();
-						if (collectionItems != null) {
-							// iterate over the objects of the collection
-							for (Object item : collectionItems) {
-								collectionJson.add(getObjectJson(item, filteredFields, filteredFieldPath));
-							}
-						}
-						// put the array with object values in the result JSON
-						resultJson.set(fieldTitle, collectionJson);
-					} else {
-
-						if (InterchangeableClassMapper.containsSupportedClass(field.getType()) || object == null) {
-							resultJson.set(fieldTitle, object);
-						} else {
-							if (interchangeable.pickIdOnly()) {
-								resultJson.set(fieldTitle, getId(object));
-							} else {
-								resultJson.set(fieldTitle, getObjectJson(object, filteredFields, filteredFieldPath));
-							}
-						}
-					}
-				}
-			}
-		}		
-	}
-	
-	/**
-	 * 
-	 * @param item
-	 * @param filteredFields
-	 * @param fieldPath
-	 * @return itemJson
-	 */
-	private static JsonBean getObjectJson(Object item, List<String> filteredFields, String fieldPath) throws IllegalArgumentException, IllegalAccessException, 
-	NoSuchMethodException, SecurityException, InvocationTargetException {
-		
-		Field[] itemFields = item.getClass().getDeclaredFields();
-		JsonBean itemJson = new JsonBean();
-		
-		// iterate the fields of the object and generate the JSON
-		for (Field itemField : itemFields) {
-			readFieldValue(itemField, item, itemJson, filteredFields, fieldPath);	
-		}
-		
-		return itemJson;
-	}
-	
-	/**
-	 * Generate the composite collection. E.g: we have a list of sectors, 
-	 * in JSON the list should be written by classification 
-	 * (primary programs, secondary programs, etc.)
-	 * @param field
-	 * @param fieldInstance
-	 * @param resultJson
-	 * @param filteredFields
-	 * @param fieldPath
-	 * @return
-	 */
-	private static void generateCompositeCollection(Field field, Object object, JsonBean resultJson, List<String> filteredFields, String fieldPath) throws IllegalArgumentException, 
-	IllegalAccessException, NoSuchMethodException, SecurityException, InvocationTargetException {
-		
-		InterchangeableDiscriminator discriminator = field.getAnnotation(InterchangeableDiscriminator.class);
-		Interchangeable[] settings = discriminator.settings();
-		
-		Map<String, List<JsonBean>> compositeMap = new HashMap<String, List<JsonBean>>();
-		Map<String, String> filteredFieldsMap = new HashMap<String, String>();
-		
-		// create the map containing the correlation between the discriminatorOption and the JSON generated objects
-		for (Interchangeable setting : settings) {
-			compositeMap.put(setting.discriminatorOption(), new ArrayList<JsonBean>());
-			String fieldTitle = underscorify(setting.fieldTitle());
-			String filteredFieldPath = fieldPath == null ? fieldTitle : fieldPath + "~" + fieldTitle;
-			
-			filteredFieldsMap.put(setting.discriminatorOption(), filteredFieldPath);
-		}
-		
-		if (isCollection(field) && object != null) {
-			Collection<Object> compositeCollection = (Collection <Object>) object;
-			if (compositeCollection.size() > 0) {
-				for (Object obj : compositeCollection) {
-					if (obj instanceof AmpActivitySector) {
-						AmpActivitySector sector = (AmpActivitySector) obj;
-						String filteredFieldPath = filteredFieldsMap.get(sector.getClassificationConfig().getName());
-						compositeMap.get(sector.getClassificationConfig().getName()).add(getObjectJson(sector, filteredFields, filteredFieldPath));
-					} else if (obj instanceof AmpActivityProgram) {
-						AmpActivityProgram program = (AmpActivityProgram) obj;
-						String filteredFieldPath = filteredFieldsMap.get(program.getProgramSetting().getName());
-						compositeMap.get(program.getProgramSetting().getName()).add(getObjectJson(program, filteredFields, filteredFieldPath));
-					}
-				}
-			}
-		}
-		
-		// put in the result JSON the generated structure
-		for (Interchangeable setting : settings) {
-			String fieldTitle = underscorify(setting.fieldTitle());
-			if (isFiltered(fieldTitle, filteredFields)) {
-				resultJson.set(underscorify(setting.fieldTitle()), compositeMap.get(setting.discriminatorOption()));
-			}
-		}
-	}
-	
-	/**
-	 * 
-	 * @param filteredFieldPath
-	 * @param filteredFields
-	 * @return boolean, if the field should be exported in the result Json 
-	 */
-	private static boolean isFiltered(String filteredFieldPath, List<String> filteredFields) {
-		if (filteredFields.isEmpty()) 
-			return true;
-			
-		for (String s : filteredFields) {
-			if (s.startsWith(filteredFieldPath) || filteredFieldPath.startsWith(s)) 
-				return true;
-		}
-		
-		return false;
-	}
-
-
-
-
 	/**
 	 * Gets the field required value. 
 	 * 
@@ -465,9 +222,6 @@ public class InterchangeUtils {
 		}
 		return requiredValue;
 	}
-
-	
-	
 
 	public static boolean isSimpleType(Class<?> clazz) {
 		return InterchangeableClassMapper.containsSimpleClass(clazz);

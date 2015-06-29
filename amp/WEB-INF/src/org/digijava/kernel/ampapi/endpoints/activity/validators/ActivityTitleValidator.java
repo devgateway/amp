e39c6c9df6b4396b3ffc767aab2b6de70931ec0d
eperez <eperez@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-06-29 15:56:04 +0000

    AMP-20465 Implement ActivityTitleValidator
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27856 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/ActivityTitleValidator.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/ActivityTitleValidator.java
@@ -4,6 +4,7 @@
 package org.digijava.kernel.ampapi.endpoints.activity.validators;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -36,22 +37,34 @@ public class ActivityTitleValidator extends InputValidator {
 		return ActivityErrors.UNIQUE_ACTIVITY_TITLE;
 	}
 
-	@Override 
-	public  boolean isValid(ActivityImporter importer, JsonBean newFieldParent, JsonBean oldFieldParent, 
+	@Override
+	public boolean isValid(ActivityImporter importer, JsonBean newFieldParent, JsonBean oldFieldParent,
 			JsonBean fieldDescription, String fieldPath) {
 		boolean isValid = true;
 		String fieldName = (String) fieldDescription.get(ActivityEPConstants.FIELD_NAME);
-		//this validator only validates project title
+		
+		// this validator only validates project title
 		if (InterchangeUtils.underscorify(ActivityFieldsConstants.PROJECT_TITLE).equals(fieldName)) {
 			HttpServletRequest request = TLSUtils.getRequest();
-			String title = newFieldParent.getString(fieldName);
-			String langCode = null;
-			if (ContentTranslationUtil.multilingualIsEnabled()) {
-				langCode = TLSUtils.getEffectiveLangCode ();
+			JsonBean translatedTitles = (JsonBean) newFieldParent.get(fieldName);
+		
+			// project_title = {en : "English Title",fr:"French title",...}
+			Map<String, Object> titleMap = translatedTitles.any();
+			for (String langCode : titleMap.keySet()) {
+				String title = (String) titleMap.get(langCode);
+			
+				// we validate titles in the current language or titles in ALL
+				// languages when multilingual is enabled
+				if (ContentTranslationUtil.multilingualIsEnabled() || TLSUtils.getEffectiveLangCode().equals(langCode)) {
+					List<AmpActivity> list = LuceneUtil.findActivitiesMoreLikeThis(request.getServletContext()
+							.getRealPath("/") + LuceneUtil.ACTVITY_INDEX_DIRECTORY, title, langCode, 2);
+					isValid = !isTitleExistent(importer.getOldActivity(), list, importer.isUpdate());
+					if (!isValid) {
+						break;
+					}
+				}
+
 			}
-			List<AmpActivity> list = LuceneUtil.findActivitiesMoreLikeThis(request.getServletContext().getRealPath("/")
-					+ LuceneUtil.ACTVITY_INDEX_DIRECTORY, title, langCode, 2);
-			isValid = !isTitleExistent(importer.getOldActivity(), list, importer.isUpdate());
 		}
 
 		return isValid;
@@ -60,16 +73,20 @@ public class ActivityTitleValidator extends InputValidator {
 	/**
 	 * Checks if the Activity title already exists on the system.
 	 * 
-	 * @param oldActivity, null if it is an insert, the activity already on the system on updates.
-	 * @param list the List <AmpActivity> with matching titles
-	 * @param update, whether this is an update operation or an insert
+	 * @param oldActivity
+	 *            , null if it is an insert, the activity already on the system
+	 *            on updates.
+	 * @param list
+	 *            the List <AmpActivity> with matching titles
+	 * @param update
+	 *            , whether this is an update operation or an insert
 	 * @return if there is already an activity with a matching title.
 	 */
-	private boolean isTitleExistent(AmpActivityVersion oldActivity, List<AmpActivity> list,boolean update) {
+	private boolean isTitleExistent(AmpActivityVersion oldActivity, List<AmpActivity> list, boolean update) {
 		boolean isExistent = false;
 		if (!list.isEmpty()) {
 			for (AmpActivity activity : list)
-				if (!update  || oldActivity == null || !oldActivity.getName().equals(activity.getName())) {
+				if (!update || oldActivity == null || !oldActivity.getName().equals(activity.getName())) {
 					isExistent = true;
 				}
 

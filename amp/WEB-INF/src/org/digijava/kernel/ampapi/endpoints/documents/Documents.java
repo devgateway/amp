3f4bd5f8c456b9022d4dbcd9c19142228a42739e
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-07-02 06:56:37 +0000

    AMP-20331. Add time tracing message
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@27912 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/documents/Documents.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/documents/Documents.java
@@ -33,6 +33,7 @@ public class Documents {
 	@Path("/getTopDocuments")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 	public final String getDocuments() throws IOException {
+        long start = System.currentTimeMillis();
 		DesktopDocumentsUtil desktopDocumentsUtil = new DesktopDocumentsUtil();
 		Collection<DocumentData> documents = desktopDocumentsUtil.getLatestDesktopLinks(httpRequest, MAX_NUMBER_OF_DOCS);
 
@@ -46,6 +47,9 @@ public class Documents {
                         getDefaultVisibilityChecker().
                         withFieldVisibility(JsonAutoDetect.Visibility.ANY).
                         withGetterVisibility(JsonAutoDetect.Visibility.NONE));
+
+        long end = System.currentTimeMillis();
+        logger.debug("getTopDocuments. execution time is: " + (end - start));
 
 		return mapper.writeValueAsString(documents);
 	}

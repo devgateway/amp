c9928aaedb8e28af5520bfe3a4d6d49400d22d20
acartaleanu <acartaleanu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-07-16 13:42:54 +0000

    AMP-20457: rewrite logic of RequiredValidator, add support for updating as draft mode
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@28100 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/RequiredValidator.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/validators/RequiredValidator.java
@@ -19,42 +19,46 @@ import org.digijava.module.aim.dbentity.AmpActivityVersion;
  * @author Nadejda Mandrescu
  */
 public class RequiredValidator extends InputValidator {
-
+	private boolean draftDisabled = false;
+	
 	@Override
 	public ApiErrorMessage getErrorMessage() {
-		return ActivityErrors.FIELD_REQUIRED;
+		if (this.draftDisabled)
+			return ActivityErrors.SAVE_AS_DRAFT_FM_DISABLED;
+		else
+			return ActivityErrors.FIELD_REQUIRED;
 	}
-
+	
 	@Override
 	public boolean isValid(ActivityImporter importer, Map<String, Object> newFieldParent, 
 			Map<String, Object> oldFieldParent, JsonBean fieldDescription, String fieldPath) {
-		boolean isValid = true;
 		String fieldName = (String) fieldDescription.get(ActivityEPConstants.FIELD_NAME);
 		Object fieldValue = newFieldParent.get(fieldName);
 		String requiredStatus = fieldDescription.getString(ActivityEPConstants.REQUIRED);
-		// On insert or draft activities update...
-		if (!importer.isUpdate() || isSaveAsDraft(importer.getOldActivity())) {
-			if (isSaveAsDraft(importer.getOldActivity()) && !importer.isDraftFMEnabled()) {
-				isValid = false;
-			}
-			if (ActivityEPConstants.FIELD_ALWAYS_REQUIRED.equals(requiredStatus) && fieldValue == null) {
-				isValid = false;
-			}
-		}
-		// on update of non-draft activities
-		else {
-			if (!importer.getAllowSaveAsDraftShift()
-					&& (ActivityEPConstants.NON_DRAFT_REQUIRED.equals(requiredStatus) || ActivityEPConstants.FIELD_ALWAYS_REQUIRED
-							.equals(requiredStatus)) && fieldValue == null) {
-				isValid = false;
-			}
-		}
 
-		return isValid;
+		//don't care if value has something 
+		if (fieldValue == null) {
+			if (ActivityEPConstants.FIELD_ALWAYS_REQUIRED.equals(requiredStatus)) {
+				//field is always required -> can't save it even as a draft
+				return false;
+			} else if (ActivityEPConstants.FIELD_NON_DRAFT_REQUIRED.equals(requiredStatus)) {
+				//field required for submitted activities, but we can save it as a draft
+				
+				//unless it's disabled in FM
+				if (!importer.isDraftFMEnabled()) {
+					this.draftDisabled = true;
+					return false;
+				}
+				//ok, it's enabled, save as draft
+				importer.setSaveAsDraft(true);
+				return true;
+			}
+			
+		} 
+		//field value != null, it's fine from this validator's POV
+		return true;	
+	
 	}
 
-	private boolean isSaveAsDraft(AmpActivityVersion oldActivity) {
-		return oldActivity != null && oldActivity.getDraft().equals(Boolean.TRUE);
-	}
 
 }

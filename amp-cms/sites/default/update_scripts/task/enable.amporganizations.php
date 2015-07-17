02fe4b46ef3c533eebfe0f6f75073fcd59b396d5
wschneider <wschneider@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-07-17 13:17:23 +0000

    Merged from AMP_2_11_RELEASE AMP-20598: Add new organization landing page. [from revision 28123]
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@28124 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ amp-cms/sites/default/update_scripts/task/enable.amporganizations.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file: Enable AMP Organization Profiles.
+ * @desc: This script can be used to enable the Organization Profiles listing, it also adds a new menu item.
+ */
+
+// Provide a list of modules to be installed.
+$modules = array(
+  'taxonomy',
+);
+_us_module__install($modules);
+
+
+// Prepare a list of features to be reverted.
+$feature_names = array(
+  'ampfileentity',
+  'ampviewsettings',
+);
+_us_features__revert($feature_names);
+
+// Clear system caches.
+drupal_flush_all_caches();
+
+
+// Prepare a list of features to be installed.
+$feature_names = array(
+  'amporganization_profiles',
+);
+_us_features__install($feature_names);
+
+// Clear system caches.
+drupal_flush_all_caches();
+
+
+// Create a menu link.
+$link = array(
+  'link_path' => 'organizations',
+  'link_title' => 'Organizations',
+  'weight' => 0,
+);
+_us_menu__create_link($link, 'main-menu');

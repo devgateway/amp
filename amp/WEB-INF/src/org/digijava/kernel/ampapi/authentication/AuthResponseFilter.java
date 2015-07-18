aec23087e5666c0d4bc1834ac009b266e8a6c9b9
jdeanquin <jdeanquin@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-07-18 16:26:59 +0000

    AMP-20504 log when removing team member from session to debug staging server
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@28141 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/authentication/AuthResponseFilter.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/authentication/AuthResponseFilter.java
@@ -1,5 +1,6 @@
 package org.digijava.kernel.ampapi.authentication;
 
+import org.apache.log4j.Logger;
 import org.digijava.kernel.ampapi.endpoints.util.SecurityUtil;
 import org.digijava.kernel.request.TLSUtils;
 import org.digijava.module.aim.helper.Constants;
@@ -9,6 +10,8 @@ import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 
 public class AuthResponseFilter implements ContainerResponseFilter {
+	protected static final Logger logger = Logger.getLogger(AuthResponseFilter.class);
+
 
 	@Override
 	public ContainerResponse filter(ContainerRequest request,
@@ -16,6 +19,7 @@ public class AuthResponseFilter implements ContainerResponseFilter {
 			if("true".equals(TLSUtils.getRequest().getAttribute(SecurityUtil.REMOVE_SESSION))){
 				TLSUtils.getRequest().getSession().removeAttribute(Constants.CURRENT_MEMBER);
 				TLSUtils.getRequest().removeAttribute(SecurityUtil.REMOVE_SESSION);
+				logger.error("Session removed");
 			}
 		return response;
 	}

9e732d2c5297ecaaa42ef4c4e3f8560d0601a3b1
asavca <asavca@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-07-21 11:12:51 +0000

    AMP-20662
    Lazy loading organizations by explicitly invoking Hibernate
    
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@28186 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/module/aim/action/WorkspaceManager.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/WorkspaceManager.java
@@ -19,6 +19,7 @@ import org.digijava.module.aim.dbentity.AmpTeam;
 import org.digijava.module.aim.form.WorkspaceForm;
 import org.digijava.module.aim.util.TeamUtil;
 import org.digijava.module.search.util.SearchUtil;
+import org.hibernate.Hibernate;
 
 public class WorkspaceManager extends Action {
 
@@ -159,7 +160,7 @@ public class WorkspaceManager extends Action {
 		Collection<AmpTeam> workspacesFiltered = new ArrayList<AmpTeam>();
 		if (! workspaces.isEmpty()) {
 			for (AmpTeam team : workspaces) {
-				team.getOrganizations().size(); // initialize lazy collection
+				Hibernate.initialize(team.getOrganizations()); // initialize lazy collection
 				boolean found = false;
 				for (String keyw : keywords) {
 					java.util.Locale currentLocale = new java.util.Locale(TLSUtils.getEffectiveLangCode());

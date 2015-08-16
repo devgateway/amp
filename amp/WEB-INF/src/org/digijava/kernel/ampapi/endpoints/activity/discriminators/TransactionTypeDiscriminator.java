16fc7faa97002b362154cb7e83124544c4f774bf
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-08-16 06:58:03 +0000

    AMP-20787. Read possible values dynamically
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@28456 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/discriminators/TransactionTypeDiscriminator.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/discriminators/TransactionTypeDiscriminator.java
@@ -1,55 +1,57 @@
 package org.digijava.kernel.ampapi.endpoints.activity.discriminators;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.dgfoundation.amp.ar.ArConstants;
 import org.digijava.kernel.ampapi.endpoints.activity.FieldsDiscriminator;
+import org.digijava.module.categorymanager.dbentity.AmpCategoryClass;
+import org.digijava.module.categorymanager.dbentity.AmpCategoryValue;
+import org.digijava.module.categorymanager.util.CategoryConstants;
+import org.digijava.module.categorymanager.util.CategoryManagerUtil;
 
 public class TransactionTypeDiscriminator extends FieldsDiscriminator {
 	
-//	public TransactionTypeDiscriminator(Object value) {
-//		super(value);
-//	}
-
-
-
-
 	@Override
 	public Map<String, Object> getPossibleValues() {
 		Map<String, Object> valuesMap = new HashMap<String, Object>();
-		for (Map.Entry<String, Integer> entry : ArConstants.TRANSACTION_TYPE_NAME_TO_ID.entrySet())
+		for (Map.Entry<String, Integer> entry : getTransactionTypeMap().entrySet())
 			valuesMap.put(entry.getValue().toString(), entry.getKey());
 		return valuesMap;
 	}
 	
-	
-	
-	
 	@Override
 	public Object toJsonOutput(Object obj) {
 		Integer intValue = (Integer) obj;
-		for (Map.Entry<String, Integer> entry : ArConstants.TRANSACTION_TYPE_NAME_TO_ID.entrySet())
+		for (Map.Entry<String, Integer> entry : getTransactionTypeMap().entrySet())
 			if (entry.getValue().equals(intValue))
 				return entry.getKey();
 		return null;
 	}
 
+    private Map<String, Integer> getTransactionTypeMap() {
+        Map<String, Integer> valuesMap = new HashMap<String, Integer>();
 
+        AmpCategoryClass categoryClass = CategoryManagerUtil.loadAmpCategoryClassByKey(CategoryConstants.TRANSACTION_TYPE_KEY);
+        List<AmpCategoryValue> possibleValues = categoryClass.getPossibleValues();
+        if (possibleValues != null) {
+            for (AmpCategoryValue transactionType : possibleValues) {
+                if (transactionType.isVisible()) {
+                    valuesMap.put(transactionType.getValue(), transactionType.getIndex());
+                }
+            }
+        }
 
+        return valuesMap;
+    }
 
 	@Override
 	public Long getIdOf(Object value) {
 		return Long.decode(value.toString());
 	}
 
-
-
-
 	@Override
 	public Object toAmpFormat(Object obj) {
 		return obj;
-//		return ArConstants.TRANSACTION_TYPE_NAME_TO_ID.get(obj);
 	}
-
 
 }

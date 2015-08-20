2882af002c364affe96b307e543c911e40d32cbb
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-08-20 22:52:35 +0000

    AMP-20787
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@28489 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/ar/ArConstants.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/ar/ArConstants.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;

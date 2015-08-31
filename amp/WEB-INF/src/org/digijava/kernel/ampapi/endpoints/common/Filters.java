7a0a19ac8bddc9a506d59ff35253c52e062fbbbf
cdolghier <cdolghier@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-08-31 12:47:32 +0000

    AMP-20950
    fix erroneous identification of "DisasterResponseMarker" as "HumanitarianAid" in Filters
    
    
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@28605 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/Filters.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/Filters.java
@@ -539,7 +539,7 @@ public class Filters {
 	@GET
 	@Path("/humanitarianAid/")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(ui = true, id = "HumanitarianAidValues", columns = ColumnConstants.HUMANITARIAN_AID, name="Humanitarian Aid")
+	@ApiMethod(ui = true, id = "HumanitarianAidValues", columns = ColumnConstants.HUMANITARIAN_AID, name=ColumnConstants.HUMANITARIAN_AID)
 	public JsonBean getHumanitarianAid() {
 		return buildYesNoJsonBean(ColumnConstants.HUMANITARIAN_AID);
 	}
@@ -547,7 +547,7 @@ public class Filters {
 	@GET
 	@Path("/disasterResponse/")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(ui = true, id = "DisasterResponseValues", columns = ColumnConstants.DISASTER_RESPONSE_MARKER, name="Disaster Response Marker")
+	@ApiMethod(ui = true, id = "DisasterResponseValues", columns = ColumnConstants.DISASTER_RESPONSE_MARKER, name=ColumnConstants.DISASTER_RESPONSE_MARKER)
 	public JsonBean getDisasterResponse() {
 		return buildYesNoJsonBean(ColumnConstants.DISASTER_RESPONSE_MARKER);
 	}

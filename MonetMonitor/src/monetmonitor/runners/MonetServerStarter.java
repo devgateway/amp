196a61ba64251fbaa46e7f264d127abfad2d20b9
acartaleanu <acartaleanu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-09-21 17:11:25 +0000

    AMP-21205: another follow-up commit
    git-svn-id: file:///svn/trunk@28766 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

---
+++ MonetMonitor/src/monetmonitor/runners/MonetServerStarter.java
@@ -0,0 +1,44 @@
+package monetmonitor.runners;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import monetmonitor.BeholderObservationResult;
+import monetmonitor.Constants;
+import monetmonitor.MonetBeholderConnectivity;
+
+/**
+ * @author acartaleanu
+ * starts the server, based on Constants, eventually imported from settings.conf
+ * runs the server in a separate thread for listening to output
+ * doesn't start it if server already running 
+ * */
+
+public class MonetServerStarter extends Runner {
+
+
+
+	@Override
+	/**
+	 * 
+	 */
+	protected String getCommand() {
+		return Constants.getMonetExecPath() + " " + Constants.getMonetFarmPath();
+	}
+
+	@Override
+	/**
+	 * Checks whether BeholderConnectivity could connect to the server or not. 
+	 * If he couldn't, server is OK to be started.
+	 * If he could, no point in starting the server, it's already started. 
+	 */
+	public boolean allowedToRun() {
+		return !(new MonetBeholderConnectivity()).check().equals(BeholderObservationResult.SUCCESS);
+
+	}
+
+	@Override
+	protected String getCustomMessage() {
+		return "Starting server...";
+	}
+}

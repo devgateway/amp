188ff6ba34d5df31e44fff61a481ca25e5a4e203
vlimansky <vlimansky@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-10-07 12:32:06 +0000

    AMP-19110.
    git-svn-id: file:///svn/branches/AMP_2_10_RELEASE@28881 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/reports/ReportErrors.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/reports/ReportErrors.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.digijava.kernel.ampapi.endpoints.reports;
 
@@ -8,16 +8,18 @@ import org.digijava.kernel.ampapi.endpoints.errors.ApiErrorMessage;
 /**
  * Defines errors used by Reports API. Please define concrete errors, normally is an invalid request input. <br>
  * Anything that is out of control, should be re-thrown and it will be handled by API Exception Filter.
- * 
+ *
  * @author vlimansky
  */
 public class ReportErrors {
 
-	// Validation errors
-	public static final ApiErrorMessage REPORT_NAME_REQUIRED = new ApiErrorMessage(1, "Report name not specified");
-    public static final ApiErrorMessage LIST_NAME_REQUIRED = new ApiErrorMessage(2, "List name not specified: ");
-    public static final ApiErrorMessage LIST_INVALID = new ApiErrorMessage(3, "Not allowed / invalid listName: ");
-    public static final ApiErrorMessage REPORT_TYPE_INVALID = new ApiErrorMessage(4, "Invalid report type: ");
-    public static final ApiErrorMessage REPORT_TYPE_REQUIRED = new ApiErrorMessage(5, "Project type is required");
-    public static final ApiErrorMessage ACTIVITY_TYPE_LIST_INVALID = new ApiErrorMessage(6, "Invalid list of activity types: ");
+    private static final String PREFIX = "listName";
+
+    // Validation errors
+    public static final ApiErrorMessage REPORT_NAME_REQUIRED = new ApiErrorMessage(1, "Report name not specified", PREFIX);
+    public static final ApiErrorMessage LIST_NAME_REQUIRED = new ApiErrorMessage(2, "Not specified: ", PREFIX);
+    public static final ApiErrorMessage LIST_INVALID = new ApiErrorMessage(3, "Not allowed / invalid: ", PREFIX);
+    public static final ApiErrorMessage REPORT_TYPE_INVALID = new ApiErrorMessage(4, "Invalid report type: ", PREFIX);
+    public static final ApiErrorMessage REPORT_TYPE_REQUIRED = new ApiErrorMessage(5, "Project type is required", PREFIX);
+    public static final ApiErrorMessage ACTIVITY_TYPE_LIST_INVALID = new ApiErrorMessage(6, "Invalid list of activity types: ", PREFIX);
 }

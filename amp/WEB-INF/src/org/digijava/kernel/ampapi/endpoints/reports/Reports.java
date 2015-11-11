ac8c0c35c5d70dc8efa1469cf5692bbbca114f48
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-11-11 19:12:42 +0000

    AMP-19644: Settings are not being saved on tabs
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@29131 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/reports/Reports.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/reports/Reports.java
@@ -868,9 +868,9 @@ public class Reports {
 					
 					// TODO: Testing what happens if we use only the last column
 					// coming from jqgrid (specially on hierarchical reports).
-					if (i == auxColumns.length - 1) {
+					//if (i == auxColumns.length - 1) {
 						sorting.add(sort);
-					}
+					//}
 				}
 			}
 			formParams.set(EPConstants.SORTING, sorting);

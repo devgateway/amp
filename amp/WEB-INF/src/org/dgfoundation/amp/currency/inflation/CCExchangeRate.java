e0e27ea9aac49099318115df13f73a8b6009079c
nmandrescu <nmandrescu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2015-12-24 15:39:34 +0000

    AMP-21955 reduce accuracy to speed up
    git-svn-id: file:///svn/branches/AMP_2_11_RELEASE@29305 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/currency/inflation/CCExchangeRate.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/currency/inflation/CCExchangeRate.java
@@ -10,8 +10,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 import org.dgfoundation.amp.currency.ConstantCurrency;
@@ -25,6 +29,7 @@ import org.digijava.module.aim.dbentity.AmpInflationRate;
 import org.digijava.module.aim.util.CurrencyUtil;
 import org.digijava.module.aim.util.FiscalCalendarUtil;
 import org.digijava.module.aim.util.caching.AmpCaching;
+import org.hibernate.Session;
 
 
 /**
@@ -35,6 +40,11 @@ import org.digijava.module.aim.util.caching.AmpCaching;
  */
 public class CCExchangeRate {
 	protected static final Logger logger = Logger.getLogger(CCExchangeRate.class);
+	
+	/** 1 quarter in ms (approximate quarter to 90 days) */
+	protected static final long _1_QUARTER_TO_MS = 90* TimeUnit.DAYS.toMillis(1);
+	protected static final long _1_DAY_TO_MS = TimeUnit.DAYS.toMillis(1);
+	protected static final long LOWEST_PERIOD_MS = _1_QUARTER_TO_MS;
 	
 	public final AmpCurrency baseCurrency;
 	
@@ -78,27 +88,7 @@ public class CCExchangeRate {
 			InflationRateGenerator irg = getInflationRateGenerator(inflationRatesCurrency, inflationRates);
 			Long periodEnd = getClosedPeriodEnd(irg.getSortedByDateInflationRates());
 			
-			// standard exchange rates from the base currency to the inflation rate currency
-			Map<Long, Double> fromBaseRates = fromBaseToOtherCurrency.get(inflationRatesCurrency);
-			ExchangeRates exchageRates = new ExchangeRates(baseCurrency.getAmpCurrencyId(), -1);
-			exchageRates.importRate(fromBaseRates);
-			/*
-			 * also keep approximate exchange rates for start & end periods of given inflation rates,
-			 * to minimize inflation rates impact for approximate exchange rates to the constant currency
-			 */
-			Calendar c = Calendar.getInstance();
-			for (Long inflRatePeriodStart : irg.getSortedByDateInflationRates().keySet()) {
-				for (int day = -1; day < 1; day ++) {
-					c.setTimeInMillis(inflRatePeriodStart);
-					c.add(Calendar.DATE, day);
-					long date = c.getTime().getTime();
-					if (!fromBaseRates.containsKey(date)) {
-						fromBaseRates.put(date, exchageRates.getRatesOnDate(date).rate);
-					}
-				}
-			}
-			
-			return generateExchangeRates(periodEnd, fromBaseRates, irg);
+			return generateExchangeRates(periodEnd, irg);
 		}
 		return Collections.emptyList();
 	}
@@ -113,8 +103,7 @@ public class CCExchangeRate {
 	 * @param fromBaseToInflCurrRates inflation rates from Base to Inflation Currency 
 	 * @param inflationRates
 	 */
-	protected List<AmpCurrencyRate> generateExchangeRates(Long periodEnd, Map<Long, Double> fromBaseToInflCurrRates, 
-			InflationRateGenerator irg) {
+	protected List<AmpCurrencyRate> generateExchangeRates(Long periodEnd, InflationRateGenerator irg) {
 		List<AmpCurrencyRate> exchangeRatesToCC = new ArrayList<AmpCurrencyRate>();
 		// check if inflation rate is provided for the base currency
 		boolean isInflCurrBaseCurrency = baseCurrency.getCurrencyCode().equals(inflationRatesCurrency);
@@ -126,12 +115,14 @@ public class CCExchangeRate {
 		// inflation rate currency exchange rate to std currency of the constant currency on To date
 		double iRCurrToCCstdCurrOnToDate = iRcurrToBaseOnToDate * baseToCCstdCurrOnToDate;
 		
-		Calendar c = Calendar.getInstance();
+		ExchangeRates fromBaseToInflCurrRates = new ExchangeRates(baseCurrency.getAmpCurrencyId(), -1);
+		fromBaseToInflCurrRates.importRate(fromBaseToOtherCurrency.get(inflationRatesCurrency));
 		
-		for (Entry<Long, Double> inflRatesCurrExchangeRates : fromBaseToInflCurrRates.entrySet()) {
-			Long from = inflRatesCurrExchangeRates.getKey();
+		Calendar c = Calendar.getInstance();
+		SortedSet<Long> ccExchangeRatesDates = generateCCExchangeRateDates(irg.getSortedByDateInflationRates().keySet());
+		for (Long from : ccExchangeRatesDates) {
 			Long to = periodEnd;
-			double baseToIRCurrOnFromDate = isInflCurrBaseCurrency ? 1 : getExchangeRate(fromBaseToInflCurrRates, from);
+			double baseToIRCurrOnFromDate = isInflCurrBaseCurrency ? 1 : fromBaseToInflCurrRates.getRatesOnDate(from).rate;
 			
 			c.setTimeInMillis(from);
 			
@@ -165,6 +156,55 @@ public class CCExchangeRate {
 	}
 	
 	/**
+	 * Build CC exchange rate dates:
+	 * 1) for the currency in which we have inflation rates: use only inflation rate dates
+	 * 2) for other currencies to which we propagate inflation rate via exchange rate:
+	 * 	a) for no exchange rates periods: all dates for available inflation rates, 
+	 * 		e.g. if we have exchange rates between 2010-2015 and quarterly inflation rates between 1970-2015, 
+	 * 		then we will consider the dates 1970-01-01,1970-04-01, ..., 2009-10-01
+	 *  
+	 *  b) for infl. rates period that has exchange rates to CC std currency: 
+	 *   i) keep approximate exchange rates for start & end periods, to minimize inflation rates impact 
+	 *   for approximate exchange rates to the constant currency 
+	 *   ii) build up at least quarterly intervals when infl.rate period is higher (e.g. yearly), 
+	 *   to be sure to reflect the other currency inflation somehow reflected via exchange rate
+	 * 
+	 * NOTE:
+	 * Keep in mind that taking all suitable exchange rate dates for accuracy will end up with AMP-21954
+	 * 
+	 * @return sorted dates for the new exchange rates to be generated for the current CC  
+	 */
+	protected SortedSet<Long> generateCCExchangeRateDates(Set<Long> inflationDates) {
+		// 1)
+		SortedSet<Long> dates = new TreeSet<Long>(inflationDates);
+		// 2)
+		if (dates.size() > 0 && !this.inflationRatesCurrency.equals(this.standardCurrencyReferredByCC)) {
+			SortedSet<Long> availableExchRateDates = new TreeSet<Long>();
+			if (fromBaseToOtherCurrency.containsKey(inflationRatesCurrency))
+				availableExchRateDates.addAll(fromBaseToOtherCurrency.get(this.inflationRatesCurrency).keySet());
+			if (fromBaseToOtherCurrency.containsKey(standardCurrencyReferredByCC))
+				availableExchRateDates.addAll(fromBaseToOtherCurrency.get(this.standardCurrencyReferredByCC).keySet());
+			SortedSet<Long> allDates = new TreeSet<Long>(availableExchRateDates);
+			allDates.addAll(dates);
+			Long[] inflDates = dates.toArray(new Long[dates.size()]);
+			for (int i = 0; i + 1 < inflDates.length; i++) {
+				Long from = inflDates[i] + 1;
+				Long to = inflDates[i + 1];
+				SortedSet<Long> fromTo = availableExchRateDates.subSet(from, to);
+				if (fromTo.size() > 0) {
+					// add last exchange rate from current period
+					do {
+						dates.add(fromTo.last());
+						// and repeat as long as there are lower periods with exchange rates
+						fromTo = fromTo.subSet(from, Math.max(from, Math.min(to - LOWEST_PERIOD_MS, fromTo.last())));
+					} while (fromTo.size() > 0 && from + LOWEST_PERIOD_MS < fromTo.last());
+				}
+			}
+		}
+		return dates;
+	}
+	
+	/**
 	 * @param reference the inflation rates reference
 	 * @return
 	 */
@@ -186,10 +226,7 @@ public class CCExchangeRate {
 			periodEnd = next;
 		}
 		// adjust to previous period end date by deducting 1 day
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(periodEnd);
-		c.add(Calendar.DATE, -1);
-		return c.getTime().getTime();
+		return periodEnd - _1_DAY_TO_MS;
 	}
 	
 	protected boolean isValid() {
@@ -275,35 +312,35 @@ public class CCExchangeRate {
 	 */
 	public static synchronized void regenerateConstantCurrenciesExchangeRates(boolean calledFromQuartzJob, 
 			AmpFiscalCalendar cal) {
-		logger.info("regenerateConstantCurrenciesExchangeRates START");
+		logger.debug("regenerateConstantCurrenciesExchangeRates START");
 		CCExchangeRate ccER = new CCExchangeRate(CurrencyInflationUtil.getInflationRates());
-		logger.info("after CurrencyInflationUtil.getInflationRates()");
+		logger.debug("after CurrencyInflationUtil.getInflationRates()");
 		// limit the constant currencies to regenerate to calendar if specified
 		List<ConstantCurrency> ccs = cal == null ? CurrencyInflationUtil.getAllConstantCurrencies() :
 			CurrencyInflationUtil.wrap(cal.getConstantCurrencies());
 		List<AmpCurrencyRate> newRates = new ArrayList<AmpCurrencyRate>();
 		List<String> ccCodes = new ArrayList<String>();
 		
-		logger.info("before Actual regeneration process");
+		logger.debug("before Actual regeneration process");
 		for (ConstantCurrency cc : ccs) {
 			ccCodes.add(cc.currency.getCurrencyCode());
 			newRates.addAll(ccER.generateExchangeRates(cc));
 		}
-		logger.info("after Actual regeneration process");
+		logger.debug("after Actual regeneration process");
 		// cleanup all previous rates for these constant currencies
 		CurrencyUtil.deleteCurrencyRates(ccCodes);
-		logger.info("after deleteing deleting old currency rates");
+		logger.debug("after deleting old currency rates");
 		if (!calledFromQuartzJob) {
 			AmpCaching.getInstance().currencyCache.reset();
 		}
 		try {
+			Session session = PersistenceManager.getSession();
 			for (AmpCurrencyRate acr: newRates) {
-				PersistenceManager.getSession().save(acr);
+				session.save(acr);
 			}
-			logger.info("after saving new currency rates");
-			// commenting out, since it significantly increases the duration
-			//PersistenceManager.getSession().flush();
-			logger.info("regenerateConstantCurrenciesExchangeRates END");
+			logger.debug("after saving new currency rates");
+			session.flush();
+			logger.debug("regenerateConstantCurrenciesExchangeRates END");
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			throw new RuntimeException(e);

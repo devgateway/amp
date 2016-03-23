0952858706e896aa6323e8b88fc3254bb896cc6b
cdolghier <cdolghier@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-03-23 16:28:35 +0000

    AMP-22313
    minor code reformat
    
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_NIREPORTS_DEVELOPMENT@29767 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/nireports/amp/AmpTrivialMeasure.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/nireports/amp/AmpTrivialMeasure.java
@@ -20,7 +20,7 @@ public class AmpTrivialMeasure extends NiTransactionMeasure {
 					cac.metaInfo.containsMeta(MetaCategory.TRANSACTION_TYPE.category, Long.valueOf(transactionType)) &&
 					cac.metaInfo.containsMeta(MetaCategory.ADJUSTMENT_TYPE.category, adjustmentTypeName) &&
 					(directed ? isDirected(cac) : isDonorSourced(cac)),
-					directed ? new DirectedMeasureBehaviour() : TrivialMeasureBehaviour.getInstance(),
+				directed ? new DirectedMeasureBehaviour() : TrivialMeasureBehaviour.getInstance(),
 				AmpReportsSchema.measureDescriptions.get(measureName)
 			);
 	}
@@ -32,9 +32,8 @@ public class AmpTrivialMeasure extends NiTransactionMeasure {
 	 */
 	public AmpTrivialMeasure(String measureName, long transactionType) {
 		super(measureName, 
-				cac -> 
-					cac.metaInfo.containsMeta(MetaCategory.TRANSACTION_TYPE.category, Long.valueOf(transactionType)),
-					TrivialMeasureBehaviour.getInstance(),
+				cac -> cac.metaInfo.containsMeta(MetaCategory.TRANSACTION_TYPE.category, Long.valueOf(transactionType)),
+				TrivialMeasureBehaviour.getInstance(),
 				AmpReportsSchema.measureDescriptions.get(measureName)
 			);
 	}

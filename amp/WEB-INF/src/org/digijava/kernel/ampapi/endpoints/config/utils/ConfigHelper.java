f0823f1b2257222a7ee3b66739abf14521117c66
apicca <apicca@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-05-02 10:10:22 +0000

    AMP-21644 Create an Endpoint for being able to save desired global settings
    Adding validations to diff possibleValues
    
    
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_11_RELEASE@29994 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/config/utils/ConfigHelper.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/config/utils/ConfigHelper.java
@@ -27,7 +27,17 @@ public class ConfigHelper {
 	private static final String SETTINGS_VALUE = "settingValue";
 	private static final String POSSIBLE_VALUES = "possibleValues";
 	private static final String SETTINGS_NAME = "settingName";
+	private static final String VALUE_TRANSLATABLE = "valueTranslatable";
 	private static final String ORG_DIGIJAVA_MODULE_AIM_HELPER_GLOBAL_SETTINGS_CONSTANTS = "org.digijava.module.aim.helper.GlobalSettingsConstants";
+	
+	private static final String T_BOOLEAN = "t_Boolean";
+	private static final String T_INTEGER = "t_Integer";
+	private static final String T_YEAR_DEFAULT_START = "t_year_default_start";
+	private static final String T_YEAR_DEFAULT_END = "t_year_default_end";
+	private static final String T_STATIC_RANGE = "t_static_range";
+	private static final String T_DOUBLE = "t_Double";
+	private static final String NULL_VALUE = "null";
+	
 	private static final Logger logger = Logger.getLogger(ConfigHelper.class);
 	/**
 	 * Retrieves the class specified as type for Generics
@@ -64,8 +74,12 @@ public class ConfigHelper {
 		set.setGlobalSettingsName(String.valueOf(object.get(SETTINGS_NAME)));
 		set.setGlobalSettingsPossibleValues(String.valueOf(object.get(POSSIBLE_VALUES)));
 		set.setGlobalSettingsValue(String.valueOf(object.get(SETTINGS_VALUE)));
+		if ("null".equalsIgnoreCase(set.getGlobalSettingsPossibleValues())) {
+			set.setGlobalSettingsPossibleValues("");
+		}
 		set.setGlobalSettingsDescription(String.valueOf(object.get(DESCRIPTION)));
 		set.setSection(String.valueOf(object.get(SECTION)));
+		set.setValueTranslatable(Boolean.valueOf(String.valueOf(object.get(VALUE_TRANSLATABLE))));
 		return set;
 	}
 	
@@ -101,18 +115,62 @@ public class ConfigHelper {
 	 */
 	public static boolean validateGlobalSetting(AmpGlobalSettings ampGlobalSetting) {
 		boolean isValid = false;
-		List<KeyValue> possiblesValues = ConfigHelper.getPossibleValues(ampGlobalSetting.getGlobalSettingsPossibleValues());
-		if (possiblesValues!=null) {
-			for (KeyValue value : possiblesValues) {
-				if (ampGlobalSetting.getGlobalSettingsValue().equals(value.getKey()) ) {
-					isValid = true;
-				}
-			}
-		} else {
-			isValid = true;
+		List<KeyValue> possiblesValues = ConfigHelper.getPossibleValues(ampGlobalSetting.getGlobalSettingsPossibleValues());	
+		switch(ampGlobalSetting.getGlobalSettingsPossibleValues()) {
+        case T_BOOLEAN:
+        	isValid = "true".equalsIgnoreCase(ampGlobalSetting.getGlobalSettingsValue()) || "false".equalsIgnoreCase(ampGlobalSetting.getGlobalSettingsValue());
+        	break;
+        case T_INTEGER: case T_STATIC_RANGE:
+        	isValid = isValidNumber(Integer.class,ampGlobalSetting.getGlobalSettingsValue());
+        	break;
+        case T_DOUBLE:
+        	isValid = isValidNumber(Double.class,ampGlobalSetting.getGlobalSettingsValue());
+        	break;
+        case T_YEAR_DEFAULT_START: case T_YEAR_DEFAULT_END:
+        	isValid = isValidNumber(Integer.class,ampGlobalSetting.getGlobalSettingsValue());
+        	if (isValid) {
+        		int value = Integer.parseInt(ampGlobalSetting.getGlobalSettingsValue()); 
+        		isValid = isValid && (value>1900 && value<2099);
+        	}
+        	break;
+        case NULL_VALUE: case "":
+        	isValid = true;
+        	break;
+        default:
+    		if (possiblesValues!=null) {
+    			for (KeyValue value : possiblesValues) {
+    				if (ampGlobalSetting.getGlobalSettingsValue().equals(value.getKey()) ) {
+    					isValid = true;
+    				}
+    			}
+    		}
+            break;
 		}
-		
 		return isValid;
+	}
+	
+	/**
+	* Does a try catch (Exception) on parsing the given string to the given type
+	*
+	* @param c the number type. Valid types are (wrappers included): double, int, float, long.
+	* @param numString number to check
+	* @return false if there is an exception, true otherwise
+	*/
+	public static boolean isValidNumber(Class c, String numString) {
+	  try {
+	    if (c == double.class || c == Double.class) {
+	      Double.parseDouble(numString);
+	    } else if (c == int.class || c == Integer.class) {
+	      Integer.parseInt(numString);
+	    } else if (c == float.class || c == Float.class) {
+	      Float.parseFloat(numString);
+	    } else if (c == long.class || c == Long.class) {
+	      Long.parseLong(numString);
+	    }
+	  } catch (Exception ex) {
+	    return false;
+	  }
+	  return true;
 	}
 	
 	public static String getGlobalSettingName(LinkedHashMap<String, Object> object) {

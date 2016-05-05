c7165fcf2948a0a984ead595491e458f9ab7fe34
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-05-05 20:57:50 +0000

    AMP-22628: Implement report token generation in Saiku for caching mechanism
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_12_RELEASE@30032 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/TEMPLATE/ampTemplate/saikuui_nireports/js/saiku/models/Query.js
+++ amp/TEMPLATE/ampTemplate/saikuui_nireports/js/saiku/models/Query.js
@@ -200,6 +200,8 @@ var Query = Backbone.Model.extend({
             return;
         }
         this.firstLoad = false;
+        
+        exModel.MD5 = this.calculateMD5FromParameters(exModel, this.get('report_id'));
 
         // Run it
         this.workspace.table.clearOut();
@@ -225,5 +227,82 @@ var Query = Backbone.Model.extend({
     url: function() {
     	Saiku.logger.log("Query.url");
    		return "/TEMPLATE/ampTemplate/saikuui_nireports/mockData/query.json";
+    },
+    
+    /**
+     * This function will calculate the MD5 string of the parameters that define a report's result: id, filters, etc.
+     * Also will make some cleanup and reordering to get the same MD5 for the "same" parameters.
+     */
+    calculateMD5FromParameters: function (model, id) {
+    	Saiku.logger.log("QueryRouter.calculateMD5FromParameters");
+    	var parameters = {filters: {}, settings: {}, page: null, id: null};
+    	
+    	parameters.page = model.queryModel.page;
+    	parameters.id = id;
+    	
+    	if (model.queryModel.filters) {
+    		// Everything non-date filters.
+    		if (model.queryModel.filters.columnFilters) {
+    			var columnFilters = {};
+    			for (var property in model.queryModel.filters.columnFilters) {
+    				// To avoid problems with prototype´s properties.
+    				if (model.queryModel.filters.columnFilters.hasOwnProperty(property)) {
+    					// Sort ID's.
+    					columnFilters[property] = _.sortBy(model.queryModel.filters.columnFilters[property], function(item) {return item;});
+    				}
+    			}
+    			// Now sort the properties of the object so stringify will return the same string.
+    			var auxPropertiesArray = [];
+    			for (var property in columnFilters) {
+    				auxPropertiesArray.push(property);
+    			}
+    			var sortedFilters = {};
+    			auxPropertiesArray = auxPropertiesArray.sort();
+    			_.each(auxPropertiesArray, function(item) {
+    				sortedFilters[item] = columnFilters[item];
+    			})
+    			parameters.filters.columnFilters = sortedFilters;
+    		}
+    		
+    		// Date filters.
+    		if (model.queryModel.filters.otherFilters) {
+    			// Sort the properties by name so stringify will return the same string.
+    			var auxPropertiesArray = [];
+    			for (var property in model.queryModel.filters.otherFilters) {
+    				auxPropertiesArray.push(property);
+    			}
+    			var sortedFilters = {};
+    			auxPropertiesArray = auxPropertiesArray.sort();
+    			_.each(auxPropertiesArray, function(item) {
+    				sortedFilters[item] = model.queryModel.filters.otherFilters[item];
+    			})
+    			parameters.filters.otherFilters = sortedFilters;
+    		}
+    	}    	
+    	
+    	if (model.queryModel.settings) {
+    		var auxSettings = model.queryModel.settings;
+			// Now sort the properties of the object so stringify will return the same string.
+			var auxPropertiesArray = [];
+			for (var property in auxSettings) {
+				auxPropertiesArray.push(property);
+			}
+			var sortingSettings = {};
+			auxPropertiesArray = auxPropertiesArray.sort();
+			_.each(auxPropertiesArray, function(item) {
+				sortingSettings[item] = auxSettings[item];
+			})
+			parameters.settings = sortingSettings;    		
+    	}
+    	
+    	if (model.queryModel.sorting) {
+    		// No need to rearrange sorting parameters.
+    		parameters.sorting = model.queryModel.sorting; 
+    	}
+  	
+    	Saiku.logger.log(JSON.stringify(parameters));
+    	var md5 = hex_md5(JSON.stringify(parameters));
+    	Saiku.logger.log(md5);
+		return md5;
     }
 });

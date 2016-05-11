8d18c87c4bcefa47515c25f60ded31fb953ba800
ginchauspe <ginchauspe@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-05-11 15:37:30 +0000

    AMP-22807: Public report settings greyed out
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_12_RELEASE@30055 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/repository/aim/view/scripts/reportWizard/prefilters.js
+++ amp/repository/aim/view/scripts/reportWizard/prefilters.js
@@ -90,8 +90,8 @@ Filters.prototype.showFilters	= function(reportContextId) {
 	YAHOO.util.Connect.asyncRequest("GET", "/aim/reportsFilterPicker.do?sourceIsReportWizard=true&reportContextId=" + reportContextId + avoidIECacheParam +this.resetString+this.additionalParameter, this);
 	this.resetString		= "";
 	
-	// Fix z-index problem on Public Report Generator without changing css loading order (AMP-22742).
-	$("#new_mask").attr('style', function(i,s) { return s + 'z-index: 3 !important;' });
+	// Fix z-index problem on Public Report Generator without changing css loading order.
+	$(".mask").attr('style', function(i,s) { return s + 'z-index: 3 !important;' });
 };
 
 Filters.prototype.showSettings	= function() {
@@ -107,6 +107,9 @@ Filters.prototype.showSettings	= function() {
 	YAHOO.util.Event.removeListener("applyFormatBtn", "click");
 	document.getElementById("applyFormatBtn").onclick	= function() { return false;};
 	YAHOO.util.Event.addListener("applyFormatBtn", "click", this.saveFilters.validateAndSaveFilters, this.saveFilters, this.saveFilters);
+	
+	// Fix z-index problem on Public Report Generator without changing css loading order.
+	$(".mask").attr('style', function(i,s) { return s + 'z-index: 3 !important;' });
 };
 
 function SaveFilters (filterObj, showSettings) {

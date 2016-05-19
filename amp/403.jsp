7e0699a39592dd9da1e921186dd7ef51efea7a1c
acartaleanu <acartaleanu@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-05-19 20:10:39 +0000

    AMP-22744: configure Spring-based authentication for admin pages (now sending to 403 if user not ADMIN)
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_12_RELEASE@30109 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/403.jsp
+++ amp/403.jsp
@@ -1,1 +1,9 @@
-REQUEST DENIED
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head>
+	<body>
+		<h1>Forbidden</h1>
+		<p>You don't have permission to access this page.</p>
+	</body>
+</html>
--- amp/WEB-INF/src/org/dgfoundation/amp/permissionmanager/web/pages/PermissionManager.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/permissionmanager/web/pages/PermissionManager.java
@@ -9,8 +9,15 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.wicket.RestartResponseAtInterceptPageException;
+import org.apache.wicket.authroles.authentication.pages.SignInPage;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.flow.RedirectToUrlException;
+import org.apache.wicket.request.http.handler.RedirectRequestHandler;
+import org.apache.wicket.util.tester.DummyHomePage;
 import org.dgfoundation.amp.permissionmanager.components.features.PermissionManagerFormFeature;
 import org.dgfoundation.amp.permissionmanager.components.features.models.AmpPMFieldPermissionViewer;
 import org.digijava.module.aim.dbentity.AmpModulesVisibility;
@@ -30,6 +37,19 @@ public class PermissionManager extends AmpPMHeaderFooter {
 	public PermissionManager() throws Exception{
 		// TODO Auto-generated constructor stub
 		super();
+		
+		
+		
+		HttpSession session = this.getHttpSession();
+		if (session.getAttribute("ampAdmin") == null) {
+			throw new RedirectToUrlException("/");
+
+		} else {
+			String str = (String)session.getAttribute("ampAdmin");
+			if (str.equals("no")) {
+				throw new RedirectToUrlException("/");
+			}
+		}
 		
 		
 		//managing workspaces

3f4338daec98da77e0b91417ffcbd18069d0b661
cdolghier <cdolghier@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-06-03 09:08:33 +0000

    AMP-22347
    minor change as part of code review/validation
    
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_12_RELEASE@30181 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/dgfoundation/amp/nireports/amp/TaggedMeasure.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/nireports/amp/TaggedMeasure.java
@@ -1,16 +1,16 @@
 package org.dgfoundation.amp.nireports.amp;
 
 import org.dgfoundation.amp.nireports.schema.NiTransactionMeasure;
-import org.dgfoundation.amp.nireports.schema.TrivialMeasureBehaviour;
 
+/**
+ * a trivial measure which splits itself according to the string Meta values stored in a meta of the cells
+ * @author Alexandru Cartaleanu
+ *
+ */
 public class TaggedMeasure extends NiTransactionMeasure {
 
 	/**
 	 * trivial measure which is split down by a category
-	 * @param measureName
-	 * @param transactionType
-	 * @param adjustmentTypeName
-	 * @param directed
 	 */
 	public TaggedMeasure(String measureName, long transactionType, String adjustmentTypeName, MetaCategory metacat, String pseudocolumnName) {
 		super(measureName, 
@@ -18,19 +18,6 @@ public class TaggedMeasure extends NiTransactionMeasure {
 					cac.metaInfo.containsMeta(MetaCategory.TRANSACTION_TYPE.category, Long.valueOf(transactionType)) &&
 					cac.metaInfo.containsMeta(MetaCategory.ADJUSTMENT_TYPE.category, adjustmentTypeName),
 					new TaggedMeasureBehaviour("Total " + measureName, metacat, pseudocolumnName) ,
-				AmpReportsSchema.measureDescriptions.get(measureName), false
-			);
-	}
-	
-	/**
-	 * trivial measure which filters by transactionType only
-	 * @param measureName
-	 * @param transactionType
-	 */
-	public TaggedMeasure(String measureName, long transactionType) {
-		super(measureName, 
-				cac -> cac.metaInfo.containsMeta(MetaCategory.TRANSACTION_TYPE.category, Long.valueOf(transactionType)),
-				TrivialMeasureBehaviour.getInstance(),
 				AmpReportsSchema.measureDescriptions.get(measureName), false
 			);
 	}

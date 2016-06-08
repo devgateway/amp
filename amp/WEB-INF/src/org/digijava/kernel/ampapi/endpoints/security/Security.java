b2a0f0e749ea934695fc7e18071132f645054c13
esoliani <esoliani@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-06-08 13:08:36 +0000

    AMP-22957 create a new API for login and retrieve the token
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_12_RELEASE@30194 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/security/Security.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/security/Security.java
@@ -1,18 +1,21 @@
 package org.digijava.kernel.ampapi.endpoints.security;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.digijava.kernel.ampapi.endpoints.util.AmpApiToken;
 import org.digijava.kernel.ampapi.endpoints.util.ApiMethod;
@@ -32,10 +35,23 @@ import org.digijava.module.aim.dbentity.AmpTeamMember;
 import org.digijava.module.aim.helper.Constants;
 import org.digijava.module.aim.helper.TeamMember;
 import org.digijava.module.aim.util.FeaturesUtil;
+import org.digijava.module.aim.util.TeamMemberUtil;
 import org.digijava.module.aim.util.TeamUtil;
+import org.digijava.module.gateperm.core.GatePermConst;
+import org.digijava.module.gateperm.util.PermissionUtil;
+import org.digijava.module.um.dbentity.SuspendLogin;
+import org.digijava.module.um.util.UmUtil;
+import org.jetbrains.annotations.NotNull;
+import org.joda.time.DateTime;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.xml.sax.SAXException;
 
 import com.sun.jersey.spi.container.ContainerRequest;
+
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 
 /**
  * This class should have all security / permissions related methods
@@ -46,10 +62,14 @@ import com.sun.jersey.spi.container.ContainerRequest;
 @Path("security")
 public class Security {
 	private static final Logger logger = Logger.getLogger(Security.class);
+	private static final String KEY = "testNonce"; // hardcoded value, it must be exactly as application-context.xml
+	private static final String REAL_NAME = "AMP-Realm";
+	private static final String QOP = "auth";
+	private static final String NONCE_COUNTER = "00000001";
+	private static final String CNONCE = "testCnonce";
 	private static String SITE_CONFIG_PATH = "TEMPLATE" + System.getProperty("file.separator") + "ampTemplate"
 			+ System.getProperty("file.separator") + "site-config.xml";
 
-	
 	public static String getSiteConfigPath() {
 		return SITE_CONFIG_PATH;
 	}
@@ -64,7 +84,6 @@ public class Security {
 	@Path("/user/")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 	public JsonBean user() {
-		JsonBean authenticationResult = new JsonBean();
 		boolean isAdmin;
 
 		AmpApiToken apiToken = SecurityUtil.getTokenFromSession();
@@ -92,11 +111,22 @@ public class Security {
 			}
 
 		}
-	    String port="";
-	    //if we are in secure mode and the port is not 443 or if we are not secure and the port is not 80 we have to add the port to the url
-	    if( (TLSUtils.getRequest().isSecure() && TLSUtils.getRequest().getServerPort()!=443 ) ||( !TLSUtils.getRequest().isSecure() && TLSUtils.getRequest().getServerPort()!=80 )){
-	    	port=":"+TLSUtils.getRequest().getServerPort();
-	    }
+		return createResponse(isAdmin, apiToken, username, team, addActivity);
+	}
+
+	@NotNull
+	private String getPort() {
+		String port="";
+		//if we are in secure mode and the port is not 443 or if we are not secure and the port is not 80 we have to add the port to the url
+		if( (this.httpRequest.isSecure() && this.httpRequest.getServerPort()!=443 ) ||( !this.httpRequest.isSecure() && this.httpRequest.getServerPort()!=80 )){
+            port=":"+this.httpRequest.getServerPort();
+        }
+		return port;
+	}
+
+	private JsonBean createResponse(boolean isAdmin, AmpApiToken apiToken, String username, String team, boolean addActivity) {
+		String port = getPort();
+		final JsonBean authenticationResult = new JsonBean();
 		authenticationResult.set("token", apiToken!=null && apiToken.getToken()!=null?apiToken.getToken():null);
 		authenticationResult.set("url", "http"+ (TLSUtils.getRequest().isSecure()?"s":"") +"://"+ TLSUtils.getRequest().getServerName() + port +"/showLayout.do?layout=login");
 		authenticationResult.set("team", team);
@@ -104,9 +134,97 @@ public class Security {
 		authenticationResult.set("is-admin", isAdmin);
 		authenticationResult.set("add-activity", addActivity); //to check if the user can add activity in the selected ws
 		authenticationResult.set("view-activity", !isAdmin); ///at this stage the user can view activities only if you are not admin
-		
 		return authenticationResult;
 	}
+
+	/**
+	 * This is hack EP to avoid Digest Authentication in Activity EP.
+	 *
+	 * @param authentication Json bean with username/password/workspace information
+	 * @return JSON with the response, error or user info
+     */
+	@POST
+	@Path("/user/")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public JsonBean authenticate(JsonBean authentication) {
+		String username = authentication.getString("j_username");
+		String password = authentication.getString("j_password");
+		Integer workspaceId = (Integer) authentication.get("j_autoWorkspaceId");
+		if(StringUtils.isBlank(username) || StringUtils.isBlank(password) || workspaceId == null) {
+			return getErrorResponse(400, "Invalid Parameters");
+		}
+		try {
+			User user = UserUtils.getUserByEmail(username);
+			if(user == null || !user.getPassword().equals(password)) {
+				return getErrorResponse(401, "UNAUTHORIZED");
+			}
+			if(user.isBanned()) { // user is banned
+				SecurityContextHolder.getContext().setAuthentication(null);
+				return getErrorResponse(401, "userBanned");
+			}
+			List<SuspendLogin> su = UmUtil.getUserSuspendReasons (user);
+			if (su != null && !su.isEmpty()) {
+				StringBuilder suReasons = new StringBuilder("userSuspended=");
+				for (SuspendLogin suObject : su) {
+					suReasons.append("'").append(suObject.getReasonText()).append("'");
+				}
+				SecurityContextHolder.getContext().setAuthentication(null);
+				return getErrorResponse(401, suReasons.toString());
+			}
+			AmpTeamMember teamMember = TeamMemberUtil.getAmpTeamMember(new Long(workspaceId));
+			if (teamMember == null || teamMember.getUser().getId() != user.getId()){
+				return getErrorResponse(401, "The user is not part of Workspace ");
+			}
+
+			String token = generateDigest(username, password);
+			UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+			authRequest.setDetails(new WebAuthenticationDetails(this.httpRequest));
+			SecurityContextHolder.getContext().setAuthentication(authRequest);
+
+			AmpApiToken apiToken = new AmpApiToken();
+			apiToken.setToken(token);
+			apiToken.setTeamMember(teamMember.toTeamMember());
+			apiToken.setExpirationTime(new DateTime().plusHours(1));
+
+			// setting to session
+			HttpSession session = this.httpRequest.getSession();
+			PermissionUtil.putInScope(session, GatePermConst.ScopeKeys.CURRENT_MEMBER, teamMember);
+			Map<String, AmpApiToken> tokens = (Map<String, AmpApiToken>) this.httpRequest.getServletContext().getAttribute(SecurityUtil.TOKENS);
+			if(tokens == null) {
+				tokens = new HashMap<>();
+				this.httpRequest.getServletContext().setAttribute(SecurityUtil.TOKENS, tokens);
+			}
+			tokens.put(token, apiToken);
+			return createResponse(user.isGlobalAdmin(), apiToken, username, teamMember.getAmpTeam().getName(), true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return getErrorResponse(500, "Internal Server Error");
+		}
+	}
+
+	private String generateDigest(String username, String password) throws IllegalArgumentException {
+		long expiryTime = System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1);
+		String signatureValue = md5Hex(expiryTime + ":" + KEY);
+		String nonceValue = expiryTime + ":" + signatureValue;
+		String nonceValueBase64 = new String(Base64.encode(nonceValue.getBytes()));
+		String a2 = this.httpRequest.getMethod() + ":" + this.httpRequest.getRequestURI();
+		String a2Md5 = md5Hex(a2);
+		String a1 = username + ":" + REAL_NAME + ":" + password;
+		String a1Md5 = md5Hex(a1);
+
+		String digest = a1Md5 + ":" + nonceValueBase64 + ":" + NONCE_COUNTER + ":" + CNONCE + ":" + QOP + ":" + a2Md5;
+
+		return md5Hex(digest);
+	}
+
+	@NotNull
+	private JsonBean getErrorResponse(int code, String message) {
+		JsonBean response = new JsonBean();
+		response.set("errorCode", code);
+		response.set("errorMgs", message);
+		return response;
+	}
+
 	/**
 	 * @return menu structure for the current view, user and state
 	 */

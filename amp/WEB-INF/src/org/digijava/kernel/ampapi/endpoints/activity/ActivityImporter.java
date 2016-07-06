b6c3d480ff5d52d3db0bf758e1ad4dd0f666bd2f
vchihai <vchihai@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-07-06 09:45:07 +0000

    AMP-22937
    Fixed IATI import if org_role, source_role are missing. Fixed fmPath for Organizations.
    
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_11_RELEASE@30367 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/ActivityImporter.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/ActivityImporter.java
@@ -907,6 +907,7 @@ public class ActivityImporter {
         postprocessActivityReferences();
         updatePPCAmount();
         updateRoleFundings();
+        updateOrgRoles();
 	}
 	
 
@@ -1053,6 +1054,7 @@ public class ActivityImporter {
     /**
 	 * Updates activity fundings with a default source role if the item is disabled from FM (or is null)
 	 */
+	
 	protected void updateRoleFundings() {
 		boolean isSourceRoleEnalbed = FMVisibility.isVisible("/Activity Form/Funding/Funding Group/Funding Item/Source Role", null);
 
@@ -1064,6 +1066,30 @@ public class ActivityImporter {
 				}
 			}
         }
+	}
+	
+	/**
+	 * Updates activity org roles (missing org roles from fundings)
+	 */
+	protected void updateOrgRoles() {
+		for (AmpFunding f : newActivity.getFunding()) {
+			boolean found = false;
+			for (AmpOrgRole role : newActivity.getOrgrole()) {
+				if (role.getRole().getRoleCode().equals(f.getSourceRole()) 
+						&& role.getOrganisation().getAmpOrgId().equals(f.getAmpDonorOrgId())) {
+					found = true;
+					break;
+				}
+			}
+			
+			if (!found) {
+				AmpOrgRole role = new AmpOrgRole();
+				role.setOrganisation(f.getAmpDonorOrgId());
+				role.setActivity(newActivity);
+				role.setRole(f.getSourceRole());
+				newActivity.getOrgrole().add(role);
+			}
+		}
 	}
 	
 	

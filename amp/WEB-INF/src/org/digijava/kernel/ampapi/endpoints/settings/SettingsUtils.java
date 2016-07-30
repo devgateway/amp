d8808b4ad43bd9a1ce212ce26cb7c5fbf5f99a0b
cdolghier <cdolghier@9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4>
2016-07-30 10:49:13 +0000

    AMP-23416
    do not crash the API when the amountUnits does not specify maxFracDigits
    
    git-svn-id: http://svn.dgfoundation.org/amp/branches/AMP_2_12_RELEASE@30611 9e8d75aa-d0cb-44a9-8341-a4b8ba1a38b4

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsUtils.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsUtils.java
@@ -642,7 +642,7 @@ public class SettingsUtils {
 		if (amountFormat != null) {
 			String decimalSymbol = (String) amountFormat.get(SettingsConstants.DECIMAL_SYMBOL);
 			String maxFractDigits = (String) amountFormat.get(SettingsConstants.MAX_FRACT_DIGITS);
-			Integer maxFractDigitsNum  = StringUtils.isNumber(maxFractDigits) ? Integer.valueOf(maxFractDigits) : null;
+			Integer maxFractDigitsNum  = maxFractDigits != null && StringUtils.isNumber(maxFractDigits) ? Integer.valueOf(maxFractDigits) : null;
 			Boolean useGrouping  = (Boolean) amountFormat.get(SettingsConstants.USE_GROUPING);
 			String groupingSeparator  = (String) amountFormat.get(SettingsConstants.GROUP_SEPARATOR);
 			Integer groupingSize  = (Integer) amountFormat.get(SettingsConstants.GROUP_SIZE);

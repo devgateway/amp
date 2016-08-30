948171f1ee87c69a992cf8a32f598e28646d832e
Gerald Mutuhu <geraldmutuhu@gmail.com>
2016-08-30 10:04:10 +0300

    AMP-23705 - Value format on Infowindow, legend
    AMP-23705 Issue - Infowindow showing '.000' for all user added layers
    Modify formating of values on Infowindow and legend.

---
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/libs/local/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+		INDICATOR_TYPE_COUNT: 'Count',
+		INDICATOR_TYPE_RATIO_PERCENTAGE: 'Ratio (% of Total Population)',
+		INDICATOR_TYPE_RATIO_OTHER: 'Ratio (other)',
+		INDICATOR_TYPE_PER_CAPITA:'Per Capita'	
+}

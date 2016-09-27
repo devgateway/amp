e7a8a2b66bc88e315769ed8e390777e70eca1b98
Alexandru Cartaleanu <acartaleanu@developmentgateway.org>
2016-09-27 10:30:15 +0300

    AMP-22061
    more refactoring on ExoticMonthNames

--- amp/WEB-INF/src/org/digijava/module/translation/exotic/ExoticMonthNames.java
+++ amp/WEB-INF/src/org/digijava/module/translation/exotic/ExoticMonthNames.java
@@ -16,22 +16,24 @@ import java.util.concurrent.ConcurrentHashMap;
  *
  */
 public class ExoticMonthNames {
+
+	private static ConcurrentHashMap<Locale, ExoticMonthNames> localesToMonthNames = new ConcurrentHashMap<>();
+
+	static ExoticMonthNames forLocale(Locale loc) {
+		return localesToMonthNames.computeIfAbsent(loc, z -> new ExoticMonthNames(loc));
+	}
+	
 	
 	private final Locale locale;
 	private final List<String> names;
 	
-	private ExoticMonthNames(List<String> names, Locale loc) {
-		this.names = Collections.unmodifiableList(names);
+	
+	private ExoticMonthNames(Locale loc) {
+		this.names = Collections.unmodifiableList(loadMonthNamesForLocale(loc));
 		this.locale = loc;
 	}
 	
-	static ExoticMonthNames forLocale(Locale loc) {
-		return localesToMonthNames.computeIfAbsent(loc, z -> new ExoticMonthNames(loadMonthNamesForLocale(loc), loc));
-	}
-	
-	private static ConcurrentHashMap<Locale, ExoticMonthNames> localesToMonthNames = new ConcurrentHashMap<>();
-	
-	private static List<String> loadMonthNamesForLocale(Locale loc) {
+	private List<String> loadMonthNamesForLocale(Locale loc) {
 		List<String> res = new ArrayList<>();
 		ResourceBundle rb = ResourceBundle.getBundle("org.digijava.module.translation.exotic.ExoticMonthNames", loc);
 		Enumeration<String> keys = rb.getKeys();
@@ -42,29 +44,6 @@ public class ExoticMonthNames {
 		}
 		return res;
 	}
-
-	
-	List<String> getMonthNames() {
-		return names;
-	}
-	
-	/**
-	 * 
-	 * @param monthNumber number of the month, starting from 1
-	 * @return capitalized month name in that language
-	 */
-	public String getFullMonthName(int monthNumber) {
-		return getMonthNames().get(monthNumber - 1);
-
-	}
-	
-	private static String shortenMonthName(String in) {
-		return in.substring(0, 3);
-	}
-	
-	public String getShortMonthName(int monthNumber) {
-		return shortenMonthName(getFullMonthName(monthNumber));
-	}
 	
 	public int getMonthNumber(String shortMonthName) {
 		List<String> monthNames = getMonthNames();
@@ -74,5 +53,26 @@ public class ExoticMonthNames {
 		}
 		throw new RuntimeException("Couldn't find month with short name" + shortMonthName + " in locale " + locale.toString());
 	}
+	
+	/**
+	 * 
+	 * @param monthNumber number of the month, starting from 1
+	 * @return capitalized month name in that language
+	 */
+	public String getFullMonthName(int monthNumber) {
+		return getMonthNames().get(monthNumber - 1);
+	}
 
+	List<String> getMonthNames() {
+		return names;
+	}
+	
+	public String getShortMonthName(int monthNumber) {
+		return shortenMonthName(getFullMonthName(monthNumber));
+	}
+
+	private static String shortenMonthName(String in) {
+		return in.substring(0, 3);
+	}
+	
 }

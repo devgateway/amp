a03b05ca542f4410b126e08200efcac0a857ba22
Gerald Mutuhu <geraldmutuhu@gmail.com>
2016-09-30 13:28:18 +0300

    AMP-24018 Gis map sharing losses statistical layer selection
    AMP-24018 Gis map sharing losses statistical layer selection

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/sidebar/layers/views/statistical-data-view.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/sidebar/layers/views/statistical-data-view.js
@@ -33,20 +33,24 @@ module.exports = BaseControlView.extend({
     var self = this;    
     var layerManager = new LayersManager({sections: self.sections, app: this.app});   
     self.$('.content', self).append(layerManager.render().el);
+   
+    self.app.data.indicators.loadAll().then(function() {
+    	
+    	_.each(self.subSections, function(SectionView) {
+    	      /* Note: This object will access the radioButtonGroup
+    	      * of this parent BaseControlView */
+    	      var section = new SectionView({app: self.app, parent: self});    	      
+    	      /* For Mutual Exclusion: */
+    	      self.radioButtonGroup.push(section.collection);
+    	      self.$('.content', self).append(section.render().el);
+    	    });
+    	
+    	self.addSection(StatisticalLayersConfig.STANDARD);
+    	self.addSection(StatisticalLayersConfig.MY_LAYERS);
+    	self.addSection(StatisticalLayersConfig.SHARED);
+	 });     
     
-    this.addSection(StatisticalLayersConfig.STANDARD);
-    this.addSection(StatisticalLayersConfig.MY_LAYERS);
-    this.addSection(StatisticalLayersConfig.SHARED);
-    _.each(this.subSections, function(SectionView) {
-      /* Note: This object will access the radioButtonGroup
-      * of this parent BaseControlView */
-      var section = new SectionView({app: self.app, parent: self});
-      
-      /* For Mutual Exclusion: */
-      self.radioButtonGroup.push(section.collection);
-
-      self.$('.content', self).append(section.render().el);
-    });
+    
     
     return this;
   },

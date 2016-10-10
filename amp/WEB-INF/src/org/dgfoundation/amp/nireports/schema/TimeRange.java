6e1f6f9f6f16133f172bb4313185f78a6de5048f
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2016-10-10 15:24:19 +0300

    AMP-24010: Reverted accidental commit

--- amp/WEB-INF/src/org/dgfoundation/amp/nireports/schema/TimeRange.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/nireports/schema/TimeRange.java
@@ -87,7 +87,7 @@ public enum TimeRange implements Comparable<TimeRange> {
 
 			@Override
 			public ComparableValue<String> getTotalSubcolumnName() {
-				if (TimeRange.this == QUARTER) {
+				if (TimeRange.this == QUARTER || TimeRange.this == MONTH) {
 					String name = engine.schemaSpecificScratchpad.getTimeRangeSubTotalColumnName(engine.spec);
 					if (name != null) {
 						return new ComparableValue<>(name, Integer.MAX_VALUE);

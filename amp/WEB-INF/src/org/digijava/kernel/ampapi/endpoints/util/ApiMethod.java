b42e3841f67e00cf1630e3c6cae959428c2ebe93
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2016-10-18 12:17:50 +0300

    AMP-24225: Documented ApiMethod

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/util/ApiMethod.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/util/ApiMethod.java
@@ -6,15 +6,71 @@ import java.lang.annotation.RetentionPolicy;
 import org.digijava.kernel.ampapi.endpoints.common.EPConstants;
 import org.digijava.kernel.ampapi.endpoints.security.AuthRule;
 
+/**
+ * This class has three responsibilities:
+ * <ol>
+ * <li>Expose endpoint methods using id & name properties
+ * <li>Authorize calls to endpoint methods using authTypes property
+ * <li>Expose filters using the rest of the properties
+ * </ol>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ApiMethod {
+
+	/**
+	 * Is this method visible in UI?
+	 * <p>Note: Probably this field is related only to <b>filters</b> because ui = true is only in filters endpoint</p>
+	 */
 	boolean ui();
+
+	/**
+	 * Translatable method name.
+	 */
 	String name() default "";
-	String id() ;
+
+	/**
+	 * Method identifier, as opposed to translatable name.
+	 */
+	String id();
+
+	/**
+	 * Link this filter to a list of columns.
+	 *
+	 * <p>Note: Used in filters only.</p>
+	 */
 	String[] columns() default EPConstants.NA;
-	FilterType[] filterType() default FilterType.ALL;
-	String tab() default EPConstants.TAB_UNASSIGNED;
-	String visibilityCheck() default "";
+
+	/**
+	 * Not used anywhere yet. TODO Remove if unused.
+	 *
+	 * <p>Note: Used in filters only.</p>
+	 */
 	String columnGroup() default EPConstants.NA;
+
+	/**
+	 * Specified where this filter can be used.
+	 *
+	 * <p>Note: Used in filters only.</p>
+	 */
+	FilterType[] filterType() default FilterType.ALL;
+
+	/**
+	 * In which tab to display this filter.
+	 *
+	 * <p>Note: Used in filters only.</p>
+	 */
+	String tab() default EPConstants.TAB_UNASSIGNED;
+
+	/**
+	 * Method name in same class to be called to determine visibility of this method. The signature must be:
+	 * public boolean methodName()
+	 *
+	 * <p>Note: Used in filters only.</p>
+	 */
+	String visibilityCheck() default "";
+
+	/**
+	 * Authorization rules that must be applied to this method. Default is no authorization to be done.
+	 */
 	AuthRule[] authTypes() default AuthRule.NONE;
 }

91a48e5c07211865aacb799ed3e582f12f1f9020
Gerald Mutuhu <geraldmutuhu@gmail.com>
2016-11-18 18:34:10 +0300

    AMP-24361 Reuse new settings widget in Tabs
    AMP-24361 Reuse new settings widget in Tabs
    Update Legend when settings are applied
    Configure settings widget to use tabs definitions EP

--- amp/TEMPLATE/ampTemplate/tabs/js/business/settings/settingsManager.js
+++ amp/TEMPLATE/ampTemplate/tabs/js/business/settings/settingsManager.js
@@ -20,7 +20,7 @@ define(['marionette', 'translationManager',
 			draggable : true,
 			caller : 'TABS',
 			isPopup : true,
-			definitionUrl : '/rest/settings-definitions/gis'
+			definitionUrl : '/rest/settings-definitions/tabs'
 		});
 
 		app.TabsApp.listenTo(app.TabsApp.settingsWidget, 'close', function() {
@@ -29,6 +29,7 @@ define(['marionette', 'translationManager',
 
 		app.TabsApp.listenTo(app.TabsApp.settingsWidget, 'applySettings', function() {
 			GridManager.filter(app.TabsApp.currentTab.get('id'), app.TabsApp.serializedFilters, app.TabsApp.settingsWidget.toAPIFormat());
+			SettingsManager.updateLegend(app.TabsApp.settingsWidget.toAPIFormat());
 			$('#settings-popup').hide();
 		});
 		
@@ -39,6 +40,22 @@ define(['marionette', 'translationManager',
 	SettingsManager.openDialog = function() {		
 		$('#settings-popup').show();
 		app.TabsApp.settingsWidget.show();
+	};
+	
+	SettingsManager.updateLegend = function(appliedSettings){
+		// Update the legend section.
+		var currentLegendModel = app.TabsApp.dynamicContentRegion.currentView.legends.currentView.model;
+        var unitsValue = currentLegendModel.attributes.units;
+		var currencyCode = appliedSettings[app.TabsApp.settingsWidget.Constants.CURRENCY_ID];
+		var currencyValue = app.TabsApp.settingsWidget.definitions.findCurrencyById(currencyCode).value;
+		var legend = new Legend({
+			currencyCode : currencyCode,
+			currencyValue : currencyValue,
+			id : null,
+			units : unitsValue
+		});
+		app.TabsApp.dynamicContentRegion.currentView.legends.currentView.model = legend;
+		app.TabsApp.dynamicContentRegion.currentView.legends.currentView.render();
 	};
 
 	return SettingsManager;

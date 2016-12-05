9d35ab0d9643176c6e504dcf8c718b04a217e5b0
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2016-12-05 11:29:38 +0200

    AMP-24801: Moved boundaries from src/main/config to .

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/BoundariesService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/BoundariesService.java
@@ -1,5 +1,7 @@
 package org.digijava.kernel.ampapi.endpoints.gis.services;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -9,14 +11,19 @@ import java.util.Map;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.digijava.kernel.request.TLSUtils;
 import org.digijava.module.aim.helper.GlobalSettingsConstants;
 import org.digijava.module.aim.util.FeaturesUtil;
 
 public class BoundariesService {
 
 	protected static Logger logger = Logger.getLogger(BoundariesService.class);
+
+	private static final String CONTEXT_PATH = TLSUtils.getRequest().getServletContext().getRealPath("/");
+	private static final String BOUNDARY_PATH = "gis" + File.separator + "boundaries" + File.separator;
 
 	/**
 	 * Return the list .json files for this country as a JSONArray object.
@@ -25,9 +32,8 @@ public class BoundariesService {
 	 */
 	public static JSONArray getBoundaries() {
 		String countryIso = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.DEFAULT_COUNTRY);
-		String path = String.format("/gis/boundaries/%s/list.json", countryIso.toUpperCase());
-
-		try (InputStream is = BoundariesService.class.getResourceAsStream(path)) {
+		String path = CONTEXT_PATH + BOUNDARY_PATH + countryIso.toUpperCase() + File.separator + "list.json";
+		try (InputStream is = new FileInputStream(path)) {
 			String jsonTxt = IOUtils.toString(is, StandardCharsets.UTF_8);
 			return (JSONArray) JSONSerializer.toJSON(jsonTxt);
 		} catch (IOException e) {

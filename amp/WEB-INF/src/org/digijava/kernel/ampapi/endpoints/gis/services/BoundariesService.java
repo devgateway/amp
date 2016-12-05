636a4eecd30b9758a107e3b8baccb67eec175fc5
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2016-12-05 10:03:39 +0200

    AMP-24801: Load boundaries list.json using Java Resources API instead of accessing file system

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/BoundariesService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/BoundariesService.java
@@ -1,24 +1,16 @@
 package org.digijava.kernel.ampapi.endpoints.gis.services;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.digijava.kernel.request.TLSUtils;
 import org.digijava.module.aim.helper.GlobalSettingsConstants;
 import org.digijava.module.aim.util.FeaturesUtil;
 
@@ -26,34 +18,22 @@ public class BoundariesService {
 
 	protected static Logger logger = Logger.getLogger(BoundariesService.class);
 
-	private static final String CONTEXT_PATH = TLSUtils.getRequest()
-			.getServletContext().getRealPath("/");
-	private static final String BOUNDARY_PATH = "src" + File.separator + "main"
-			+ File.separator + "resources" + File.separator + "gis"
-			+ File.separator + "boundaries" + File.separator;
-
 	/**
 	 * Return the list .json files for this country as a JSONArray object.
 	 * 
-	 * @param path
 	 * @return
 	 */
 	public static JSONArray getBoundaries() {
-		String countryIso = FeaturesUtil
-				.getGlobalSettingValue(GlobalSettingsConstants.DEFAULT_COUNTRY);
-		String path = CONTEXT_PATH + BOUNDARY_PATH + countryIso.toUpperCase()
-				+ File.separator + "list.json";
-		JSONArray json = null;
-		try {
-			InputStream is = new FileInputStream(path);
-			String jsonTxt = IOUtils.toString(is);
-			json = (JSONArray) JSONSerializer.toJSON(jsonTxt);
+		String countryIso = FeaturesUtil.getGlobalSettingValue(GlobalSettingsConstants.DEFAULT_COUNTRY);
+		String path = String.format("/gis/boundaries/%s/list.json", countryIso.toUpperCase());
 
+		try (InputStream is = BoundariesService.class.getResourceAsStream(path)) {
+			String jsonTxt = IOUtils.toString(is, StandardCharsets.UTF_8);
+			return (JSONArray) JSONSerializer.toJSON(jsonTxt);
 		} catch (IOException e) {
-			logger.error(e);
+			logger.error("Failed to load boundaries for " + countryIso, e);
 			throw new RuntimeException(e);
 		}
-		return json;
 	}
 
 	/**

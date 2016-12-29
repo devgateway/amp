8412eba4184b5399c3a7e7a1f7ac935f42be15ca
Aldo Picca <apicca@developmentgateway.org>
2016-12-29 09:58:26 -0300

    AMP-24632 Adding documentation to document's API

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/documents/Documents.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/documents/Documents.java
@@ -29,16 +29,88 @@ public class Documents {
 	private HttpServletResponse httpResponse;
 
 	/**
-	 * Retrieve a list of top documents.
+	 * Retrieve a list of documents.
 	 * </br>
 	 * <dl>
+	 * The JSON object holds information regarding:
+	 * <dt><b>name</b><dd> - the name of document
+	 * <dt><b>uuid</b><dd> - the uuid of document
+	 * <dt><b>title</b><dd> - the title of document
+	 * <dt><b>description</b><dd> - the description of document
+	 * <dt><b>notes</b><dd> - the notes of document
+	 * <dt><b>calendar</b><dd> - the creation date of document
+	 * <dt><b>contentType</b><dd> - the contentType of document
+	 * <dt><b>webLink</b><dd> - the webLink of document
+	 * <dt><b>generalLink</b><dd> - the generalLink of document
+	 * <dt><b>cmDocType</b><dd> - the DocType of document
+	 * <dt><b>category</b><dd> - the category of document
+	 * <dt><b>organisations</b><dd> - the  of document
+	 * <dt><b>fileSize</b><dd> - the file size of document
+	 * <dt><b>date</b><dd> - the date of document
+	 * <dt><b>yearofPublication</b><dd> - the year of publication of document
+	 * <dt><b>labels</b><dd> - the labels of document
+	 * <dt><b>creatorEmail</b><dd> - the email of the creator of document
+	 * <dt><b>versionNumber</b><dd> - the version of document
+	 * <dt><b>translatedTitles</b><dd> - the translated titles of document
+	 * <dt><b>translatedDescriptions</b><dd> - the translated descriptions of document
+	 * <dt><b>translatedNotes</b><dd> - the translated notes of document
 	 * </dl></br></br>
 	 *
 	 * </br>
 	 * <h3>Sample Output:</h3><pre>
-	 * </pre>
+	 * [
+	 *   {
+	 *      "name": "atl user ratio coutnry.xls",
+	 *      "uuid": "7d345859-a49e-4d52-b26c-5ce1af1141d4",
+	 *      "title": "knkjn",
+	 *      "description": "",
+	 *      "notes": "",
+	 *      "calendar": "12/27/2016",
+	 *      "contentType": "application/vnd.ms-excel",
+	 *      "webLink": null,
+	 *      "generalLink": "/contentrepository/downloadFile.do?uuid=7d345859-a49e-4d52-b26c-5ce1af1141d4",
+	 *      "cmDocType": "Budget",
+	 *      "category": null,
+	 *      "index": null,
+	 *      "organisations": null,
+	 *      "fileSize": 0.014,
+	 *      "date": 1482850002475,
+	 *      "iconPath": "images/icons/xls.gif",
+	 *      "yearofPublication": "2016",
+	 *      "labels": [],
+	 *      "cmDocTypeId": 50,
+	 *      "creatorTeamId": 2,
+	 *      "creatorEmail": "atl@amp.org",
+	 *      "versionNumber": 1,
+	 *      "hasDeleteRights": true,
+	 *      "hasViewRights": false,
+	 *      "hasShowVersionsRights": true,
+	 *      "hasVersioningRights": true,
+	 *      "hasMakePublicRights": true,
+	 *      "hasDeleteRightsOnPublicVersion": true,
+	 *      "hasAddParticipatingOrgRights": false,
+	 *      "isPublic": false,
+	 *      "lastVersionIsPublic": false,
+	 *      "showVersionHistory": true,
+	 *      "hasShareRights": false,
+	 *      "hasUnshareRights": false,
+	 *      "hasApproveVersionRights": false,
+	 *      "shareWith": null,
+	 *      "needsApproval": false,
+	 *      "isShared": false,
+	 *      "lastVersionIsShared": false,
+	 *      "currentVersionNeedsApproval": false,
+	 *      "hasAnyVersionPendingApproval": false,
+	 *      "baseNodeUUID": null,
+	 *      "nodeVersionUUID": null,
+	 *      "translatedTitles": null,
+	 *      "translatedDescriptions": null,
+	 *      "translatedNotes": null
+	 *   {,
+	 *   ....
+	 * ]</pre>
 	 *
-	 * @return nothing or error
+	 * @return a list of JSON objects with the documents
 	 */
 	@GET
 	@Path("/getTopDocuments")

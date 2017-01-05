bc5b6c6cc24d5222c110722fe9f941405d359a1f
Aldo Picca <apicca@developmentgateway.org>
2017-01-05 11:06:45 -0300

    AMP-24620 adding new SettingRange class

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsDefinitionsEndpoint.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsDefinitionsEndpoint.java
@@ -1,16 +1,12 @@
 package org.digijava.kernel.ampapi.endpoints.settings;
 
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
-import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.*;
-
-import java.util.Arrays;
-import java.util.List;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.getCalendarCurrenciesField;
+import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.getCalendarField;
+import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.getCurrencyField;
+import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.getFundingTypeField;
+import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.getReportAmountFormatField;
+import static org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils.getReportYearRangeField;
 
 import org.dgfoundation.amp.newreports.ReportSpecification;
 import org.dgfoundation.amp.reports.mondrian.converters.AmpReportsToReportSpecification;
@@ -18,6 +14,14 @@ import org.digijava.kernel.ampapi.endpoints.errors.ApiErrorResponse;
 import org.digijava.kernel.ampapi.endpoints.errors.ErrorReportingEndpoint;
 import org.digijava.module.aim.dbentity.AmpReports;
 import org.digijava.module.aim.util.DbUtil;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * This endpoint returns settings definitions for each module.
@@ -197,9 +201,9 @@ public class SettingsDefinitionsEndpoint implements ErrorReportingEndpoint {
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public final List<SettingField> getSettingDefinitionsForReports() {
         return Arrays.asList(
-                getCurrencyField(),
-                getCalendarField(),
-                getCalendarCurrenciesField(),
+                //getCurrencyField(),
+                //getCalendarField(),
+                //getCalendarCurrenciesField(),
                 getReportYearRangeField());
     }
 
--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsUtils.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsUtils.java
@@ -1,17 +1,5 @@
 package org.digijava.kernel.ampapi.endpoints.settings;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.apache.log4j.Logger;
 import org.dgfoundation.amp.ar.AmpARFilter;
 import org.dgfoundation.amp.currency.ConstantCurrency;
@@ -47,6 +35,17 @@ import org.digijava.module.common.util.DateTimeUtil;
 import org.digijava.module.translation.util.ContentTranslationUtil;
 import org.h2.util.StringUtils;
 import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Utility class for amp settings handling
@@ -205,11 +204,11 @@ public class SettingsUtils {
 		return selectedId;
 	}
 	
-	private static Settings.YearRange getReportYearRange(ReportSpecification spec) {
-		Settings.YearRange yearRange = null;
+	private static SettingRange getReportYearRange(ReportSpecification spec) {
+		SettingRange yearRange = null;
 
 		if (spec != null && spec.getSettings() != null && spec.getSettings().getYearRangeFilter() != null) {
-			yearRange = new Settings.YearRange();
+			yearRange = new SettingRange(SettingRange.Type.INT_VALUE);
 			yearRange.setFrom(getReportYear(spec.getSettings().getYearRangeFilter().min));
 			yearRange.setTo(getReportYear(spec.getSettings().getYearRangeFilter().max));
 		}
@@ -319,55 +318,22 @@ public class SettingsUtils {
 	 * @return field that defines the year range in reports
 	 */
 	static SettingField getReportYearRangeField(ReportSpecification spec) {
-		SettingOptions yearsOptions = getReportYearsOptions();
 
-		Settings.YearRange range = getReportYearRange(spec);
+		SettingRange range = getReportYearRange(spec);
 		if (range == null) {
-			range = new Settings.YearRange();
+			range = new SettingRange(SettingRange.Type.INT_VALUE);
 			range.setFrom(EndpointUtils.getDefaultReportStartYear());
 			range.setTo(EndpointUtils.getDefaultReportEndYear());
 		}
 
-		List<SettingField> rangeFields = Arrays.asList(
-				getSelectedOptions(range.getFrom(), yearsOptions, SettingsConstants.YEAR_FROM),
-				getSelectedOptions(range.getTo(), yearsOptions, SettingsConstants.YEAR_TO));
-
 		return new SettingField(SettingsConstants.YEAR_RANGE_ID, null,
-				SettingsConstants.ID_NAME_MAP.get(SettingsConstants.YEAR_RANGE_ID), rangeFields);
+				SettingsConstants.ID_NAME_MAP.get(SettingsConstants.YEAR_RANGE_ID), range);
 	}
 	
 	private static String getReportYear(String year) {
 		if (year == null || MoConstants.FILTER_UNDEFINED_MAX.equals(year))
 			return SettingsConstants.YEAR_ALL;
 		return year;
-	}
-	
-	/**
-	 * @return report default year list options
-	 */
-	private static SettingOptions getReportYearsOptions() {
-		// build year  options
-		List<SettingOptions.Option> options = new ArrayList<SettingOptions.Option>();
-		Long optionRangeStart = FeaturesUtil.getGlobalSettingValueLong(Constants.GlobalSettings.YEAR_RANGE_START);
-		Long optionRangeEnd = optionRangeStart +  
-				FeaturesUtil.getGlobalSettingValueLong(Constants.GlobalSettings.NUMBER_OF_YEARS_IN_RANGE);
-		
-		// add "All" option
-		SettingOptions.Option yearOption = new SettingOptions.Option(
-				SettingsConstants.YEAR_ALL, 
-				SettingsConstants.ID_NAME_MAP.get(SettingsConstants.YEAR_ALL),
-				SettingsConstants.YEAR_MAP.get(SettingsConstants.YEAR_ALL),
-				true);
-		options.add(yearOption);
-		
-		// add actual years list to select from  
-		for (long year =  optionRangeStart; year <= optionRangeEnd; year++ ) {
-			final String yearStr = String.valueOf(year);
-			yearOption = new SettingOptions.Option(yearStr, yearStr);
-			options.add(yearOption);
-		}
-		
-		return new SettingOptions(null, options);
 	}
 	
 	private static SettingField getSelectedOptions(String selectedId, SettingOptions defaults, String id) {

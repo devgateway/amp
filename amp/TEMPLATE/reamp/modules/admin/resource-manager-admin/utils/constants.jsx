ce918a0452e0be175df52c1ca209b13fb156dbb0
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-02-23 12:48:03 -0300

    AMP-25517 adjust endpoints

--- amp/TEMPLATE/reamp/modules/admin/resource-manager-admin/utils/constants.jsx
+++ amp/TEMPLATE/reamp/modules/admin/resource-manager-admin/utils/constants.jsx
@@ -1,10 +1,10 @@
 /**
  * Created by JulianEduardo on 15/2/2017.
  */
-export const REST_BASE='/rest';
-export const ALLOWED_FILE_TYPES_ENDPOINT = '/filetypes/allowed';
-export const AVAILABLE_FILE_TYPES_ENDPOINT='/filetypes';
-export const SAVE_FILE_TYPES_ENDPOINT = '/filetypes/allowed';
+export const REST_BASE = '/rest';
+export const ALLOWED_FILE_TYPES_ENDPOINT = '/resourcemanager/file-types-allowed';
+export const AVAILABLE_FILE_TYPES_ENDPOINT = '/resourcemanager/file-types';
+export const SAVE_FILE_TYPES_ENDPOINT = '/resourcemanager/file-types-allowed';
 export const ALERT_TYPE = {
     NONE: "none",
     SUCCESS: "success",

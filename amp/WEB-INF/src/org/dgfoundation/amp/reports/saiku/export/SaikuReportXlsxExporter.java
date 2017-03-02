6423a40f4d66239c4e423afce83aba335e6715ef
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2017-03-02 17:54:40 +0200

    AMP-25530: Checkstyle fixes
    (cherry picked from commit ef8781f)

--- amp/WEB-INF/src/org/dgfoundation/amp/reports/saiku/export/SaikuReportXlsxExporter.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/reports/saiku/export/SaikuReportXlsxExporter.java
@@ -287,14 +287,16 @@ public class SaikuReportXlsxExporter implements SaikuReportExporter {
 			int rowPos = renderTableRow(sheet, report, reportArea, level+1, row);
 			if (level == 0 || report.hasMeasures()) {
 				CellRangeAddress hierarchyCell = new CellRangeAddress(rowPosInit, rowPos, level, level);
-				if (hierarchyCell.getNumberOfCells() > 1)
+				if (hierarchyCell.getNumberOfCells() > 1) {
 					sheet.addMergedRegion(hierarchyCell);
+				}
 				row = sheet.createRow(rowPos + 1);
 			}
 		}
 
-		if (level > 0 && report.hasMeasures())
+		if (level > 0 && report.hasMeasures()) {
 			renderSubTotalRow(sheet, report, reportContents, level, row);
+		}
 		
 		return row.getRowNum();
 	}

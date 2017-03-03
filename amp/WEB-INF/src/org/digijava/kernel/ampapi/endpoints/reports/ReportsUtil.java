8c08da8304868d3bad2d16eae0720ba3193c9f10
Aldo Picca <apicca@developmentgateway.org>
2017-03-03 17:47:19 -0300

    AMP-17716 replacing string by constants

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/reports/ReportsUtil.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/reports/ReportsUtil.java
@@ -81,6 +81,7 @@ public class ReportsUtil {
     
 	protected static final Logger logger = Logger.getLogger(ReportsUtil.class);
 
+	//describing the timeframe by which to group funding data in the report
 	public static final String GROUPING_YEARLY = "A";
 	public static final String GROUPING_QUARTERLY = "Q";
 	public static final String GROUPING_MONTHLY = "M";

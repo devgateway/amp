d70aecb0c0df084fc5607f31d07adc57f2f48ea6
Gerald Mutuhu <geraldmutuhu@gmail.com>
2017-03-10 15:45:40 +0300

    AMP-25383 refactor Donor Notes reducer
    AMP-25383 refactor Donor Notes reducer

--- amp/TEMPLATE/reamp/modules/gpi/reducers/DonorNotesReducer.jsx
+++ amp/TEMPLATE/reamp/modules/gpi/reducers/DonorNotesReducer.jsx
@@ -9,77 +9,67 @@ const defaultState = {
                 offset: 0,
                 currentPageNumber: 1,
                 totalPageCount : 1
-             },
-             sorting: {
-                 orderBy: 'notesDate',
-                 sortOrder: 'desc'
-             }             
-           }
+            },
+            sorting: {
+                orderBy: 'notesDate',
+                sortOrder: 'desc'
+            }             
+        }
 };
 
 export default function donorNotesReducer(state: Object = defaultState.data, action: Object) {    
     switch (action.type) {
     case 'LOAD_DONOR_NOTES_LIST_SUCCESS':
-        const data = Object.assign({}, action.data)
-        data.cid = state.cid;               
-        return data;
-    case 'DONOR_NOTES_ON_SAVE':
-        var data = {};
-        if (action.data.errors && action.data.errors.length > 0) {
-            data.donorNotesList = [...state.donorNotesList]
+        var newState = Object.assign({}, action.data);        
+        newState.cid = state.cid;               
+        return newState;
+    case 'DONOR_NOTES_ON_SAVE':       
+        var newState = Object.assign({}, state);
+        var actionData = Object.assign({}, action.data);
+        if (actionData.errors && actionData.errors.length > 0) {
+            newState.donorNotesList = [...newState.donorNotesList]
         } else {
-            data.donorNotesList = state.donorNotesList.map(function(donorNotes) { return ((donorNotes.id && donorNotes.id === action.data.donorNotes.id) || (donorNotes.cid && donorNotes.cid === action.data.donorNotes.cid)) ? Object.assign({}, action.data.donorNotes) : donorNotes; });
+            newState.donorNotesList = newState.donorNotesList.map(function(donorNotes) { return ((donorNotes.id && donorNotes.id === actionData.donorNotes.id) || (donorNotes.cid && donorNotes.cid === actionData.donorNotes.cid)) ? Object.assign({}, actionData.donorNotes) : donorNotes; });
         }   
-        data.errors = action.data.errors || [];
-        data.infoMessages = action.data.infoMessages || []; 
-        data.cid = state.cid;
-        data.paging = state.paging;
-        data.sorting = state.sorting;
-        return data;
+        newState.errors = actionData.errors || [];
+        newState.infoMessages = actionData.infoMessages || [];         
+        return newState;
     case 'DONOR_NOTES_ON_SAVE_ALL_EDITS':        
-        var data = {};
-        data.donorNotesList = state.donorNotesList.map(function(donorNotes) { 
-            var found = action.data.donorNotesList.find(obj =>{ obj
+        var newState = Object.assign({}, state);
+        var actionData = Object.assign({}, action.data);
+        newState.donorNotesList = newState.donorNotesList.map(function(donorNotes) { 
+            var found = actionData.donorNotesList.find(obj =>{ obj
                 return ((donorNotes.id && donorNotes.id === obj.id) || (donorNotes.cid && donorNotes.cid === obj.cid))     
             })
             
             return found ? Object.assign({}, found) : donorNotes;            
-        });
-        
-        data.errors = action.data.errors || [];
-        data.infoMessages = action.data.infoMessages || []; 
-        data.cid = state.cid;
-        data.paging = state.paging;
-        data.sorting = state.sorting;
-        return data;        
+        });        
+        newState.errors = actionData.errors || [];
+        newState.infoMessages = actionData.infoMessages || [];        
+        return newState;        
     case 'DONOR_NOTES_DELETE_SUCCESS':
-        var data = {};
-        data.donorNotesList = [...state.donorNotesList.filter(donorNotes => donorNotes.id !== action.data.donorNotes.id || donorNotes.cid !== action.data.donorNotes.cid)]
-        data.errors = action.data.errors || [];
-        data.infoMessages = action.data.infoMessages || []; 
-        data.cid = state.cid;
-        data.paging = state.paging;
-        data.sorting = state.sorting;
-        return data;
+        var newState = Object.assign({}, state);
+        var actionData = Object.assign({}, action.data);
+        newState.donorNotesList = [...newState.donorNotesList.filter(donorNotes => donorNotes.id !== actionData.donorNotes.id || donorNotes.cid !== actionData.donorNotes.cid)]
+        newState.errors = actionData.errors || [];
+        newState.infoMessages = actionData.infoMessages || []; 
+        return newState;
     case 'ADD_DONOR_NOTES':
-        var data = {};
-        action.data.donorNotes.cid = state.cid;
-        data.donorNotesList = [Object.assign({}, action.data.donorNotes), ...state.donorNotesList];
-        data.errors = [];
-        data.infoMessages = [];
-        data.cid = ++state.cid;
-        data.paging = state.paging;
-        data.sorting = state.sorting;
-        return data; 
+        var newState = Object.assign({}, state);
+        var actionData = Object.assign({}, action.data);
+        actionData.donorNotes.cid = state.cid;
+        newState.donorNotesList = [Object.assign({}, actionData.donorNotes), ...newState.donorNotesList];
+        newState.errors = [];
+        newState.infoMessages = [];
+        newState.cid = ++newState.cid;        
+        return newState; 
     case 'UPDATE_DONOR_NOTES':
-        var data = {};
-        data.donorNotesList =  state.donorNotesList.map(function(donorNotes) { return ((donorNotes.id && donorNotes.id === action.data.donorNotes.id) || (donorNotes.cid && donorNotes.cid === action.data.donorNotes.cid)) ? Object.assign({}, action.data.donorNotes) : donorNotes; });     
-        data.errors = action.data.errors || [];
-        data.infoMessages = action.data.infoMessages || []; 
-        data.cid = state.cid; 
-        data.paging = state.paging;
-        data.sorting = state.sorting;
-        return data;   
+        var newState = Object.assign({}, state);
+        var actionData = Object.assign({}, action.data);
+        newState.donorNotesList =  newState.donorNotesList.map(function(donorNotes) { return ((donorNotes.id && donorNotes.id === actionData.donorNotes.id) || (donorNotes.cid && donorNotes.cid === actionData.donorNotes.cid)) ? Object.assign({}, actionData.donorNotes) : donorNotes; });     
+        newState.errors = actionData.errors || [];
+        newState.infoMessages = actionData.infoMessages || [];         
+        return newState;   
     default:            
         return state;
     }

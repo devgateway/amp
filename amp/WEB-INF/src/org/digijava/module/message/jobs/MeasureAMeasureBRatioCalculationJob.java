aeb99ebcc2f1199547273a1e9b41365059ef4719
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-03-19 22:06:59 -0300

    AMP-21988 swich MeasureAMeasureBRatioCalculationJob  to use amp_global_event_log  to record job usage

--- amp/WEB-INF/src/org/digijava/module/message/jobs/MeasureAMeasureBRatioCalculationJob.java
+++ amp/WEB-INF/src/org/digijava/module/message/jobs/MeasureAMeasureBRatioCalculationJob.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -29,9 +30,11 @@ import org.dgfoundation.amp.newreports.ReportSpecificationImpl;
 import org.dgfoundation.amp.visibility.data.MeasuresVisibility;
 import org.digijava.kernel.ampapi.endpoints.common.EndpointUtils;
 import org.digijava.kernel.ampapi.endpoints.scorecard.model.Quarter;
+import org.digijava.kernel.ampapi.endpoints.security.Security;
 import org.digijava.kernel.ampapi.exception.AmpApiException;
 import org.digijava.kernel.persistence.PersistenceManager;
 import org.digijava.kernel.request.TLSUtils;
+import org.digijava.kernel.startup.BuildVersionVerifier;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
 import org.digijava.module.aim.dbentity.AmpFiscalCalendar;
 import org.digijava.module.aim.helper.GlobalSettingsConstants;
@@ -50,7 +53,6 @@ public class MeasureAMeasureBRatioCalculationJob extends ConnectionCleaningJob i
 	protected static Logger logger = Logger.getLogger(MeasureAMeasureBRatioCalculationJob.class);
 	private static Double DEFAULT_PERCENTAGE = 1D;
 	private static BigDecimal HUNDRED = new BigDecimal(100);
-	private DateTime previousFireTime;
 
 	@Override
 	public void executeInternal(JobExecutionContext context) throws JobExecutionException {
@@ -58,9 +60,7 @@ public class MeasureAMeasureBRatioCalculationJob extends ConnectionCleaningJob i
 		if (TLSUtils.getRequest() == null) {
 			TLSUtils.populateMockTlsUtils();
 		}
-		if (context.getTrigger().getPreviousFireTime() != null) {
-			previousFireTime = new DateTime(context.getTrigger().getPreviousFireTime());
-		}
+		
 		Long ampTeamId = FeaturesUtil
 				.getGlobalSettingValueLong(GlobalSettingsConstants.WORKSPACE_TO_RUN_REPORT_FUNDING_GAP_NOTIFICATION);
 		final ValueWrapper<Long> ampTeamMemberId = new ValueWrapper<Long>(null);
@@ -219,6 +219,21 @@ public class MeasureAMeasureBRatioCalculationJob extends ConnectionCleaningJob i
 					} catch (Exception e) {
 						logger.error("Cannot execute JOB", e);
 					}
+				//after the job was run we store an event in global event log
+
+					final String eventName=MeasureAMeasureBRatioCalculationJob.class.getName();
+					final String quarter=previousQuarter.toString();
+					PersistenceManager.getSession().doWork(new Work(){
+						@Override
+						public void execute(Connection conn) throws SQLException {
+							
+							SQLUtils.executeQuery(conn, String.format("insert into amp_global_event_log "+ 
+							" select  (select max(id)+1 from amp_global_event_log) ,now(),'%s','%s',amp_version,amp_version_encoded  "+
+							" from amp_global_event_log where id =(select max(id) from amp_global_event_log where event_name='AMP startup') "
+								, eventName, quarter));
+						}
+					});
+
 				} else {
 					// in this case the measures were configured but not visible
 					// any
@@ -230,7 +245,7 @@ public class MeasureAMeasureBRatioCalculationJob extends ConnectionCleaningJob i
 				}
 			} else {
 				// should run report
-				logger.error(this.getClass() + " Shouldnt run since its not 25 days after the last quarter ended ");
+				logger.error(this.getClass() + " Should not run since its not 25 days after the last quarter ended or it has already run for this quarter");
 			}
 		} else {
 			logger.error(this.getClass() + " could not run because the team is not correctly configured for setting "
@@ -241,16 +256,37 @@ public class MeasureAMeasureBRatioCalculationJob extends ConnectionCleaningJob i
 	private Quarter checkIfShouldRunReport() {
 		Long gsCalendarId = FeaturesUtil.getGlobalSettingValueLong(GlobalSettingsConstants.DEFAULT_CALENDAR);
 		AmpFiscalCalendar fiscalCalendar = FiscalCalendarUtil.getAmpFiscalCalendar(gsCalendarId);
+		DateTime today = new DateTime();
 
 		Quarter currentQuarter = new Quarter(fiscalCalendar, new Date());
 
 		DateTime lastQuarterStartDayPlus25 = new DateTime(currentQuarter.getPreviousQuarter().getQuarterEndDate());
 		lastQuarterStartDayPlus25 = lastQuarterStartDayPlus25.plusDays(25);
 
-		// we at least have
-		if (previousFireTime == null || previousFireTime.isBefore(lastQuarterStartDayPlus25)) {
-			// if the last fire date is before 25 days after the quarter has
-			// ended
+		//we check if we are 25 days after the quarter has ended and that it has not run for that quarter
+		final ValueWrapper<Boolean> shouldRunJob = new ValueWrapper<Boolean>(true);
+		try {
+			final String previousQuarterName = currentQuarter.getPreviousQuarter().toString();
+			final String eventName = MeasureAMeasureBRatioCalculationJob.class.getName();
+			PersistenceManager.getSession().doWork(new Work() {
+				public void execute(Connection conn) throws SQLException {
+					String query = String.format(
+							"select * from amp_global_event_log where event_name ='%s' and message='%s'", eventName,
+							previousQuarterName);
+					RsInfo shouldRunJobRS = SQLUtils.rawRunQuery(conn, query, null);
+					while (shouldRunJobRS.rs.next()) {
+						shouldRunJob.value = false;
+					}
+					shouldRunJobRS.close();
+				}
+
+			});
+		} catch (Exception e) {
+			logger.error("could get last job run for this quarter", e);
+		}
+
+		if (shouldRunJob.value && today.isAfter((lastQuarterStartDayPlus25) ) ) {
+
 			return currentQuarter.getPreviousQuarter();
 		} else {
 

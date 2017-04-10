bfcf2dfcc28a442c0d5fe2b6540bf8d671599576
Gerald Mutuhu <geraldmutuhu@gmail.com>
2017-04-10 11:20:29 +0300

    AMP-25790 - API documentation, EP security
    AMP-25790 - API documentation, EP security

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gpi/GPIEndPoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gpi/GPIEndPoints.java
@@ -1,7 +1,6 @@
 package org.digijava.kernel.ampapi.endpoints.gpi;
 
 import java.util.List;
-import java.util.Set;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,15 +12,61 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.digijava.kernel.ampapi.endpoints.errors.ErrorReportingEndpoint;
+import org.digijava.kernel.ampapi.endpoints.security.AuthRule;
 import org.digijava.kernel.ampapi.endpoints.util.ApiMethod;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
-import org.digijava.module.aim.dbentity.AmpOrganisation;
-import org.digijava.module.aim.dbentity.AmpTeamMember;
 
 @Path("gpi")
 public class GPIEndPoints implements ErrorReportingEndpoint {
-	/***
-	 * retrieves a list of aid on budget objects
+
+	/**
+	 * Retrieves a list of aid on budget objects </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding:
+	 * <dt><b>data</b><dd> - list of aid on budget objects
+	 * <dt><b>totalRecords</b><dd> - total number of aid on budget records
+	 * </dl></br></br>
+	 * 
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * 
+	 * {
+	 * "data" : [{
+	 *	"id" : 5,
+	 *	 "donorId" : 1113,
+	 *			"currencyCode" : "ETB",
+	 *			"amount" : 7888.0,
+	 *			"indicatorDate" : "2017-04-21"
+	 *		}, {
+	 *			"id" : 1,
+	 *			"donorId" : 1370,
+	 *			"currencyCode" : "CHF",
+	 *			"amount" : 4777.0,
+	 *			"indicatorDate" : "2017-04-07"
+	 *		}, {
+	 *			"id" : 4,
+	 *			"donorId" : 1333,
+	 *			"currencyCode" : "USD",
+	 *			"amount" : 7888.0,
+	 *			"indicatorDate" : "2017-04-07"
+	 *		}, {
+	 *			"id" : 2,
+	 *			"donorId" : 1443,
+	 *			"currencyCode" : "XUA",
+	 *			"amount" : 88328.0,
+	 *			"indicatorDate" : "2017-04-06"
+	 *		}, {
+	 *			"id" : 3,
+	 *			"donorId" : 1370,
+	 *			"currencyCode" : "XUA",
+	 *			"amount" : 6999.0,
+	 *			"indicatorDate" : "2017-04-06"
+	 *		}
+	 *	],
+	 *	"totalRecords" : 5	
+	 *	} 
+	 * </pre>
 	 * @param offset
 	 * @param count maximum number of records to return
 	 * @param orderBy field that will be used for sorting
@@ -31,93 +76,308 @@ public class GPIEndPoints implements ErrorReportingEndpoint {
 	@GET
 	@Path("/aid-on-budget")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "getAidOnBudgetList", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "getAidOnBudgetList", ui = false)
 	public JsonBean getAidOnBudgetList(@QueryParam("offset") Integer offset, @QueryParam("count") Integer count,
 			@QueryParam("orderby") String orderBy, @QueryParam("sort") String sort) {
 		return GPIDataService.getAidOnBudgetList(offset, count, orderBy, sort);
 	}
 
-	/***
-	 * Retrieves object aid on budget by id 
+	/**
+	 * Retrieves object aid on budget by id
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding:
+	 * <dt><b>id</b><dd> - id of the returned object
+	 * <dt><b>donorId</b><dd> - id of a donor agency
+	 * <dt><b>currencyCode</b><dd> - currency code e.g USD
+	 * <dt><b>amount</b><dd> - aid amount
+	 * <dt><b>indicatorDate</b><dd> - date
+	 * </dl></br></br>
+	 * 
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *	   "id" : 5,
+	 *	    "donorId" : 1113,
+	 *		"currencyCode" : "ETB",
+	 *		"amount" : 7888.0,
+	 *		"indicatorDate" : "2017-04-21"
+	 *	}
+	 * </pre>
 	 * @param id the ID that will be used to query the database
 	 * @return a json object of the found aid on budget object
 	 */
 	@GET
 	@Path("/aid-on-budget/{id}")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "getAidOnBudgetById", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "getAidOnBudgetById", ui = false)
 	public JsonBean getAidOnBudgetById(@PathParam("id") long id) {
 		return GPIDataService.getAidOnBudgetById(id);
 	}
 
-	/***
+	/**
 	 * saves an aid on budget object to the database
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding:
+	 * <dt><b>data</b><dd> - saved aid on budget object
+	 * <dt><b>result</b><dd> - result string that indicates if the save was successful or not - SAVED/SAVE_FAILED
+	 * <dt><b>errors</b><dd> - an array of error objects for all the errors that occurred while saving
+	 * </dl></br></br> 
+	 * <h3>Sample Input:</h3>
+	 * <pre>
+	 * {
+	 *    "id" : 5,
+	 *	  "donorId" : 1113,
+	 *	  "currencyCode" : "ETB",
+	 *	  "amount" : 7888.0,
+	 *    "indicatorDate" : "2017-04-21"
+	 * }
+	 * </pre>
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *    "data" : {
+	 *                 "id" : 5,
+	 *                  "donorId" : 1113,
+	 *                   "currencyCode" : "ETB",
+	 *                   "amount" : 7888.0,
+	 *                   "indicatorDate" : "2017-04-21"
+	 * },
+	 *     "result" : "SAVED",
+	 *      "errors" : []
+	 * }
+	 * </pre>
 	 * @param aidOnBudget json data that will be used to create or update an aid on budget row
 	 * @return json object of the created or updated aid on budget object. Also returns any error messages that occur
 	 */
 	@POST
 	@Path("/aid-on-budget")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "saveAidOnBudget", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "saveAidOnBudget", ui = false)
 	public JsonBean saveAidOnBudget(JsonBean aidOnBudget) {
 		return GPIDataService.saveAidOnBudget(aidOnBudget);
 	}
 
 	/**
 	 * Saves a list of aid on budget objects to the database
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * Returns a list of JSON objects. The JSON objects holds information regarding:
+	 * <dt><b>data</b><dd> - saved aid on budget object
+	 * <dt><b>result</b><dd> - result string that indicates if the save was successful or not - SAVED/SAVE_FAILED
+	 * <dt><b>errors</b><dd> - an array of error objects for all the errors that occurred while saving
+	 * </dl></br></br> 
+	 * <h3>Sample Input:</h3>
+	 * <pre>
+	 * [{
+	 *    "id" : 5,
+	 *	  "donorId" : 1113,
+	 *	  "currencyCode" : "ETB",
+	 *	  "amount" : 7888.0,
+	 *    "indicatorDate" : "2017-04-21"
+	 * },
+     * {
+	 *    "donorId" : 1114,
+	 *	  "currencyCode" : "ETB",
+	 *	  "amount" : 9000,
+	 *    "indicatorDate" : "2017-01-21"
+	 * }]
+	 * </pre>
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * [{
+	 *    "data" : {
+	 *                 "id" : 5,
+	 *                  "donorId" : 1113,
+	 *                   "currencyCode" : "ETB",
+	 *                   "amount" : 7888.0,
+	 *                   "indicatorDate" : "2017-04-21"
+	 * },
+	 *     "result" : "SAVED",
+	 *     "errors" : []
+	 * },
+     * {
+	 *    "data" : {
+	 *                 "id" : 6,
+	 *                  "donorId" : 1114,
+	 *                   "currencyCode" : "ETB",
+	 *                   "amount" : 9000,
+	 *                   "indicatorDate" : "2017-01-21"
+	 * },
+	 *     "result" : "SAVED",
+	 *     "errors" : []
+	 * }]
+	 * </pre>
 	 * @param aidOnBudgetList - list of aid on budget objects that will be saved
 	 * @return list of aid on budget objects that were saved. Also returns any server side validation errors
 	 */
 	@POST
 	@Path("/aid-on-budget/save-all")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "saveAllEdits", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "saveAllEdits", ui = false)
 	public List<JsonBean> saveAllEdits(List<JsonBean> aidOnBudgetList) {
 		return GPIDataService.saveAidOnBudget(aidOnBudgetList);
 	}
 
-	/***
-	 * Deletes the aid on budget row on the database
+	/**
+	 *  Deletes the aid on budget row on the database
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding the result of the delete action:
+	 * <dt><b>result</b><dd> - result string that indicates if the delete was successful
+	 * </dl></br></br> 
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *    "result" : "deleted"
+	 * }
+	 * </pre>
 	 * @param id identifier to query for aid on budget to delete
-	 * @return object containing the result
+	 * @return JSON object containing the result
 	 */
 	@DELETE
 	@Path("/aid-on-budget/{id}")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "deleteAidOnBudgetById", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "deleteAidOnBudgetById", ui = false)
 	public JsonBean deleteAidOnBudgetById(@PathParam("id") long id) {
 		return GPIDataService.deleteAidOnBudgetById(id);
 	}
 
-	/***
+	/**
 	 * saves a donorNotes object to the database
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding:
+	 * <dt><b>data</b><dd> - saved donorNotes object
+	 * <dt><b>result</b><dd> - result string that indicates if the save was successful or not - SAVED/SAVE_FAILED
+	 * <dt><b>errors</b><dd> - an array of error objects for all the errors that occurred while saving
+	 * </dl></br></br> 
+	 * <h3>Sample Input:</h3>
+	 * <pre>
+	 * {
+	 *		"id" : 1,
+	 *		"donorId" : 1061,
+	 *		"notes" : "Sample note 1",
+	 *		"notesDate" : "2017-04-14"
+	 *	}
+	 * </pre>
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *    "data" : {
+	 *                 "id" : 1,
+	 *                 "donorId" : 1061,
+	 *                 "notes" : "Sample note 1",
+	 *                 "notesDate" : "2017-04-14"                  
+	 * },
+	 *     "result" : "SAVED",
+	 *     "errors" : []
+	 * }
+	 * </pre>
 	 * @param donorNotes json representation of the donorNotes object to be saved
 	 * @return json object containing the donorNotes object that and any server side validation errors
 	 */
 	@POST
 	@Path("/donor-notes")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "saveDonorNotes", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "saveDonorNotes", ui = false)
 	public JsonBean saveDonorNotes(JsonBean donorNotes) {
 		return GPIDataService.saveDonorNotes(donorNotes);
 	}
 
-	/***
+	/**
 	 * saves a list donorNotes objects to the database
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding:
+	 * <dt><b>data</b><dd> - saved donorNotes object
+	 * <dt><b>result</b><dd> - result string that indicates if the save was successful or not - SAVED/SAVE_FAILED
+	 * <dt><b>errors</b><dd> - an array of error objects for all the errors that occurred while saving
+	 * </dl></br></br> 
+	 * <h3>Sample Input:</h3>
+	 * <pre>
+	 * [{
+	 *		"id" : 1,
+	 *		"donorId" : 1061,
+	 *		"notes" : "Sample note 1",
+	 *		"notesDate" : "2017-04-14"
+	 * },
+     * {
+	 *		"donorId" : 1062,
+	 *		"notes" : "Sample note 2",
+	 *		"notesDate" : "2017-04-14"
+	 * }]
+	 * </pre>
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * [{
+	 *    "data" : {
+	 *                 "id" : 1,
+	 *                 "donorId" : 1061,
+	 *                 "notes" : "Sample note 1",
+	 *                 "notesDate" : "2017-04-14"                
+	 * },
+	 *     "result" : "SAVED",
+	 *     "errors" : []
+	 * },
+     *  {
+	 *    "data" : {
+	 *                 "id" : 2,
+	 *                 "donorId" : 1062,
+	 *                 "notes" : "Sample note 2",
+	 *                 "notesDate" : "2017-04-14"
+	 * },
+	 *     "result" : "SAVED",
+	 *     "errors" : []
+	 * }
+     * ]
+	 * </pre>
 	 * @param donorNotes list of donorNotes objects
 	 * @return list of donorNotes objects that were saved. Also returns any server side validation errors
 	 */
 	@POST
 	@Path("/donor-notes/save-all")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "saveAllDonorNotes", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "saveAllDonorNotes", ui = false)
 	public List<JsonBean> saveAllDonorNotes(List<JsonBean> donorNotes) {
 		return GPIDataService.saveDonorNotes(donorNotes);
 	}
 
-	/***
+	/**
 	 * Retrieves a list of donor notes
-	 * @param offset
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding:
+	 * <dt><b>data</b><dd> - list of donor notes objects
+	 * <dt><b>totalRecords</b><dd> - total number of donor notes records
+	 * </dl></br></br> 
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *    "data" :[{
+	 *                 "id" : 1,
+	 *                 "donorId" : 1061,
+	 *                 "notes" : "Sample note 1",
+	 *                 "notesDate" : "2017-04-14"                
+	 *     },  
+	 *     {
+	 *                 "id" : 2,
+	 *                 "donorId" : 1062,
+	 *                 "notes" : "Sample note 2",
+	 *                 "notesDate" : "2017-04-14"
+	 *    }],
+     *    "totalRecords" : 2	 
+	 * }     
+	 * </pre>
+	 * @param offset first element in the list
 	 * @param count maximum number of records to return
 	 * @param orderBy field that will be used for sorting
 	 * @param sort asc or desc order
@@ -126,33 +386,63 @@ public class GPIEndPoints implements ErrorReportingEndpoint {
 	@GET
 	@Path("/donor-notes")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "getDonorNotesList", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "getDonorNotesList", ui = false)
 	public JsonBean getDonorNotesList(@QueryParam("offset") Integer offset, @QueryParam("count") Integer count,
 			@QueryParam("orderby") String orderBy, @QueryParam("sort") String sort) {
 		return GPIDataService.getDonorNotesList(offset, count, orderBy, sort);
 	}
 
-	/***
+	/**
 	 * Deletes the donor notes object from the database
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * The JSON object holds information regarding the result of the delete action:
+	 * <dt><b>result</b><dd> - result string that indicates if the delete was successful
+	 * </dl></br></br> 
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *    "result" : "deleted"
+	 * }
+	 * </pre>
 	 * @param id identifier to query for object to delete
-	 * @return json object containing the result
+	 * @return JSON object containing the result
 	 */
 	@DELETE
 	@Path("/donor-notes/{id}")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "deleteDonorNotesId", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "deleteDonorNotesId", ui = false)
 	public JsonBean deleteDonorNotesById(@PathParam("id") long id) {
 		return GPIDataService.deleteDonorNotesById(id);
 	}
 
-	/***
+	/**
 	 * Retrieves a list of verified organizations associated with the logged in user
+	 * </br>
+	 * <dl>
+	 * </br>
+	 * An array of JSON objects. The JSON objects contain the following fields:
+	 * <dt><b>id</b><dd> - database id of the organization
+     * <dt><b>name</b><dd> - name of the organisation
+	 * </dl></br></br> 
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 *[{
+	 *	  "id" : 3443,
+	 *	  "name" : "African Development Bank"
+	 * },{
+	 *	   "id" : 3444,
+	 *	   "name" : "World Bank Group"
+	 * }
+     *]
+	 * </pre>
 	 * @return list of verified organizations associated with the logged in user
 	 */
 	@GET
 	@Path("/users-verified-orgs")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-	@ApiMethod(id = "getUsersVerifiedOrganizations", ui = false)
+	@ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "getUsersVerifiedOrganizations", ui = false)
 	public List<JsonBean> getUsersVerifiedOrganizations() {
 		return GPIDataService.getUsersVerifiedOrganizations();
 	}

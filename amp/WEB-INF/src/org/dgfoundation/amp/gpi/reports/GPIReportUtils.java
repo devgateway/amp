5894410b531624bec9bff5baf0bbfa497c8de978
Viorel Chihai <vchihai@developmentgateway.org>
2017-04-28 12:23:21 +0300

    AMP-25899: Define the nireport for indicator 5b

--- amp/WEB-INF/src/org/dgfoundation/amp/gpi/reports/GPIReportUtils.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/gpi/reports/GPIReportUtils.java
@@ -1,6 +1,8 @@
 package org.dgfoundation.amp.gpi.reports;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.dgfoundation.amp.ar.ArConstants;
@@ -22,6 +24,72 @@ import org.digijava.kernel.ampapi.endpoints.util.FilterUtils;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
 
 public class GPIReportUtils {
+	
+	public static GeneratedReport getGeneratedReportForIndicator(String indicatorCode, JsonBean formParams, boolean isSummary) {
+		switch(indicatorCode) {
+		case GPIReportConstants.REPORT_5b:
+			return  getGeneratedReportForIndicator5b(formParams, isSummary);
+		case GPIReportConstants.REPORT_9b:
+			return  getGeneratedReportForIndicator9b(formParams, isSummary);
+		default:
+			return null;
+		}
+	}
+	
+	/**
+	 * create the template for the gpi report 5b. It can be refactored to another class
+	 * 
+	 * @param formParams
+	 * @return
+	 */
+	public static GeneratedReport getGeneratedReportForIndicator5b(JsonBean formParams, boolean isSummary) {
+		
+		ReportSpecificationImpl spec = new ReportSpecificationImpl(GPIReportConstants.REPORT_5b, ArConstants.GPI_TYPE);
+		
+		if (!isSummary)  {
+			String hierarchyColumn = getHierarchyColumn(formParams);
+			if (hierarchyColumn.equals(GPIReportConstants.HIERARCHY_DONOR_GROUP)) {
+				spec.addColumn(new ReportColumn(ColumnConstants.DONOR_GROUP));
+				spec.getHierarchies().add(new ReportColumn(ColumnConstants.DONOR_GROUP));
+			} else {
+				spec.addColumn(new ReportColumn(ColumnConstants.DONOR_AGENCY));
+				spec.getHierarchies().add(new ReportColumn(ColumnConstants.DONOR_AGENCY));
+			}
+			spec.setGroupingCriteria(GroupingCriteria.GROUPING_YEARLY);
+		}
+		
+		
+		for (String mtefColumn : getMTEFColumnsForIndicator5b()) {
+			spec.addColumn(new ReportColumn(mtefColumn));
+		}
+		
+		spec.setSummaryReport(true);
+		
+ 		if(formParams != null){
+ 			Map<String, Object> filters= (Map<String, Object>) formParams.get(EPConstants.FILTERS);
+ 			AmpReportFilters filterRules = FilterUtils.getFilterRules(filters, null);
+ 			
+ 			if(filterRules == null) {
+ 				filterRules = new AmpReportFilters();
+ 			} else if (!isSummary) {
+ 				filterRules.getFilterRules().clear();
+ 			}
+ 			
+ 			ReportElement elem = new ReportElement(new ReportColumn(ColumnConstants.APPROVAL_STATUS));
+			
+ 			// Validated Activities - 4
+ 			FilterRule filterRule = new FilterRule(Arrays.asList("4"), true);
+			filterRules.addFilterRule(elem, filterRule);
+ 			
+			spec.setFilters(filterRules);
+ 		}
+ 		
+ 		SettingsUtils.applySettings(spec, formParams, true);
+		
+ 		GeneratedReport generatedReport = EndpointUtils.runReport(spec, ReportAreaImpl.class, null);
+ 		
+ 		return generatedReport;
+	}
 	
 	/**
 	 * create the template for the gpi report 9b. It can be refactored to another class
@@ -92,12 +160,15 @@ public class GPIReportUtils {
 		
 		return false;
 	}
-	
-	public static GeneratedReport getGeneratedReportForIndicator(String indicatorCode, JsonBean formParams, boolean isSummary) {
-		if (indicatorCode.equals(GPIReportConstants.REPORT_9b)) {
-			return getGeneratedReportForIndicator9b(formParams, isSummary);
+
+	public static List<String> getMTEFColumnsForIndicator5b() {
+		List<String> mtefColumns = new ArrayList<>();
+		
+		int mtefYear = 2010;
+		for (int i = 0; i < 3; i++) {
+			mtefColumns.add("MTEF " + (mtefYear + i));
 		}
 		
-		return null;
+		return mtefColumns;
 	}
 }

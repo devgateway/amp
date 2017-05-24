251543a9e661bde085a3ce8c08cf16ebaf55ea8f
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-05-24 11:54:56 -0300

    AMP-26215 Use global setting constant

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/LocationService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/LocationService.java
@@ -169,7 +169,7 @@ public class LocationService {
 		PersistenceManager.getSession().doWork(new Work() {
 			public void execute(Connection conn) throws SQLException {
 				String countryIdQuery = " select geo_code from amp_category_value_location acvl,amp_global_settings gs "
-						+ " where acvl.iso=gs.settingsvalue and gs.settingsname ='Default Country'";
+						+ " where acvl.iso=gs.settingsvalue and gs.settingsname ='"+ GlobalSettingsConstants.DEFAULT_COUNTRY +"'";
 				RsInfo rsi = SQLUtils.rawRunQuery(conn, countryIdQuery, null);
 				if (rsi.rs.next()) {
 					geoCode.value = rsi.rs.getString(1);

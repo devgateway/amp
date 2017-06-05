c0a33863a9134bcea15bd9c7ef008d62d4e6f73e
Viorel Chihai <vchihai@developmentgateway.org>
2017-06-05 13:14:53 +0300

    AMP-26132: Implment remark column in gpi pdf export indicator 5a

--- amp/WEB-INF/src/org/dgfoundation/amp/gpi/reports/export/pdf/GPIReportIndicator5aPdfExporter.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/gpi/reports/export/pdf/GPIReportIndicator5aPdfExporter.java
@@ -6,6 +6,7 @@ import org.dgfoundation.amp.ar.ColumnConstants;
 import org.dgfoundation.amp.gpi.reports.GPIReport;
 import org.dgfoundation.amp.gpi.reports.GPIReportConstants;
 import org.dgfoundation.amp.gpi.reports.GPIReportOutputColumn;
+import org.dgfoundation.amp.gpi.reports.GPIReportUtils;
 
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
@@ -17,22 +18,22 @@ import com.lowagie.text.pdf.PdfPTable;
  *
  */
 public class GPIReportIndicator5aPdfExporter extends GPIReportPdfExporter {
-	
+
 	public GPIReportIndicator5aPdfExporter() {
-		relativeWidths = new float [] {5f, 25f, 10f, 10f, 10f, 10f, 10f, 10f, 10f};
+		relativeWidths = new float[] { 4f, 20f, 10f, 10f, 8f, 8f, 8f, 8f, 8f, 16f };
 		reportTitle = "Indicator 5a";
 	}
-	
+
 	public void renderReportTableSummary(GPIReport report, Paragraph body) {
 		PdfPTable table = new PdfPTable(report.getSummary().size());
-		
+
 		// set table width a percentage of the page width
 		table.setWidthPercentage(50f);
 		table.setHorizontalAlignment(0);
-		
-		Font bfBold14 = new Font(Font.HELVETICA, 13, Font.BOLD, new Color(0, 0, 0));
+
+		Font bfBold14 = new Font(Font.HELVETICA, 12, Font.BOLD, new Color(0, 0, 0));
 		Color bkgColor = Color.ORANGE;
-		
+
 		for (int i = 0; i < report.getPage().getHeaders().size(); i++) {
 			GPIReportOutputColumn column = report.getPage().getHeaders().get(i);
 			if (report.getSummary().containsKey(column)) {
@@ -44,58 +45,62 @@ public class GPIReportIndicator5aPdfExporter extends GPIReportPdfExporter {
 		body.add(table);
 		body.add(new Paragraph());
 	}
-	
+
 	public void renderReportTableHeader(GPIReport report, PdfPTable table) {
-		Font bfBold11 = new Font(Font.HELVETICA, 10, Font.BOLD, new Color(0, 0, 0));
+		Font bfBold11 = new Font(Font.HELVETICA, 9, Font.BOLD, new Color(0, 0, 0));
 		Color bkgColor = Color.LIGHT_GRAY;
 
-		report.getPage().getHeaders().stream().filter(c -> !isHiddenColumn(c.originalColumnName))
-				.forEach(column -> insertCell(table, getHeaderColumnName(column), 
-						Element.ALIGN_CENTER, 1, bfBold11, bkgColor));
+		report.getPage().getHeaders().stream().forEach(
+				column -> insertCell(table, getHeaderColumnName(column), Element.ALIGN_CENTER, 1, bfBold11, bkgColor));
 
 		table.setHeaderRows(1);
 
 	}
-	
+
 	@Override
 	protected void renderReportTableData(GPIReport report, PdfPTable table) {
-		Font bf11 = new Font(Font.HELVETICA, 10);
+		Font bf11 = new Font(Font.HELVETICA, 9);
 		Color bkgColor = Color.WHITE;
 
 		report.getPage().getContents().forEach(row -> {
 			report.getPage().getHeaders().forEach(col -> {
-				if (!isHiddenColumn(col.originalColumnName)) {
+				if (!isRemarkColumn(col.originalColumnName)) {
 					insertCell(table, row.get(col), getCellAlignment(col.originalColumnName), 1, bf11, bkgColor);
+				} else {
+					insertCell(table, GPIReportUtils.getRemarksForExport(row), getCellAlignment(col.originalColumnName),
+							1, bf11, bkgColor);
 				}
 			});
 		});
 	}
-	
+
 	@Override
 	public int getCellAlignment(String columnName) {
-		switch(columnName) {
-			case ColumnConstants.DONOR_AGENCY:
-			case ColumnConstants.DONOR_GROUP:
-				return Element.ALIGN_LEFT;
-			case GPIReportConstants.COLUMN_CONCESSIONAL:
-			case GPIReportConstants.COLUMN_YEAR:
-				return Element.ALIGN_CENTER;
-			default:
-				return Element.ALIGN_RIGHT;
+		switch (columnName) {
+		case ColumnConstants.DONOR_AGENCY:
+		case ColumnConstants.DONOR_GROUP:
+			return Element.ALIGN_LEFT;
+		case GPIReportConstants.COLUMN_CONCESSIONAL:
+		case GPIReportConstants.COLUMN_YEAR:
+			return Element.ALIGN_CENTER;
+		case GPIReportConstants.COLUMN_REMARK:
+			return Element.ALIGN_LEFT;
+		default:
+			return Element.ALIGN_RIGHT;
 		}
 	}
-	
-	protected boolean isHiddenColumn(String columnName) {
+
+	protected boolean isRemarkColumn(String columnName) {
 		return columnName.equals(GPIReportConstants.COLUMN_REMARK);
 	}
-	
+
 	@Override
 	protected String getHeaderColumnName(GPIReportOutputColumn column) {
 		return getColumnHeaderLabel(column.originalColumnName);
 	}
-	
+
 	private String getColumnHeaderLabel(String columnName) {
-		return INDICATOR_5A_COLUMN_LABELS.containsKey(columnName) ? 
-				INDICATOR_5A_COLUMN_LABELS.get(columnName) : columnName;
+		return INDICATOR_5A_COLUMN_LABELS.containsKey(columnName) ? INDICATOR_5A_COLUMN_LABELS.get(columnName)
+				: columnName;
 	}
 }

19c15c7fec7adc7464114fedabe747fb125b3836
Aldo Picca <apicca@developmentgateway.org>
2017-06-22 15:08:57 -0300

    AMP-26380 adding new field to Activity form
    (cherry picked from commit 8f5fa22)

--- amp/WEB-INF/src/org/digijava/module/aim/action/EditActivity.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/EditActivity.java
@@ -1934,6 +1934,7 @@ private void setLineMinistryObservationsToForm(AmpActivityVersion activity, Edit
 				fd.setFiscalYear(DateConversion.convertDateToFiscalYearString(ampCompFund.getTransactionDate()));
 				fd.setTransactionType(ampCompFund.getTransactionType().intValue());
 				fd.setComponentOrganisation(ampCompFund.getReportingOrganization());
+				fd.setSecondReportingOrganisation(ampCompFund.getSecondReportingOrganisation());
 				fd.setComponentTransactionDescription(ampCompFund.getDescription());
 				
 				if (fd.getTransactionType() == 0) {

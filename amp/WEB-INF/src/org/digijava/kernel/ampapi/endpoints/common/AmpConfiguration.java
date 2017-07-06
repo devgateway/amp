6a05d8b7c10b1ab36f723dca8441af30ab8c9427
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2017-07-06 17:53:51 +0300

    AMP-24864: AMP Offline compatibility check

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/AmpConfiguration.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/AmpConfiguration.java
@@ -1,21 +1,28 @@
 package org.digijava.kernel.ampapi.endpoints.common;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.digijava.kernel.ampapi.endpoints.security.AuthRule;
 import org.digijava.kernel.ampapi.endpoints.settings.SettingsUtils;
 import org.digijava.kernel.ampapi.endpoints.util.ApiMethod;
 import org.digijava.kernel.ampapi.endpoints.util.JsonBean;
+import org.digijava.kernel.ampapi.filters.AmpOfflineModeHolder;
+import org.digijava.kernel.request.TLSUtils;
 import org.digijava.kernel.util.SpringUtil;
 import org.digijava.kernel.services.AmpVersionService;
 import org.digijava.module.aim.dbentity.AmpGlobalSettings;
+import org.digijava.module.aim.dbentity.AmpOfflineCompatibleVersionRange;
 import org.digijava.module.aim.util.FeaturesUtil;
 
 /**
@@ -26,6 +33,8 @@ import org.digijava.module.aim.util.FeaturesUtil;
 
 @Path("amp")
 public class AmpConfiguration {
+
+	private AmpVersionService ampVersionService = SpringUtil.getBean(AmpVersionService.class);
 	
 	/**
 	 * Provides available settings and their possible values.
@@ -86,23 +95,34 @@ public class AmpConfiguration {
 
 	/**
 	 * Check if AMP Offline App is compatible with AMP.
-	 * <p>This method will check if AMP Offline App is compatible with AMP. The only input it takes is version of
-	 * AMP Offline. Also returns AMP version and whenever AMP Offline is enabled or not.</p>
+	 * <p>This method will check if AMP Offline App is compatible with AMP. AMP Offline version is read from
+	 * User-Agent header. Also returns AMP version and whenever AMP Offline is enabled or not.</p>
 	 */
 	@GET
 	@Path("/amp-offline-version-check")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 	@ApiMethod(ui = false, id = "version-check")
-	public VersionCheckResponse ampOfflineVersionCheck(
-			@QueryParam("amp-offline-version") String ampOfflineVersion) {
-
-		AmpVersionService ampVersionService = SpringUtil.getBean(AmpVersionService.class);
-
+	public VersionCheckResponse ampOfflineVersionCheck() {
 		VersionCheckResponse response = new VersionCheckResponse();
-		response.setAmpOfflineCompatible(ampVersionService.isAmpOfflineCompatible(ampOfflineVersion));
+		response.setAmpOfflineCompatible(isAmpOfflineCompatible());
 		response.setAmpOfflineEnabled(true);
 		response.setAmpVersion(ampVersionService.getVersionInfo().getAmpVersion());
 		return response;
+	}
+
+	private boolean isAmpOfflineCompatible() {
+		String userAgent = TLSUtils.getRequest().getHeader("User-Agent");
+		boolean compatible = false;
+		if (AmpOfflineModeHolder.isAmpOfflineMode()) {
+			String version;
+			if (userAgent.indexOf(' ') > 0) {
+				version = userAgent.substring(userAgent.indexOf('/') + 1, userAgent.indexOf(' '));
+			} else {
+				version = userAgent.substring(userAgent.indexOf('/') + 1);
+			}
+			compatible = ampVersionService.isAmpOfflineCompatible(version);
+		}
+		return compatible;
 	}
 
 	/**
@@ -128,5 +148,95 @@ public class AmpConfiguration {
 				.collect(Collectors.toMap(
 						AmpGlobalSettings::getGlobalSettingsName,
 						AmpGlobalSettings::getGlobalSettingsValue));
+	}
+
+	/**
+	 * Returns all compatible AMP Offline version ranges.
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * [
+	 *     {
+	 *         "id": 1,
+	 *         "from-version": "1.0.0",
+	 *         "to-version": "2.0.0"
+	 *     }
+	 * ]
+	 * </pre>
+	 */
+	@GET
+	@Path("compatible-version-range")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<AmpOfflineCompatibleVersionRange> getCompatibleVersionRanges() {
+		return ampVersionService.getCompatibleVersionRanges();
+	}
+
+	/**
+	 * Create a new version range to denote AMP Offline compatibility.
+	 * <h3>Sample Input:</h3>
+	 * <pre>
+	 * {
+	 *     "from-version": "1.0.0",
+	 *     "to-version": "2.0.0"
+	 * }
+	 * </pre>
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *     "id": 3,
+	 *     "from-version": "1.0.0",
+	 *     "to-version": "2.0.0"
+	 * }
+	 * </pre>
+	 */
+	@PUT
+	@Path("compatible-version-range")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public AmpOfflineCompatibleVersionRange addCompatibleVersionRange(AmpOfflineCompatibleVersionRange versionRange) {
+		return ampVersionService.addCompatibleVersionRange(versionRange);
+	}
+
+	/**
+	 * Update an existing version range that denotes AMP Offline compatibility.
+	 * <h3>Sample Input:</h3>
+	 * <pre>
+	 * {
+	 *     "from-version": "1.1.0",
+	 *     "to-version": "2.0.0"
+	 * }
+	 * </pre>
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *     "id": 1,
+	 *     "from-version": "1.1.0",
+	 *     "to-version": "2.0.0"
+	 * }
+	 * </pre>
+	 */
+	@POST
+	@Path("compatible-version-range/{id}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public AmpOfflineCompatibleVersionRange updateCompatibleVersionRange(@PathParam("id") Long id,
+			AmpOfflineCompatibleVersionRange versionRange) {
+		versionRange.setId(id);
+		return ampVersionService.updateCompatibleVersionRange(versionRange);
+	}
+
+	/**
+	 * Delete an existing version range that denotes AMP Offline compatibility.
+	 * <h3>Sample Output:</h3>
+	 * <pre>
+	 * {
+	 *     "id": 3,
+	 *     "from-version": "1.0.0",
+	 *     "to-version": "2.0.0"
+	 * }
+	 * </pre>
+	 */
+	@DELETE
+	@Path("compatible-version-range/{id}")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public AmpOfflineCompatibleVersionRange deleteCompatibleVersionRange(@PathParam("id") Long id) {
+		return ampVersionService.deleteCompatibleVersionRange(id);
 	}
 }

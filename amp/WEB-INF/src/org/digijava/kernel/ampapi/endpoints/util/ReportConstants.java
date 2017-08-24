a5a305594673428ce6afb7d094e0856232646b65
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-08-24 23:42:02 +0300

    AMP-26797 implement groping and summary in custom reports

---
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/util/ReportConstants.java
@@ -0,0 +1,16 @@
+package org.digijava.kernel.ampapi.endpoints.util;
+
+/**
+ * Report related constants.
+ * 
+ * @author apicca
+ *
+ */
+public class ReportConstants {
+
+	//describing the timeframe by which to group funding data in the report
+	public static final String GROUPING_YEARLY = "A";
+	public static final String GROUPING_QUARTERLY = "Q";
+	public static final String GROUPING_MONTHLY = "M";
+
+}

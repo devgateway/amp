53df6f53361ba7270e7994cf19a30b0831595802
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2017-09-04 18:16:54 +0300

    AMP-26744: Converted idents to spaces

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/ActivityExporter.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/ActivityExporter.java
@@ -13,50 +13,52 @@ import org.digijava.module.aim.dbentity.AmpActivityVersion;
  */
 public class ActivityExporter extends ObjectExporter<AmpActivityVersion> {
 
-	private JsonBean filter;
-	private List<String> filteredFields = new ArrayList<>();
+    private JsonBean filter;
+    private List<String> filteredFields = new ArrayList<>();
 
-	public ActivityExporter(JsonBean filter) {
-		this.filter = filter;
-	}
+    public ActivityExporter(JsonBean filter) {
+        this.filter = filter;
+    }
 
-	@Override
-	public JsonBean export(AmpActivityVersion object) {
-		JsonBean resultJson = new JsonBean();
+    @Override
+    public JsonBean export(AmpActivityVersion object) {
+        JsonBean resultJson = new JsonBean();
 
-		if (!InterchangeUtils.validateFilterActivityFields(filter, resultJson)) {
-			return resultJson;
-		}
+        if (!InterchangeUtils.validateFilterActivityFields(filter, resultJson)) {
+            return resultJson;
+        }
 
-		if (filter != null) {
-			this.filteredFields = (List<String>) filter.get(ActivityEPConstants.FILTER_FIELDS);
-		}
+        if (filter != null) {
+            this.filteredFields = (List<String>) filter.get(ActivityEPConstants.FILTER_FIELDS);
+        }
 
-		return super.export(object);
-	}
+        return super.export(object);
+    }
 
-	@Override
-	protected Class<?> getClassOf(Object object) {
-		if (object instanceof AmpActivityVersion) {
-			return object.getClass().getSuperclass();
-		} else {
-			return object.getClass();
-		}
-	}
-	
-	/**
-	 * @param filteredFieldPath the underscorified path to the field
-	 * @return boolean, if the field should be exported in the result Json 
-	 */
-	protected boolean isFiltered(String filteredFieldPath) {
-		if (filteredFields.isEmpty()) 
-			return true;
-			
-		for (String s : filteredFields) {
-			if (s.startsWith(filteredFieldPath) || filteredFieldPath.startsWith(s)) 
-				return true;
-		}
-		
-		return false;
-	}
+    @Override
+    protected Class<?> getClassOf(Object object) {
+        if (object instanceof AmpActivityVersion) {
+            return object.getClass().getSuperclass();
+        } else {
+            return object.getClass();
+        }
+    }
+
+    /**
+     * @param filteredFieldPath the underscorified path to the field
+     * @return boolean, if the field should be exported in the result Json
+     */
+    protected boolean isFiltered(String filteredFieldPath) {
+        if (filteredFields.isEmpty()) {
+            return true;
+        }
+
+        for (String s : filteredFields) {
+            if (s.startsWith(filteredFieldPath) || filteredFieldPath.startsWith(s)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }

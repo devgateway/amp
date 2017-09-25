c89e07500aa26b443eb9d0e1c8426eef00e5aaa9
Gabriel Inchauspe <ginchauspe@gmail.com>
2017-09-25 11:52:55 -0300

    AMP-26084: Show download links.

--- amp/TEMPLATE/reamp/modules/ampoffline/download/components/DownloadLinks.jsx
+++ amp/TEMPLATE/reamp/modules/ampoffline/download/components/DownloadLinks.jsx
@@ -1,17 +1,59 @@
 import React from "react";
+import Link from "react-router";
 import ReactDOM from "react-dom";
+import { fetchJson } from "amp/tools";
 
 var DownloadLinks = React.createClass( {
+
     displayName: '',
 
     getInitialState: function() {
         return { data: [] };
     },
 
+    componentDidMount: function() {
+        fetchJson( this.props.url ).then( installers => this.setState( { data: installers } ) );
+    },
+
+    _buildLinksTable() {
+        const links = [];
+        if ( this.state.data ) {
+            this.state.data.sort(( i, j ) => i.os > j.os ).map( i => {
+                links.push( <a href={`${this.props.url}/${i.id}`} >{this._getInstallerName( i.os, i.arch )}</a> );
+            } );
+            return ( <ul>
+                {links.map( l => ( <li>{l}</li> ) )}
+            </ul> );
+        } else {
+            return null;
+        }
+    },
+
+    _getInstallerName( os, arch ) {
+        let name = '';
+        switch ( os ) {
+            case 'windows':
+                name = `Windows Vista/7/8/10 - ${arch} bits`;
+                break;
+            case 'debian':
+                name = `Ubuntu Linux (.deb) - ${arch} bits`;
+                break;
+            case 'mac':
+                name = `MacOS - ${arch} bits`;
+                break;
+        }
+        return name;
+    },
+
     render: function() {
         var __ = key => this.props.translations[key];
         return (
-            <span>Lista de versiones para descargar</span>
+            <div>
+                <h4>Available installers</h4>
+                <div>
+                    {this._buildLinksTable()}
+                </div>
+            </div>
         );
     }
 } );
--- amp/TEMPLATE/reamp/modules/ampoffline/download/index.jsx
+++ amp/TEMPLATE/reamp/modules/ampoffline/download/index.jsx
@@ -2,6 +2,7 @@ import * as AMP from "amp/architecture";
 import React from "react";
 import { OFFLINE_INSTALLERS } from "amp/config/endpoints";
 import { loadTranslations } from "amp/modules/translate";
+import DownloadLinks from "./components/DownloadLinks.jsx";
 
 var AMPOfflineDownload = React.createClass( {
 
@@ -21,12 +22,15 @@ var AMPOfflineDownload = React.createClass( {
         return (
             <div>
                 <div className='col-md-5'>
-                    <img src='/TEMPLATE/reamp/modules/ampoffline/download/images/monitor.png'/>
+                    <img src='/TEMPLATE/reamp/modules/ampoffline/download/images/monitor.png' />
                 </div>
                 <div className='col-md-7'>
                     <div>
                         <h2>Download the Offline Client</h2>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae dapibus lectus. Donec sed ipsum neque. Sed orci nunc, fringilla vitae urna sed, porttitor tempor odio. Donec pulvinar accumsan mollis. Phasellus quis tellus interdum, consectetur lacus eget, mollis ante. Quisque turpis est, lobortis vitae ex at, porttitor dapibus neque. Phasellus finibus dui ut sem pellentesque, a tincidunt elit elementum. Sed suscipit, quam in pretium tincidunt, sem nisl pulvinar tortor, sit amet sagittis nibh risus eu erat. Proin ultricies gravida odio at molestie. Vestibulum eros quam, congue dignissim luctus quis, vulputate at risus. Duis feugiat, enim sit amet iaculis mattis, magna neque rhoncus nunc, sed scelerisque libero massa at lectus. Fusce tempus, leo eu ultrices congue, tortor ex ultrices est, at facilisis metus mi id leo. Sed tincidunt urna in ornare fermentum. Nulla neque felis, elementum et elementum nec, vestibulum sit amet nibh. Donec quis dolor vitae mi molestie blandit quis sed mi. Donec rutrum elit sed euismod efficitur.</span>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae dapibus lectus. Donec sed ipsum neque. Sed orci nunc, fringilla vitae urna sed, porttitor tempor odio. Donec pulvinar accumsan mollis. Phasellus quis tellus interdum, consectetur lacus eget, mollis ante. Quisque turpis est, lobortis vitae ex at, porttitor dapibus neque. Phasellus finibus dui ut sem pellentesque, a tincidunt elit elementum. Sed suscipit, quam in pretium tincidunt, sem nisl pulvinar tortor, sit amet sagittis nibh risus eu erat. Proin ultricies gravida odio at molestie. Vestibulum eros quam, congue dignissim luctus quis, vulputate at risus. Duis feugiat, enim sit amet iaculis mattis, magna neque rhoncus nunc, sed scelerisque libero massa at lectus.</span>
+                    </div>
+                    <div>
+                        <DownloadLinks url={OFFLINE_INSTALLERS} translations = {this.state.translations} />
                     </div>
                 </div>
             </div>

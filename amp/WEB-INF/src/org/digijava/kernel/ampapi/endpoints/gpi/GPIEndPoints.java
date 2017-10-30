7ccdc077fbc930c7c89150b50bb86fce7baaf949
Julian de Anquin <jdeanquin@developmentgateway.org>
2017-10-30 18:15:05 -0300

    AMP-26301 fix merge issue

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gpi/GPIEndPoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gpi/GPIEndPoints.java
@@ -689,14 +689,6 @@ public class GPIEndPoints implements ErrorReportingEndpoint {
         return FiscalCalendarUtil.getAllAmpFiscalCalendars();
     }
 
-    @GET
-    @Path("/report/donors/")
-    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    @ApiMethod(authTypes = { AuthRule.IN_WORKSPACE }, id = "getDonors", ui = false)
-    public List<JsonBean> getDonors() {
-        return GPIDataService.getDonors();
-    }
-
     /**
      * Retrieves a converted date between two calendars </br>
      * <dl>

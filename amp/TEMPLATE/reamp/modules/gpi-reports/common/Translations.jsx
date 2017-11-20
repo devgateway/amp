8e5503628a784b8813b83f22918aae9aed0aa0a1
gmutuhu <geraldmutuhu@gmail.com>
2017-11-20 09:02:26 +0300

    AMP-27325 IE issue - remove duplicate entry in translation file
    AMP-27325 IE issue - remove duplicate entry in translation file
    
    AMP-27325 IE issue - remove duplicate entry in translation file

--- amp/TEMPLATE/reamp/modules/gpi-reports/common/Translations.jsx
+++ amp/TEMPLATE/reamp/modules/gpi-reports/common/Translations.jsx
@@ -48,8 +48,7 @@ export const initialTranslations = {
         'amp-gpi-reports:selected': 'Selected: ',
         'amp-gpi-reports:loading-message': 'Loading...',
         'amp-gpi-reports:5b-column-legend':'(Yes = 1 / No = 0)',
-        'amp-gpi-reports:5b-stat-label':'indicator 5b % at country level',
-        'amp-gpi-reports:annual-planned-disbursements': 'Actual Planned Disbursements',     
+        'amp-gpi-reports:5b-stat-label':'indicator 5b % at country level',           
         'amp-gpi-reports:no-data': 'The specified filtered report does not hold any data. Either pick a different filter criteria or use another report.',
         'amp-gpi-reports:annual-planned-disbursements': 'Actual Planned Disbursements'
 };

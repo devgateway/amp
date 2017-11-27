a0a20c3b298689b32fad3c8a6f111e20e9a7a2a8
Aldo Picca <apicca@developmentgateway.org>
2017-11-27 14:25:14 -0300

    AMP-27348 fixing IE report output issues to show rtl or ltr

--- amp/TEMPLATE/ampTemplate/saikuui_nireports/js/saiku/plugins/AMPDirection/plugin.js
+++ amp/TEMPLATE/ampTemplate/saikuui_nireports/js/saiku/plugins/AMPDirection/plugin.js
@@ -1,7 +1,7 @@
 var AMPDirection = {};
 
 AMPDirection.checkRtlDirection = function() {
-    $.getJSON("/rest/amp/settings", function(data) {
+    $.getJSON("/rest/amp/settings?" + new Date().getTime(), function(data) {
         var isRtl = data["rtl-direction"];
         if (isRtl) {
             $('head').append('<link rel="stylesheet" href="/TEMPLATE/ampTemplate/css_2/amp-rtl.css">');

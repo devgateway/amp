366fb2032e00b23f667f9cc30c666cd7acb5b08a
Julian de Anquin <jdeanquin@developmentgateway.org>
2018-03-22 10:46:04 -0300

    AMP-27200 make loading from dashboards translatable

---
+++ amp/TEMPLATE/ampTemplate/js_2/translations/common.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param originalTranslation labesl to be translated
+ * @param processTranslations Function that will process the success event.
+ */
+
+function getTranslations(originalTranslation,processTranslations){
+    $.ajax({
+        type: "POST",
+        url: "http://local.amp.org:8080/rest/translations/label-translations",
+        data: JSON.stringify(originalTranslation),
+        contentType: "application/json; charset=utf-8",
+        Accept:"application/json",
+        crossDomain: true,
+        dataType: "json",
+        success: processTranslations,
+        error: function (jqXHR, status) {
+            // error handler
+            // No need to do further processing, the labels will be shown in the original language
+            console.log(jqXHR);
+        }
+    });
+}

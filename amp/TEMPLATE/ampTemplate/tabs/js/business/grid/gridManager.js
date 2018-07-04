afc8818e3f29a7045011ba6fcef3b75b5a523f9a
Viorel Chihai <vchihai@developmentgateway.org>
2018-07-04 18:54:40 +0300

    AMP-28009: Fix activity title status color in tabs

--- amp/TEMPLATE/ampTemplate/tabs/js/business/grid/gridManager.js
+++ amp/TEMPLATE/ampTemplate/tabs/js/business/grid/gridManager.js
@@ -316,9 +316,10 @@ define([ 'business/grid/columnsMapping', 'translationManager', 'util/tabUtils','
 								};
 
 								// Assign colors for each row for loggued users.
+								var statusClass = '';
 								var x = getApprovalStatus(draft, approvalStatus);
 								if (x === statusMapping.Approved) {
-									row.className = className + ' status_1';
+									statusClass = ' status_1';
 									// Create link to edit activity.
 									if (teamtype !== app.TabsApp.MANAGER_TYPE) {
 										jQuery(row.cells[0]).html(iconedit + link);
@@ -327,11 +328,11 @@ define([ 'business/grid/columnsMapping', 'translationManager', 'util/tabUtils','
 									}
 
 								} else if (x === statusMapping.Existing_Draft || x === statusMapping.New_Draft) {
-									row.className = className + ' status_2';
+									statusClass = ' status_2';
 									jQuery(row.cells[0]).html(iconedit);
 
 								} else if (x === statusMapping.Existing_Unvalidated || x === statusMapping.New_Unvalidated) {
-									row.className = className + ' status_3';
+									statusClass = ' status_3';
 									// Cross team enable team lead and validators able to validate show icon.
 									if (crossTeamValidation && (teamlead || validator)) {
 										if (teamtype !== app.TabsApp.MANAGER_TYPE) {
@@ -347,6 +348,7 @@ define([ 'business/grid/columnsMapping', 'translationManager', 'util/tabUtils','
 										jQuery(row.cells[0]).html(iconedit);
 									}
 								}
+								row.className = className + statusClass;
 
 								// Create link to preview activity on first not grouped column.
 								var colIndex = -1;
@@ -355,7 +357,7 @@ define([ 'business/grid/columnsMapping', 'translationManager', 'util/tabUtils','
 										colIndex = i;
 									}
 								});
-                                var newContent = "<a class='preview-cell' href='" + getPreviewPageURL(id) + "'>"
+                                var newContent = "<a class='preview-cell" + statusClass + "' href='" + getPreviewPageURL(id) + "'>"
 									+ jQuery(row.cells[colIndex]).html() + "</a>";
 								jQuery(row.cells[colIndex]).html(newContent);
 							}

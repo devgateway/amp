8687bf8c4eeca67643d72249cdcdb91a2d0e223c
Julian de Anquin <jdeanquin@developmentgateway.org>
2018-08-27 21:07:03 -0300

    AMP-28240 sort organization

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpOrganisationSearchModel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AmpOrganisationSearchModel.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -124,6 +125,7 @@ public class AmpOrganisationSearchModel extends AbstractAmpAutoCompleteModel<Amp
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
+	Collections.sort(ret);
 	return ret;
     }
 

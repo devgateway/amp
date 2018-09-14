9a7d4d0ab3fa8c3c37e6915373f09f43b230905c
Julian de Anquin <jdeanquin@developmentgateway.org>
2018-09-14 12:21:11 -0300

    AMP-28307 check style

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsUtils.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/SettingsUtils.java
@@ -422,7 +422,7 @@ public class SettingsUtils {
         settings.set("number-group-separator", formatSymbols.getGroupingSeparator());
         settings.set("number-decimal-separator", formatSymbols.getDecimalSeparator());
 
-        settings.set(SettingsConstants.WORKSPACE_CURRENCY_CODE,getDefaultCurrencyCode());
+        settings.set(SettingsConstants.WORKSPACE_CURRENCY_CODE, getDefaultCurrencyCode());
 
         settings.set(SettingsConstants.REORDER_FUNDING_ITEM_ID,
                 FeaturesUtil.getGlobalSettingValueLong(GlobalSettingsConstants.REORDER_FUNDING_ITEMS));

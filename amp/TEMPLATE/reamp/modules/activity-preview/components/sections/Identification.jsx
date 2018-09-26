aed74b635b9eada1dd3d41ec1c58a94c4a943d74
dbianco <dbianco@developmentgateway.org>
2018-09-26 16:43:24 -0300

    AMP-28175 AMP-28295 hide fields when is off budget

--- amp/TEMPLATE/reamp/modules/activity-preview/components/sections/Identification.jsx
+++ amp/TEMPLATE/reamp/modules/activity-preview/components/sections/Identification.jsx
@@ -15,13 +15,17 @@ class Identification extends Component {
     const { buildSimpleField } = this.props;
     const { activity, settings } = this.props.params;
     const fieldPaths = [AC.STATUS_REASON, AC.OBJECTIVE, AC.DESCRIPTION, AC.PROJECT_COMMENTS, 
-      AC.CRIS_NUMBER, AC.ACTIVITY_BUDGET, AC.MINISTRY_CODE, AC.VOTE, AC.SUB_VOTE, AC.SUB_PROGRAM, AC.PROJECT_CODE];
+      AC.CRIS_NUMBER, AC.ACTIVITY_BUDGET];
+    const budgetPaths = [AC.MINISTRY_CODE, AC.PROJECT_CODE, AC.VOTE, AC.SUB_VOTE, AC.SUB_PROGRAM];
     const translations = this.props.params.translations;
     const noDataValue = translations['amp.activity-preview:noData'];
     const inline = this.props.params.inline;
     return (
       <div>
         {fieldPaths.map(fieldPath => buildSimpleField(activity, fieldPath, settings, true, noDataValue, inline))}
+        {(activity[AC.ACTIVITY_BUDGET].value === 'On Budget') &&
+          budgetPaths.map(fieldPath => buildSimpleField(activity, fieldPath, settings, true, noDataValue, inline))
+        }
       </div>
     );
   }

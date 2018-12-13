22623ebf89bc55ae03597ed84560d936c3eb7f8b
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2018-12-13 13:58:36 +0200

    AMP-28146: Excluded activities without AMP ID from sync diff

--- amp/WEB-INF/src/org/digijava/kernel/services/sync/SyncService.java
+++ amp/WEB-INF/src/org/digijava/kernel/services/sync/SyncService.java
@@ -352,7 +352,7 @@ public class SyncService implements InitializingBean {
         String wsFilter = getCompleteWorkspaceFilter(teamMembers);
         String sql = String.format("select amp_id ampId, modified_date modifiedDate, deleted "
                 + "from amp_activity "
-                + "where amp_activity_id in (%s)", wsFilter);
+                + "where amp_activity_id in (%s) and amp_id is not null", wsFilter);
         return jdbcTemplate.query(sql, emptyMap(), ACTIVITY_CHANGE_ROW_MAPPER);
     }
 

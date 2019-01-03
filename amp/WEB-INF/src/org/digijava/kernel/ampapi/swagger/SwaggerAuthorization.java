3f6335ba906fc7668197e9927066272ea5658823
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2019-01-03 16:02:02 +0200

    AMP-28721 add security lock to EPs requiring authorization

---
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/swagger/SwaggerAuthorization.java
@@ -0,0 +1,29 @@
+package org.digijava.kernel.ampapi.swagger;
+
+import java.lang.reflect.Method;
+import java.util.Iterator;
+
+import org.digijava.kernel.ampapi.endpoints.security.AuthRule;
+import org.digijava.kernel.ampapi.endpoints.util.ApiMethod;
+
+import io.swagger.jaxrs.ext.AbstractSwaggerExtension;
+import io.swagger.jaxrs.ext.SwaggerExtension;
+import io.swagger.models.Operation;
+
+/**
+ * @author Nadejda Mandrescu
+ *
+ */
+public class SwaggerAuthorization extends AbstractSwaggerExtension {
+	
+    @Override
+	public void decorateOperation(Operation operation, Method method, Iterator<SwaggerExtension> chain) {
+		ApiMethod apiMethod = method.getAnnotation(ApiMethod.class);
+		if (apiMethod != null && apiMethod.authTypes() != null) {
+			for (AuthRule authRule: apiMethod.authTypes()) {
+				operation.addSecurity(authRule.name(), null);
+			}
+		}
+		super.decorateOperation(operation, method, chain);
+	}
+}

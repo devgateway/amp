3c8709f9b2229acfb9e4fbeb08655617387b2a2a
Gabriel Inchauspe <ginchauspe@gmail.com>
2019-01-24 12:27:22 -0300

    AMP-28808: Add some constants.

--- amp/repository/aim/view/scripts/reportWizard/filterWidgetLoader.js
+++ amp/repository/aim/view/scripts/reportWizard/filterWidgetLoader.js
@@ -1,11 +1,15 @@
+var PLEDGE_ID = '5';
+var PLEDGE_TYPE = 'P';
+var DONOR_TYPE = 'D';
+
 var widgetFilter = null;
 $(document).ready(function () {
     // Load the filter after we rendered the DOM for perceived speed.
     var container = $('#filter-popup');
-    var reportTypeCode = 'D';
+    var reportTypeCode = DONOR_TYPE;
     // TODO: replace the magic number with "P" in case of Pledges/D for Donors.
-    if (new URL(window.location).searchParams.get('type') === '5') {
-        reportTypeCode = 'P';
+    if (new URL(window.location).searchParams.get('type') === PLEDGE_ID) {
+        reportTypeCode = PLEDGE_TYPE;
     }
     widgetFilter = new ampFilter({el: container, draggable: true, caller: 'REPORTS', reportType: reportTypeCode});
     var events = _.extend({}, Backbone.Events);

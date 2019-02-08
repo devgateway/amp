c46f5b7bde011dba54616ae72488eadb46da1c4c
Octavian Ciubotaru <ociubotaru@developmentgateway.org>
2019-02-08 17:45:24 +0200

    AMP-28907: Fixed setenv.sh

--- amp/docker/setenv.sh
+++ amp/docker/setenv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 JAVA_OPTS="-server -Xmx2g -Djava.awt.headless=true"
-JAVA_OPTS+=" -DsmtpHost=sulfur -DsmtpFrom=system@digijava.org"
-JAVA_OPTS+=" -XX:HeapDumpPath=/opt/heapdumps -XX:+HeapDumpOnOutOfMemoryError"
+JAVA_OPTS="$JAVA_OPTS -DsmtpHost=sulfur -DsmtpFrom=system@digijava.org"
+JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=/opt/heapdumps -XX:+HeapDumpOnOutOfMemoryError"
 
 CATALINA_OPTS="-Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false"
-CATALINA_OPTS+=" -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false"
-CATALINA_OPTS+=" -DAMP_DEVELOPMENT=true"
-CATALINA_OPTS+=" -Djavamelody.datasources=java:comp/env/ampDS,java:comp/env/jcrDS"
+CATALINA_OPTS="$CATALINA_OPTS -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false"
+CATALINA_OPTS="$CATALINA_OPTS -DAMP_DEVELOPMENT=true"
+CATALINA_OPTS="$CATALINA_OPTS -Djavamelody.datasources=java:comp/env/ampDS,java:comp/env/jcrDS"

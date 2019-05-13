3ccd86ee2c7a3c8a6dd4c79ee185a63236bb6ab1
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2019-05-13 12:29:30 -0300

    AMP-29165 avoid editor key collision

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/utils/AIHelper.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/utils/AIHelper.java
@@ -17,9 +17,12 @@ import org.digijava.module.aim.annotations.interchange.ActivityFieldsConstants;
  * @author Nadejda Mandrescu
  */
 public class AIHelper {
-
+    
     private static final Logger logger = Logger.getLogger(AIHelper.class);
-    /**
+
+    private static final Integer RANDOM_UPPER_LIMIT = 10000;
+
+    /** 
      * Stores all field paths within AmpActivityVersion tree that back reference the Activity.
      */
     private ActivityImporter activityImporter;
@@ -88,5 +91,18 @@ public class AIHelper {
         } catch (NumberFormatException e) {
             return null;
         }
+    }
+
+    /**
+     * Generate editor key used by importer when adding a new
+     * @param fieldName
+     * @return
+     */
+    public static String getEditorKey(String fieldName) {
+        // must start with "aim-" since it is expected by AF like this...
+        // The activity form to avoid this issue is using team member see EditorWrapperModel.generateEditorKey
+        // to avoid collisions even  under the the same member we add a random number from 0 to RANDOM_UPPER_LIMIT -1
+        return "aim-importer-" + fieldName + "-" + (System.currentTimeMillis()
+                + (int) (Math.random() * RANDOM_UPPER_LIMIT));
     }
 }

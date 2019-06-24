da72d6fa92d17a7050d508070bdbcc74f98b4eb2
Viorel Chihai <vchihai@developmentgateway.org>
2019-06-24 18:08:54 +0300

    AMP-29015: Remove Archived column from Report Schema

--- amp/WEB-INF/src/org/dgfoundation/amp/visibility/data/ColumnsVisibility.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/visibility/data/ColumnsVisibility.java
@@ -296,7 +296,6 @@ public class ColumnsVisibility extends DataVisibility implements FMSettings {
             put("Activity Updated On", ColumnConstants.ACTIVITY_UPDATED_ON);
             put("Actors", ColumnConstants.ACTORS);
             put("Age of Project (Months)", ColumnConstants.AGE_OF_PROJECT_MONTHS);
-            put("Archived", ColumnConstants.ARCHIVED);
             put("Average Size of Disbursements", ColumnConstants.AVERAGE_SIZE_OF_DISBURSEMENTS);
             put("Average Size of Projects", ColumnConstants.AVERAGE_SIZE_OF_PROJECTS);
             put("Budget Department", ColumnConstants.BUDGET_DEPARTMENT);

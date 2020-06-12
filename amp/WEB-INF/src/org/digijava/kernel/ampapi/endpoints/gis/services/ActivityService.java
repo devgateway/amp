b8dc8c8dc5b1d09829ebd4cc00b03ea840e55d45
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2020-06-12 11:02:50 -0300

    AMP-29598 add year to endpoint. also add most recent year.

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/ActivityService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/ActivityService.java
@@ -15,6 +15,7 @@ import org.dgfoundation.amp.ar.ColumnConstants;
 import org.dgfoundation.amp.ar.MeasureConstants;
 import org.dgfoundation.amp.newreports.AmountsUnits;
 import org.dgfoundation.amp.newreports.AmpReportFilters;
+import org.dgfoundation.amp.newreports.DateCell;
 import org.dgfoundation.amp.newreports.GeneratedReport;
 import org.dgfoundation.amp.newreports.IdentifiedReportCell;
 import org.dgfoundation.amp.newreports.ReportArea;
@@ -32,6 +33,7 @@ import org.dgfoundation.amp.onepager.util.ActivityGatekeeper;
 import org.digijava.kernel.ampapi.endpoints.common.EndpointUtils;
 import org.digijava.kernel.ampapi.endpoints.dto.FilterValue;
 import org.digijava.kernel.ampapi.endpoints.dto.GisActivity;
+import org.digijava.kernel.ampapi.endpoints.dto.gis.SscDashboardActivity;
 import org.digijava.kernel.ampapi.endpoints.dto.gis.SscDashboardObject;
 import org.digijava.kernel.ampapi.endpoints.dto.gis.SscDashboardObjectFactory;
 import org.digijava.kernel.ampapi.endpoints.dto.gis.SscDashboardResult;
@@ -43,35 +45,37 @@ import org.digijava.kernel.ampapi.endpoints.util.FilterUtils;
 import org.digijava.kernel.translator.TranslatorWorker;
 import org.digijava.module.aim.dbentity.AmpSector;
 import org.digijava.module.aim.util.SectorUtil;
+import org.digijava.module.common.util.DateTimeUtil;
 
 public class ActivityService {
     protected static Logger logger = Logger.getLogger(ActivityService.class);
     private static final Map<String, String> columnHeaders;
     private static final Set<String> columnsToProvide;
-     static
-        {
-         columnHeaders = new HashMap<String, String>();
-         columnHeaders.put(ColumnConstants.PROJECT_TITLE,"Title" );
-         columnHeaders.put(ColumnConstants.DONOR_AGENCY, "Donor Agency");
-         columnHeaders.put(ColumnConstants.EXECUTING_AGENCY, "Executing Agency");
-         columnHeaders.put(ColumnConstants.ACTIVITY_UPDATED_ON, "Date");
-         
-         columnsToProvide = new HashSet<String>();
-         columnsToProvide.add(ColumnConstants.AMP_ID);
-         columnsToProvide.add(ColumnConstants.PROJECT_TITLE);
-         columnsToProvide.add(MeasureConstants.ACTUAL_COMMITMENTS);
-         columnsToProvide.add(MeasureConstants.ACTUAL_DISBURSEMENTS);
-         columnsToProvide.add(MeasureConstants.PLANNED_COMMITMENTS);
-         columnsToProvide.add(MeasureConstants.PLANNED_DISBURSEMENTS);
-         columnsToProvide.add(MeasureConstants.BILATERAL_SSC_COMMITMENTS);
-         columnsToProvide.add(MeasureConstants.TRIANGULAR_SSC_COMMITMENTS);
-        }
+    private static final Integer DEFAULT_PAGE_SIZE = 10;
+
+    static {
+        columnHeaders = new HashMap<String, String>();
+        columnHeaders.put(ColumnConstants.PROJECT_TITLE, "Title");
+        columnHeaders.put(ColumnConstants.DONOR_AGENCY, "Donor Agency");
+        columnHeaders.put(ColumnConstants.EXECUTING_AGENCY, "Executing Agency");
+        columnHeaders.put(ColumnConstants.ACTIVITY_UPDATED_ON, "Date");
+
+        columnsToProvide = new HashSet<String>();
+        columnsToProvide.add(ColumnConstants.AMP_ID);
+        columnsToProvide.add(ColumnConstants.PROJECT_TITLE);
+        columnsToProvide.add(MeasureConstants.ACTUAL_COMMITMENTS);
+        columnsToProvide.add(MeasureConstants.ACTUAL_DISBURSEMENTS);
+        columnsToProvide.add(MeasureConstants.PLANNED_COMMITMENTS);
+        columnsToProvide.add(MeasureConstants.PLANNED_DISBURSEMENTS);
+        columnsToProvide.add(MeasureConstants.BILATERAL_SSC_COMMITMENTS);
+        columnsToProvide.add(MeasureConstants.TRIANGULAR_SSC_COMMITMENTS);
+    }
 
     public static ActivityList getActivities(PerformanceFilterParameters config, List<String> activitIds, Integer page,
-            Integer pageSize) {
-        
-         List<GisActivity> activities = new ArrayList<>();
-        
+                                             Integer pageSize) {
+
+        List<GisActivity> activities = new ArrayList<>();
+
         //we check if we have filter by keyword
         LinkedHashMap<String, Object> filters = null;
         if (config != null) {
@@ -81,8 +85,8 @@ public class ActivityService {
             }
             activitIds.addAll(FilterUtils.applyKeywordSearch(filters));
         }
-        
-        
+
+
         String name = "ActivityList";
         ReportSpecificationImpl spec = new ReportSpecificationImpl(name, ArConstants.DONOR_TYPE);
 
@@ -91,26 +95,26 @@ public class ActivityService {
         spec.addColumn(new ReportColumn(ColumnConstants.DONOR_AGENCY));
         spec.addColumn(new ReportColumn(ColumnConstants.EXECUTING_AGENCY));
         spec.addColumn(new ReportColumn(ColumnConstants.PRIMARY_SECTOR));
-        
+
         OutputSettings outSettings = new OutputSettings(new HashSet<String>() {{
             add(ColumnConstants.AMP_ID);
             add(ColumnConstants.DONOR_AGENCY);
             add(ColumnConstants.EXECUTING_AGENCY);
             add(ColumnConstants.PRIMARY_SECTOR);
         }});
-        
+
         //for now we are going to return the donor_id as matchesfilters
-        
+
         //then we have to fetch all other matchesfilters outisde mondrian
 
         // apply default settings
         SettingsUtils.applySettings(spec, config.getSettings(), true);
         // apply custom settings
         SettingsUtils.configureMeasures(spec, config.getSettings());
-        
+
         // AMP-19772: Needed to avoid problems on GIS js. 
         spec.setDisplayEmptyFundingRows(true);
-        
+
         ReportSettingsImpl mrs = (ReportSettingsImpl) spec.getSettings();
         mrs.setUnitsOption(AmountsUnits.AMOUNTS_OPTION_UNITS);
 
@@ -121,14 +125,14 @@ public class ActivityService {
         }
         GeneratedReport report = EndpointUtils.runReport(spec, ReportAreaImpl.class, outSettings);
         //if pagination is requested
-        List<ReportArea> ll=null;
+        List<ReportArea> ll = null;
         if (page != null && pageSize != null && page >= 0 && pageSize > 0) {
             ReportArea pagedReport = PaginatedReport.getPage(report.reportContents, page, pageSize);
             ll = pagedReport.getChildren();
-        }else{ 
+        } else {
             ll = report.reportContents.getChildren();
         }
-        Integer count=report.reportContents.getChildren().size();
+        Integer count = report.reportContents.getChildren().size();
 
         for (ReportArea reportArea : ll) {
             GisActivity activity = new GisActivity();
@@ -176,9 +180,9 @@ public class ActivityService {
                             FilterValue sector = new FilterValue();
                             sector.setId(ampSector.getAmpSectorId());
                             sector.setCode(ampSector.getSectorCodeOfficial());
-                            sector.setName(ampSector.getName());                        
+                            sector.setName(ampSector.getName());
                             sectors.add(sector);
-                        }                       
+                        }
                         matchesFilters.put(columnName, sectors);
                     } else {
                         matchesFilters.put(columnName, ids);
@@ -192,7 +196,6 @@ public class ActivityService {
     }
 
     /**
-     * 
      * @param extraColumns
      * @param pageSize
      * @param config
@@ -200,37 +203,37 @@ public class ActivityService {
      */
 
     public static RecentlyUpdatedActivities getLastUpdatedActivities(List<String> extraColumns, Integer pageSize,
-            SettingsAndFiltersParameters config) {
-    if (pageSize == null) {
-        pageSize = new Integer(10);
-    }
-    ReportSpecificationImpl spec = new ReportSpecificationImpl("LastUpdated", ArConstants.DONOR_TYPE);
-    spec.addColumn(new ReportColumn(ColumnConstants.ACTIVITY_UPDATED_ON));
-    spec.addColumn(new ReportColumn(ColumnConstants.PROJECT_TITLE));
-    spec.addColumn(new ReportColumn(ColumnConstants.DONOR_AGENCY));
-    for (String columnName : extraColumns) {
-        if (!columnName.equals(ColumnConstants.ACTIVITY_UPDATED_ON)
-                && !columnName.equals(ColumnConstants.DONOR_AGENCY)
-                && !columnName.equals(ColumnConstants.PROJECT_TITLE)) {
-        spec.addColumn(new ReportColumn(columnName));
-
+                                                                     SettingsAndFiltersParameters config) {
+        if (pageSize == null) {
+            pageSize = new Integer(DEFAULT_PAGE_SIZE);
         }
-    }
+        ReportSpecificationImpl spec = new ReportSpecificationImpl("LastUpdated", ArConstants.DONOR_TYPE);
+        spec.addColumn(new ReportColumn(ColumnConstants.ACTIVITY_UPDATED_ON));
+        spec.addColumn(new ReportColumn(ColumnConstants.PROJECT_TITLE));
+        spec.addColumn(new ReportColumn(ColumnConstants.DONOR_AGENCY));
+        for (String columnName : extraColumns) {
+            if (!columnName.equals(ColumnConstants.ACTIVITY_UPDATED_ON)
+                    && !columnName.equals(ColumnConstants.DONOR_AGENCY)
+                    && !columnName.equals(ColumnConstants.PROJECT_TITLE)) {
+                spec.addColumn(new ReportColumn(columnName));
 
-    spec.addMeasure(new ReportMeasure(MeasureConstants.ALWAYS_PRESENT));
-    spec.addSorter(new SortingInfo(new ReportColumn(ColumnConstants.ACTIVITY_UPDATED_ON), false));
-    
-    if (config != null) {
-        SettingsUtils.applySettings(spec, config.getSettings(), true);
-        FilterUtils.applyFilterRules(config.getFilters(), spec, null);
-    }
-    GeneratedReport report = EndpointUtils.runReport(spec);
-    
-    //ReportAreaMultiLinked[] areasDFArray = ReportPaginationUtils.convert(report.reportContents);
-    ReportArea pagedReport = PaginatedReport.getPage(report.reportContents, 0, pageSize);
-    List<Map<String, String>> activities = new ArrayList<>();
-    List<Map<String, String>> headers = new ArrayList<>();
-    
+            }
+        }
+
+        spec.addMeasure(new ReportMeasure(MeasureConstants.ALWAYS_PRESENT));
+        spec.addSorter(new SortingInfo(new ReportColumn(ColumnConstants.ACTIVITY_UPDATED_ON), false));
+
+        if (config != null) {
+            SettingsUtils.applySettings(spec, config.getSettings(), true);
+            FilterUtils.applyFilterRules(config.getFilters(), spec, null);
+        }
+        GeneratedReport report = EndpointUtils.runReport(spec);
+
+        //ReportAreaMultiLinked[] areasDFArray = ReportPaginationUtils.convert(report.reportContents);
+        ReportArea pagedReport = PaginatedReport.getPage(report.reportContents, 0, pageSize);
+        List<Map<String, String>> activities = new ArrayList<>();
+        List<Map<String, String>> headers = new ArrayList<>();
+
         if (pagedReport != null) {
             List<ReportArea> area = pagedReport.getChildren();
 
@@ -274,7 +277,13 @@ public class ActivityService {
         spec.addColumn(new ReportColumn(ColumnConstants.DONOR_COUNTRY));
         spec.addColumn(new ReportColumn(ColumnConstants.PRIMARY_SECTOR));
         spec.addColumn(new ReportColumn(ColumnConstants.SSC_MODALITIES));
-        spec.getHierarchies().addAll(spec.getColumns());
+        spec.addColumn(new ReportColumn(ColumnConstants.ACTUAL_START_DATE));
+        spec.addColumn(new ReportColumn(ColumnConstants.PROPOSED_START_DATE));
+
+        spec.getHierarchies().add(new ReportColumn(ColumnConstants.DONOR_COUNTRY));
+        spec.getHierarchies().add(new ReportColumn(ColumnConstants.PRIMARY_SECTOR));
+        spec.getHierarchies().add(new ReportColumn(ColumnConstants.SSC_MODALITIES));
+
         ReportsUtil.configureSSCWorkspaceFilter(spec, true);
         GeneratedReport report = EndpointUtils.runReport(spec);
 
@@ -282,7 +291,6 @@ public class ActivityService {
             for (ReportArea reportArea : report.reportContents.getChildren()) {
                 SscDashboardObject countries = SscDashboardObjectFactory.
                         getSscDashboardObject(reportArea.getOwner().columnName, reportArea.getOwner().id);
-                result.getChildren().add(countries);
                 List<ReportArea> sectorsReportArea;
                 sectorsReportArea = reportArea.getChildren();
                 for (ReportArea sectorReportArea : sectorsReportArea) {
@@ -295,16 +303,53 @@ public class ActivityService {
                                 getSscDashboardObject(modalitiesReportArea.getOwner().columnName,
                                         modalitiesReportArea.getOwner().id);
                         for (ReportArea activitiesReportArea : modalitiesReportArea.getChildren()) {
-
-                            modality.getChildren().add(SscDashboardObjectFactory.
-                                    getSscDashboardObject(ColumnConstants.ACTIVITY_ID,
-                                            activitiesReportArea.getOwner().id));
+                            Long actualStartDate = null;
+                            Long proposedStartDate = null;
+                            SscDashboardActivity da = new SscDashboardActivity(activitiesReportArea.getOwner().id);
+                            Map<ReportOutputColumn, ReportCell> activityRow = activitiesReportArea.getContents();
+                            Set<ReportOutputColumn> activityColumns = activityRow.keySet();
+                            for (ReportOutputColumn activitiesCls : activityColumns) {
+                                DateCell cellActivity = (DateCell) activityRow.get(activitiesCls);
+                                if (activitiesCls.columnName.equals(ColumnConstants.ACTUAL_START_DATE)) {
+                                    actualStartDate = cellActivity.entityId;
+                                } else {
+                                    if (activitiesCls.columnName.equals(ColumnConstants.PROPOSED_START_DATE)) {
+                                        proposedStartDate = cellActivity.entityId;
+                                    }
+                                }
+                            }
                             result.getActivitiesId().add(activitiesReportArea.getOwner().id);
+                            if (actualStartDate == null && proposedStartDate == null) {
+                                continue;
+                            } else {
+                                if (actualStartDate == null) {
+                                    da.setYear(DateTimeUtil.getYearFromJulianNumber(proposedStartDate));
+                                } else {
+                                    if (proposedStartDate == null) {
+                                        da.setYear(DateTimeUtil.getYearFromJulianNumber(actualStartDate));
+                                    }
+                                    if (proposedStartDate < actualStartDate) {
+                                        da.setYear(DateTimeUtil.getYearFromJulianNumber(proposedStartDate));
+                                    } else {
+                                        da.setYear(DateTimeUtil.getYearFromJulianNumber(actualStartDate));
+                                    }
+                                }
+                            }
+                            if (da.getYear() > result.getMostRecentYear()) {
+                                result.setMostRecentYear(da.getYear());
+                            }
+                            modality.getChildren().add(da);
                         }
-
-                        sectors.getChildren().add(modality);
+                        if (modality.getChildren().size() > 0) {
+                            sectors.getChildren().add(modality);
+                        }
                     }
-                    countries.getChildren().add(sectors);
+                    if (sectors.getChildren().size() > 0) {
+                        countries.getChildren().add(sectors);
+                    }
+                }
+                if (countries.getChildren().size() > 0) {
+                    result.getChildren().add(countries);
                 }
             }
         }

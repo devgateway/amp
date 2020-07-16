51e878218edac58c11407a7b023d611a7c7083b5
Viorel Chihai <vchihai@developmentgateway.org>
2020-07-16 22:53:23 +0300

    AMP-29685: Filter resources that are not documents

--- amp/WEB-INF/src/org/digijava/kernel/content/ContentRepositoryManager.java
+++ amp/WEB-INF/src/org/digijava/kernel/content/ContentRepositoryManager.java
@@ -258,8 +258,9 @@ public final class ContentRepositoryManager {
         try {
             QueryManager queryManager = session.getWorkspace().getQueryManager();
 
-            Query query = queryManager.createQuery(String.format("SELECT * FROM nt:base WHERE %s "
-                    + "IS NOT NULL AND jcr:path LIKE '/%s/%%/'", CrConstants.PROPERTY_CREATOR, path), Query.SQL);
+            Query query = queryManager.createQuery(String.format("SELECT * FROM nt:base WHERE %s IS NOT NULL "
+                    + "AND ampdoc:cmDocType IS NOT NULL "
+                    + "AND jcr:path LIKE '/%s/%%/'", CrConstants.PROPERTY_CREATOR, path), Query.SQL);
             NodeIterator nodes = query.execute().getNodes();
             while (nodes.hasNext()) {
                 uuids.add(nodes.nextNode().getIdentifier());

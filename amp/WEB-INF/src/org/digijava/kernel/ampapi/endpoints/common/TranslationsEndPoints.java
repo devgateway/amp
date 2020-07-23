4107a76fd8ccfd1502789d927cfc1ea888a0d7cc
Gabriel Inchauspe <ginchauspe@gmail.com>
2020-07-23 12:54:36 -0300

    AMPOFFLINE-1527: EP description.

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/TranslationsEndPoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/common/TranslationsEndPoints.java
@@ -25,6 +25,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
 import org.digijava.kernel.ampapi.endpoints.dto.Language;
 import org.digijava.kernel.ampapi.endpoints.util.ApiMethod;
 import org.digijava.kernel.ampapi.endpoints.util.AvailableMethod;
@@ -40,7 +41,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Path("translations")
 @Api("translations")
 public class TranslationsEndPoints {
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     @ApiOperation("Get the list of available methods in translations endpoint.")
@@ -69,24 +70,24 @@ public class TranslationsEndPoints {
                     + "```"
     )
     @ApiResponses(@ApiResponse(code = SC_OK, message = "Map of translated labels.", examples =
-        @Example(value = {
+    @Example(value = {
             @ExampleProperty(
                     mediaType = "application/json;charset=utf-8",
                     value = "{\n \"chWS\": \"Changer d'Espace de Travail\",\n \"logout\": \"Déconnecter\"\n}"
-                )
-            })
+            )
+    })
     ))
     @ApiMethod(ui = false, id = "Translations")
     public Map<String, String> getLangPack(
             @ApiParam(name = "param", required = true, value = "Key-label pairs to translate",
-            examples =
-                // this is not working (yet), using sample in ApiOperation.notes
-                @Example(value = {
-                    @ExampleProperty(
-                            mediaType = "application/json;charset=utf-8",
-                            value = "{\n \"chWS\": \"Change workspace\",\n \"logout\": \"Log out\"\n}"
-                        )
-                }))
+                    examples =
+                            // this is not working (yet), using sample in ApiOperation.notes
+                    @Example(value = {
+                            @ExampleProperty(
+                                    mediaType = "application/json;charset=utf-8",
+                                    value = "{\n \"chWS\": \"Change workspace\",\n \"logout\": \"Log out\"\n}"
+                            )
+                    }))
             @RequestBody final Map<String, String> param) {
         return getLangPack(null, param);
     }
@@ -110,32 +111,32 @@ public class TranslationsEndPoints {
                     + "   \"logout\": \"Log out\",\n"
                     + "}\n"
                     + "```"
-            )
+    )
     @ApiResponses(@ApiResponse(code = SC_OK, message = "Map of translated labels.", examples =
-        @Example(value = {
+    @Example(value = {
             @ExampleProperty(
                     mediaType = "application/json;charset=utf-8",
                     value = "{\n \"chWS\": \"Changer d'Espace de Travail\",\n \"logout\": \"Déconnecter\"\n}"
-                )
-            })
+            )
+    })
     ))
     @ApiMethod(ui = false, id = "CustomLanguageTranslations")
     public Map<String, String> getLangPack(@PathParam("langCode") @ApiParam(example = "en") String langCode,
-            @ApiParam(name = "param", required = true, value = "Key-label pairs to translate")
-            @RequestBody final Map<String, String> param) {
-        
+                                           @ApiParam(name = "param", required = true, value = "Key-label pairs to translate")
+                                           @RequestBody final Map<String, String> param) {
+
         String language = langCode == null ? TLSUtils.getEffectiveLangCode() : langCode;
-        
+
         for (String key : param.keySet()) {
             String translating = param.get(key);
             String newValue = TranslatorWorker.translateText(translating, language, SiteUtils.DEFAULT_SITE_ID);
             param.put(key, newValue);
         }
-        
+
         return param;
-        
+
     }
-    
+
     @GET
     @Path("/languages/")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -145,18 +146,18 @@ public class TranslationsEndPoints {
     public List<Language> getLanguages() {
         return TranslationManager.getAmpLanguages();
     }
-    
+
     @GET
     @Path("/languages/{langCode}")
     @ApiOperation("Change the language used in session.")
     @ApiMethod(ui = false, id = "LanguageSwitch")
     public void switchLanguage(@PathParam("langCode") @ApiParam(name = "langCode", example = "fr") String langCode,
-            @Context HttpServletResponse response) {
+                               @Context HttpServletResponse response) {
         Locale locale = new Locale();
         locale.setCode(langCode);
         DgUtil.switchLanguage(locale, TLSUtils.getRequest(), response);
     }
-    
+
     @GET
     @Path("/multilingual-languages/")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -188,16 +189,16 @@ public class TranslationsEndPoints {
                     + "   \"Log out\"\n"
                     + "]\n"
                     + "```"
-            )
+    )
     @ApiResponses(@ApiResponse(code = SC_OK, message = "Map of translation grouped by labels and locale code.",
-        examples =
+            examples =
             @Example(value = {
-                @ExampleProperty(
-                        mediaType = "application/json;charset=utf-8",
-                        value = "{\n  \"Log out\": {\n    \"fr\": \"Déconnecter\"\n  }\n}"
+                    @ExampleProperty(
+                            mediaType = "application/json;charset=utf-8",
+                            value = "{\n  \"Log out\": {\n    \"fr\": \"Déconnecter\"\n  }\n}"
                     )
-                })
-        ))
+            })
+    ))
     public Map<String, Map<String, String>> translateLabels(
             @ApiParam(name = "labels", required = true) List<String> labels) {
         return TranslationUtil.translateLabels(labels);
@@ -217,7 +218,8 @@ public class TranslationsEndPoints {
                     + "]\n"
                     + "```"
     )
-    @ApiResponses(@ApiResponse(code = SC_OK, message = "Map of translation grouped by labels and locale code.",
+    @ApiResponses(@ApiResponse(code = SC_OK, message = "Map of translation grouped by workspace prefix and then by "
+            + "labels and locale code.",
             examples =
             @Example(value = {
                     @ExampleProperty(

fb4d9f564cf622f074a9ea76b6c3e453f5faa0b7
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2020-08-17 11:41:12 -0300

    AMP-29685 properly filter out non documents

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/resource/ResourceService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/resource/ResourceService.java
@@ -57,6 +57,13 @@ public class ResourceService {
             ApiErrorResponseService.reportResourceNotFound(ResourceErrors.RESOURCE_NOT_FOUND);
         }
 
+        try {
+            if (!readNode.hasProperty(CrConstants.PROPERTY_CM_DOCUMENT_TYPE)) {
+                ApiErrorResponseService.reportResourceNotFound(ResourceErrors.RESOURCE_NOT_VALID);
+            }
+        } catch (RepositoryException e) {
+            return new JsonApiResponse(ApiError.toError(ResourceErrors.RESOURCE_ERROR));
+        }
         boolean isMultilingual = ContentTranslationUtil.multilingualIsEnabled();
 
         NodeWrapper nodeWrapper = new NodeWrapper(readNode);
@@ -165,8 +172,9 @@ public class ResourceService {
         List<String> uuids = new ArrayList<>();
         try {
             QueryManager queryManager = session.getWorkspace().getQueryManager();
-             Query query = queryManager.createQuery(String.format("SELECT * FROM nt:base WHERE %s IS NOT NULL "
-                     + "AND jcr:path LIKE '/%s/%%/'", CrConstants.PROPERTY_CREATOR, path), Query.SQL);
+            Query query = queryManager.createQuery(String.format("SELECT * FROM nt:base WHERE %s "
+                    + "IS NOT NULL AND " + CrConstants.PROPERTY_CONTENT_TYPE +" IS NOT NULL "
+                    + "AND jcr:path LIKE '/%s/%%/'", CrConstants.PROPERTY_CREATOR, path), Query.SQL);
             NodeIterator nodes = query.execute().getNodes();
             while (nodes.hasNext()) {
                 uuids.add(nodes.nextNode().getIdentifier());

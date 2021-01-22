8e8833ec98a017d1a2635b402cefdf9afb018394
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-01-22 12:28:06 -0300

    AMP-29838 fix minify reamp

--- amp/TEMPLATE/reamp/webpack.config.js
+++ amp/TEMPLATE/reamp/webpack.config.js
@@ -1,5 +1,7 @@
 var config = require('./webpack.dev.config.js');
 var webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 config.entry["admin/currency/deflator/script"] = "./modules/admin/currency/deflator/script.es6";
 config.entry["admin/dashboard/script"] = "./modules/admin/dashboard/script.es6";
 config.entry["gpi-data/script"] = "./modules/gpi-data/script.es6";
@@ -14,17 +16,31 @@ config.output.filename = "modules/[name].min.js";
 delete config.output.publicPath;
 delete config.devtool;
 config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-	    	name: 'dependencies-bundle',
-	        filename: 'dependencies-bundle.js',
-	        minChunks(module, count) {
-	            var context = module.context;
-	            return context && context.indexOf('node_modules') >= 0 && count > 1;
-	        }
-	      }));
+  name: 'dependencies-bundle',
+  filename: 'dependencies-bundle.js',
+  minChunks(module, count) {
+    var context = module.context;
+    return context && context.indexOf('node_modules') >= 0 && count > 0;
+  }
+}));
 config.plugins.push(new webpack.DefinePlugin({'process.env': {
-		'NODE_ENV': JSON.stringify('production')
+		'NODE_ENV': JSON.stringify('PRODUCTION')
 	}
 }));
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: false, compress: false}));
+config.plugins.push(new UglifyJsPlugin({
+  test: /\.js($|\?)/i,
+  sourceMap: true,
+  uglifyOptions: {
+    mangle: {
+      keep_fnames: true,
+    },
+    compress: {
+      warnings: false,
+    },
+    output: {
+      beautify: false,
+    },
+  },
+}));
 config.plugins.push(new webpack.optimize.DedupePlugin());
 module.exports = config;

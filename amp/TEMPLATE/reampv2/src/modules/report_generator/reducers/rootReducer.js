48e38d36119d155a63e210d0321e1eb9aac32c6d
ginchauspe <ginchauspe@gmail.com>
2021-04-22 16:33:00 -0300

    AMP-30084: Create action and reducer for future metadata's endpoint.

--- amp/TEMPLATE/reampv2/src/modules/report_generator/reducers/rootReducer.js
+++ amp/TEMPLATE/reampv2/src/modules/report_generator/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import translationsReducer from '../../../utils/reducers/translationsReducer';
+import loadMetadataReducer from './loadMetadataReducer';
 
 export default combineReducers({
-  translationsReducer
+  translationsReducer,
+  loadMetadataReducer,
 });

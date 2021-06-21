230e087e4bcc53c5ea6715e67c22b111644a7bcb
Viorel Chihai <vchihai@developmentgateway.org>
2021-06-21 13:17:57 +0300

    AMP-30232: Geocoding style adjustments

--- amp/TEMPLATE/reampv2/src/modules/geocoder/components/layout/table/GeocodingTableHeader.jsx
+++ amp/TEMPLATE/reampv2/src/modules/geocoder/components/layout/table/GeocodingTableHeader.jsx
@@ -19,10 +19,10 @@ class GeocodingTableHeader extends Component {
 
         return (
             <div className='panel-body custom-panel geocoder-header'>
-                <ResetAllButton title={translations['amp.geocoder:resetAll']} tooltip={translations['amp.geocoder:resetAllTooltip']}/>
                 <CancelGeocodingButton title={translations['amp.geocoder:cancelGeocoding']} />
                 <SaveAllEditsButton className={'pull-right button-header'} title={translations['amp.geocoder:saveAllEdits']}
                                     tooltip={translations['amp.geocoder:saveAllEditsTooltip']}/>
+                <ResetAllButton title={translations['amp.geocoder:resetAll']} tooltip={translations['amp.geocoder:resetAllTooltip']}/>
             </div>);
     }
 }

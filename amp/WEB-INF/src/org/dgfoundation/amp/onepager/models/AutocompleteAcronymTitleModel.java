6e7498cf021cd2d05991944d923dd732837c0505
Julian Eduardo de Anquin <jdeanquin@developmentgateway.org>
2021-10-01 20:36:42 -0300

    AMP-30300 allow to disable aggrement code

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AutocompleteAcronymTitleModel.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/models/AutocompleteAcronymTitleModel.java
@@ -5,33 +5,31 @@ import org.apache.wicket.model.Model;
 import org.dgfoundation.amp.onepager.yui.AmpAutocompleteFieldPanel;
 
 /**
- * 
  * @author aartimon@developmentgateway.org
  * @since Nov 14, 2012
- *
  */
 
-public class AutocompleteAcronymTitleModel extends Model<String>{
+public class AutocompleteAcronymTitleModel extends Model<String> {
     private static final long serialVersionUID = 1L;
     private IModel<String> acronymModel;
     private IModel<String> titleModel;
     private String nullText;
-    
+
     public AutocompleteAcronymTitleModel(IModel<String> acronymModel, IModel<String> titleModel, String nullText) {
         this.acronymModel = acronymModel;
         this.titleModel = titleModel;
         this.nullText = nullText;
     }
-    
+
     public String getObject() {
         String ret = "";
         if ((acronymModel == null || acronymModel.getObject() == null) && titleModel.getObject() == null) {
             return nullText;
         }
-        
-        if (acronymModel != null){
+
+        if (acronymModel != null && acronymModel.getObject() != null) {
             ret = ret + AmpAutocompleteFieldPanel.ACRONYM_DELIMITER_START +
-                    acronymModel.getObject() + 
+                    acronymModel.getObject() +
                     AmpAutocompleteFieldPanel.ACRONYM_DELIMITER_STOP;
         }
         ret = ret + titleModel.getObject();

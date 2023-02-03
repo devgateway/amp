298a5dc4653802e89437d0ddd54a0b1040ebea42
timothymugodev <tmugo@developmentgateway.org>
2023-02-03 13:55:48 +0300

    AMP-30428 added assets folder inside the src directory, temporalily disabled eslint

--- amp/TEMPLATE/reampv2/src/modules/admin/indicator_manager/components/table/IndicatorTable.tsx
+++ amp/TEMPLATE/reampv2/src/modules/admin/indicator_manager/components/table/IndicatorTable.tsx
@@ -1,10 +1,12 @@
-/* eslint-disable */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import React, { useState, useMemo, useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import SkeletonTable from './Table';
 import { ColumnDescription } from 'react-bootstrap-table-next';
+import SkeletonTable from './Table';
 import styles from './IndicatorTable.module.css';
 import { DefaultComponentProps } from '../../types';
 import sampleData from './test_data.json';
@@ -16,103 +18,111 @@ import EditIndicatorModal from '../modals/EditIndicatorModal';
 interface IndicatorTableProps extends DefaultComponentProps {
 }
 
-
 const IndicatorTable: React.FC<IndicatorTableProps> = ({ translations }) => {
+  const [selectedRow, setSelectedRow] = useState<any>(null);
+  const [showViewIndicatorModal, setShowViewIndicatorModal] = useState<boolean>(false);
+  const [showEditIndicatorModal, setShowEditIndicatorModal] = useState<boolean>(false);
 
-    const [selectedRow, setSelectedRow] = useState<any>(null);
-    const [showViewIndicatorModal, setShowViewIndicatorModal] = useState<boolean>(false);
-    const [showEditIndicatorModal, setShowEditIndicatorModal] = useState<boolean>(false);
+  const viewIndicatorModalHandler = (row: any) => {
+    setSelectedRow(row);
+    setShowViewIndicatorModal(true);
+  };
 
-    const viewIndicatorModalHandler = (row: any) => {
-        setSelectedRow(row);
-        setShowViewIndicatorModal(true);
-    };
+  const editIndicatorModalHandler = (row: any) => {
+    setSelectedRow(row);
+    setShowEditIndicatorModal(true);
+  };
 
-    const editIndicatorModalHandler = (row: any) => {
-        setSelectedRow(row);
-        setShowEditIndicatorModal(true);
-    };
+  const columns: ColumnDescription<any, any>[] = useMemo(() => [
+    {
+      dataField: 'id',
+      text: 'ID',
+      sort: true,
+      headerStyle: { width: '10%' },
+    },
+    {
+      dataField: 'indicatorName',
+      text: 'Indicator Name',
+      sort: true,
+      headerStyle: { width: '50%' },
+    },
+    {
+      dataField: 'sector',
+      text: 'Sector',
+      sort: true,
+      headerStyle: { width: '30%' },
+    },
+    {
+      dataField: 'action',
+      text: 'Action',
+      headerStyle: { width: '30%' },
+      csvExport: false,
+      formatter: (_cell: any, row: any) => (
+        <Row
+          sm={8}
+          className={styles.action_wrapper}>
+          <div className={styles.action_container}>
+            <i
+              style={{ fontSize: 20, color: '#007bff' }}
+              className="fa fa-eye"
+              aria-hidden="true"
+              onClick={() => viewIndicatorModalHandler(row)} />
+          </div>
+          <div className={styles.action_container}>
+            <i
+              style={{ fontSize: 20, color: '#198754' }}
+              className="fa fa-pencil"
+              aria-hidden="true"
+              onClick={() => editIndicatorModalHandler(row)} />
+          </div>
+          <div className={styles.action_container}>
+            <i
+              style={{ fontSize: 20, color: '#dc3545' }}
+              className="fa fa-trash"
+              aria-hidden="true" />
+          </div>
+        </Row>
+      ),
+    },
+  ], []);
 
-    const columns: ColumnDescription<any, any>[] = useMemo(() => [
-        {
-            dataField: 'id',
-            text: 'ID',
-            sort: true,
-            headerStyle: { width: '10%' },
-        },
-        {
-            dataField: 'indicatorName',
-            text: 'Indicator Name',
-            sort: true,
-            headerStyle: { width: '50%' }
-        },
-        {
-            dataField: 'sector',
-            text: 'Sector',
-            sort: true,
-            headerStyle: { width: '30%' },
-        },
-        {
-            dataField: 'action',
-            text: 'Action',
-            headerStyle: { width: '30%' },
-            csvExport: false,
-            formatter: (cell: any, row: any) => {
-                return (
-                    <Row sm={8} className={styles.action_wrapper}>
-                        <div className={styles.action_container}>
-                            <i style={{ fontSize: 20, color: '#007bff' }} className="fa fa-eye" aria-hidden="true" onClick={() => viewIndicatorModalHandler(row)}></i>
-                        </div>
-                        <div className={styles.action_container}>
-                            <i style={{ fontSize: 20, color: '#198754' }} className="fa fa-pencil" aria-hidden="true" onClick={() => editIndicatorModalHandler(row)}></i>
-                        </div>
-                        <div className={styles.action_container}>
-                            <i style={{ fontSize: 20, color: '#dc3545' }} className="fa fa-trash" aria-hidden="true"></i>
-                        </div>
-                    </Row>
-                )
-            },
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    ], []);
+  const [inidcatorsData, setIndicatorsData] = useState<any>(useMemo(() => [], []));
 
-    const [inidcatorsData, setIndicatorsData] = useState<any>(useMemo(() => [], []));
+  const fetchData = async () => {
+    setIndicatorsData(sampleData);
+  };
 
-    const fetchData = async () => {
-        setIndicatorsData(sampleData);
-    }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    return (
-        <>
-            <ViewIndicatorModal
-                show={showViewIndicatorModal}
-                setShow={setShowViewIndicatorModal}
-                indicator={selectedRow}
+  return (
+    <>
+      <ViewIndicatorModal
+        show={showViewIndicatorModal}
+        setShow={setShowViewIndicatorModal}
+        indicator={selectedRow}
             />
 
-            <EditIndicatorModal
-                show={showEditIndicatorModal}
-                setShow={setShowEditIndicatorModal}
-                indicator={selectedRow}
+      <EditIndicatorModal
+        show={showEditIndicatorModal}
+        setShow={setShowEditIndicatorModal}
+        indicator={selectedRow}
             />
-            
-            <SkeletonTable
-                title={translations['amp.indicatormanager:table-title']}
-                data={inidcatorsData}
-                columns={columns}
+
+      <SkeletonTable
+        title={translations['amp.indicatormanager:table-title']}
+        data={inidcatorsData}
+        columns={columns}
             />
-        </>
-    )
-}
+    </>
+  );
+};
 
 const InidcatorTableMemo = React.memo(IndicatorTable);
 
 const mapStateToProps = (state: any) => ({
-    translations: state.translationsReducer.translations
+  translations: state.translationsReducer.translations,
 });
 
 const mapDispatchToProps = (dispatch: any) => bindActionCreators({}, dispatch);

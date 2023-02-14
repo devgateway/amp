1adb20e7a40a0ece2e4f3e0ed452938907587a7a
Julian de Anquin <jdeanquin@developmentgateway.org>
2023-02-14 17:31:28 -0300

    AMP-30478 fix download resources

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpResourcesFormTableFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpResourcesFormTableFeature.java
@@ -1,6 +1,5 @@
 /**
  * Copyright (c) 2011 Development Gateway (www.developmentgateway.org)
- *
  */
 package org.dgfoundation.amp.onepager.components.features.tables;
 
@@ -56,8 +55,8 @@ import org.digijava.module.translation.util.ContentTranslationUtil;
 public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpActivityVersion, TemporaryActivityDocument> {
 
     boolean refreshExistingDocs = false;
-    
-    
+
+
     /**
      * @param id
      * @param fmName
@@ -65,25 +64,24 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
      * @throws Exception
      */
     public AmpResourcesFormTableFeature(String id, String fmName,
-            final IModel<AmpActivityVersion> am) throws Exception {
+                                        final IModel<AmpActivityVersion> am) throws Exception {
         super(id, am, fmName);
         super.setTitleHeaderColSpan(10);
 
 
-        
-        final IModel<Set<AmpActivityDocument>> setModel=new PropertyModel<Set<AmpActivityDocument>>(am,"activityDocuments");
+        final IModel<Set<AmpActivityDocument>> setModel = new PropertyModel<Set<AmpActivityDocument>>(am, "activityDocuments");
 
         if (am.getObject().getActivityDocuments() == null)
             am.getObject().setActivityDocuments(new HashSet<AmpActivityDocument>());
-        
-        IModel<List<TemporaryActivityDocument>> listModel  = new AbstractReadOnlyModel<List<TemporaryActivityDocument>>() {
+
+        IModel<List<TemporaryActivityDocument>> listModel = new AbstractReadOnlyModel<List<TemporaryActivityDocument>>() {
 
             private transient List<TemporaryActivityDocument> existingTmpDocs = getExistingObject();
-            
+
             private List<TemporaryActivityDocument> getExistingObject() {
                 Iterator<AmpActivityDocument> it = setModel.getObject().iterator();
                 List<TemporaryActivityDocument> ret = new ArrayList<TemporaryActivityDocument>();
-                HashSet <TemporaryActivityDocument> existingDocTitles = new HashSet<TemporaryActivityDocument>();
+                HashSet<TemporaryActivityDocument> existingDocTitles = new HashSet<TemporaryActivityDocument>();
 
                 while (it.hasNext()) {
                     AmpActivityDocument d = (AmpActivityDocument) it.next();
@@ -112,7 +110,7 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                     td.setContentType(nw.getContentType());
 
                     ret.add(td);
-                    
+
                     // Existing doc titles should be populated only when multilingual is enabled
                     if (ContentTranslationUtil.multilingualIsEnabled()) {
                         TemporaryActivityDocument titleHolder = new TemporaryActivityDocument();
@@ -122,7 +120,7 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                         existingDocTitles.add(td);
                     }
                 }
-                
+
                 getSession().setMetaData(OnePagerConst.RESOURCES_EXISTING_ITEM_TITLES, existingDocTitles);
                 refreshExistingDocs = false;
                 return ret;
@@ -131,14 +129,14 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
             @Override
             public List<TemporaryActivityDocument> getObject() {
                 HashSet<TemporaryActivityDocument> newItems = getSession().getMetaData(OnePagerConst.RESOURCES_NEW_ITEMS);
-                if(newItems == null)
+                if (newItems == null)
                     newItems = new HashSet<TemporaryActivityDocument>();
                 HashSet<AmpActivityDocument> delItems = getSession().getMetaData(OnePagerConst.RESOURCES_DELETED_ITEMS);
-                if(delItems == null)
+                if (delItems == null)
                     delItems = new HashSet<AmpActivityDocument>();
                 List<TemporaryActivityDocument> ret = new ArrayList<TemporaryActivityDocument>();
-               
-                if(refreshExistingDocs)     
+
+                if (refreshExistingDocs)
                     existingTmpDocs = getExistingObject();
                 ret.addAll(existingTmpDocs);
 
@@ -165,73 +163,71 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
             }
         };
 
-        
+
         list = new ListView<TemporaryActivityDocument>("list", listModel) {
             private static final long serialVersionUID = 7218457979728871528L;
-            
+
             @Override
             protected void onAfterRender() {
                 super.onAfterRender();
                 DocumentManagerUtil.logoutJcrSessions(SessionUtil.getCurrentServletRequest());
             }
-            
+
             @Override
             protected void populateItem(final ListItem<TemporaryActivityDocument> item) {
-                if (item.getModel() == null && item.getModelObject() == null){
+                if (item.getModel() == null && item.getModelObject() == null) {
                     logger.error("ola");
                     return;
                 }
-                
-                TemporaryDocument document = (TemporaryDocument)item.getModelObject();
 
-                if (!ContentTranslationUtil.multilingualIsEnabled()){
-                        item.add(new Label("title",item.getModel().getObject().getTitle()));
-                }
-                else {
+                TemporaryDocument document = (TemporaryDocument) item.getModelObject();
+
+                if (!ContentTranslationUtil.multilingualIsEnabled()) {
+                    item.add(new Label("title", item.getModel().getObject().getTitle()));
+                } else {
                     String id;
                     if (document.getExistingDocument() != null) {
                         id = document.getExistingDocument().getUuid();
+                    } else {
+                        id = document.getNewTemporaryDocumentId();
                     }
-                    
-                    else {
-                         id = document.getNewTemporaryDocumentId();
-                    }
-                    Model<String> newResourceIdModel = new Model <String> (id);
-                    final ResourceTranslationModel titleModel = new ResourceTranslationModel(new PropertyModel<String>(item.getModel().getObject(), "title"),newResourceIdModel);
-                    final AmpTextFieldPanel<String> name = new AmpTextFieldPanel<String>("title",titleModel , "Title",AmpFMTypes.MODULE,Boolean.TRUE);
+                    Model<String> newResourceIdModel = new Model<String>(id);
+                    final ResourceTranslationModel titleModel = new ResourceTranslationModel(new PropertyModel<String>(item.getModel().getObject(), "title"), newResourceIdModel);
+                    final AmpTextFieldPanel<String> name = new AmpTextFieldPanel<String>("title", titleModel, "Title", AmpFMTypes.MODULE, Boolean.TRUE);
                     name.setEnabled(false);
                     name.getTextContainer().setRequired(true);
-                    item.add (name);
-                    
-                    
+                    item.add(name);
+
+
                 }
 
-                if (item.getModel().getObject().getFileName()==null){
-                    item.add(new Label("resourceName",item.getModel().getObject().getWebLink()));
-                }else{
-                    item.add(new Label("resourceName",item.getModel().getObject().getFileName()));
+                if (item.getModel().getObject().getFileName() == null) {
+                    item.add(new Label("resourceName", item.getModel().getObject().getWebLink()));
+                } else {
+                    item.add(new Label("resourceName", item.getModel().getObject().getFileName()));
                 }
-                
+
                 PropertyModel<Date> dateModel = new PropertyModel<Date>(item.getModel(), "date.time");
                 String pattern = FeaturesUtil.getGlobalSettingValue(Constants.GLOBALSETTINGS_DATEFORMAT);
                 pattern = pattern.replace('m', 'M');
                 SimpleDateFormat formater = new SimpleDateFormat(pattern);
                 String formatedDate = formater.format(dateModel.getObject());
-        
+
                 item.add(new AmpLabelFieldPanel<String>("date", new Model<String>(formatedDate), "Document Date", true));
                 item.add(new AmpLabelFieldPanel<String>("year", new PropertyModel<String>(item.getModel(), "year"), "Document Year", true));
                 item.add(new AmpLabelFieldPanel<Double>("size", new PropertyModel<Double>(item.getModel(), "fileSize"), "Document Size", true));
                 item.add(new AmpLabelFieldPanel<String>("docType", new PropertyModel<String>(item.getModel(), "type.label"), "Document Type", true));
-                
+
                 final DownloadResourceStream drs;
                 if (item.getModelObject().isExisting())
-                    drs = new DownloadResourceStream(new PersistentObjectModel<AmpActivityDocument>(item.getModelObject().getExistingDocument()));
+                    drs = new DownloadResourceStream(new PersistentObjectModel<>
+                            (item.getModelObject().getExistingDocument()), item.getModelObject().getFileName());
                 else
-                    drs = new DownloadResourceStream(item.getModelObject().getFile());
-                
+                    drs = new DownloadResourceStream(item.getModelObject().getFile(), item.getModelObject().getFileName());
+
                 String webLink = item.getModelObject().getWebLink();
-                
-                if (webLink!=null && webLink.length()>0 ){
+
+                if (webLink != null && webLink.length() > 0) {
                     if (!webLink.startsWith("http"))
                         webLink = "http://" + webLink;
                     ExternalLink link = new ExternalLink("download", new Model<String>(webLink));
@@ -239,19 +235,18 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                     WebMarkupContainer downloadLinkImg = new WebMarkupContainer("downloadImage");
                     downloadLinkImg.add(new AttributeModifier("src", new Model("/TEMPLATE/ampTemplate/img_2/ico_attachment.png")));
                     link.add(downloadLinkImg);
-                }
-                else{
+                } else {
                     Link downloadLink = new Link("download") {
                         @Override
                         public void onClick() {
                             getRequestCycle().scheduleRequestHandlerAfterCurrent(new ResourceStreamRequestHandler(drs, drs.getFileName()));
                         }
-                    }; 
+                    };
                     item.add(downloadLink);
-                    
+
                     String contentType = item.getModelObject().getFileName();
                     int index = contentType.lastIndexOf('.');
-                    
+
                     String extension = contentType.substring(index + 1, contentType.length());
                     String extPath = "/TEMPLATE/ampTemplate/images/icons/" + extension + ".gif";
                     File extImgFile = new File(WebApplication.get().getServletContext().getRealPath(extPath));
@@ -259,24 +254,23 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                         extPath = "/TEMPLATE/ampTemplate/images/icons/default.icon.gif";
                     else
                         extPath = "/TEMPLATE/ampTemplate/images/icons/" + extension + ".gif";
-                    
+
                     WebMarkupContainer downloadLinkImg = new WebMarkupContainer("downloadImage");
                     downloadLinkImg.add(new AttributeModifier("src", new Model(extPath)));
                     downloadLink.add(downloadLinkImg);
                 }
-                
+
                 AmpDeleteLinkField delRelOrg = new AmpDeleteLinkField("delete", "Delete Resource") {
                     @Override
                     public void onClick(AjaxRequestTarget target) {
-                        if (item.getModelObject().isExisting()){
+                        if (item.getModelObject().isExisting()) {
                             HashSet<AmpActivityDocument> delItems = getSession().getMetaData(OnePagerConst.RESOURCES_DELETED_ITEMS);
-                            if (delItems == null){
+                            if (delItems == null) {
                                 delItems = new HashSet<AmpActivityDocument>();
                                 getSession().setMetaData(OnePagerConst.RESOURCES_DELETED_ITEMS, delItems);
                             }
                             delItems.add(item.getModelObject().getExistingDocument());
-                        }
-                        else{
+                        } else {
                             HashSet<TemporaryActivityDocument> newItems = getSession().getMetaData(OnePagerConst.RESOURCES_NEW_ITEMS);
                             newItems.remove(item.getModelObject());
                         }
@@ -286,12 +280,12 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                 item.add(delRelOrg);
             }
         };
-        
+
         add(list);
 
     }
-    
-    
+
+
     public void setRefreshExistingDocs(boolean refreshExistingDocs) {
         this.refreshExistingDocs = refreshExistingDocs;
     }

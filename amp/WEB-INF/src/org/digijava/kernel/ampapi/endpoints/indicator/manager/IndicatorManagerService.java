f41e13fa8df40593f5a1e09cc30e7c9d4355278d
Damian Rychter <damian@coinsource.com>
2023-03-08 17:31:06 -0300

    AMP-30433: Improve name program sector uniquenes

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/indicator/manager/IndicatorManagerService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/indicator/manager/IndicatorManagerService.java
@@ -1,5 +1,6 @@
 package org.digijava.kernel.ampapi.endpoints.indicator.manager;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.digijava.kernel.ampapi.endpoints.common.TranslationUtil;
 import org.digijava.kernel.ampapi.endpoints.errors.ApiError;
@@ -10,17 +11,25 @@ import org.digijava.module.aim.dbentity.AmpIndicator;
 import org.digijava.module.aim.dbentity.AmpIndicatorGlobalValue;
 import org.digijava.module.aim.dbentity.AmpSector;
 import org.digijava.module.aim.dbentity.AmpTheme;
+import org.digijava.module.aim.util.FeaturesUtil;
 import org.digijava.module.aim.util.ProgramUtil;
 import org.digijava.module.aim.util.SectorUtil;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
+import java.text.SimpleDateFormat;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static org.digijava.module.aim.helper.Constants.GlobalSettings.END_YEAR_DEFAULT_VALUE;
+import static org.digijava.module.aim.helper.Constants.GlobalSettings.START_YEAR_DEFAULT_VALUE;
 
 
 /**
@@ -29,6 +38,10 @@ import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
  * @author vchihai
  */
 public class IndicatorManagerService {
+
+    private final DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy");
+
+    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     protected static final Logger logger = Logger.getLogger(IndicatorManagerService.class);
 
@@ -62,6 +75,9 @@ public class IndicatorManagerService {
     public MEIndicatorDTO createMEIndicator(final MEIndicatorDTO indicatorRequest) {
         Session session = PersistenceManager.getSession();
         AmpIndicator indicator = new AmpIndicator();
+        String name = indicatorRequest.getName();
+        validateYear(indicatorRequest);
+        validateNameProgramSectorUnique(name, indicatorRequest, session);
 
         indicator.setName(indicatorRequest.getName());
         indicator.setDescription(indicatorRequest.getDescription());
@@ -93,6 +109,74 @@ public class IndicatorManagerService {
         session.save(indicator);
 
         return new MEIndicatorDTO(indicator);
+    }
+
+    private void validateYear(MEIndicatorDTO value) {
+        String startYear = FeaturesUtil.getGlobalSettingValue(START_YEAR_DEFAULT_VALUE);
+        String endYear = FeaturesUtil.getGlobalSettingValue(END_YEAR_DEFAULT_VALUE);
+        validateYearRange(startYear, endYear, value.getBaseValue(), "Base");
+        validateYearRange(startYear, endYear, value.getTargetValue(), "Target");
+
+    }
+
+    private void validateYearRange(String startYear, String endYear, AmpIndicatorGlobalValue value, String error){
+        String startInString = "01/01/" + startYear;
+        DateTime dateTime = DateTime.parse(startInString, formatter);
+
+        if (dateTime.isAfter(value.getOriginalValueDate().getTime())) {
+            throw new ApiRuntimeException(BAD_REQUEST,
+                    ApiError.toError(error + " Original value date " + simpleDateFormat.format(value.getOriginalValueDate())
+                            + " should be greater than " + startInString));
+        }
+
+        String endInString = "01/01/" + endYear;
+        dateTime = DateTime.parse(endInString, formatter);
+
+        if (dateTime.isAfter(value.getRevisedValueDate().getTime())) {
+            throw new ApiRuntimeException(BAD_REQUEST,
+                    ApiError.toError(error + "Revised value date " + simpleDateFormat.format(value.getRevisedValueDate())
+                            + " should be greater than " + endInString));
+        }
+    }
+
+
+    private void validateNameProgramSectorUnique(String name, MEIndicatorDTO indicator, Session session) {
+        long indicatorSector = -1;
+        long indicatorProgram = -1;
+        if (!indicator.getSectorIds().isEmpty()) {
+            indicatorSector = session.createCriteria(AmpIndicator.class)
+                    .add(Restrictions.eq("name", name))
+                    .createCriteria("sectors")
+                    .add(Restrictions.in("ampSectorId", indicator.getSectorIds()))
+                    .list().size();
+        }
+
+
+        if (!indicator.getProgramIds().isEmpty()) {
+            indicatorProgram = session.createCriteria(AmpIndicator.class)
+                    .add(Restrictions.eq("name", name))
+                    .createCriteria("programs")
+                    .add(Restrictions.in("ampThemeId", indicator.getProgramIds()))
+                    .list().size();
+        }
+
+        if (indicatorSector > 0 && indicatorProgram > 0) {
+            throw new ApiRuntimeException(BAD_REQUEST,
+                    ApiError.toError("Indicator with name " + indicator.getName() + " sectors " +
+                            StringUtils.join(indicator.getSectorIds(), ",") +
+                            " and programs " + StringUtils.join(indicator.getProgramIds(), ",") + " already exist"));
+        }
+
+        if (indicatorSector > 0  && indicatorProgram == -1) {
+            throw new ApiRuntimeException(BAD_REQUEST,
+                    ApiError.toError("Indicator with name " + indicator.getName() + " and sectors " +
+                            StringUtils.join(indicator.getSectorIds(), ",") + " already exist"));
+        } else if (indicatorSector == -1 && indicatorProgram > 0) {
+            throw new ApiRuntimeException(BAD_REQUEST,
+                    ApiError.toError("Indicator with name " + indicator.getName() + " and program " +
+                            StringUtils.join(indicator.getProgramIds(), ",") + " already exist"));
+        }
+
     }
 
     public void deleteMEIndicator(final Long indicatorId) {

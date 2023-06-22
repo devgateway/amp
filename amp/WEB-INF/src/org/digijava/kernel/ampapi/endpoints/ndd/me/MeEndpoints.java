65f1fb795e8aea3d39d08defeb53add0787effb8
Timothy Mugo <tmugo@developmentgateway.org>
2023-06-22 21:37:44 +0300

    AMP-30565 created API for aggregated values by indicators and programs

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/ndd/me/MeEndpoints.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/ndd/me/MeEndpoints.java
@@ -3,13 +3,16 @@ package org.digijava.kernel.ampapi.endpoints.ndd.me;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.digijava.kernel.ampapi.endpoints.gis.SettingsAndFiltersParameters;
+import org.digijava.kernel.ampapi.endpoints.indicator.manager.MEIndicatorDTO;
 import org.digijava.kernel.ampapi.endpoints.util.ApiMethod;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("me")
 @Api("me")
@@ -21,5 +24,32 @@ public class MeEndpoints {
     @ApiOperation("")
     public final MeReportDTO getMeProgramReport(SettingsAndFiltersParameters params) {
         return MeService.generateProgramsByValueReport(params);
+    }
+
+    @GET
+    @Path("indicatorsByProgram/{id}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @ApiMethod(id = "getMeIndicatorsByProgramReport")
+    @ApiOperation(value = "Retrieve and provide a list of M&E indicators by program.")
+    public final List<MEIndicatorDTO> getIndicatorsByProgram(@PathParam("id") Long programId) {
+        return MeService.getIndicatorsByProgram(programId);
+    }
+
+    @POST
+    @Path("indicatorsByProgramReport")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @ApiMethod(id = "getMeIndicatorsByProgramReport")
+    @ApiOperation("")
+    public final MeReportDTO getIndicatorsByProgramReport(SettingsAndFiltersParameters params) {
+        return MeService.generateIndicatorsByProgramsReport(params);
+    }
+
+    @GET
+    @Path("indicatorsBySector/{id}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    @ApiMethod(id = "getMeIndicatorsBySectorReport")
+    @ApiOperation(value = "Retrieve and provide a list of M&E indicators by sector.")
+    public final List<MEIndicatorDTO> getIndicatorsBySector(@PathParam("id") Long sectorId) {
+        return MeService.getIndicatorsBySector(sectorId);
     }
 }
--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/ndd/me/MeService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/ndd/me/MeService.java
@@ -4,28 +4,47 @@ import org.apache.log4j.Logger;
 import org.dgfoundation.amp.newreports.AmpReportFilters;
 import org.dgfoundation.amp.newreports.ReportMeasure;
 import org.digijava.kernel.ampapi.endpoints.gis.SettingsAndFiltersParameters;
+import org.digijava.kernel.ampapi.endpoints.indicator.manager.MEIndicatorDTO;
 import org.digijava.kernel.ampapi.endpoints.ndd.utils.DashboardUtils;
+import org.digijava.kernel.persistence.PersistenceManager;
+import org.digijava.module.aim.dbentity.AmpIndicator;
 import org.digijava.module.aim.dbentity.AmpIndicatorValue;
+import org.digijava.module.aim.dbentity.AmpSector;
 import org.digijava.module.aim.dbentity.AmpTheme;
+import org.digijava.module.aim.dbentity.IndicatorActivity;
 import org.digijava.module.aim.dbentity.IndicatorTheme;
 import org.digijava.module.aim.helper.DateConversion;
 import org.digijava.module.aim.util.ProgramUtil;
+import org.digijava.module.aim.util.SectorUtil;
+import org.hibernate.Session;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class MeService {
     protected static final Logger logger = Logger.getLogger(MeService.class);
+    
+    private static String getYearString(Date date) {
+        int year = DateConversion.getYear(DateConversion.convertDateToString(date));
+        return Integer.toString(year);
+    }
 
-    static MeReportDTO generateProgramsByValueReport(SettingsAndFiltersParameters params) {
-        AmpReportFilters filters = DashboardUtils.getFiltersFromParams(params.getFilters());
-        ReportMeasure measures = DashboardUtils.getMeasureFromParams(params.getSettings());
+    private static int calculateProgress(double base, double actual, double target) {
+        if (target == 0) {
+            return 0;
+        }
 
+        // formula:  [(Current value - Base value) / (Target value - Base value)]*100
+        double progressAggregate = (actual - base) / (target - base);
+        return Math.round((float) progressAggregate * 100);
+    }
 
-        Long programId = Long.valueOf(params.getSettings().get("programId").toString());
-
+    private static IndicatorValues createAggregate (Long programId) {
         IndicatorValues indicatorValues = new IndicatorValues();
 
         AtomicReference<Double> baseAggregate = new AtomicReference<>(0.0);
@@ -52,7 +71,6 @@ public class MeService {
                 } else if (indicatorType == AmpIndicatorValue.ACTUAL) {
                     actualAggregate.updateAndGet(v -> v + value.getValue());
 
-                    // set the last date
                     if (actualDate.get() == null || actualDate.get().before(value.getValueDate())) {
                         actualDate.set(value.getValueDate());
                     }
@@ -68,27 +86,119 @@ public class MeService {
 
         // set indicator values
         MeValue baseValue = new MeValue();
-        int baseYear = DateConversion.getYear(DateConversion.convertDateToString(baseDate.get()));
-        baseValue.setYear(Integer.toString(baseYear));
+        baseValue.setYear(MeService.getYearString(baseDate.get()));
         baseValue.setDetail(baseAggregate.get());
         indicatorValues.setBase(baseValue);
 
         MeValue actualValue = new MeValue();
-        int actualYear = DateConversion.getYear(DateConversion.convertDateToString(actualDate.get()));
-        actualValue.setYear(Integer.toString(actualYear));
+        actualValue.setYear(MeService.getYearString(actualDate.get()));
         actualValue.setDetail(actualAggregate.get());
         indicatorValues.setActual(actualValue);
 
         MeValue targetValue = new MeValue();
-        int targetYear = DateConversion.getYear(DateConversion.convertDateToString(targetDate.get()));
-        targetValue.setYear(Integer.toString(targetYear));
+        targetValue.setYear(MeService.getYearString(targetDate.get()));
         targetValue.setDetail(targetAggregate.get());
         indicatorValues.setTarget(targetValue);
 
-        // formula:  [(Current value - Base value) / (Target value - Base value)]*100
-        double progressAggregate = ((actualAggregate.get() - baseAggregate.get()) / (targetAggregate.get() - baseAggregate.get()));
-        int progress =  Math.round((float) progressAggregate * 100);
+        return indicatorValues;
+    }
+
+    static MeReportDTO generateProgramsByValueReport(SettingsAndFiltersParameters params) {
+        AmpReportFilters filters = DashboardUtils.getFiltersFromParams(params.getFilters());
+        ReportMeasure measures = DashboardUtils.getMeasureFromParams(params.getSettings());
+
+        Long programId = Long.valueOf(params.getSettings().get("programId").toString());
+
+        IndicatorValues indicatorValues = MeService.createAggregate(programId);
+        double baseAggregate = indicatorValues.getBase().getDetail();
+        double actualAggregate = indicatorValues.getActual().getDetail();
+        double targetAggregate = indicatorValues.getTarget().getDetail();
+
+        int progress = MeService.calculateProgress(baseAggregate, actualAggregate, targetAggregate);
 
         return new MeReportDTO(progress, indicatorValues);
+    }
+
+    static List<MEIndicatorDTO> getIndicatorsByProgram(Long programId) {
+        List<AmpIndicator> indicators = new ArrayList<>();
+        AmpTheme program = ProgramUtil.getThemeById(programId);
+        Set<IndicatorTheme> programIndicators =  program.getIndicators();
+
+
+        programIndicators.forEach(indicator -> {
+            indicators.add(indicator.getIndicator());
+        });
+
+        return indicators.stream().map(MEIndicatorDTO::new).collect(Collectors.toList());
+    }
+
+    static MeReportDTO generateIndicatorsByProgramsReport(SettingsAndFiltersParameters params) {
+        AmpReportFilters filters = DashboardUtils.getFiltersFromParams(params.getFilters());
+        ReportMeasure measures = DashboardUtils.getMeasureFromParams(params.getSettings());
+
+        AtomicReference<Double> baseValueAggregate = new AtomicReference<>(0.0);
+        AtomicReference<Double> currentValueAggregate = new AtomicReference<>(0.0);
+        AtomicReference<Double> targetValueAggregate = new AtomicReference<>(0.0);
+
+        AtomicReference<Date> baseDate = new AtomicReference<>(null);
+        AtomicReference<Date> actualDate = new AtomicReference<>(null);
+        AtomicReference<Date> targetDate = new AtomicReference<>(null);
+
+        Long indicatorId = Long.valueOf(params.getSettings().get("indicatorId").toString());
+        Session session = PersistenceManager.getSession();
+        AmpIndicator indicator = (AmpIndicator) session.get(AmpIndicator.class, indicatorId);
+
+        indicator.getValuesActivity().forEach(valueActivity -> {
+            if (valueActivity.getValues() != null)  {
+                valueActivity.getValues().forEach(value -> {
+                    if (value.getValueType() == AmpIndicatorValue.BASE) {
+                        baseValueAggregate.updateAndGet(v -> v + value.getValue());
+
+                        if (baseDate.get() == null || baseDate.get().before(value.getValueDate())) {
+                            baseDate.set(value.getValueDate());
+                        }
+                    } else if (value.getValueType()  == AmpIndicatorValue.ACTUAL) {
+                        currentValueAggregate.updateAndGet(v -> v + value.getValue());
+
+                        if (actualDate.get() == null || actualDate.get().before(value.getValueDate())) {
+                            actualDate.set(value.getValueDate());
+                        }
+                    } else if (value.getValueType()  == AmpIndicatorValue.TARGET) {
+                        targetValueAggregate.updateAndGet(v -> v + value.getValue());
+
+                        if (targetDate.get() == null || targetDate.get().before(value.getValueDate())) {
+                            targetDate.set(value.getValueDate());
+                        }
+                    }
+                });
+            }
+        });
+
+        IndicatorValues indicatorValues = new IndicatorValues();
+
+        MeValue baseValue = new MeValue();
+        baseValue.setYear(MeService.getYearString(baseDate.get()));
+        baseValue.setDetail(baseValueAggregate.get());
+        indicatorValues.setBase(baseValue);
+
+
+        MeValue actualValue = new MeValue();
+        actualValue.setYear(MeService.getYearString(actualDate.get()));
+        actualValue.setDetail(baseValueAggregate.get());
+        indicatorValues.setActual(actualValue);
+
+        MeValue targetValue = new MeValue();
+        targetValue.setYear(MeService.getYearString(targetDate.get()));
+        targetValue.setDetail(targetValueAggregate.get());
+        indicatorValues.setTarget(targetValue);
+
+        int progress = MeService.calculateProgress(baseValueAggregate.get(), currentValueAggregate.get(), targetValueAggregate.get());
+
+        return new MeReportDTO(progress, indicatorValues);
+    }
+
+    static List<MEIndicatorDTO> getIndicatorsBySector(Long sectorId) {
+        AmpSector sector = SectorUtil.getAmpSector(sectorId);
+        return sector.getIndicators().stream().map(MEIndicatorDTO::new).collect(Collectors.toList());
     }
 }

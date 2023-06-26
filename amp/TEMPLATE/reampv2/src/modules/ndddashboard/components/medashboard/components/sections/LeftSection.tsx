31a6498675a01429a4af1102dc9a925029e10990
Timothy Mugo <tmugo@developmentgateway.org>
2023-06-26 21:40:25 +0300

    AMP-30554 created program grouped by indicators chart

--- amp/TEMPLATE/reampv2/src/modules/ndddashboard/components/medashboard/components/sections/LeftSection.tsx
+++ amp/TEMPLATE/reampv2/src/modules/ndddashboard/components/medashboard/components/sections/LeftSection.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import IndicatorByProgram from './IndicatorByProgram'
+import ProgramGroupedByIndicator from './ProgramGroupedByIndicator'
 
 const LeftSection = () => {
   return (
     <div>
         <IndicatorByProgram />
+        <ProgramGroupedByIndicator/>
     </div>
   )
 }

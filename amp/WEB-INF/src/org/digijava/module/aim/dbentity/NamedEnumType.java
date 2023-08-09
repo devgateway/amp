8d220956ba7664e802728dbdac09c86cb06b3e24
brianbrix <bmokandu@developmentgateway.org>
2023-08-09 13:27:01 +0300

    AMP-30620 AMP-30675 Issue IATI import issue

--- amp/WEB-INF/src/org/digijava/module/aim/dbentity/NamedEnumType.java
+++ amp/WEB-INF/src/org/digijava/module/aim/dbentity/NamedEnumType.java
@@ -1,25 +1,30 @@
 package org.digijava.module.aim.dbentity;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
-import org.hibernate.HibernateException;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.type.EnumType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.internal.util.ReflectHelper;
+import org.hibernate.property.access.spi.Getter;
+import org.hibernate.property.access.spi.GetterMethodImpl;
+import org.hibernate.type.EnumType;
+import org.hibernate.type.StringType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Octavian Ciubotaru
+ */
 public class NamedEnumType extends EnumType {
     private final Logger logger = LoggerFactory.getLogger(NamedEnumType.class);
+
     private BiMap<Enum, String> values = HashBiMap.create();
 
     @Override
@@ -31,50 +36,34 @@ public class NamedEnumType extends EnumType {
         String enumClassName = parameters.getProperty(ENUM);
         Class<? extends Enum> enumClass;
         try {
-            enumClass = Class.forName(enumClassName).asSubclass(Enum.class);
+            enumClass = ReflectHelper.classForName(enumClassName, this.getClass()).asSubclass(Enum.class);
         } catch (ClassNotFoundException e) {
             throw new HibernateException("Enum class not found", e);
         }
+        Method getterMethod = ReflectHelper.getterMethodOrNull(enumClass, valueProperty);
+        Getter getter = new GetterMethodImpl(enumClass, valueProperty, getterMethod);
+        logger.info("Getter is: "+getter.getMethodName());
 
-        String getterMethodName = "get" + valueProperty.substring(0, 1).toUpperCase() + valueProperty.substring(1);
-        Method getter;
-        try {
-            getter = enumClass.getMethod(getterMethodName);
-            logger.info("Getter is: "+getter.getName());
-        } catch (NoSuchMethodException e) {
-            throw new HibernateException("Getter method not found for enum value", e);
-        }
         for (Enum enumConstant : enumClass.getEnumConstants()) {
-            try {
-                Object value = getter.invoke(enumConstant);
-                values.put(enumConstant, value.toString());
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new HibernateException("Error invoking getter method", e);
-            }
+            values.put(enumConstant, (String) getter.get(enumConstant));
         }
         super.setParameterValues(parameters);
     }
 
     @Override
-    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner)
-            throws SQLException {
-        String value = rs.getString(names[0]);
-        if (rs.wasNull() || value == null) {
+    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner) throws SQLException {
+        String value = StringType.INSTANCE.nullSafeGet(rs, names[0], session);
+        if (value == null || rs.wasNull()) {
             return null;
         } else {
-            return values.entrySet()
-                    .stream()
-                    .filter(entry -> entry.getValue().equals(value))
-                    .findFirst()
-                    .map(Map.Entry::getKey)
-                    .orElse(null);
+            return values.inverse().get(value);
         }
     }
 
     @Override
-    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
-            throws HibernateException, SQLException {
-        String jdbcValue = value != null ? values.get(value) : null;
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session) throws SQLException {
+        String jdbcValue = value != null ? values.get((Enum) value) : null;
+        StringType.INSTANCE.nullSafeSet(st, jdbcValue, index, session);
         if (jdbcValue == null) {
             st.setNull(index, Types.VARCHAR);
         } else {

3ae4a5bea9893f93b1bed90bc956ca0530b3cc81
brianbrix <bmokandu@developmentgateway.org>
2023-08-09 12:55:00 +0300

    AMP-30620 AMP-30675 Issue IATI import issue

--- amp/WEB-INF/src/org/digijava/module/aim/dbentity/NamedEnumType.java
+++ amp/WEB-INF/src/org/digijava/module/aim/dbentity/NamedEnumType.java
@@ -1,73 +1,80 @@
 package org.digijava.module.aim.dbentity;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.type.EnumType;
-import org.hibernate.usertype.ParameterizedType;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
-public class NamedEnumType extends EnumType implements ParameterizedType {
 
-    private Class<? extends Enum> enumClass;
-    private Method getter;
-    private Method inverseGetter;
+public class NamedEnumType extends EnumType {
+    private BiMap<Enum, String> values = HashBiMap.create();
 
     @Override
     public void setParameterValues(Properties parameters) {
-        super.setParameterValues(parameters);
+        parameters.setProperty(NAMED, "true");
 
-        String enumClassName = parameters.getProperty(ENUM);
         String valueProperty = parameters.getProperty("valueProperty");
 
+        String enumClassName = parameters.getProperty(ENUM);
+        Class<? extends Enum> enumClass;
         try {
             enumClass = Class.forName(enumClassName).asSubclass(Enum.class);
-            getter = enumClass.getMethod(valueProperty);
-            inverseGetter = enumClass.getMethod("valueOf", String.class);
-        } catch (ClassNotFoundException | NoSuchMethodException e) {
-            throw new HibernateException("Enum class or getter not found", e);
+        } catch (ClassNotFoundException e) {
+            throw new HibernateException("Enum class not found", e);
         }
+
+        String getterMethodName = "get" + valueProperty.substring(0, 1).toUpperCase() + valueProperty.substring(1);
+        Method getter;
+        try {
+            getter = enumClass.getMethod(getterMethodName);
+        } catch (NoSuchMethodException e) {
+            throw new HibernateException("Getter method not found for enum value", e);
+        }
+        for (Enum enumConstant : enumClass.getEnumConstants()) {
+            try {
+                Object value = getter.invoke(enumConstant);
+                values.put(enumConstant, value.toString());
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new HibernateException("Error invoking getter method", e);
+            }
+        }
+        super.setParameterValues(parameters);
     }
 
     @Override
-    public Object nullSafeGet(
-            ResultSet rs,
-            String[] names,
-            SharedSessionContractImplementor session,
-            Object owner
-    ) throws SQLException {
+    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner)
+            throws SQLException {
         String value = rs.getString(names[0]);
         if (rs.wasNull() || value == null) {
             return null;
         } else {
-            try {
-                return inverseGetter.invoke(enumClass, value);
-            } catch (Exception e) {
-                throw new HibernateException("Error invoking inverse getter", e);
-            }
+            return values.entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue().equals(value))
+                    .findFirst()
+                    .map(Map.Entry::getKey)
+                    .orElse(null);
         }
     }
 
     @Override
-    public void nullSafeSet(
-            PreparedStatement st,
-            Object value,
-            int index,
-            SharedSessionContractImplementor session
-    ) throws HibernateException, SQLException {
-        if (value == null) {
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
+            throws HibernateException, SQLException {
+        String jdbcValue = value != null ? values.get(value) : null;
+        if (jdbcValue == null) {
             st.setNull(index, Types.VARCHAR);
         } else {
-            try {
-                String jdbcValue = (String) getter.invoke(value);
-                st.setString(index, jdbcValue);
-            } catch (Exception e) {
-                throw new HibernateException("Error invoking getter", e);
-            }
+            st.setString(index, jdbcValue);
         }
     }
 }

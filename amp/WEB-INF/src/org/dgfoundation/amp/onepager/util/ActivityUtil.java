6042b7c56a0134b5d7d98a66d034bce1ec101d58
brianbrix <bmokandu@developmentgateway.org>
2023-08-14 01:13:50 +0300

    AMP-30620 AMP-30680 Issue- problem validating scc activity

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/ActivityUtil.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/ActivityUtil.java
@@ -1372,19 +1372,8 @@ public class ActivityUtil {
     private static void saveProjectCosts(AmpActivityVersion a, Session session) {
         if (a.getCostAmounts() != null) {
             for (AmpFundingAmount afa : a.getCostAmounts()) {
+                afa.setActivity(a);
                 if (afa.getAmpFundingAmountId() == null) {
-                    String hql = "FROM "+ AmpFundingAmount.class.getName()+ " e WHERE e.funType = :fundingTypeValue and e.activity= :activityValue";
-                    List<AmpFundingAmount> results = session.createQuery(hql, AmpFundingAmount.class)
-                            .setParameter("activityValue", afa.getActivity(), ObjectType.INSTANCE)
-                            .setParameter("fundingTypeValue", afa.getFunType())
-                            .list();
-                    if (!results.isEmpty())
-                    {
-                        afa.setActivity(null);
-                        session.merge(afa);
-                        session.flush();
-                    }
-                    afa.setActivity(a);
                     session.saveOrUpdate(afa);
                 } else {
                     session.merge(afa);

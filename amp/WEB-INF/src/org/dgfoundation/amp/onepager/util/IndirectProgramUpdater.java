60800723698b8cb2be405911b88f2640f3fd1d79
brianbrix <bmokandu@developmentgateway.org>
2023-08-18 09:31:54 +0300

    AMP-30620 AMP-30685 Issue - saving activity in drc lead to exception 1 AMP-30689 Issue - saving activity form exceptions AMP-30693 Issue -add new calendar event cause user to logout(csrf) Admin changes issue(Database flushing)

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/IndirectProgramUpdater.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/IndirectProgramUpdater.java
@@ -114,8 +114,10 @@ public class IndirectProgramUpdater {
     }
 
     private List<AmpTheme> getIndirectPrograms(Map<AmpTheme, Set<AmpTheme>> themeMapping, AmpTheme program) {
+        if (program!=null){
         while (program.getIndlevel() > getMappingLevel(MAPPING_PROGRAM_LEVEL)) {
             program = program.getParentThemeId();
+        }
         }
 
         return new ArrayList<>(themeMapping.getOrDefault(program, emptySet()));

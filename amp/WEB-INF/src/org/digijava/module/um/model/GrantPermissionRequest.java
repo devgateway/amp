26795711e7a6d519308231d2bc14ea352a9b8352
brianbrix <bmokandu@developmentgateway.org>
2023-08-28 21:14:57 +0300

    AMP-30711 Add webclient

---
+++ amp/WEB-INF/src/org/digijava/module/um/model/GrantPermissionRequest.java
@@ -0,0 +1,56 @@
+package org.digijava.module.um.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GrantPermissionRequest {
+    @JsonProperty("apiVersion")
+    private String apiVersion;
+
+    @JsonProperty("data")
+    private Data data;
+
+    // Getters and setters
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public static class Data {
+        @JsonProperty("identity")
+        private String identity;
+
+        @JsonProperty("intent")
+        private String intent;
+
+        // Getters and setters
+
+        public String getIdentity() {
+            return identity;
+        }
+
+        public void setIdentity(String identity) {
+            this.identity = identity;
+        }
+
+        public String getIntent() {
+            return intent;
+        }
+
+        public void setIntent(String intent) {
+            this.intent = intent;
+        }
+    }
+}
+

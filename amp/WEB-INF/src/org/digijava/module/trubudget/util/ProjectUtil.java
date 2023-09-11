d598d390d75c95d783afa1f3c3a2941b490062bc
brianbrix <bmokandu@developmentgateway.org>
2023-09-11 19:02:32 +0300

    AMP-30712 Store trubudget login token in cache

--- amp/WEB-INF/src/org/digijava/module/trubudget/util/ProjectUtil.java
+++ amp/WEB-INF/src/org/digijava/module/trubudget/util/ProjectUtil.java
@@ -3,12 +3,27 @@ package org.digijava.module.trubudget.util;
 import org.digijava.kernel.cache.AbstractCache;
 import org.digijava.kernel.cache.ehcache.EhCacheWrapper;
 import org.digijava.module.aim.dbentity.AmpActivityVersion;
+import org.digijava.module.aim.util.TeamUtil;
+import org.digijava.module.trubudget.model.project.CreateProjectModel;
+
+import java.util.Arrays;
+import java.util.UUID;
 
 public class ProjectUtil {
     public static void createProject(AmpActivityVersion ampActivityVersion)
     {
         AbstractCache myCache = new EhCacheWrapper("trubudget");
-        String token =(String) myCache.get("loginToken");
+        String token =(String) myCache.get("truBudgetToken");
+        String user =(String) myCache.get("truBudgetUser");
         System.out.println("Token:"+token);
+        CreateProjectModel projectModel = new CreateProjectModel();
+        CreateProjectModel.Data data = new CreateProjectModel.Data();
+        CreateProjectModel.Project project = new CreateProjectModel.Project();
+        project.setAssignee(user);
+        project.setId(UUID.randomUUID().toString().replaceAll("-",""));
+        project.setDisplayName(ampActivityVersion.getName());
+        project.setDescription(ampActivityVersion.getDescription());
+        project.setTags(Arrays.asList((ampActivityVersion.getName()+" "+ampActivityVersion.getDescription()).split(" ")));
+
     }
 }

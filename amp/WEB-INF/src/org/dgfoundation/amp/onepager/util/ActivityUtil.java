0e0a0c42a92e9cd4b297eea11d2808e91a606b82
brianbrix <bmokandu@developmentgateway.org>
2023-09-19 00:14:29 +0300

    AMP-30723 Feature -implement amp TruBudget integration AMP-30725 CREATION OF COMPONENTS

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/ActivityUtil.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/ActivityUtil.java
@@ -328,7 +328,7 @@ public class ActivityUtil {
             {
                 //update project
                 ProjectUtil.updateProject(truBudgetActivity.getTruBudgetId(),a);
-                session.flush();
+//                session.flush();
             }
         }
 

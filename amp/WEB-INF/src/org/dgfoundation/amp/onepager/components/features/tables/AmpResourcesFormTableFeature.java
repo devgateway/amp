8b1e29af9fc955ba9fdbd31ebf952d4841afde71
brianbrix <bmokandu@developmentgateway.org>
2023-10-09 12:49:48 +0300

    AMP-30723 -implement amp TruBudget integration AMP-30733 -ADD DOCUMENTS TO AMP COMPONENT

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpResourcesFormTableFeature.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/components/features/tables/AmpResourcesFormTableFeature.java
@@ -221,10 +221,10 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                 if (webLink != null && webLink.length() > 0) {
                     if (!webLink.startsWith("http"))
                         webLink = "http://" + webLink;
-                    ExternalLink link = new ExternalLink("download", new Model<String>(webLink));
+                    ExternalLink link = new ExternalLink("download", new Model<>(webLink));
                     item.add(link);
                     WebMarkupContainer downloadLinkImg = new WebMarkupContainer("downloadImage");
-                    downloadLinkImg.add(new AttributeModifier("src", new Model("/TEMPLATE/ampTemplate/img_2/ico_attachment.png")));
+                    downloadLinkImg.add(new AttributeModifier("src", new Model<>("/TEMPLATE/ampTemplate/img_2/ico_attachment.png")));
                     link.add(downloadLinkImg);
                 } else {
                     Link downloadLink = new Link("download") {
@@ -238,7 +238,7 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                     String contentType = item.getModelObject().getFileName();
                     int index = contentType.lastIndexOf('.');
 
-                    String extension = contentType.substring(index + 1, contentType.length());
+                    String extension = contentType.substring(index + 1);
                     String extPath = "/TEMPLATE/ampTemplate/images/icons/" + extension + ".gif";
                     File extImgFile = new File(WebApplication.get().getServletContext().getRealPath(extPath));
                     if (!extImgFile.exists())
@@ -247,7 +247,7 @@ public class AmpResourcesFormTableFeature extends AmpFormTableFeaturePanel<AmpAc
                         extPath = "/TEMPLATE/ampTemplate/images/icons/" + extension + ".gif";
 
                     WebMarkupContainer downloadLinkImg = new WebMarkupContainer("downloadImage");
-                    downloadLinkImg.add(new AttributeModifier("src", new Model(extPath)));
+                    downloadLinkImg.add(new AttributeModifier("src", new Model<>(extPath)));
                     downloadLink.add(downloadLinkImg);
                 }
 

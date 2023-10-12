5b2cce065643ec883f5002c200e9990154251d75
brianbrix <bmokandu@developmentgateway.org>
2023-10-12 20:03:51 +0300

    AMP-30723 -implement amp TruBudget integration AMP-30733 -ADD DOCUMENTS TO AMP COMPONENT

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/helper/TemporaryDocument.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/helper/TemporaryDocument.java
@@ -17,7 +17,7 @@ public class TemporaryDocument implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
-    private String title;
+    private String title="Document Title";
     private FileUpload file;
     private boolean existing;
     private double fileSize;

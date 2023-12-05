eb59f17c140bb2f40cd14b25ca5445cc0977b754
brianbrix <mokandubrian@gmail.com>
2023-12-05 17:32:09 +0300

    AMP-30677 remove the ant build process

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/datasources/datasources-item-view.js
+++
@@ -1,7 +0,0 @@
-
-// model-specific item views
-var ADMClustersDSView = require('./datasources-item-adm-clusters');
-
-module.exports = function(options) {
-  return new ADMClustersDSView(options);
-};
--- amp/TEMPLATE/reamp/modules/activity/preview/utils/translate.jsx
+++
@@ -1,5 +0,0 @@
-import TranslationManager from '../utils/TranslationManager';
-export default (k, lng) => {
-    // We ignore lang since the pack is already in the navigation language
-    return TranslationManager.translate(k);
-}

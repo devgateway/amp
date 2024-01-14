bb816afbb1a5d13b65996646447f0347164cc19f
brianbrix <mokandubrian@gmail.com>
2024-01-14 14:13:53 +0300

    GGW-207 Map - Make map logged in

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/gis/views/gis-main.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/gis/views/gis-main.js
@@ -103,6 +103,7 @@ module.exports = Backbone.View.extend({
           if (result.isLoggedIn===false) {
             // User is logged in, do something
             alert('User is not logged in');
+            return this;
           }
           else
           {

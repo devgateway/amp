aa1ab9d56f9255562bf9494bfb77851d62dc1672
brianbrix <mokandubrian@gmail.com>
2024-01-18 13:29:47 +0300

    GGW-207 Map - Make map logged in GGW-202 Map - Add GGW belt B

--- amp/WEB-INF/src/org/digijava/kernel/util/GisSecurityUtilService.java
+++ amp/WEB-INF/src/org/digijava/kernel/util/GisSecurityUtilService.java
@@ -1,13 +1,9 @@
 package org.digijava.kernel.util;
 
 import org.digijava.module.aim.util.FeaturesUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 

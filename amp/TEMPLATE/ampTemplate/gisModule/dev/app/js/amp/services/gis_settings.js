c2512ca0584f8e6e0cc27b1ecbe7ab0c9b0e3a27
brianbrix <mokandubrian@gmail.com>
2024-01-25 08:57:33 +0300

    GGW-203 Map - Add configuration to turn on and off sectors B

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/services/gis_settings.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/services/gis_settings.js
@@ -1,31 +1,30 @@
 let cachedGisSettings = null;
 
 function getGisSettings() {
-    return new Promise((resolve, reject) => {
-        if (cachedGisSettings !== null) {
-            resolve(cachedGisSettings);
-            return;
-        }
+    if (cachedGisSettings !== null) {
+        return Promise.resolve(cachedGisSettings);
+    }
 
-        fetch('/rest/amp/settings/gis')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                // Cache the GIS settings
-                cachedGisSettings = data;
+    // Return the fetch Promise
+    return fetch('/rest/amp/settings/gis')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Cache the GIS settings
+            cachedGisSettings = data;
 
-                // Resolve the promise with the JSON response
-                resolve(data);
-            })
-            .catch(error => {
-                // Reject the promise with the error
-                reject(error);
-            });
-    });
+            // Resolve the promise with the JSON response
+            return data;
+        })
+        .catch(error => {
+            // Reject the promise with the error
+            console.log(error);
+            throw error; // Re-throw the error to continue propagating it
+        });
 }
 
 module.exports = getGisSettings;

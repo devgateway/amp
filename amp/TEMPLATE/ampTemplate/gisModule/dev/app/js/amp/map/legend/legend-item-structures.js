b977b62f7c0e8de9769618f67820d491b443eae9
brianbrix <mokandubrian@gmail.com>
2024-01-25 09:13:23 +0300

    GGW-203 Map - Add configuration to turn on and off sectors B

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/legend/legend-item-structures.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/map/legend/legend-item-structures.js
@@ -1,6 +1,7 @@
 /* global app */
 var fs = require('fs');
-const { getGisSettings } = require('../../services/gis_settings');
+import {getGisSettings} from "../../services/gis_settings";
+
 
 var _ = require('underscore');
 const Backbone = require('backbone');

c44203a0d7dca441dc83c58f897976d81f245bfa
brianbrix <mokandubrian@gmail.com>
2024-02-06 16:59:29 +0300

    GGW-204- Add configuration to turn on and off programs B GGW-203- Add configuration to turn on and off sectors B

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/services/program_settings.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/services/program_settings.js
@@ -1,34 +1,29 @@
-/*/A
- * The GIS Data Engine
- * attaches to app.data
- */
-
 var _ = require('underscore');
 var $ = require('jquery');
 var Backbone = require('backbone');
-
-
 
 var ProgramSettings = function() {
     this.initialize.apply(this, arguments);
 };
 
-
 _.extend(ProgramSettings.prototype, Backbone.Events, {
 
     initialize: function() {
         var self = this;
-        this.programSettings = {}
+        this.programSettings = {};
         $.ajax({
             url: '/rest/filters/programs'
         }).done(function(data) {
-            console.log("Programs Settings data", data)
-            self.programSettings = data;
+            // Parse the data as JSON
+            try {
+                self.programSettings = JSON.parse(data);
+                console.log("Programs Settings data", self.programSettings);
+            } catch (error) {
+                console.error("Error parsing JSON:", error);
+            }
         });
     },
 
 });
 
-
 module.exports = ProgramSettings;
-

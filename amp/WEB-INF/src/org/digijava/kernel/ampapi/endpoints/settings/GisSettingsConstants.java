d4ea81fa1b98bf9dfa4b76011cfa9e336bdfa440
brianbrix <mokandubrian@gmail.com>
2024-03-04 12:23:52 +0300

    GGW-204 Configuration for program switch in GIS

---
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/settings/GisSettingsConstants.java
@@ -0,0 +1,7 @@
+package org.digijava.kernel.ampapi.endpoints.settings;
+
+public class GisSettingsConstants {
+    public static  String MULTICOUNTRY_ENABLED="Multicountry Enabled";
+    public static  String GIS_SECTORS_ENABLED="GIS Sectors Enabled";
+    public static  String GIS_PROGRAMS_ENABLED="GIS Programs Enabled";
+}

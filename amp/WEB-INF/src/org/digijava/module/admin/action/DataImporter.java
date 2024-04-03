1779ae01580d34ceebb7532bdf121d4bfdef1cbc
brianbrix <mokandubrian@gmail.com>
2024-04-03 12:05:57 +0300

    AMP- Add data importer for Burkina

---
+++ amp/WEB-INF/src/org/digijava/module/admin/action/DataImporter.java
@@ -0,0 +1,16 @@
+package org.digijava.module.admin.action;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DataImporter extends Action {
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}

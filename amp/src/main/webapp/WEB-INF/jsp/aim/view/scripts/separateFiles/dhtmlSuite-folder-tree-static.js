ced98f246ef3c05f9bdc7d9e329407783d4f1100
brianbrix <mokandubrian@gmail.com>
2024-04-04 21:59:17 +0300

    AMP-30858- Create data importer for Burkina

--- amp/src/main/webapp/WEB-INF/jsp/aim/view/scripts/separateFiles/dhtmlSuite-folder-tree-static.js
+++ amp/src/main/webapp/WEB-INF/jsp/aim/view/scripts/separateFiles/dhtmlSuite-folder-tree-static.js
@@ -54,9 +54,9 @@ Owner of DHTMLgoodies.com
 	   var start = document.cookie.indexOf(name+"=");
 	   var len = start+name.length+1;
 	   if ((!start) && (name != document.cookie.substring(0,name.length))) return null;
-	   if (start == -1) return null;
+	   if (start === -1) return null;
 	   var end = document.cookie.indexOf(";",len);
-	   if (end == -1) end = document.cookie.length;
+	   if (end === -1) end = document.cookie.length;
 	   return unescape(document.cookie.substring(len,end));
 	}
 	// This function has been slightly modified

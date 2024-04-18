4d5b5d8b17ae0c9bec8e5440b1f87b1f98b97fff
brianbrix <mokandubrian@gmail.com>
2024-04-18 19:10:28 +0300

    AMP-204-Move programs and sectors switch to feature template

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/activity-model.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/activity-model.js
@@ -104,6 +104,10 @@ module.exports = Backbone.Model.extend({
       } else if (matchesFilters[name][0] && matchesFilters[name][0].attributes) {
         return matchesFilters[name][0].get('name');
       }
+      else {
+          return matchesFilters[name][0].get('name');
+
+      }
     }
     return 'nil';
   }

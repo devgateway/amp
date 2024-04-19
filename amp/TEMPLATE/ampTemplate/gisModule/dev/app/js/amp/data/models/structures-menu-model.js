b9b5a9ebb0926745fbac1301254d27173af1c094
brianbrix <mokandubrian@gmail.com>
2024-04-19 12:09:47 +0300

    AMP-204-Move programs and sectors switch to feature template

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/structures-menu-model.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/structures-menu-model.js
@@ -21,9 +21,9 @@ module.exports = Backbone.Model
     this.filter = options.filter;
     this.settingsWidget = options.settingsWidget;
     this.structuresCollection = this.appData.structures;
-    console.log("Attributes ",this.appData.generalSettings.attributes.toJSON())
-    var sectorsEnabled= this.appData.generalSettings.attributes.toJSON()['gis-sectors-enabled'];
-    var programsEnabled= this.appData.generalSettings.attributes.toJSON()['gis-programs-enabled'];
+    console.log("Attributes ",app.data.generalSettings)
+    var sectorsEnabled= app.data.generalSettings.get('gis-sectors-enabled');
+    var programsEnabled= app.data.generalSettings.get('gis-programs-enabled');
     console.log(programsEnabled,sectorsEnabled)
     if (programsEnabled && !sectorsEnabled) {
       this.set('filterVertical','Programs');

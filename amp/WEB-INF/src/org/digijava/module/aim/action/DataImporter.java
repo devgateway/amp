a726c70df641ce6829949d5bde35b0afb61140cd
brianbrix <mokandubrian@gmail.com>
2024-05-20 11:43:03 +0300

    AMP-30858- Create txt importer

--- amp/WEB-INF/src/org/digijava/module/aim/action/DataImporter.java
+++ amp/WEB-INF/src/org/digijava/module/aim/action/DataImporter.java
@@ -60,7 +60,7 @@ import static com.fasterxml.jackson.core.JsonGenerator.Feature.ESCAPE_NON_ASCII;
 public class DataImporter extends Action {
     static Logger logger = LoggerFactory.getLogger(DataImporter.class);
     private static final int BATCH_SIZE = 1000;
-    private static Map<String, Long> constantsMap=new HashMap<>();
+    private static Map<String, Long> constantsMap = new HashMap<>();
 
 
     @Override
@@ -68,190 +68,189 @@ public class DataImporter extends Action {
         // List of fields
         List<String> fieldsInfo = getEntityFieldsInfo();
         request.setAttribute("fieldsInfo", fieldsInfo);
-        DataImporterForm dataImporterForm = (DataImporterForm)form;
+        DataImporterForm dataImporterForm = (DataImporterForm) form;
 
 
         if (Objects.equals(request.getParameter("action"), "uploadTemplate")) {
-            logger.info(" this is the action "+request.getParameter("action"));
-        if (request.getParameter("uploadTemplate")!=null) {
-            logger.info(" this is the action "+request.getParameter("uploadTemplate"));
-            Set<String> headersSet = new HashSet<>();
+            logger.info(" this is the action " + request.getParameter("action"));
+            if (request.getParameter("uploadTemplate") != null) {
+                logger.info(" this is the action " + request.getParameter("uploadTemplate"));
+                Set<String> headersSet = new HashSet<>();
 
-            if (request.getParameter("fileType")!=null) {
-                InputStream fileInputStream = dataImporterForm.getTemplateFile().getInputStream();
-                if ((Objects.equals(request.getParameter("fileType"), "excel") || Objects.equals(request.getParameter("fileType"), "csv"))) {
-                    Workbook workbook = new XSSFWorkbook(fileInputStream);
-                    int numberOfSheets = workbook.getNumberOfSheets();
-                    for (int i = 0; i < numberOfSheets; i++) {
-                        Sheet sheet = workbook.getSheetAt(i);
-                        Row headerRow = sheet.getRow(0);
-                        Iterator<Cell> cellIterator = headerRow.cellIterator();
-                        while (cellIterator.hasNext()) {
-                            Cell cell = cellIterator.next();
-                            headersSet.add(cell.getStringCellValue());
+                if (request.getParameter("fileType") != null) {
+                    InputStream fileInputStream = dataImporterForm.getTemplateFile().getInputStream();
+                    if ((Objects.equals(request.getParameter("fileType"), "excel") || Objects.equals(request.getParameter("fileType"), "csv"))) {
+                        Workbook workbook = new XSSFWorkbook(fileInputStream);
+                        int numberOfSheets = workbook.getNumberOfSheets();
+                        for (int i = 0; i < numberOfSheets; i++) {
+                            Sheet sheet = workbook.getSheetAt(i);
+                            Row headerRow = sheet.getRow(0);
+                            Iterator<Cell> cellIterator = headerRow.cellIterator();
+                            while (cellIterator.hasNext()) {
+                                Cell cell = cellIterator.next();
+                                headersSet.add(cell.getStringCellValue());
+                            }
+
+                        }
+                        workbook.close();
+
+                    } else if (Objects.equals(request.getParameter("fileType"), "text")) {
+                        CSVParser parser = new CSVParserBuilder().withSeparator(request.getParameter("dataSeparator").charAt(0)).build();
+
+                        try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(fileInputStream)).withCSVParser(parser).build()) {
+                            String[] headers = reader.readNext(); // Read the first line which contains headers
+
+                            if (headers != null) {
+                                // Print each header
+                                headersSet.addAll(Arrays.asList(headers));
+                            } else {
+                                logger.info("File is empty or does not contain headers.");
+                            }
+                        } catch (IOException | CsvValidationException e) {
+                            e.printStackTrace();
                         }
 
                     }
-                    workbook.close();
-
-                } else if (Objects.equals(request.getParameter("fileType"), "text")) {
-                    CSVParser parser = new CSVParserBuilder().withSeparator(request.getParameter("dataSeparator").charAt(0)).build();
-
-                    try (CSVReader reader = new CSVReaderBuilder(new InputStreamReader(fileInputStream)).withCSVParser(parser).build()) {
-                        String[] headers = reader.readNext(); // Read the first line which contains headers
-
-                        if (headers != null) {
-                            // Print each header
-                            headersSet.addAll(Arrays.asList(headers));
-                        } else {
-                           logger.info("File is empty or does not contain headers.");
-                        }
-                    } catch (IOException | CsvValidationException e) {
-                        e.printStackTrace();
-                    }
-
                 }
+                headersSet = headersSet.stream().sorted().collect(Collectors.toCollection(LinkedHashSet::new));
+                StringBuilder headers = new StringBuilder();
+                headers.append("  <label for=\"columnName\">Select Column Name:</label>\n<select  class=\"select2\" style=\"width: 300px;\" id=\"columnName\">");
+                for (String option : headersSet) {
+                    headers.append("<option>").append(option).append("</option>");
+                }
+                headers.append("</select>");
+                response.setHeader("selectTag", headers.toString());
+
+                response.setHeader("updatedMap", "");
+
+                dataImporterForm.getColumnPairs().clear();
+
             }
-            headersSet = headersSet.stream().sorted().collect(Collectors.toCollection(LinkedHashSet::new));
-            StringBuilder headers = new StringBuilder();
-            headers.append("  <label for=\"columnName\">Select Column Name:</label>\n<select  class=\"select2\" style=\"width: 300px;\" id=\"columnName\">");
-            for (String option: headersSet) {
-                headers.append("<option>").append(option).append("</option>");
-            }
-            headers.append("</select>");
-            response.setHeader("selectTag",headers.toString());
-
-            response.setHeader("updatedMap","");
-
-            dataImporterForm.getColumnPairs().clear();
-
-            }
-
-
 
 
             if (Objects.equals(request.getParameter("action"), "addField")) {
-            logger.info(" this is the action "+request.getParameter("action"));
+                logger.info(" this is the action " + request.getParameter("action"));
 
-            String columnName = request.getParameter("columnName");
-            String selectedField = request.getParameter("selectedField");
-            dataImporterForm.getColumnPairs().put(columnName, selectedField);
-            logger.info("Column Pairs:"+dataImporterForm.getColumnPairs());
+                String columnName = request.getParameter("columnName");
+                String selectedField = request.getParameter("selectedField");
+                dataImporterForm.getColumnPairs().put(columnName, selectedField);
+                logger.info("Column Pairs:" + dataImporterForm.getColumnPairs());
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString( dataImporterForm.getColumnPairs());
+                ObjectMapper objectMapper = new ObjectMapper();
+                String json = objectMapper.writeValueAsString(dataImporterForm.getColumnPairs());
 
-            // Send JSON response
-            response.setContentType("application/json");
-            response.getWriter().write(json);
-            response.setCharacterEncoding("UTF-8");
+                // Send JSON response
+                response.setContentType("application/json");
+                response.getWriter().write(json);
+                response.setCharacterEncoding("UTF-8");
 
                 return null;
 
-        }
+            }
 
 
-        if (Objects.equals(request.getParameter("action"), "removeField")) {
-            logger.info(" this is the action "+request.getParameter("action"));
+            if (Objects.equals(request.getParameter("action"), "removeField")) {
+                logger.info(" this is the action " + request.getParameter("action"));
 
-            String columnName = request.getParameter("columnName");
-            String selectedField = request.getParameter("selectedField");
-            dataImporterForm.getColumnPairs().put(columnName, selectedField);
-            removeMapItem(dataImporterForm.getColumnPairs(),columnName,selectedField);
-            logger.info("Column Pairs:"+dataImporterForm.getColumnPairs());
+                String columnName = request.getParameter("columnName");
+                String selectedField = request.getParameter("selectedField");
+                dataImporterForm.getColumnPairs().put(columnName, selectedField);
+                removeMapItem(dataImporterForm.getColumnPairs(), columnName, selectedField);
+                logger.info("Column Pairs:" + dataImporterForm.getColumnPairs());
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString( dataImporterForm.getColumnPairs());
+                ObjectMapper objectMapper = new ObjectMapper();
+                String json = objectMapper.writeValueAsString(dataImporterForm.getColumnPairs());
 
-            // Send JSON response
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
+                // Send JSON response
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
 //            response.setHeader("updatedMap",json);
-            response.getWriter().write(json);
+                response.getWriter().write(json);
 
-            return null;
+                return null;
 
-        }
-
-        if (Objects.equals(request.getParameter("action"), "uploadDataFile")) {
-            logger.info("This is the action Upload " + request.getParameter("uploadDataFile"));
-            String fileName = dataImporterForm.getDataFile().getFileName();
-            String tempDirPath = System.getProperty("java.io.tmpdir");
-            File tempDir = new File(tempDirPath);
-            if (!tempDir.exists()) {
-                tempDir.mkdirs();
             }
-            String tempFilePath = tempDirPath + File.separator + fileName;
-            try (InputStream inputStream = dataImporterForm.getDataFile().getInputStream();
-                 FileOutputStream outputStream = new FileOutputStream(tempFilePath)) {
-                byte[] buffer = new byte[8192];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
+
+            if (Objects.equals(request.getParameter("action"), "uploadDataFile")) {
+                logger.info("This is the action Upload " + request.getParameter("uploadDataFile"));
+                String fileName = dataImporterForm.getDataFile().getFileName();
+                String tempDirPath = System.getProperty("java.io.tmpdir");
+                File tempDir = new File(tempDirPath);
+                if (!tempDir.exists()) {
+                    tempDir.mkdirs();
                 }
-            }
+                String tempFilePath = tempDirPath + File.separator + fileName;
+                try (InputStream inputStream = dataImporterForm.getDataFile().getInputStream();
+                     FileOutputStream outputStream = new FileOutputStream(tempFilePath)) {
+                    byte[] buffer = new byte[8192];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                }
 
-            // Check if the file is readable and has correct content
-            File tempFile = new File(tempFilePath);
-            List<ImportedFilesRecord> similarFiles = ImportedFileUtil.getSimilarFiles(tempFile);
-            if (similarFiles!=null && !similarFiles.isEmpty()) {
-            for (ImportedFilesRecord similarFilesRecord : similarFiles) {
-                logger.info("Similar file: " + similarFilesRecord);
-                if (similarFilesRecord.getImportStatus().equals(ImportStatus.IN_PROGRESS))
-                {
-                    response.setHeader("errorMessage","You have a similar file in progress. Please try again later.");
+                // Check if the file is readable and has correct content
+                File tempFile = new File(tempFilePath);
+                List<ImportedFilesRecord> similarFiles = ImportedFileUtil.getSimilarFiles(tempFile);
+                if (similarFiles != null && !similarFiles.isEmpty()) {
+                    for (ImportedFilesRecord similarFilesRecord : similarFiles) {
+                        logger.info("Similar file: " + similarFilesRecord);
+                        if (similarFilesRecord.getImportStatus().equals(ImportStatus.IN_PROGRESS)) {
+                            response.setHeader("errorMessage", "You have a similar file in progress. Please try again later.");
+                            response.setStatus(400);
+                            return mapping.findForward("importData");
+                        }
+                    }
+                }
+                if (dataImporterForm.getColumnPairs().isEmpty() || !dataImporterForm.getColumnPairs().containsValue("{projectTitle}")) {
+                    response.setHeader("errorMessage", "You must have atleast the {projectTitle} key in your config.");
                     response.setStatus(400);
                     return mapping.findForward("importData");
                 }
-            }
-            }
-            if (dataImporterForm.getColumnPairs().isEmpty() || !dataImporterForm.getColumnPairs().containsValue("{projectTitle}"))
-            {
-                response.setHeader("errorMessage","You must have atleast the {projectTitle} key in your config.");
-                response.setStatus(400);
-                return mapping.findForward("importData");
-            }
-            if (!isFileReadable(tempFile) || !isFileContentValid(tempFile)) {
-                // Handle invalid file
-                logger.error("Invalid file or content.");
-                response.setHeader("errorMessage","Unable to parse the file. Please check the file format/content and try again.");
-                response.setStatus(400);
-                return mapping.findForward("importData");
-
-                // Optionally, you can respond with an error message to the client.
-            } else {
-                // Proceed with processing the file
-                ImportedFilesRecord importedFilesRecord = ImportedFileUtil.saveFile(tempFile, fileName);
-                // Process the file in batches
-                int res = processFileInBatches(importedFilesRecord, tempFile, request, dataImporterForm.getColumnPairs());
-                if (res != 1) {
-                    // Handle error
-                    logger.info("Error processing file  " + tempFile);
-                    response.setHeader("errorMessage","Unable to parse the file. Please check the file format/content and try again.");
+                if (!isFileReadable(tempFile) || !isFileContentValid(tempFile)) {
+                    // Handle invalid file
+                    logger.error("Invalid file or content.");
+                    response.setHeader("errorMessage", "Unable to parse the file. Please check the file format/content and try again.");
                     response.setStatus(400);
                     return mapping.findForward("importData");
+
+                    // Optionally, you can respond with an error message to the client.
+                } else {
+                    // Proceed with processing the file
+                    ImportedFilesRecord importedFilesRecord = ImportedFileUtil.saveFile(tempFile, fileName);
+                    // Process the file in batches
+                    int res = processFileInBatches(importedFilesRecord, tempFile, request, dataImporterForm.getColumnPairs());
+                    if (res != 1) {
+                        // Handle error
+                        logger.info("Error processing file  " + tempFile);
+                        response.setHeader("errorMessage", "Unable to parse the file. Please check the file format/content and try again.");
+                        response.setStatus(400);
+                        return mapping.findForward("importData");
+                    }
+
+                    // Clean up
+                    Files.delete(tempFile.toPath());
+                    logger.info("Cache map size: " + constantsMap.size());
+                    constantsMap.clear();
+                    logger.info("File path is " + tempFilePath + " and size is " + tempFile.length() / (1024 * 1024) + " mb");
+                    Instant start = Instant.now();
+                    logger.info("Start time: " + start);
+                    Instant finish = Instant.now();
+                    long timeElapsed = Duration.between(start, finish).toMillis();
+                    logger.info("Time Elapsed: " + timeElapsed);
+
+                    // Send response
+                    response.setHeader("updatedMap", "");
+                    dataImporterForm.getColumnPairs().clear();
                 }
-
-                // Clean up
-                Files.delete(tempFile.toPath());
-                logger.info("Cache map size: " + constantsMap.size());
-                constantsMap.clear();
-                logger.info("File path is " + tempFilePath + " and size is " + tempFile.length() / (1024 * 1024) + " mb");
-                Instant start = Instant.now();
-                logger.info("Start time: " + start);
-                Instant finish = Instant.now();
-                long timeElapsed = Duration.between(start, finish).toMillis();
-                logger.info("Time Elapsed: " + timeElapsed);
-
-                // Send response
-                response.setHeader("updatedMap", "");
-                dataImporterForm.getColumnPairs().clear();
+                return null;
             }
-            return null;
-        }
 
-        return mapping.findForward("importData");
+            return mapping.findForward("importData");
+        }
+        return null;
     }
+
 
     private void removeMapItem(Map<String,String> map,String columnName, String selectedField)
     {

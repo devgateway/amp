4dae3f76d109805b7476132e8f81358010064d4d
brianbrix <mokandubrian@gmail.com>
2024-06-07 00:40:54 +0300

    AMP-30891-Filter ndicators by program

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/values/AmpIndicatorPossibleValuesProvider.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/activity/values/AmpIndicatorPossibleValuesProvider.java
@@ -7,8 +7,10 @@ import org.digijava.kernel.ampapi.endpoints.common.TranslatorService;
 import org.digijava.kernel.ampapi.endpoints.common.values.providers.AbstractPossibleValuesBaseProvider;
 import org.digijava.module.aim.dbentity.AmpIndicator;
 import org.digijava.module.aim.dbentity.AmpSector;
+import org.digijava.module.aim.dbentity.IndicatorTheme;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,7 +26,7 @@ public class AmpIndicatorPossibleValuesProvider extends AbstractPossibleValuesBa
         List<PossibleValue> pvs = new ArrayList<>();
         for (AmpIndicator indicator : indicators) {
             List<Long> sectorIds = getSectorIds(indicator.getSectors());
-            Set<Long> programIds = indicator.getValuesTheme().stream().map(value->value.getTheme().getAmpThemeId()).collect(Collectors.toSet());
+            Set<Long> programIds = getProgramIds(indicator.getValuesTheme());
             IndicatorExtraInfo extraInfo = new IndicatorExtraInfo(indicator.getCode(), sectorIds, programIds);
             pvs.add(new PossibleValue(indicator.getIndicatorId(), indicator.getName(), ImmutableMap.of(), extraInfo));
         }
@@ -37,6 +39,14 @@ public class AmpIndicatorPossibleValuesProvider extends AbstractPossibleValuesBa
             sectorIds.add(sector.getAmpSectorId());
         }
         return sectorIds;
+    }
+
+    private Set<Long> getProgramIds(Set<IndicatorTheme> indicatorThemes) {
+        Set<Long> programIds = new HashSet<>();
+        for (IndicatorTheme indicatorTheme : indicatorThemes) {
+            programIds.add(indicatorTheme.getTheme().getAmpThemeId());
+        }
+        return programIds;
     }
 
     @Override

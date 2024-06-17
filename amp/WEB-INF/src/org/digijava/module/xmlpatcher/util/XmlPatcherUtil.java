7cb39ca9239915693747e65886ccaa88702b2bdf
brianbrix <mokandubrian@gmail.com>
2024-06-17 22:58:53 +0300

    AMP-30853 Single /Multiple country switch

--- amp/WEB-INF/src/org/digijava/module/xmlpatcher/util/XmlPatcherUtil.java
+++ amp/WEB-INF/src/org/digijava/module/xmlpatcher/util/XmlPatcherUtil.java
@@ -386,6 +386,7 @@ public final class XmlPatcherUtil {
     public static List<AmpXmlPatch> getAllDiscoveredUnclosedPatches()
             throws HibernateException {
         Session session = PersistenceManager.getRequestDBSession();
+        session.flush();
         Query query = session
                 .createQuery("from " + AmpXmlPatch.class.getName()
                         + " p WHERE p.state NOT IN ("

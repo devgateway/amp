9ce84ab1649619228ca6e49d004bcfafce405254
brianbrix <mokandubrian@gmail.com>
2024-11-22 16:51:04 +0300

    GGW-368:Add wocat initiatives to GGW map cluster

--- amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/adm-cluster-model.js
+++ amp/TEMPLATE/ampTemplate/gisModule/dev/app/js/amp/data/models/adm-cluster-model.js
@@ -19,42 +19,37 @@ module.exports = Backbone.Model
         // this.on('sync', this.modifySync);
       },
 
-        modifySync: async function (response) {
+        modifySync: function (response) {
             console.log('Original response from /cluster:', response);
 
             // Check if the response has features
             if (!response || !response.features) {
                 console.error('No features found in response!');
-                return response; // Return early if there's no valid response
+                return Promise.resolve(response); // Return a resolved Promise with the original response
             }
 
             // Create an array of promises to handle all fetchWocat calls
-            let promises = response.features.map(async (feature) => {
-                const country = countries.getAlpha3Code(feature.properties.admName, 'en');
+            var promises = response.features.map(function (feature) {
+                var country = countries.getAlpha3Code(feature.properties.admName, 'en');
                 console.log('Fetching activityIds for', feature.properties.admName);
 
-                try {
-                    // Fetch new activity IDs for the given country
-                    const newActivityIds = await this.fetchWocat(country);
+                // Fetch new activity IDs for the given country
+                return this.fetchWocat(country).then(function (newActivityIds) {
                     console.log("New activityIds:", newActivityIds, "for", feature.properties.admName);
-
-                    if (newActivityIds.length > 0) {
-                        if (this.get('id') === 'wocat') {
-                            feature.properties.wocat = true;
-                            feature.properties.wocatActivities = newActivityIds;
-                            feature.properties.activityid = newActivityIds;
-                        }
+                    if (newActivityIds.length > 0 && this.get('id') === 'wocat') {
+                        feature.properties.wocat = true;
+                        feature.properties.wocatActivities = newActivityIds;
+                        feature.properties.activityid = newActivityIds;
                     }
-                } catch (error) {
+                }.bind(this)).catch(function (error) {
                     console.error("Failed to fetch activityIds for", feature.properties.admName, error);
-                }
+                });
+            }.bind(this));
+
+            // Return a Promise that resolves when all fetchWocat calls are complete
+            return Promise.all(promises).then(function () {
+                return response; // Return the modified response
             });
-
-            // Wait for all promises to resolve
-            await Promise.all(promises);
-
-            // Return the modified response
-            return response;
         },
 
       fetchWocat: function (country) {
@@ -123,60 +118,61 @@ module.exports = Backbone.Model
         }
       },
 
-            fetch: function (options) {
-                var filter = {};
+        fetch: function (options) {
+            var filter = {};
 
-                if (this.lastFetchXhr && this.lastFetchXhr.readyState > 0 && this.lastFetchXhr.readyState < 4) {
-                    console.log("Aborting previous fetch...");
-                    this.lastFetchXhr.abort();
-                }
+            if (this.lastFetchXhr && this.lastFetchXhr.readyState > 0 && this.lastFetchXhr.readyState < 4) {
+                console.log("Aborting previous fetch...");
+                this.lastFetchXhr.abort();
+            }
 
-                if (this.collection.filter) {
-                    _.extend(filter, this.collection.filter.serialize());
-                }
+            if (this.collection.filter) {
+                _.extend(filter, this.collection.filter.serialize());
+            }
 
-                filter.settings = this.collection.settingsWidget.toAPIFormat();
-                filter.filters = filter.filters || {};
-                filter.filters.adminLevel = this._translateADMToMagicWord(this.get('value'));
+            filter.settings = this.collection.settingsWidget.toAPIFormat();
+            filter.filters = filter.filters || {};
+            filter.filters.adminLevel = this._translateADMToMagicWord(this.get('value'));
 
-                if (this.collection.performanceToggleModel.get('isPerformanceToggleSelected') !== null) {
-                    filter['performanceIssues'] = !this.collection.performanceToggleModel.get('isPerformanceToggleSelected');
-                }
+            if (this.collection.performanceToggleModel.get('isPerformanceToggleSelected') !== null) {
+                filter['performanceIssues'] = !this.collection.performanceToggleModel.get('isPerformanceToggleSelected');
+            }
 
-                options = _.defaults(options || {}, {
-                    type: 'POST',
-                    contentType: 'application/json',
-                    data: JSON.stringify(filter),
-                });
+            options = _.defaults(options || {}, {
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(filter),
+            });
 
-                console.log("Fetching data with filter:", filter);
+            console.log("Fetching data with filter:", filter);
 
-                // Perform the fetch
-                this.lastFetchXhr = Backbone.Model.prototype.fetch.call(this, options);
+            // Perform the fetch
+            this.lastFetchXhr = Backbone.Model.prototype.fetch.call(this, options);
 
-                // Intercept and modify the response
-                const modifiedPromise = this.lastFetchXhr.then(
-                    async function (response) {
-                        console.log("Original response:", response);
+            // Intercept and modify the response
+            var modifiedPromise = this.lastFetchXhr.then(
+                function (response) {
+                    console.log("Original response:", response);
 
-                        // Modify the response (await the async modifySync)
-                        const modifiedResponse = await this.modifySync(response);
+                    // Modify the response
+                    return this.modifySync(response).then(function (modifiedResponse) {
                         console.log("Modified response:", modifiedResponse);
 
                         // Trigger 'sync' event with the modified response
                         this.trigger('sync', this, modifiedResponse, options);
 
                         return modifiedResponse; // Return the modified response
-                    }.bind(this),
-                    function (error) {
-                        console.error("Fetch failed:", error);
-                        return Promise.reject(error); // Forward the error
-                    }
-                );
+                    }.bind(this));
+                }.bind(this),
+                function (error) {
+                    console.error("Fetch failed:", error);
+                    return Promise.reject(error); // Forward the error
+                }
+            );
 
-                // Return a Promise-compatible jqXHR that resolves to the modified response
-                return Object.assign(this.lastFetchXhr, modifiedPromise);
-            },
+            // Return a Promise-compatible jqXHR that resolves to the modified response
+            return Object.assign(this.lastFetchXhr, modifiedPromise);
+        },
 
       loadBoundary: function() {
         var boundaries = this.collection.boundaries.where({admLevel: this.get('value')});

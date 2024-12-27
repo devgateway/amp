298fcc2d1ccb2c0803f45e33bfd9a39053da0cba
brianbrix <mokandubrian@gmail.com>
2024-12-27 16:32:01 +0300

    AMP-30914: merge multicountry with single country code Check Multi/single country for OnePager form

--- amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/LocationService.java
+++ amp/WEB-INF/src/org/digijava/kernel/ampapi/endpoints/gis/services/LocationService.java
@@ -170,6 +170,7 @@ public class LocationService {
         }
 
         Set<Long> activitiesId = getActivitiesForFiltering(config, adminLevel);
+        logger.info("Activities ids: " + activitiesId);
 
         final ValueWrapper<String> qry = new ValueWrapper<String>(null);
         if (adminLevel.equals("Administrative Level 0")) {
@@ -205,36 +206,34 @@ public class LocationService {
                 + " where al.amp_activity_id in(" + Util.toCSStringForIN(activitiesId) + " ) "
                 + " order by acvl.root_location_id,al.amp_activity_id";
         }
+        logger.info("Query: " + qry.value);
 
         try {
-            PersistenceManager.getSession().doWork(new Work() {
-
-                @Override
-                public void execute(Connection connection) throws SQLException {
-                    try(RsInfo rsi = SQLUtils.rawRunQuery(connection, qry.value, null)) {
-                        ResultSet rs = rsi.rs;
-                        ClusteredPoints cp = null;
-                        Long rootLocationId = 0L;
-                        while (rs.next()) {
-                            if (!rootLocationId.equals(rs.getLong("root_location_id"))) {
-                                if (cp != null) {
-                                    l.add(cp);
-                                }
-                                rootLocationId = rs.getLong("root_location_id");
-                                cp = new ClusteredPoints();
-                                cp.setAdmId(rootLocationId);
-                                cp.setAdmin(rs.getString("root_location_description"));
-                                cp.setLat(rs.getString("gs_lat"));
-                                cp.setLon(rs.getString("gs_long"));
+            PersistenceManager.getSession().doWork(connection -> {
+                try(RsInfo rsi = SQLUtils.rawRunQuery(connection, qry.value, null)) {
+                    ResultSet rs = rsi.rs;
+                    ClusteredPoints cp = null;
+                    Long rootLocationId = 0L;
+                    while (rs.next()) {
+                        if (!rootLocationId.equals(rs.getLong("root_location_id"))) {
+                            if (cp != null) {
+                                l.add(cp);
                             }
-                            cp.getActivityids().add(rs.getLong("amp_activity_id"));
+                            rootLocationId = rs.getLong("root_location_id");
+                            cp = new ClusteredPoints();
+                            cp.setAdmId(rootLocationId);
+                            cp.setAdmin(rs.getString("root_location_description"));
+                            cp.setLat(rs.getString("gs_lat"));
+                            cp.setLon(rs.getString("gs_long"));
                         }
-                        if (cp != null) {
-                            l.add(cp);
-                        }
-                        logger.info("Point fetched is "+cp);
+                        cp.getActivityids().add(rs.getLong("amp_activity_id"));
                     }
-                }});
+                    if (cp != null) {
+                        l.add(cp);
+                    }
+                    logger.info("Point fetched is "+cp);
+                }
+            });
         } catch (HibernateException e) {
             throw new RuntimeException(e);
         }

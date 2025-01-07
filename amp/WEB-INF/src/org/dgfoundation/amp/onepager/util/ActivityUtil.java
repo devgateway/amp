c0d3ff47f3c8512a8b153fdd2765fba7e373e024
brianbrix <mokandubrian@gmail.com>
2025-01-07 13:39:47 +0300

    AMP-30914: Add the excel/txt activity importer to v4

--- amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/ActivityUtil.java
+++ amp/WEB-INF/src/org/dgfoundation/amp/onepager/util/ActivityUtil.java
@@ -222,10 +222,7 @@ public class ActivityUtil {
                     tmpGroup.setAmpActivityLastVersion(a);
 
                     //TODO this is a temporary status for the case when we have a new activity and we want to create a new version
-                    if (a.getApprovalStatus()==null)
-                    {
-                        a.setApprovalStatus(ApprovalStatus.created);
-                    }
+                    a.setApprovalStatus(ApprovalStatus.started);
                     Long id = (Long) session.save(tmpGroup);
                     tmpGroup.setAmpActivityGroupId(id);
                     a.setAmpActivityGroup(tmpGroup);

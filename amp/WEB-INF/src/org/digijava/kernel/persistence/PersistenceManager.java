957554b88c38a3762f4e920e58dc32b402ccd37f
brianbrix <mokandubrian@gmail.com>
2025-03-29 00:40:38 +0300

    AMP-30948: Adapt importer for v4

--- amp/WEB-INF/src/org/digijava/kernel/persistence/PersistenceManager.java
+++ amp/WEB-INF/src/org/digijava/kernel/persistence/PersistenceManager.java
@@ -577,7 +577,7 @@ public class PersistenceManager {
         if (!currentSessionIsManaged) {
             throw new IllegalStateException("Called outside of managed session context.");
         }
-        Session sess = sf().openSession();
+        Session sess = sf().getCurrentSession();
         sess.setFlushMode(FlushModeType.AUTO);
 
         Transaction transaction = sess.getTransaction();

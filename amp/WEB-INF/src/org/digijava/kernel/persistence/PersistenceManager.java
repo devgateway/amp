d1b190b2cd97d11049ab052906961e474e576927
brianbrix <mokandubrian@gmail.com>
2025-03-29 23:16:29 +0300

    AMP-30948: Test new session method

--- amp/WEB-INF/src/org/digijava/kernel/persistence/PersistenceManager.java
+++ amp/WEB-INF/src/org/digijava/kernel/persistence/PersistenceManager.java
@@ -577,7 +577,7 @@ public class PersistenceManager {
         if (!currentSessionIsManaged) {
             throw new IllegalStateException("Called outside of managed session context.");
         }
-        Session sess = sf().getCurrentSession();
+        Session sess = sf().openSession();
         sess.setFlushMode(FlushModeType.AUTO);
 
         Transaction transaction = sess.getTransaction();
@@ -790,7 +790,7 @@ public class PersistenceManager {
      * @see #cleanupSession(Session)
      */
     public static void endSessionLifecycle() {
-        cleanupSession(getSession());
+        cleanupSession(sf().getCurrentSession());
     }
 
     /**

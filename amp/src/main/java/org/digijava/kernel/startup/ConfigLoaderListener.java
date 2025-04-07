daa7628ddd04cb139a3219661a756432e1a254b1
brianbrix <mokandubrian@gmail.com>
2025-04-07 19:45:05 +0300

    AMP-30677: Resolved tests for maven

--- amp/src/main/java/org/digijava/kernel/startup/ConfigLoaderListener.java
+++ amp/src/main/java/org/digijava/kernel/startup/ConfigLoaderListener.java
@@ -115,7 +115,7 @@ public class ConfigLoaderListener
                 }
             }
             // Custom cache manager must be initialized first
-            DigiConfigManager.initialize(sce.getServletContext().getRealPath("/WEB-INF/moduleConfig"));
+            DigiConfigManager.initialize("./src/main/webapp/WEB-INF/moduleConfig");
             // Initialize services
             ServiceContext serviceContext = new WebappServiceContext(sce.getServletContext());
             ServiceManager.getInstance().init(serviceContext, 0);

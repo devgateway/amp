0958f4174f6ddf7033c9c7e79b4c6ef64babec52
brianbrix <mokandubrian@gmail.com>
2025-04-18 15:00:55 +0300

    AMP-30455: Merge to v4

--- amp/src/main/java/org/digijava/module/aim/util/ActivityUtil.java
+++ amp/src/main/java/org/digijava/module/aim/util/ActivityUtil.java
@@ -1670,13 +1670,8 @@ public static List<AmpTheme> getActivityPrograms(Long activityId) {
     }
 
     public static void  deleteFullActivityContent(AmpActivityVersion ampAct, Session session) throws Exception{
-
-        ActivityUtil.deleteActivityIndicators(DbUtil.getActivityMEIndValue(ampAct.getAmpActivityId()), ampAct, session);
         ActivityUtil.deleteActivityContent(ampAct,session);
-
-//        session.flush();
-
-//        Long ampActId = ;
+        Long ampActId = ampAct.getAmpActivityId();
         //This is not deleting AmpMEIndicators, just indicators, ME is deprecated.
         ActivityUtil.deleteActivityIndicators(DbUtil.getActivityMEIndValue(ampActId), ampAct, session);
     }

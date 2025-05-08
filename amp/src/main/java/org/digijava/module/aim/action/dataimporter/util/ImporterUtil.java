44ac2ef8b5c652dca5507a0938a45d2f3d35827d
brianbrix <mokandubrian@gmail.com>
2025-05-08 11:41:21 +0300

    Resolve some db issues

--- amp/src/main/java/org/digijava/module/aim/action/dataimporter/util/ImporterUtil.java
+++ amp/src/main/java/org/digijava/module/aim/action/dataimporter/util/ImporterUtil.java
@@ -194,6 +194,8 @@ public class ImporterUtil {
             logger.info("Date TYpe: " + cell.getCellType());
 //            if (DateUtil.isCellDateFormatted(cell)) {
             String date = cell.getStringCellValue();
+            logger.info("String Date: " + date);
+
             String formattedDate = formatDateFromDateObject(date);
             logger.info("Formatted Date: " + formattedDate);
             return formattedDate;
@@ -308,19 +310,20 @@ public class ImporterUtil {
                 // Parse the year and create a Date object for January 1 of that year
                 Date januaryFirst = new SimpleDateFormat("yyyy-MM-dd").parse(date + "-01-01");
                 return new SimpleDateFormat("yyyy-MM-dd").format(januaryFirst); // Return as "yyyy-MM-dd"
-            } catch (ParseException e) {
-                // Log error if needed, or handle exception for invalid date format
+            } catch (Exception e) {
+                logger.info("Error parsing date", e);
             }
         }
 
         // Try other date formats if not year-only
         for (SimpleDateFormat formatter : formatters) {
             try {
+                formatter.setLenient(false);
                 Date parsedDate = formatter.parse(date);
                 formattedDate = new SimpleDateFormat("yyyy-MM-dd").format(parsedDate); // Convert to "yyyy-MM-dd"
                 break;
             } catch (ParseException e) {
-                // Continue to next formatter
+                logger.info("Error formatting date:"+e.getMessage());
             }
         }
 
@@ -349,6 +352,7 @@ public class ImporterUtil {
                 return true;
             } catch (Exception e) {
                 // Ignore and continue with the next format
+                logger.info("Date format error: ",e);
             }
         }
 

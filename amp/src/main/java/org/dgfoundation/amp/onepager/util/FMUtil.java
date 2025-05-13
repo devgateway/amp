e5fc0f55b786f06d01e254d63c45bb40a186db3a
brianbrix <mokandubrian@gmail.com>
2025-05-13 13:59:03 +0300

    AMP-30972: Create a top countries chart

--- amp/src/main/java/org/dgfoundation/amp/onepager/util/FMUtil.java
+++ amp/src/main/java/org/dgfoundation/amp/onepager/util/FMUtil.java
@@ -472,6 +472,7 @@ public final class FMUtil {
     }
 
     private static boolean checkMultiCountryForTopCountries(String fmPathString) {
+        logger.info("Checking if top countries are allowed for "+fmPathString);
         if (fmPathString.toLowerCase().contains("dashboards") && fmPathString.toLowerCase().contains("top countries")) {
             return DynLocationManagerUtil.getDefCountryIso().equalsIgnoreCase("ZZ") || DynLocationManagerUtil.getDefCountryIso().equalsIgnoreCase("GG");
         }

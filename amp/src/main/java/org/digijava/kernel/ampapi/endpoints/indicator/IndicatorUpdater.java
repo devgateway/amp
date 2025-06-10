69614a4651fc157500aa417cd6725863c371cc70
brianbrix <mokandubrian@gmail.com>
2025-06-10 13:01:28 +0300

    AMP 30989: Fix missing actions column in layers manager

--- amp/src/main/java/org/digijava/kernel/ampapi/endpoints/indicator/IndicatorUpdater.java
+++ amp/src/main/java/org/digijava/kernel/ampapi/endpoints/indicator/IndicatorUpdater.java
@@ -16,30 +16,30 @@ import java.util.Set;
 
 /**
  * Creates a new indicator or updates an existing one
- * 
+ *
  * @author Nadejda Mandrescu
  */
 public class IndicatorUpdater {
-    
+
     private Indicator indicator;
-    
-    private ApiEMGroup errors = new ApiEMGroup(); 
+
+    private ApiEMGroup errors = new ApiEMGroup();
     private TranslationUtil contentTranslator = new TranslationUtil();
     private Long indicatorId;
     private boolean indicatorIdDetected;
-    
+
     public IndicatorUpdater(Indicator indicator) {
         this.indicator = indicator;
     }
-    
+
     public ApiEMGroup getApiErrors() {
         return errors;
     }
-    
+
     public TranslationUtil getContentTranslator() {
         return contentTranslator;
     }
-    
+
     public Long getIndicatorId() {
         if (!indicatorIdDetected) {
             indicatorIdDetected = true;
@@ -70,13 +70,13 @@ public class IndicatorUpdater {
 
         if (indicatorLayer.getId() == null) {
             indicatorLayer.setCreatedOn(new Date());
-            indicatorLayer.setCreatedBy(TeamUtil.getCurrentAmpTeamMember());
+            indicatorLayer.setCreatedBy(TeamUtil.getCurrentUser());
         }
         indicatorLayer.setUpdatedOn(new Date());
         indicatorLayer.setAccessType((indicator.getAccessTypeId() != null
                 ? IndicatorAccessType.getValueFromLong(indicator.getAccessTypeId()) : IndicatorAccessType.TEMPORARY));
-        setAdmLevel(indicatorLayer);  
-        
+        setAdmLevel(indicatorLayer);
+
         if (indicator.getZeroCategoryEnabled() != null) {
            indicatorLayer.setZeroCategoryEnabled(indicator.getZeroCategoryEnabled());
         }
@@ -93,10 +93,10 @@ public class IndicatorUpdater {
                 color.setIndicatorLayer(indicatorLayer);
                 colorRamp.add(color);
             }
-            
+
             if (indicatorLayer.getColorRamp() != null) {
                 indicatorLayer.getColorRamp().clear();
-                indicatorLayer.getColorRamp().addAll(colorRamp); 
+                indicatorLayer.getColorRamp().addAll(colorRamp);
             } else {
                 indicatorLayer.setColorRamp(colorRamp);
             }
@@ -111,10 +111,10 @@ public class IndicatorUpdater {
                 indicatorWs.setIndicatorLayer(indicatorLayer);
                 teams.add(indicatorWs);
             }
-            
+
             if (indicatorLayer.getSharedWorkspaces() != null) {
                 indicatorLayer.getSharedWorkspaces().clear();
-                indicatorLayer.getSharedWorkspaces().addAll(teams); 
+                indicatorLayer.getSharedWorkspaces().addAll(teams);
             } else {
                 indicatorLayer.setSharedWorkspaces(teams);
             }
@@ -155,10 +155,10 @@ public class IndicatorUpdater {
                             IndicatorEPConstants.FIELD_ID + " = " + locId);
                 }
             }
-            
+
             if (indicatorLayer.getIndicatorValues() != null) {
                 indicatorLayer.getIndicatorValues().clear();
-                indicatorLayer.getIndicatorValues().addAll(locationIndicatorValues); 
+                indicatorLayer.getIndicatorValues().addAll(locationIndicatorValues);
             } else {
                 indicatorLayer.setIndicatorValues(locationIndicatorValues);
             }
@@ -166,7 +166,7 @@ public class IndicatorUpdater {
 
         return indicatorLayer;
     }
-    
+
     private void setAdmLevel(AmpIndicatorLayer indicatorLayer) {
         AmpCategoryValue newAdmLevel = CategoryManagerUtil.getAmpCategoryValueFromDb(indicator.getAdmLevelId());
         // configure or update population flag: true if was already designated AND has & have the same valid admLevel
@@ -175,7 +175,7 @@ public class IndicatorUpdater {
         indicatorLayer.setPopulation(isPopulation);
         indicatorLayer.setAdmLevel(newAdmLevel);
     }
-    
+
     private void addIndicatorType(AmpIndicatorLayer ampIndicatorLayer) {
         Long typeId = indicator.getIndicatorTypeId();
         AmpCategoryValue acv = typeId == null ? null : CategoryManagerUtil.getAmpCategoryValueFromDb(typeId);

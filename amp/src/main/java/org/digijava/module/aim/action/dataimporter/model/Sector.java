b32240704a48624e88f169afebbcdda7d5a027c8
brianbrix <mokandubrian@gmail.com>
2026-03-22 21:36:35 +0300

    Split multiple sectors

--- amp/src/main/java/org/digijava/module/aim/action/dataimporter/model/Sector.java
+++ amp/src/main/java/org/digijava/module/aim/action/dataimporter/model/Sector.java
@@ -44,12 +44,29 @@ public class Sector {
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Sector)) return false;
-        Sector sector = (Sector) o;
-        return Objects.equals(getId(), sector.getId());
+        Sector other = (Sector) o;
+
+        // Existing sectors are uniquely identified by activity-sector PK id.
+        if (this.id != null && other.id != null) {
+            return Objects.equals(this.id, other.id);
+        }
+
+        // New import sectors have null id until persisted; use sector id for de-duplication.
+        if (this.sector != null && other.sector != null) {
+            return Objects.equals(this.sector, other.sector);
+        }
+
+        return Objects.equals(this.id, other.id) && Objects.equals(this.sector, other.sector);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(getId());
+        if (id != null) {
+            return Objects.hash(id);
+        }
+        if (sector != null) {
+            return Objects.hash(sector);
+        }
+        return 0;
     }
 }

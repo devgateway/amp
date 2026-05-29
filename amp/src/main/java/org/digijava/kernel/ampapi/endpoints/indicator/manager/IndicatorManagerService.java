a63f9a2a25d8468b40ad70187c52a0389092de7e
brianbrix <mokandubrian@gmail.com>
2026-05-29 17:47:32 +0300

    AMP-31091: The disaggregation order doesnt match

--- amp/src/main/java/org/digijava/kernel/ampapi/endpoints/indicator/manager/IndicatorManagerService.java
+++ amp/src/main/java/org/digijava/kernel/ampapi/endpoints/indicator/manager/IndicatorManagerService.java
@@ -23,6 +23,7 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -191,12 +192,12 @@ public class IndicatorManagerService {
         indicator.setData(indicatorRequest.getData());
         indicator.setDataSource(indicatorRequest.getDataSource());
         if (indicatorRequest.getDisaggregation() != null && !indicatorRequest.getDisaggregation().isEmpty()) {
-            Set<AmpCategoryValue> disaggregationCats = indicatorRequest.getDisaggregation().stream()
+            List<AmpCategoryValue> disaggregationCats = indicatorRequest.getDisaggregation().stream()
                 .map(id -> session.get(AmpCategoryValue.class, id))
-                .collect(Collectors.toSet());
+                .collect(Collectors.toList());
             indicator.setDisaggregation(disaggregationCats);
         } else {
-            indicator.setDisaggregation(new HashSet<>());
+            indicator.setDisaggregation(new ArrayList<>());
         }
         if (indicatorRequest.getUnitOfMeasure() != null) {
             AmpCategoryValue unitOfMeasureCat = session.get(AmpCategoryValue.class, indicatorRequest.getUnitOfMeasure());
@@ -470,14 +471,14 @@ public class IndicatorManagerService {
             indicator.setLogframeLinks(indRequest.getLogframeLinks());
             indicator.setData(indRequest.getData());
             indicator.setDataSource(indRequest.getDataSource());
-            // Convert Set<Long> disaggregation to Set<AmpCategoryValue>
+            // Convert List<Long> disaggregation to List<AmpCategoryValue>
             if (indRequest.getDisaggregation() != null && !indRequest.getDisaggregation().isEmpty()) {
-                Set<AmpCategoryValue> disaggregationCats = indRequest.getDisaggregation().stream()
+                List<AmpCategoryValue> disaggregationCats = indRequest.getDisaggregation().stream()
                     .map(id -> (AmpCategoryValue) session.get(AmpCategoryValue.class, id))
-                    .collect(Collectors.toSet());
+                    .collect(Collectors.toList());
                 indicator.setDisaggregation(disaggregationCats);
             } else {
-                indicator.setDisaggregation(new HashSet<>());
+                indicator.setDisaggregation(new ArrayList<>());
             }
             // Convert Long unitOfMeasure to AmpCategoryValue
             if (indRequest.getUnitOfMeasure() != null) {
